6ce9bb699786710d280355d6c211e60ce9781775
paolomococci <paolomococci@gmail.com>
2022-10-29 11:48:27 +0200

    modified:   console/args-view/Program.cs

--- console/args-view/Program.cs
+++ console/args-view/Program.cs
@@ -4,6 +4,27 @@ namespace Arguments {
     class Program {
         static void Main(string[] args) {
             WriteLine($"There are {args.Length} arguments.");
+
+            if (args.Length != 2) {
+                WriteLine("to this application you need to pass two arguments");
+                WriteLine("for example: dotnet run green blue");
+                return;
+            }
+
+            ForegroundColor = (ConsoleColor)Enum.Parse(
+                enumType: typeof(ConsoleColor),
+                value: args[0],
+                ignoreCase: true
+            );
+            BackgroundColor = (ConsoleColor)Enum.Parse(
+                enumType: typeof(ConsoleColor),
+                value: args[1],
+                ignoreCase: true
+            );
+
+            WriteLine(($"foreground color: {ForegroundColor}"));
+            WriteLine(($"background color: {BackgroundColor}"));
+            WriteLine((""));
         }
     }
 }
