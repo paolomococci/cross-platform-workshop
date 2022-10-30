3fe263fa242925fa4269646c7aedc3ebd20775c7
paolomococci <paolomococci@gmail.com>
2022-10-30 14:33:24 +0100

    modified:   sample/DemoticApp/Program.cs

--- sample/DemoticApp/Program.cs
+++ sample/DemoticApp/Program.cs
@@ -2,3 +2,14 @@
 
 using static System.Console;
 
+
+Person thomas = new();
+
+thomas.Name = "Thomas Jones";
+thomas.DateOfBirth = new DateTime(1972, 10, 25);
+
+WriteLine(
+    format: "{0} was born on {1:dddd, d MMMM yyyy}",
+    arg0: thomas.Name,
+    arg1: thomas.DateOfBirth
+);
