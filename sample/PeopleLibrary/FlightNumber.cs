f3f9473d10c337894aa670b3298aa32791961660
paolomococci <paolomococci@gmail.com>
2022-10-30 13:55:09 +0100

    modified:   sample/PeopleLibrary/FlightNumber.cs

--- sample/PeopleLibrary/FlightNumber.cs
+++ sample/PeopleLibrary/FlightNumber.cs
@@ -2,8 +2,8 @@ namespace Sample.Shared;
 public record FlightPlan
 {
     public int Id { get; init; }
-    public string? Company { get; init; }
-    public string? Brand { get; init; }
-    public int? Armchairs { get; init; }
-    public int? Berths { get; init; }
+    public string? Company { get; set; } = "someone";
+    public string? Brand { get; set; } = "someone";
+    public int? Armchairs { get; set; } = 0;
+    public int? Berths { get; set; } = 0;
 }
