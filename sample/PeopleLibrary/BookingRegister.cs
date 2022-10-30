5d87777e8090a29fc5af42d0b803083e133bf3cd
paolomococci <paolomococci@gmail.com>
2022-10-30 07:44:06 +0100

    modified:   sample/PeopleLibrary/BookingRegister.cs

--- sample/PeopleLibrary/BookingRegister.cs
+++ sample/PeopleLibrary/BookingRegister.cs
@@ -1,17 +1,20 @@
 namespace Sample.Shared;
 public class PersonalBooking
 {
-
+    public string? FirstName { get; init; }
+    public string? LastName { get; init; }
 }
 
-public record Pet {
+public record Pet
+{
     string Name;
     string Species;
 
     public Pet(
         string name,
         string species
-    ) {
+    )
+    {
         Name = name;
         Species = species;
     }
@@ -19,13 +22,14 @@ public record Pet {
     public void Deconstruct(
         out string name,
         out string species
-    ) {
+    )
+    {
         name = Name;
         species = Species;
     }
 }
 
 public record Animal(
-    string Name, 
+    string Name,
     string Species
 );
