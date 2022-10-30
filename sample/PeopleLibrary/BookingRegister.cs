94de224f5768e3c4cc4bb5352b673dcd1366ec0e
paolomococci <paolomococci@gmail.com>
2022-10-30 07:36:05 +0100

    modified:   sample/PeopleLibrary/BookingRegister.cs

--- sample/PeopleLibrary/BookingRegister.cs
+++ sample/PeopleLibrary/BookingRegister.cs
@@ -5,5 +5,14 @@ public class PersonalBooking
 }
 
 public record Pet {
-    
+    string Name;
+    string Species;
+
+    public Pet(
+        string name,
+        string species
+    ) {
+        Name = name;
+        Species = species;
+    }
 }
