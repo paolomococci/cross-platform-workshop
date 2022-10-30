42033c250a22122b031ee7c7190ece3ef8c7e80d
paolomococci <paolomococci@gmail.com>
2022-10-30 06:16:25 +0100

    modified:   sample/SampleLibrary/Person.cs

--- sample/SampleLibrary/Person.cs
+++ sample/SampleLibrary/Person.cs
@@ -22,5 +22,15 @@ namespace Sample.Shared
             Name = "Unknown";
             Instantiated = DateTime.Now;
         }
+
+        public Person(
+            string initialName, 
+            string homePlanet
+        )
+        {
+            Name = initialName;
+            HomePlanet = homePlanet;
+            Instantiated = DateTime.Now;
+        }
     }
 }
