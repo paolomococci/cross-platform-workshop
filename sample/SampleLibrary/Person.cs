53be1f8bca52b8f8073767135e2f311c1b3c75a5
paolomococci <paolomococci@gmail.com>
2022-10-30 06:11:49 +0100

    modified:   sample/SampleLibrary/Person.cs

--- sample/SampleLibrary/Person.cs
+++ sample/SampleLibrary/Person.cs
@@ -1,11 +1,20 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
 using static System.Console;
 
-namespace Sample.Shared {
-    public class Person : object
+namespace Sample.Shared
+{
+    public partial class Person : object
     {
-        // todo
+
+        public const string Species = "Homo Sapiens";
+        public readonly string HomePlanet = "Earth";
+        public readonly DateTime Instantiated;
+        public string Name;
+        public DateTime DateOfBirth;
+        public Wonders FavoriteWonder;
+        public Wonders BucketList;
+        public List<Person> Children = new List<Person>();
     }
 }
