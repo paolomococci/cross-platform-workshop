9df43a51dc0f286a11c492da7168d8809150ad58
paolomococci <paolomococci@gmail.com>
2022-10-31 13:07:56 +0100

    modified:   console/AncientGarden/PetLibrary/Pet.cs

--- console/AncientGarden/PetLibrary/Pet.cs
+++ console/AncientGarden/PetLibrary/Pet.cs
@@ -5,45 +5,42 @@ public class Pet : object
   public string? Name { get; set; }
   public string? Parents { get; set; }
   public DateTime? DateOfBirth { get; set; }
-  public Cub? Progeny { get; set; }
+  public Cub Progeny = new();
   public event EventHandler? Caress;
   public int DocilityLevel;
 
   public static Pet? Procreate(
     Pet mom,
     Pet dad
-  )
-  {
+  ) {
     Pet newborn = new() 
     {
       Id = Guid.NewGuid(),
       Parents = $"cub of {mom.Name} and {dad.Name}",
       DateOfBirth = DateTime.Today
     };
+    mom.Progeny.Progeny.Add(newborn);
+    dad.Progeny.Progeny.Add(newborn);
     return newborn;
   }
 
-  public Pet? ProcreateWith()
-  {
+  public Pet? ProcreateWith() {
     return null;
   }
 
-  public static int? Factorial()
-  {
+  public static int? Factorial() {
     return null;
   }
 
   public void Cuddles() { }
 
-  public int? CompareTo(Pet? other)
-  {
+  public int? CompareTo(Pet? other) {
     return null;
   }
 
   public void Weaning(DateTime when) {}
 
-  public override string? ToString()
-  {
+  public override string? ToString() {
     return base.ToString();
   }
 }
