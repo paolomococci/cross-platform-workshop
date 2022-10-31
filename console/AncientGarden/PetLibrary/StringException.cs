b9891bc6d7c22d383f263b6f2c2883627129b5e2
paolomococci <paolomococci@gmail.com>
2022-10-31 13:07:40 +0100

    modified:   console/AncientGarden/PetLibrary/StringException.cs

--- console/AncientGarden/PetLibrary/StringException.cs
+++ console/AncientGarden/PetLibrary/StringException.cs
@@ -1,5 +1,9 @@
 namespace PetLibrary;
-public class StringException
-{
 
+using System.Text.RegularExpressions;
+public static class StringException
+{
+  public static bool IsValidEmail(this string input) {
+    return Regex.IsMatch(input, @"[a-zA-Z0-9\.-_]+@[a-zA-Z0-9\.-_]+");
+  }
 }
