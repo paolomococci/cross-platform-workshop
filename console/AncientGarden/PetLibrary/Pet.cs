041e3cc968f42b2145f54f66eb60cee042ce876d
paolomococci <paolomococci@gmail.com>
2022-10-31 12:28:35 +0100

    modified:   console/AncientGarden/PetLibrary/Pet.cs

--- console/AncientGarden/PetLibrary/Pet.cs
+++ console/AncientGarden/PetLibrary/Pet.cs
@@ -1,30 +1,34 @@
 ﻿namespace PetLibrary;
 public class Pet : object
 {
-  public string Id { get; init; } = "";
-  public string? Name { get; set; }
-  public DateTime? DateOfBirth { get; set; }
-  public Cub? Progeny { get; set; }
-  public event EventHandler? Caress;
-  public int DocilityLevel;
+    public string Id { get; init; } = "";
+    public string? Name { get; set; }
+    public DateTime? DateOfBirth { get; set; }
+    public Cub? Progeny { get; set; }
+    public event EventHandler? Caress;
+    public int DocilityLevel;
 
-  public static Pet? Procreate() {
-    return null;
-  }
+    public static Pet? Procreate()
+    {
+        return null;
+    }
 
-  public Pet? ProcreateWith() {
-    return null;
-  }
+    public Pet? ProcreateWith()
+    {
+        return null;
+    }
 
-  public static int? Factorial() {
-    return null;
-  }
+    public static int? Factorial()
+    {
+        return null;
+    }
 
-  public void Cuddles() {}
+    public void Cuddles() { }
 
-  public int? CompareTo(Pet? other) {
-    return null;
-  }
+    public int? CompareTo(Pet? other)
+    {
+        return null;
+    }
 
     public override string? ToString()
     {
