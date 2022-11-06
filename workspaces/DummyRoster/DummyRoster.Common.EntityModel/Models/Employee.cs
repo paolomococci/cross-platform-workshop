6b855bbd0b62f57a36577a197c2749489f4aa452
paolomococci <paolomococci@gmail.com>
2022-11-06 08:34:27 +0100

    modified:   workspaces/DummyRoster/DummyRoster.Common.EntityModel/Models/Employee.cs

--- workspaces/DummyRoster/DummyRoster.Common.EntityModel/Models/Employee.cs
+++ workspaces/DummyRoster/DummyRoster.Common.EntityModel/Models/Employee.cs
@@ -19,6 +19,7 @@ public partial class Employee
     public long Id { get; set; }
 
     [Column(TypeName = "varchar(32)")]
+    [RegularExpression("[a-zA-Z]{32}")]
     public string Name { get; set; } = null!;
 
     public string? Description { get; set; }
