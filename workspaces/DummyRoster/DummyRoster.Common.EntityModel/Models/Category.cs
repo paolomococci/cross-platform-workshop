f781437b68d7b4d09a0eb315430c824b50f27537
paolomococci <paolomococci@gmail.com>
2022-11-06 09:10:40 +0100

    modified:   workspaces/DummyRoster/DummyRoster.Common.EntityModel/Models/Category.cs

--- workspaces/DummyRoster/DummyRoster.Common.EntityModel/Models/Category.cs
+++ workspaces/DummyRoster/DummyRoster.Common.EntityModel/Models/Category.cs
@@ -12,11 +12,11 @@ public partial class Category
 {
     [Key]
     [Column(TypeName = "BIGINT")]
-    [RegularExpression("[0-9]{9}")]
+    [RegularExpression("[0-9]{,9}")]
     public long Id { get; set; }
 
     [Column(TypeName = "varchar(32)")]
-    [RegularExpression("[a-zA-Z]{32}")]
+    [RegularExpression("[a-zA-Z]{,32}")]
     public string Name { get; set; } = null!;
 
     [StringLength(64)]
