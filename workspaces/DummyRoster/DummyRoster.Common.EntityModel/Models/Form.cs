c0c8e9e853274531c692e37b8632b5a76879932d
paolomococci <paolomococci@gmail.com>
2022-11-06 09:00:29 +0100

    modified:   workspaces/DummyRoster/DummyRoster.Common.EntityModel/Models/Form.cs

--- workspaces/DummyRoster/DummyRoster.Common.EntityModel/Models/Form.cs
+++ workspaces/DummyRoster/DummyRoster.Common.EntityModel/Models/Form.cs
@@ -36,13 +36,13 @@ public partial class Form
     public long? EmployeeId { get; set; }
 
     [Column(TypeName = "DATETIME")]
-    public byte[]? FormDate { get; set; }
+    public DateTime? FormDate { get; set; }
 
     [Column(TypeName = "DATETIME")]
-    public byte[]? RequiredDate { get; set; }
+    public DateTime? RequiredDate { get; set; }
 
     [Column(TypeName = "DATETIME")]
-    public byte[]? PromisedDate { get; set; }
+    public DateTime? PromisedDate { get; set; }
 
     public double? ShippingCost { get; set; }
 
