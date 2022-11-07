f3a3c1534523f8174d34cdf11367afd454ef84be
paolomococci <paolomococci@gmail.com>
2022-11-07 14:24:58 +0100

    modified:   workspaces/DummyRosterOne/DummyRoster.Common.EntityModel/Models/Carrier.cs

--- workspaces/DummyRosterOne/DummyRoster.Common.EntityModel/Models/Carrier.cs
+++ workspaces/DummyRosterOne/DummyRoster.Common.EntityModel/Models/Carrier.cs
@@ -45,6 +45,7 @@ public partial class Carrier
     public string? City { get; set; }
 
     [Column(TypeName = "varchar(16)")]
+    [RegularExpression(@"[a-zA-Z0-9\s\-]{3,16}")]
     public string? District { get; set; }
 
     [Column(TypeName = "varchar(8)")]
