ff7f86e8cf28619033382ade78edb1e65508c216
paolomococci <paolomococci@gmail.com>
2022-11-07 09:06:49 +0100

    new file:   workspaces/DummyRosterOne/DummyRoster.Common.EntityModel/Models/Invoice.cs

---
+++ workspaces/DummyRosterOne/DummyRoster.Common.EntityModel/Models/Invoice.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace DummyRoster.Common.EntityModel.Models;
+
+[PrimaryKey("FormId", "ProductId")]
+[Table("Invoice")]
+[Index("ProductId", Name = "IDX_ProductId")]
+public partial class Invoice
+{
+    public string? Note { get; set; }
+
+    [Key]
+    [Column(TypeName = "BIGINT")]
+    public long FormId { get; set; }
+
+    [Key]
+    [Column(TypeName = "BIGINT")]
+    public long ProductId { get; set; }
+
+    [Column(TypeName = "DATETIME")]
+    public byte[]? RegistrationDate { get; set; }
+
+    public double UnitPrice { get; set; }
+
+    [Column(TypeName = "SMALLINT")]
+    public long Quantity { get; set; }
+
+    public double PriceCut { get; set; }
+
+    [ForeignKey("FormId")]
+    [InverseProperty("Invoices")]
+    public virtual Form Form { get; set; } = null!;
+
+    [ForeignKey("ProductId")]
+    [InverseProperty("Invoices")]
+    public virtual Product Product { get; set; } = null!;
+}
