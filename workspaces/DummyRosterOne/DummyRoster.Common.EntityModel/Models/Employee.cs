dd21a46e0a77401c8bc38b86067966d7fa6bab6e
paolomococci <paolomococci@gmail.com>
2022-11-07 14:00:49 +0100

    modified:   workspaces/DummyRosterOne/DummyRoster.Common.EntityModel/Models/Employee.cs

--- workspaces/DummyRosterOne/DummyRoster.Common.EntityModel/Models/Employee.cs
+++ workspaces/DummyRosterOne/DummyRoster.Common.EntityModel/Models/Employee.cs
@@ -15,7 +15,8 @@ public partial class Employee
 {
     [Key]
     [Column(TypeName = "BIGINT")]
-    public long Id { get; set; }
+    [Range(1, 18446744073709551615)]
+    public UInt64 Id { get; set; }
 
     [Column(TypeName = "varchar(32)")]
     public string Name { get; set; } = null!;
