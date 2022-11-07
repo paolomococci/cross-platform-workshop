00eb91789cfa9b907fbaf80cb7f22a69b8fe4843
paolomococci <paolomococci@gmail.com>
2022-11-07 03:43:21 +0100

    new file:   workspaces/DummyRoster/DummyRoster.Common.DataContext/Data/DummyRosterContext.cs

---
+++ workspaces/DummyRoster/DummyRoster.Common.DataContext/Data/DummyRosterContext.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace DummyRoster.Common.EntityModel.Models;
+
+public partial class DummyRosterContext : DbContext
+{
+    public DummyRosterContext()
+    {
+    }
+
+    public DummyRosterContext(DbContextOptions<DummyRosterContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Carrier> Carriers { get; set; }
+
+    public virtual DbSet<Category> Categories { get; set; }
+
+    public virtual DbSet<Customer> Customers { get; set; }
+
+    public virtual DbSet<Employee> Employees { get; set; }
+
+    public virtual DbSet<Form> Forms { get; set; }
+
+    public virtual DbSet<Invoice> Invoices { get; set; }
+
+    public virtual DbSet<Product> Products { get; set; }
+
+    public virtual DbSet<Supplier> Suppliers { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlite("Filename=../DummyRoster.db");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Carrier>().Property(e => e.Inserted).ValueGeneratedOnAdd();
+
+        modelBuilder.Entity<Category>().Property(e => e.Inserted).ValueGeneratedOnAdd();
+
+        modelBuilder.Entity<Customer>().Property(e => e.Inserted).ValueGeneratedOnAdd();
+
+        modelBuilder.Entity<Employee>().Property(e => e.Inserted).ValueGeneratedOnAdd();
+
+        modelBuilder.Entity<Form>(entity =>
+        {
+            entity.Property(e => e.Id).ValueGeneratedOnAdd();
+            entity.Property(e => e.ShippingCost).HasDefaultValueSql("0");
+        });
+
+        modelBuilder.Entity<Invoice>(entity =>
+        {
+            entity.Property(e => e.Quantity).HasDefaultValueSql("1");
+
+            entity.HasOne(d => d.Form).WithMany(p => p.Invoices).OnDelete(DeleteBehavior.ClientSetNull);
+
+            entity.HasOne(d => d.Product).WithMany(p => p.Invoices).OnDelete(DeleteBehavior.ClientSetNull);
+        });
+
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.Property(e => e.Id).ValueGeneratedOnAdd();
+            entity.Property(e => e.ReorderLevel).HasDefaultValueSql("0");
+            entity.Property(e => e.UnitPrice).HasDefaultValueSql("0");
+            entity.Property(e => e.UnitsInStock).HasDefaultValueSql("0");
+            entity.Property(e => e.UnitsOnOrder).HasDefaultValueSql("0");
+        });
+
+        modelBuilder.Entity<Supplier>().Property(e => e.Inserted).ValueGeneratedOnAdd();
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
