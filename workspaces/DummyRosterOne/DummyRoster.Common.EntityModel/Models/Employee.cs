807aef0b4aeaba1a4282045eba728747e1c263ae
paolomococci <paolomococci@gmail.com>
2022-11-07 14:49:31 +0100

    modified:   workspaces/DummyRosterOne/DummyRoster.Common.EntityModel/Models/Employee.cs

--- workspaces/DummyRosterOne/DummyRoster.Common.EntityModel/Models/Employee.cs
+++ workspaces/DummyRosterOne/DummyRoster.Common.EntityModel/Models/Employee.cs
@@ -23,7 +23,7 @@ public partial class Employee
     public string Name { get; set; } = null!;
 
     [Column(TypeName = "DATETIME")]
-    public byte[]? BirthDate { get; set; }
+    public DateTime? BirthDate { get; set; }
 
     [RegularExpression(@"^([A-Z]{1,}+[a-zA-Z0-9\s\.\,]{1,63})$")]
     public string? Description { get; set; }
