a5a9e5c69666c53b57573c082077aaaa2bbf37ad
paolomococci <paolomococci@gmail.com>
2022-11-07 09:07:01 +0100

    new file:   workspaces/DummyRosterOne/DummyRoster.Common.EntityModel/Models/Product.cs

---
+++ workspaces/DummyRosterOne/DummyRoster.Common.EntityModel/Models/Product.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace DummyRoster.Common.EntityModel.Models;
+
+[Table("Product")]
+[Index("CategoryId", Name = "IDX_CategoryId")]
+[Index("CategoryId", Name = "IDX_ProductBelongingCategory")]
+[Index("Name", Name = "IDX_ProductName")]
+[Index("SupplierId", Name = "IDX_ProductSuppliedBy")]
+[Index("SupplierId", Name = "IDX_SupplierId")]
+public partial class Product
+{
+    [Key]
+    [Column(TypeName = "BIGINT")]
+    public long Id { get; set; }
+
+    [Column(TypeName = "varchar(32)")]
+    public string Name { get; set; } = null!;
+
+    public string? Description { get; set; }
+
+    public byte[]? Picture { get; set; }
+
+    [Column(TypeName = "DATETIME")]
+    public byte[]? RegistrationDate { get; set; }
+
+    [Column(TypeName = "BIGINT")]
+    public long? CategoryId { get; set; }
+
+    [Column(TypeName = "BIGINT")]
+    public long? SupplierId { get; set; }
+
+    [Column(TypeName = "varchar(24)")]
+    public string? QuantityPerUnit { get; set; }
+
+    public double? UnitPrice { get; set; }
+
+    [Column(TypeName = "SMALLINT")]
+    public long? UnitsInStock { get; set; }
+
+    [Column(TypeName = "SMALLINT")]
+    public long? UnitsOnOrder { get; set; }
+
+    [Column(TypeName = "SMALLINT")]
+    public long? ReorderLevel { get; set; }
+
+    [Column(TypeName = "TINYINT")]
+    public long Discontinued { get; set; }
+
+    [ForeignKey("CategoryId")]
+    [InverseProperty("Products")]
+    public virtual Category? Category { get; set; }
+
+    [InverseProperty("Product")]
+    public virtual ICollection<Invoice> Invoices { get; } = new List<Invoice>();
+
+    [ForeignKey("SupplierId")]
+    [InverseProperty("Products")]
+    public virtual Supplier? Supplier { get; set; }
+}
