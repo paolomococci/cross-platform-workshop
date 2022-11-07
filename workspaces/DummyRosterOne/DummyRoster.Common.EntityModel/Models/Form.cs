3cbc56cf44479ff8391859c5728fc9f314d157d2
paolomococci <paolomococci@gmail.com>
2022-11-07 15:15:01 +0100

    modified:   workspaces/DummyRosterOne/DummyRoster.Common.EntityModel/Models/Form.cs

--- workspaces/DummyRosterOne/DummyRoster.Common.EntityModel/Models/Form.cs
+++ workspaces/DummyRosterOne/DummyRoster.Common.EntityModel/Models/Form.cs
@@ -40,13 +40,13 @@ public partial class Form
     public UInt64? EmployeeId { get; set; }
 
     [Column(TypeName = "DATETIME")]
-    public byte[]? FormDate { get; set; }
+    public DateTime? FormDate { get; set; }
 
     [Column(TypeName = "DATETIME")]
-    public byte[]? RequiredDate { get; set; }
+    public DateTime? RequiredDate { get; set; }
 
     [Column(TypeName = "DATETIME")]
-    public byte[]? PromisedDate { get; set; }
+    public DateTime? PromisedDate { get; set; }
 
     public double? ShippingCost { get; set; }
 
