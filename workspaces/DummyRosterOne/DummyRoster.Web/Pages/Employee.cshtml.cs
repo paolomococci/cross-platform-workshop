632f0225cf158090bb622bd2f45cb49612256e88
paolomococci <paolomococci@gmail.com>
2022-11-10 20:36:19 +0100

    modified:   workspaces/DummyRosterOne/DummyRoster.Web/Pages/Employee.cshtml.cs

--- workspaces/DummyRosterOne/DummyRoster.Web/Pages/Employee.cshtml.cs
+++ workspaces/DummyRosterOne/DummyRoster.Web/Pages/Employee.cshtml.cs
@@ -6,5 +6,8 @@ public class EmployeeModel : PageModel {
 
   public IEnumerable<string>? Employees { get; set; }
 
-  public void OnGet() {}
+  public void OnGet() {
+    ViewData["Title"] = "DummyRoster Web - Employee";
+    Employees = new[] {"Sample One", "Sample Two", "Sample Three", "Sample Four", "Sample Five"};
+  }
 }
