55b502b820d8989b0236530b9d73f27c3832bca6
paolomococci <paolomococci@gmail.com>
2022-11-10 20:26:55 +0100

    modified:   workspaces/DummyRosterOne/DummyRoster.Web/Pages/Invoice.cshtml.cs

--- workspaces/DummyRosterOne/DummyRoster.Web/Pages/Invoice.cshtml.cs
+++ workspaces/DummyRosterOne/DummyRoster.Web/Pages/Invoice.cshtml.cs
@@ -3,5 +3,8 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 namespace DummyRoster.Web.Pages;
 
 public class InvoiceModel : PageModel {
+
   public IEnumerable<string>? Invoices { get; set; }
+
+  public void OnGet() {}
 }
