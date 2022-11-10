9165cdb895437446f2b4a20c67073d75b0740b62
paolomococci <paolomococci@gmail.com>
2022-11-10 20:35:44 +0100

    modified:   workspaces/DummyRosterOne/DummyRoster.Web/Pages/Carrier.cshtml.cs

--- workspaces/DummyRosterOne/DummyRoster.Web/Pages/Carrier.cshtml.cs
+++ workspaces/DummyRosterOne/DummyRoster.Web/Pages/Carrier.cshtml.cs
@@ -3,8 +3,11 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 namespace DummyRoster.Web.Pages;
 
 public class CarrierModel : PageModel {
-  
+
   public IEnumerable<string>? Carriers { get; set; }
 
-  public void OnGet() {}
+  public void OnGet() {
+    ViewData["Title"] = "DummyRoster Web - Carrier";
+    Carriers = new[] {"Sample One", "Sample Two", "Sample Three", "Sample Four", "Sample Five"};
+  }
 }
