5518ad5cc76dd6a082813efa15ad23ddd651aa41
paolomococci <paolomococci@gmail.com>
2022-11-10 20:20:09 +0100

    modified:   workspaces/DummyRosterOne/DummyRoster.Web/Pages/Employee.cshtml.cs

--- workspaces/DummyRosterOne/DummyRoster.Web/Pages/Employee.cshtml.cs
+++ workspaces/DummyRosterOne/DummyRoster.Web/Pages/Employee.cshtml.cs
@@ -2,4 +2,6 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace DummyRoster.Web.Pages;
 
-public class EmployeeModel : PageModel {}
+public class EmployeeModel : PageModel {
+  public IEnumerable<string>? Employees { get; set; }
+}
