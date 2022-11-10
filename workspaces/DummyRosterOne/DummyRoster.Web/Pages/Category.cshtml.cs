4baf8562b5d6a4b51f28566aeaf1b3e57805bd57
paolomococci <paolomococci@gmail.com>
2022-11-10 20:26:01 +0100

    modified:   workspaces/DummyRosterOne/DummyRoster.Web/Pages/Category.cshtml.cs

--- workspaces/DummyRosterOne/DummyRoster.Web/Pages/Category.cshtml.cs
+++ workspaces/DummyRosterOne/DummyRoster.Web/Pages/Category.cshtml.cs
@@ -3,5 +3,8 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 namespace DummyRoster.Web.Pages;
 
 public class CategoryModel : PageModel {
+  
   public IEnumerable<string>? Categories { get; set; }
+
+  public void OnGet() {}
 }
