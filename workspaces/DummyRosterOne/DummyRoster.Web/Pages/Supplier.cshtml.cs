cf4ec6ce492a26525e61a7b9c0cfced81a6be5b6
paolomococci <paolomococci@gmail.com>
2022-11-10 20:27:19 +0100

    modified:   workspaces/DummyRosterOne/DummyRoster.Web/Pages/Supplier.cshtml.cs

--- workspaces/DummyRosterOne/DummyRoster.Web/Pages/Supplier.cshtml.cs
+++ workspaces/DummyRosterOne/DummyRoster.Web/Pages/Supplier.cshtml.cs
@@ -3,5 +3,8 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 namespace DummyRoster.Web.Pages;
 
 public class SupplierModel : PageModel {
+
   public IEnumerable<string>? Suppliers { get; set; }
+
+  public void OnGet() {}
 }
