7b6ee533625c3ad7eddadf9dfbfeef628cc5c10c
paolomococci <paolomococci@gmail.com>
2022-11-10 20:20:45 +0100

    modified:   workspaces/DummyRosterOne/DummyRoster.Web/Pages/Product.cshtml.cs

--- workspaces/DummyRosterOne/DummyRoster.Web/Pages/Product.cshtml.cs
+++ workspaces/DummyRosterOne/DummyRoster.Web/Pages/Product.cshtml.cs
@@ -2,4 +2,6 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace DummyRoster.Web.Pages;
 
-public class ProductModel : PageModel {}
+public class ProductModel : PageModel {
+  public IEnumerable<string>? Products { get; set; }
+}
