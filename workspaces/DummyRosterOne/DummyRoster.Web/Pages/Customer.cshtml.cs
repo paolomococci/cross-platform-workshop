8d0e56aabfe00f4cc3cb36d724a5460b218b51dd
paolomococci <paolomococci@gmail.com>
2022-11-10 20:26:14 +0100

    modified:   workspaces/DummyRosterOne/DummyRoster.Web/Pages/Customer.cshtml.cs

--- workspaces/DummyRosterOne/DummyRoster.Web/Pages/Customer.cshtml.cs
+++ workspaces/DummyRosterOne/DummyRoster.Web/Pages/Customer.cshtml.cs
@@ -3,5 +3,8 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 namespace DummyRoster.Web.Pages;
 
 public class CustomerModel : PageModel {
+
   public IEnumerable<string>? Customers { get; set; }
+
+  public void OnGet() {}
 }
