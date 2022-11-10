ea5e689c84445ce62b3d82fd7e7f638e8aeaf293
paolomococci <paolomococci@gmail.com>
2022-11-10 20:20:21 +0100

    modified:   workspaces/DummyRosterOne/DummyRoster.Web/Pages/Form.cshtml.cs

--- workspaces/DummyRosterOne/DummyRoster.Web/Pages/Form.cshtml.cs
+++ workspaces/DummyRosterOne/DummyRoster.Web/Pages/Form.cshtml.cs
@@ -2,4 +2,6 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace DummyRoster.Web.Pages;
 
-public class FormModel : PageModel {}
+public class FormModel : PageModel {
+  public IEnumerable<string>? Forms { get; set; }
+}
