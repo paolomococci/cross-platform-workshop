c248255b33df7847c518fb6afb114e71716c7a6d
paolomococci <paolomococci@gmail.com>
2022-11-10 20:36:42 +0100

    modified:   workspaces/DummyRosterOne/DummyRoster.Web/Pages/Invoice.cshtml.cs

--- workspaces/DummyRosterOne/DummyRoster.Web/Pages/Invoice.cshtml.cs
+++ workspaces/DummyRosterOne/DummyRoster.Web/Pages/Invoice.cshtml.cs
@@ -6,5 +6,8 @@ public class InvoiceModel : PageModel {
 
   public IEnumerable<string>? Invoices { get; set; }
 
-  public void OnGet() {}
+  public void OnGet() {
+    ViewData["Title"] = "DummyRoster Web - Invoice";
+    Invoices = new[] {"Sample One", "Sample Two", "Sample Three", "Sample Four", "Sample Five"};
+  }
 }
