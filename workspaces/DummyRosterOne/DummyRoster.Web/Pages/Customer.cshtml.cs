123b1cf9b74a021134a0313d414bf54c11a8fb60
paolomococci <paolomococci@gmail.com>
2022-11-16 21:43:21 +0100

    modified:   workspaces/DummyRosterOne/DummyRoster.Web/Pages/Customer.cshtml.cs

--- workspaces/DummyRosterOne/DummyRoster.Web/Pages/Customer.cshtml.cs
+++ workspaces/DummyRosterOne/DummyRoster.Web/Pages/Customer.cshtml.cs
@@ -1,14 +1,26 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
+using DummyRoster.Common.DataContext.Data;
+using DummyRoster.Common.EntityModel.Models;
+
 namespace DummyRoster.Web.Pages;
 
 public class CustomerModel : PageModel {
 
-  public IEnumerable<string>? Customers { get; set; }
+  public IEnumerable<Customer>? Customers { get; set; }
+
+  private DummyRosterContext dummyRosterContext;
+
+  public CustomerModel(DummyRosterContext dummyRosterContextInjected) {
+    this.dummyRosterContext = dummyRosterContextInjected;
+  }
 
   public void OnGet() {
     ViewData["Title"] = "DummyRoster Web - Customer";
-    ViewData["CardTitle"] = "Customers";
-    Customers = new[] {"Sample One", "Sample Two", "Sample Three", "Sample Four", "Sample Five"};
+    Customers = dummyRosterContext.Customers.OrderBy(
+      customer => customer.Country
+    ).ThenBy(
+      customer => customer.Name
+    );
   }
 }
