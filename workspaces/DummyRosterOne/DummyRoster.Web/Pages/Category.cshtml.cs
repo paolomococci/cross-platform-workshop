a1b6d7f155bbee63b85132cd43f59eb70a614771
paolomococci <paolomococci@gmail.com>
2022-11-16 21:42:59 +0100

    modified:   workspaces/DummyRosterOne/DummyRoster.Web/Pages/Category.cshtml.cs

--- workspaces/DummyRosterOne/DummyRoster.Web/Pages/Category.cshtml.cs
+++ workspaces/DummyRosterOne/DummyRoster.Web/Pages/Category.cshtml.cs
@@ -1,14 +1,26 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
+using DummyRoster.Common.DataContext.Data;
+using DummyRoster.Common.EntityModel.Models;
+
 namespace DummyRoster.Web.Pages;
 
 public class CategoryModel : PageModel {
-  
-  public IEnumerable<string>? Categories { get; set; }
+
+  public IEnumerable<Category>? Categories { get; set; }
+
+  private DummyRosterContext dummyRosterContext;
+
+  public CategoryModel(DummyRosterContext dummyRosterContextInjected) {
+    this.dummyRosterContext = dummyRosterContextInjected;
+  }
 
   public void OnGet() {
     ViewData["Title"] = "DummyRoster Web - Category";
-    ViewData["CardTitle"] = "Categories";
-    Categories = new[] {"Sample One", "Sample Two", "Sample Three", "Sample Four", "Sample Five"};
+    Categories = dummyRosterContext.Categories.OrderBy(
+      category => category.RegistrationDate
+    ).ThenBy(
+      category => category.Name
+    );
   }
 }
