44208127fc41633d65067b7114cafec903cc2437
paolomococci <paolomococci@gmail.com>
2022-11-16 21:42:36 +0100

    modified:   workspaces/DummyRosterOne/DummyRoster.Web/Pages/Carrier.cshtml.cs

--- workspaces/DummyRosterOne/DummyRoster.Web/Pages/Carrier.cshtml.cs
+++ workspaces/DummyRosterOne/DummyRoster.Web/Pages/Carrier.cshtml.cs
@@ -1,14 +1,26 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
+using DummyRoster.Common.DataContext.Data;
+using DummyRoster.Common.EntityModel.Models;
+
 namespace DummyRoster.Web.Pages;
 
 public class CarrierModel : PageModel {
 
-  public IEnumerable<string>? Carriers { get; set; }
+  public IEnumerable<Carrier>? Carriers { get; set; }
+
+  private DummyRosterContext dummyRosterContext;
+
+  public CarrierModel(DummyRosterContext dummyRosterContextInjected) {
+    this.dummyRosterContext = dummyRosterContextInjected;
+  }
 
   public void OnGet() {
     ViewData["Title"] = "DummyRoster Web - Carrier";
-    ViewData["CardTitle"] = "Carriers";
-    Carriers = new[] {"Sample One", "Sample Two", "Sample Three", "Sample Four", "Sample Five"};
+    Carriers = dummyRosterContext.Carriers.OrderBy(
+      carrier => carrier.Country
+    ).ThenBy(
+      carrier => carrier.Name
+    );
   }
 }
