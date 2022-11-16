61b0b51f1fa8acf8c0a5132d486bdc6a49897103
paolomococci <paolomococci@gmail.com>
2022-11-16 21:44:45 +0100

    modified:   workspaces/DummyRosterOne/DummyRoster.Web/Pages/Product.cshtml.cs

--- workspaces/DummyRosterOne/DummyRoster.Web/Pages/Product.cshtml.cs
+++ workspaces/DummyRosterOne/DummyRoster.Web/Pages/Product.cshtml.cs
@@ -1,14 +1,26 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
+using DummyRoster.Common.DataContext.Data;
+using DummyRoster.Common.EntityModel.Models;
+
 namespace DummyRoster.Web.Pages;
 
 public class ProductModel : PageModel {
 
-  public IEnumerable<string>? Products { get; set; }
+  public IEnumerable<Product>? Products { get; set; }
+
+  private DummyRosterContext dummyRosterContext;
+
+  public ProductModel(DummyRosterContext dummyRosterContextInjected) {
+    this.dummyRosterContext = dummyRosterContextInjected;
+  }
 
   public void OnGet() {
     ViewData["Title"] = "DummyRoster Web - Product";
-    ViewData["CardTitle"] = "Products";
-    Products = new[] {"Sample One", "Sample Two", "Sample Three", "Sample Four", "Sample Five"};
+    Products = dummyRosterContext.Products.OrderBy(
+      product => product.RegistrationDate
+    ).ThenBy(
+      product => product.Name
+    );
   }
 }
