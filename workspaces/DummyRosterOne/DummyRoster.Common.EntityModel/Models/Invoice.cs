d933982194d3f3eed649ad4e4475908dee4eb960
paolomococci <paolomococci@gmail.com>
2022-11-19 12:21:14 +0100

    modified:   workspaces/DummyRosterOne/DummyRoster.Common.EntityModel/Models/Invoice.cs

--- workspaces/DummyRosterOne/DummyRoster.Common.EntityModel/Models/Invoice.cs
+++ workspaces/DummyRosterOne/DummyRoster.Common.EntityModel/Models/Invoice.cs
@@ -9,43 +9,44 @@ namespace DummyRoster.Common.EntityModel.Models;
 [Index("ProductId", Name = "IDX_ProductId")]
 public partial class Invoice
 {
-    [RegularExpression(@"[a-zA-Z0-1\s\-\,\.]{3,64}")]
-    public string? Note { get; set; }
+  [RegularExpression(@"[a-zA-Z0-1\s\-\,\.]{3,64}")]
+  public string? Note { get; set; }
 
-    [Key]
-    [Column(TypeName = "INT")]
-    [Range(0, 2147483647)]
-    public int FormId { get; set; }
+  [Key]
+  [Column(TypeName = "INT")]
+  [Range(0, 2147483647)]
+  public int FormId { get; set; }
 
-    [Key]
-    [Column(TypeName = "INT")]
-    [Range(0, 2147483647)]
-    public int ProductId { get; set; }
+  [Key]
+  [Column(TypeName = "INT")]
+  [Range(0, 2147483647)]
+  public int ProductId { get; set; }
 
-    [Column(TypeName = "DATETIME")]
-    public DateTime? RegistrationDate { get; set; }
+  [Column(TypeName = "DATETIME")]
+  public DateTime? RegistrationDate { get; set; }
 
-    public double UnitPrice { get; set; }
+  public double UnitPrice { get; set; }
 
-    [Column(TypeName = "SMALLINT")]
-    [Range(0, 65535)]
-    public UInt16 Quantity { get; set; }
+  [Column(TypeName = "SMALLINT")]
+  [Range(0, 65535)]
+  public UInt16 Quantity { get; set; }
 
-    [Range(0, 1)]
-    public decimal PriceCut { get; set; }
+  [Range(0, 1)]
+  public decimal PriceCut { get; set; }
 
-    [ForeignKey("FormId")]
-    [InverseProperty("Invoices")]
-    public virtual Form Form { get; set; } = null!;
+  [ForeignKey("FormId")]
+  [InverseProperty("Invoices")]
+  public virtual Form Form { get; set; } = null!;
 
-    [ForeignKey("ProductId")]
-    [InverseProperty("Invoices")]
-    public virtual Product Product { get; set; } = null!;
+  [ForeignKey("ProductId")]
+  [InverseProperty("Invoices")]
+  public virtual Product Product { get; set; } = null!;
 
   private const int seed = 12345;
   private Random random = new Random(seed);
 
-  public int generateRandomId() {
+  public int generateRandomId()
+  {
     return this.random.Next();
   }
 }
