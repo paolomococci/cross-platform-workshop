256a79c7d9d6eee2d5a9b5f8f19c7d34827204aa
paolomococci <paolomococci@gmail.com>
2022-11-19 12:16:17 +0100

    modified:   workspaces/DummyRosterOne/DummyRoster.Common.EntityModel/Models/Carrier.cs

--- workspaces/DummyRosterOne/DummyRoster.Common.EntityModel/Models/Carrier.cs
+++ workspaces/DummyRosterOne/DummyRoster.Common.EntityModel/Models/Carrier.cs
@@ -11,73 +11,74 @@ namespace DummyRoster.Common.EntityModel.Models;
 [Index("Postcode", Name = "IDX_CarrierPostcode")]
 public partial class Carrier
 {
-    [Key]
-    [Column(TypeName = "INT")]
-    [Range(0, 2147483647)]
-    public int Id { get; set; }
+  [Key]
+  [Column(TypeName = "INT")]
+  [Range(0, 2147483647)]
+  public int Id { get; set; }
 
-    [Required]
-    [Column(TypeName = "varchar(32)")]
-    [RegularExpression("[a-zA-Z]{1,32}")]
-    public string Name { get; set; } = null!;
+  [Required]
+  [Column(TypeName = "varchar(32)")]
+  [RegularExpression("[a-zA-Z]{1,32}")]
+  public string Name { get; set; } = null!;
 
-    [Column(TypeName = "DATETIME")]
-    public DateTime? FoundationDate { get; set; }
+  [Column(TypeName = "DATETIME")]
+  public DateTime? FoundationDate { get; set; }
 
-    [RegularExpression(@"^([A-Z]{1,}[a-zA-Z0-9\s\.\,]{1,63})$")]
-    public string? Description { get; set; }
+  [RegularExpression(@"^([A-Z]{1,}[a-zA-Z0-9\s\.\,]{1,63})$")]
+  public string? Description { get; set; }
 
-    public byte[]? Picture { get; set; }
+  public byte[]? Picture { get; set; }
 
-    [Column(TypeName = "INT")]
-    public int? Contact { get; set; }
+  [Column(TypeName = "INT")]
+  public int? Contact { get; set; }
 
-    [Column(TypeName = "DATETIME")]
-    public DateTime? RegistrationDate { get; set; }
+  [Column(TypeName = "DATETIME")]
+  public DateTime? RegistrationDate { get; set; }
 
-    [Column(TypeName = "varchar(64)")]
-    [RegularExpression(@"[a-zA-Z0-9\s\-\,\.]{3,64}")]
-    public string? Address { get; set; }
+  [Column(TypeName = "varchar(64)")]
+  [RegularExpression(@"[a-zA-Z0-9\s\-\,\.]{3,64}")]
+  public string? Address { get; set; }
 
-    [Column(TypeName = "varchar(16)")]
-    [RegularExpression(@"[a-zA-Z\s\-]{3,16}")]
-    public string? City { get; set; }
+  [Column(TypeName = "varchar(16)")]
+  [RegularExpression(@"[a-zA-Z\s\-]{3,16}")]
+  public string? City { get; set; }
 
-    [Column(TypeName = "varchar(16)")]
-    [RegularExpression(@"[a-zA-Z0-9\s\-]{3,16}")]
-    public string? District { get; set; }
+  [Column(TypeName = "varchar(16)")]
+  [RegularExpression(@"[a-zA-Z0-9\s\-]{3,16}")]
+  public string? District { get; set; }
 
-    [Column(TypeName = "varchar(8)")]
-    [RegularExpression("[a-zA-Z0-9]{1,8}")]
-    public string? Postcode { get; set; }
+  [Column(TypeName = "varchar(8)")]
+  [RegularExpression("[a-zA-Z0-9]{1,8}")]
+  public string? Postcode { get; set; }
 
-    [Column(TypeName = "varchar(16)")]
-    [RegularExpression(@"[a-zA-Z\-]{3,16}")]
-    public string? Country { get; set; }
+  [Column(TypeName = "varchar(16)")]
+  [RegularExpression(@"[a-zA-Z\-]{3,16}")]
+  public string? Country { get; set; }
 
-    [Column(TypeName = "varchar(48)")]
-    [RegularExpression(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,5})+)$")]
-    public string? Email { get; set; }
+  [Column(TypeName = "varchar(48)")]
+  [RegularExpression(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,5})+)$")]
+  public string? Email { get; set; }
 
-    [Column(TypeName = "varchar(24)")]
-    [RegularExpression(@"^\+?\d{0,2}\-?\d{7,11}")]
-    public string? Phone { get; set; }
+  [Column(TypeName = "varchar(24)")]
+  [RegularExpression(@"^\+?\d{0,2}\-?\d{7,11}")]
+  public string? Phone { get; set; }
 
-    [Column(TypeName = "varchar(24)")]
-    [RegularExpression(@"^\+?\d{0,2}\-?\d{7,11}")]
-    public string? Fax { get; set; }
+  [Column(TypeName = "varchar(24)")]
+  [RegularExpression(@"^\+?\d{0,2}\-?\d{7,11}")]
+  public string? Fax { get; set; }
 
-    [ForeignKey("Contact")]
-    [InverseProperty("Carriers")]
-    public virtual Employee? ContactNavigation { get; set; }
+  [ForeignKey("Contact")]
+  [InverseProperty("Carriers")]
+  public virtual Employee? ContactNavigation { get; set; }
 
-    [InverseProperty("Carrier")]
-    public virtual ICollection<Form> Forms { get; } = new List<Form>();
+  [InverseProperty("Carrier")]
+  public virtual ICollection<Form> Forms { get; } = new List<Form>();
 
   private const int seed = 12345;
   private Random random = new Random(seed);
 
-  public int generateRandomId() {
+  public int generateRandomId()
+  {
     return this.random.Next();
   }
 }
