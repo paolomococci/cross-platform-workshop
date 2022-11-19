0ba0763158928aa0fa4c84c65ee40f3b0b620b13
paolomococci <paolomococci@gmail.com>
2022-11-19 12:40:42 +0100

    modified:   workspaces/DummyRosterOne/DummyRoster.Common.EntityModel/Models/Invoice.cs

--- workspaces/DummyRosterOne/DummyRoster.Common.EntityModel/Models/Invoice.cs
+++ workspaces/DummyRosterOne/DummyRoster.Common.EntityModel/Models/Invoice.cs
@@ -41,12 +41,4 @@ public partial class Invoice
   [ForeignKey("ProductId")]
   [InverseProperty("Invoices")]
   public virtual Product Product { get; set; } = null!;
-
-  private const int seed = 131071;
-  private Random random = new Random(seed);
-
-  public int generateRandomId()
-  {
-    return this.random.Next();
-  }
 }
