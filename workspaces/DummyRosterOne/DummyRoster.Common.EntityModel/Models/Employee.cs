db319b41aa5d09eb5437fa33bf88c4487b1d089c
paolomococci <paolomococci@gmail.com>
2022-11-19 08:34:16 +0100

    modified:   workspaces/DummyRosterOne/DummyRoster.Common.EntityModel/Models/Employee.cs

--- workspaces/DummyRosterOne/DummyRoster.Common.EntityModel/Models/Employee.cs
+++ workspaces/DummyRosterOne/DummyRoster.Common.EntityModel/Models/Employee.cs
@@ -57,7 +57,7 @@ public partial class Employee
     public string? Country { get; set; }
 
     [Column(TypeName = "varchar(48)")]
-    [RegularExpression(@"^([a-zA-Z0-9\.\-]{2,27}@[a-zA-Z0-9\.]{2,20})$")]
+    [RegularExpression(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,5})+)$")]
     public string? Email { get; set; }
 
     [Column(TypeName = "varchar(24)")]
