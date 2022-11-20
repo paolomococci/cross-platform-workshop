24c31a7788ffae0c62035d1d3e31e4fafc20f632
paolomococci <paolomococci@gmail.com>
2022-11-20 10:19:35 +0100

    new file:   workspaces/DummyRosterTwo/DummyRoster.Web.Form/Areas/DummyRosterFeature/Pages/Form.cshtml.cs

---
+++ workspaces/DummyRosterTwo/DummyRoster.Web.Form/Areas/DummyRosterFeature/Pages/Form.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DummyRoster.Web.Form.DummyRosterFeature.Pages;
+
+public class FormModel : PageModel
+{
+    public void OnGet()
+    {
+
+    }
+}
