43d76064f2d1e19ca057e5a08012d11f236f5b45
paolomococci <paolomococci@gmail.com>
2022-11-20 11:55:16 +0100

    modified:   workspaces/DummyRosterTwo/DummyRoster.Web.Product/Areas/DummyRosterFeature/Pages/Product.cshtml.cs

--- workspaces/DummyRosterTwo/DummyRoster.Web.Product/Areas/DummyRosterFeature/Pages/Product.cshtml.cs
+++ workspaces/DummyRosterTwo/DummyRoster.Web.Product/Areas/DummyRosterFeature/Pages/Product.cshtml.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
+using DummyRoster.Common.DataContext.Data;
+
 namespace DummyRoster.Web.Product.DummyRosterFeature.Pages;
 
 public class ProductModel : PageModel
