dd40c26a9a4dab6974a8e87f88a579ad41210686
paolomococci <paolomococci@gmail.com>
2022-11-20 11:54:07 +0100

    modified:   workspaces/DummyRosterTwo/DummyRoster.Web.Carrier/Areas/DummyRosterFeature/Pages/Carrier.cshtml.cs

--- workspaces/DummyRosterTwo/DummyRoster.Web.Carrier/Areas/DummyRosterFeature/Pages/Carrier.cshtml.cs
+++ workspaces/DummyRosterTwo/DummyRoster.Web.Carrier/Areas/DummyRosterFeature/Pages/Carrier.cshtml.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
+using DummyRoster.Common.DataContext.Data;
+
 namespace DummyRoster.Web.Carrier.DummyRosterFeature.Pages;
 
 public class CarrierModel : PageModel
