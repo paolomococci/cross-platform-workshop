d84605d8b21392aad6cdfa9fb3f115246af83e6a
paolomococci <paolomococci@gmail.com>
2022-11-20 11:54:31 +0100

    modified:   workspaces/DummyRosterTwo/DummyRoster.Web.Customer/Areas/DummyRosterFeature/Pages/Customer.cshtml.cs

--- workspaces/DummyRosterTwo/DummyRoster.Web.Customer/Areas/DummyRosterFeature/Pages/Customer.cshtml.cs
+++ workspaces/DummyRosterTwo/DummyRoster.Web.Customer/Areas/DummyRosterFeature/Pages/Customer.cshtml.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
+using DummyRoster.Common.DataContext.Data;
+
 namespace DummyRoster.Web.Customer.DummyRosterFeature.Pages;
 
 public class CustomerModel : PageModel
