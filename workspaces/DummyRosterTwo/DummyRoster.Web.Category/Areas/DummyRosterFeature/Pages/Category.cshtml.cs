4269895ec156c4286ecfcb7582f2d778d94daad7
paolomococci <paolomococci@gmail.com>
2022-11-20 11:54:20 +0100

    modified:   workspaces/DummyRosterTwo/DummyRoster.Web.Category/Areas/DummyRosterFeature/Pages/Category.cshtml.cs

--- workspaces/DummyRosterTwo/DummyRoster.Web.Category/Areas/DummyRosterFeature/Pages/Category.cshtml.cs
+++ workspaces/DummyRosterTwo/DummyRoster.Web.Category/Areas/DummyRosterFeature/Pages/Category.cshtml.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
+using DummyRoster.Common.DataContext.Data;
+
 namespace DummyRoster.Web.Category.DummyRosterFeature.Pages;
 
 public class CategoryModel : PageModel
