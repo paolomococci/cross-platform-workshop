109f454bac7194a8b024be270e5124a8c923d67d
paolomococci <paolomococci@gmail.com>
2022-11-20 11:54:54 +0100

    modified:   workspaces/DummyRosterTwo/DummyRoster.Web.Form/Areas/DummyRosterFeature/Pages/Form.cshtml.cs

--- workspaces/DummyRosterTwo/DummyRoster.Web.Form/Areas/DummyRosterFeature/Pages/Form.cshtml.cs
+++ workspaces/DummyRosterTwo/DummyRoster.Web.Form/Areas/DummyRosterFeature/Pages/Form.cshtml.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
+using DummyRoster.Common.DataContext.Data;
+
 namespace DummyRoster.Web.Form.DummyRosterFeature.Pages;
 
 public class FormModel : PageModel
