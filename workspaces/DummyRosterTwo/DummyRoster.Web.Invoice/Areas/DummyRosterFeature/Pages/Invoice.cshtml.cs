d33a4be94535c998e5853226f177d4952898d99a
paolomococci <paolomococci@gmail.com>
2022-11-20 22:48:21 +0100

    modified:   workspaces/DummyRosterTwo/DummyRoster.Web.Invoice/Areas/DummyRosterFeature/Pages/Invoice.cshtml.cs

--- workspaces/DummyRosterTwo/DummyRoster.Web.Invoice/Areas/DummyRosterFeature/Pages/Invoice.cshtml.cs
+++ workspaces/DummyRosterTwo/DummyRoster.Web.Invoice/Areas/DummyRosterFeature/Pages/Invoice.cshtml.cs
@@ -8,7 +8,7 @@ namespace DummyRoster.Web.Invoice.DummyRosterFeature.Pages;
 public class InvoiceModel : PageModel
 {
   private DummyRosterContext dummyRosterContext;
-  public DummyRoster.Common.EntityModel.Models.Invoice[] invoices { get; set; } = null!;
+  public IQueryable<DummyRoster.Common.EntityModel.Models.Invoice> invoices { get; set; } = null!;
 
   public InvoiceModel(
       DummyRosterContext dummyRosterContext
