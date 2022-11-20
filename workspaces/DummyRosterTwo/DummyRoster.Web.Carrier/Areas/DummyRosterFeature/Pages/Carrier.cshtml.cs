a17737a9a6094520dea6c2b5098c186e02de2835
paolomococci <paolomococci@gmail.com>
2022-11-20 10:16:41 +0100

    new file:   workspaces/DummyRosterTwo/DummyRoster.Web.Carrier/Areas/DummyRosterFeature/Pages/Carrier.cshtml.cs

---
+++ workspaces/DummyRosterTwo/DummyRoster.Web.Carrier/Areas/DummyRosterFeature/Pages/Carrier.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DummyRoster.Web.Carrier.DummyRosterFeature.Pages;
+
+public class CarrierModel : PageModel
+{
+    public void OnGet()
+    {
+
+    }
+}
