40157794e0d36492aafb2a3490494ca5ed5429ba
paolomococci <paolomococci@gmail.com>
2022-11-20 09:39:45 +0100

    new file:   workspaces/DummyRosterTwo/DummyRoster.Web.Invoice/Areas/MyFeature/Pages/Page1.cshtml.cs

---
+++ workspaces/DummyRosterTwo/DummyRoster.Web.Invoice/Areas/MyFeature/Pages/Page1.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DummyRoster.Web.Invoice.MyFeature.Pages;
+
+public class Page1Model : PageModel
+{
+    public void OnGet()
+    {
+
+    }
+}
