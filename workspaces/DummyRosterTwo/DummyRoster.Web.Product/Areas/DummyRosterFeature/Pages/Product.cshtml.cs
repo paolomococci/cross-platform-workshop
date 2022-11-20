2a6d6c044aade6face175ce66439d37752d9e4da
paolomococci <paolomococci@gmail.com>
2022-11-20 22:48:33 +0100

    modified:   workspaces/DummyRosterTwo/DummyRoster.Web.Product/Areas/DummyRosterFeature/Pages/Product.cshtml.cs

--- workspaces/DummyRosterTwo/DummyRoster.Web.Product/Areas/DummyRosterFeature/Pages/Product.cshtml.cs
+++ workspaces/DummyRosterTwo/DummyRoster.Web.Product/Areas/DummyRosterFeature/Pages/Product.cshtml.cs
@@ -8,7 +8,7 @@ namespace DummyRoster.Web.Product.DummyRosterFeature.Pages;
 public class ProductModel : PageModel
 {
   private DummyRosterContext dummyRosterContext;
-  public DummyRoster.Common.EntityModel.Models.Product[] products { get; set; } = null!;
+  public IQueryable<DummyRoster.Common.EntityModel.Models.Product> products { get; set; } = null!;
 
   public ProductModel(
       DummyRosterContext dummyRosterContext
