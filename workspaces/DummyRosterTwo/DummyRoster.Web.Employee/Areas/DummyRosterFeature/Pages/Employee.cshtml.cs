9b0651e207e34d42f1c682a56b4e89d530eb97ab
paolomococci <paolomococci@gmail.com>
2022-11-20 10:18:52 +0100

    new file:   workspaces/DummyRosterTwo/DummyRoster.Web.Employee/Areas/DummyRosterFeature/Pages/Employee.cshtml.cs

---
+++ workspaces/DummyRosterTwo/DummyRoster.Web.Employee/Areas/DummyRosterFeature/Pages/Employee.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DummyRoster.Web.Employee.DummyRosterFeature.Pages;
+
+public class EmployeeModel : PageModel
+{
+    public void OnGet()
+    {
+
+    }
+}
