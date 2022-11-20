829440e5d4170a9107837c71eff12949008de674
paolomococci <paolomococci@gmail.com>
2022-11-20 10:21:36 +0100

    new file:   workspaces/DummyRosterTwo/DummyRoster.Web.Supplier/Areas/DummyRosterFeature/Pages/Supplier.cshtml.cs

---
+++ workspaces/DummyRosterTwo/DummyRoster.Web.Supplier/Areas/DummyRosterFeature/Pages/Supplier.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DummyRoster.Web.Supplier.DummyRosterFeature.Pages;
+
+public class SupplierModel : PageModel
+{
+    public void OnGet()
+    {
+
+    }
+}
