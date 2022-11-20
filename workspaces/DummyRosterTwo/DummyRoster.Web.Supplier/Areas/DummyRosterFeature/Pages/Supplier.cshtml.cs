81dec50dd81635719dbac64f2bcb5ddcc0addeb6
paolomococci <paolomococci@gmail.com>
2022-11-20 11:55:27 +0100

    modified:   workspaces/DummyRosterTwo/DummyRoster.Web.Supplier/Areas/DummyRosterFeature/Pages/Supplier.cshtml.cs

--- workspaces/DummyRosterTwo/DummyRoster.Web.Supplier/Areas/DummyRosterFeature/Pages/Supplier.cshtml.cs
+++ workspaces/DummyRosterTwo/DummyRoster.Web.Supplier/Areas/DummyRosterFeature/Pages/Supplier.cshtml.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
+using DummyRoster.Common.DataContext.Data;
+
 namespace DummyRoster.Web.Supplier.DummyRosterFeature.Pages;
 
 public class SupplierModel : PageModel
