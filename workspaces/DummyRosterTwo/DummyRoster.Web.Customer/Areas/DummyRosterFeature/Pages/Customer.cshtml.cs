f6413ce1bde1cf8517df4f3a4f06d23ce2b3dbb9
paolomococci <paolomococci@gmail.com>
2022-11-20 10:18:07 +0100

    new file:   workspaces/DummyRosterTwo/DummyRoster.Web.Customer/Areas/DummyRosterFeature/Pages/Customer.cshtml.cs

---
+++ workspaces/DummyRosterTwo/DummyRoster.Web.Customer/Areas/DummyRosterFeature/Pages/Customer.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DummyRoster.Web.Customer.DummyRosterFeature.Pages;
+
+public class CustomerModel : PageModel
+{
+    public void OnGet()
+    {
+
+    }
+}
