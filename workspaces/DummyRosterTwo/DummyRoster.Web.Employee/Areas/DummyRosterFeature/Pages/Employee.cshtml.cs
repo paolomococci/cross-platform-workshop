753cd29bcb6cc49209f13c0e6e4f1463ac2e9140
paolomococci <paolomococci@gmail.com>
2022-11-20 11:54:42 +0100

    modified:   workspaces/DummyRosterTwo/DummyRoster.Web.Employee/Areas/DummyRosterFeature/Pages/Employee.cshtml.cs

--- workspaces/DummyRosterTwo/DummyRoster.Web.Employee/Areas/DummyRosterFeature/Pages/Employee.cshtml.cs
+++ workspaces/DummyRosterTwo/DummyRoster.Web.Employee/Areas/DummyRosterFeature/Pages/Employee.cshtml.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
+using DummyRoster.Common.DataContext.Data;
+
 namespace DummyRoster.Web.Employee.DummyRosterFeature.Pages;
 
 public class EmployeeModel : PageModel
