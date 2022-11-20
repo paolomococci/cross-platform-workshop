a67791ec202c9e6d89c25faa6ce46d618b05249a
paolomococci <paolomococci@gmail.com>
2022-11-20 11:55:05 +0100

    modified:   workspaces/DummyRosterTwo/DummyRoster.Web.Invoice/Areas/DummyRosterFeature/Pages/Invoice.cshtml.cs

--- workspaces/DummyRosterTwo/DummyRoster.Web.Invoice/Areas/DummyRosterFeature/Pages/Invoice.cshtml.cs
+++ workspaces/DummyRosterTwo/DummyRoster.Web.Invoice/Areas/DummyRosterFeature/Pages/Invoice.cshtml.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
+using DummyRoster.Common.DataContext.Data;
+
 namespace DummyRoster.Web.Invoice.DummyRosterFeature.Pages;
 
 public class InvoiceModel : PageModel
