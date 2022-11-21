dc9359e3eb02999fb679e3141e63fdf84588837e
paolomococci <paolomococci@gmail.com>
2022-11-21 23:27:45 +0100

    modified:   workspaces/DummyRosterTwo/DummyRoster.Web/Startup.cs

--- workspaces/DummyRosterTwo/DummyRoster.Web/Startup.cs
+++ workspaces/DummyRosterTwo/DummyRoster.Web/Startup.cs
@@ -1,2 +1,47 @@
 namespace DummyRoster.Web;
-public class Startup {}
+
+public class Startup {
+  public void ConfigureServices(
+    IServiceCollection services
+  ) {
+    services.AddRazorPages();
+  }
+
+  public void Configure(
+    IApplicationBuilder applicationBuilder,
+    IWebHostEnvironment webHostEnvironment
+  ) {
+    if (!webHostEnvironment.IsDevelopment())
+    {
+      applicationBuilder.UseHsts();
+    }
+
+    applicationBuilder.UseRouting();
+
+    applicationBuilder.Use(
+      async (context, next) => {
+        RouteEndpoint? routeEndpoint = context.GetEndpoint() as RouteEndpoint;
+        if (routeEndpoint is not null)
+        {
+          Console.WriteLine($"Endpoint name: {routeEndpoint.DisplayName}");
+          Console.WriteLine($"Endpoint route pattern: {routeEndpoint.RoutePattern.RawText}");
+        }
+        await next(context);
+      }
+    );
+
+    applicationBuilder.UseHttpsRedirection();
+    applicationBuilder.UseDefaultFiles();
+    applicationBuilder.UseStaticFiles();
+
+    applicationBuilder.UseEndpoints(
+      endpoints => {
+        endpoints.MapRazorPages();
+        endpoints.MapGet(
+          "/endpoint",
+          () => "Endpoint at work!"
+        );
+      }
+    );
+  }
+}
