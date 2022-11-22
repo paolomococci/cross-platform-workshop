9f77406f61e607aeb1bd21e6292b7a9531d8a88e
paolomococci <paolomococci@gmail.com>
2022-11-22 10:56:17 +0100

    modified:   workspaces/DummyRosterTwo/DummyRoster.Web/Pages/Carrier.cshtml.cs

--- workspaces/DummyRosterTwo/DummyRoster.Web/Pages/Carrier.cshtml.cs
+++ workspaces/DummyRosterTwo/DummyRoster.Web/Pages/Carrier.cshtml.cs
@@ -15,7 +15,14 @@ public class CarrierModel : PageModel {
     this.dummyRosterContext = dummyRosterContextInjected;
   }
 
-  public void OnGet() {}
+  public void OnGet() {
+    ViewData["Title"] = "DummyRoster Web - Carrier";
+    carriers = dummyRosterContext.Carriers.OrderBy(
+      entity => entity.Country
+    ).ThenBy(
+      entity => entity.Name
+    );
+  }
 
   public IActionResult OnPost() {
     return Page();
