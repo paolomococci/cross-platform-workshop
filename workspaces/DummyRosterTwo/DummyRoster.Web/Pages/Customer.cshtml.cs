1008b6ec5b8de360f3ed89a662b6435c80fb8f62
paolomococci <paolomococci@gmail.com>
2022-11-22 10:56:29 +0100

    modified:   workspaces/DummyRosterTwo/DummyRoster.Web/Pages/Customer.cshtml.cs

--- workspaces/DummyRosterTwo/DummyRoster.Web/Pages/Customer.cshtml.cs
+++ workspaces/DummyRosterTwo/DummyRoster.Web/Pages/Customer.cshtml.cs
@@ -15,7 +15,14 @@ public class CustomerModel : PageModel {
     this.dummyRosterContext = dummyRosterContextInjected;
   }
 
-  public void OnGet() {}
+  public void OnGet() {
+    ViewData["Title"] = "DummyRoster Web - Customer";
+    customers = dummyRosterContext.Customers.OrderBy(
+      entity => entity.Country
+    ).ThenBy(
+      entity => entity.Name
+    );
+  }
 
   public IActionResult OnPost() {
     return Page();
