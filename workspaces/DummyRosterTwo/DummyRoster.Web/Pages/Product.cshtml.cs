74b5d6ea3ec9a2afd788967b8524786d66f39278
paolomococci <paolomococci@gmail.com>
2022-11-22 14:41:40 +0100

    modified:   workspaces/DummyRosterTwo/DummyRoster.Web/Pages/Product.cshtml.cs

--- workspaces/DummyRosterTwo/DummyRoster.Web/Pages/Product.cshtml.cs
+++ workspaces/DummyRosterTwo/DummyRoster.Web/Pages/Product.cshtml.cs
@@ -18,7 +18,14 @@ public class ProductModel : PageModel {
     this.dummyRosterContext = dummyRosterContextInjected;
   }
 
-  public void OnGet() {}
+  public void OnGet() {
+    ViewData["Title"] = "DummyRoster Web - Product";
+    products = dummyRosterContext.Products.OrderBy(
+      entity => entity.Description
+    ).ThenBy(
+      entity => entity.Name
+    );
+  }
 
   public IActionResult OnPost() {
     return Page();
