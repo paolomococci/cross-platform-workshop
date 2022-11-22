3394723a39272b3782775126dcf9b59b465f3833
paolomococci <paolomococci@gmail.com>
2022-11-22 10:48:46 +0100

    modified:   workspaces/DummyRosterTwo/DummyRoster.Web/Pages/Category.cshtml.cs

--- workspaces/DummyRosterTwo/DummyRoster.Web/Pages/Category.cshtml.cs
+++ workspaces/DummyRosterTwo/DummyRoster.Web/Pages/Category.cshtml.cs
@@ -15,4 +15,10 @@ public class CategoryModel : PageModel {
     this.dummyRosterContext = dummyRosterContextInjected;
   }
 
+  public void OnGet() {}
+
+  public IActionResult OnPost() {
+    return Page();
+  }
+
 }
