98aeb49886deb41b7d1daa2dd167da60ef8e8e2d
paolomococci <paolomococci@gmail.com>
2022-11-22 10:24:00 +0100

    modified:   workspaces/DummyRosterTwo/DummyRoster.Web/Pages/Form.cshtml.cs

---
+++ workspaces/DummyRosterTwo/DummyRoster.Web/Pages/Form.cshtml.cs
@@ -0,0 +1,7 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc;
+
+using DummyRoster.Common.DataContext.Data;
+using DummyRoster.Common.EntityModel.Models;
+
+namespace DummyRoster.Web.Pages;
