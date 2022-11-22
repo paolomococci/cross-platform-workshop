5143ae2ceb4e7d0565aaaefcd761b1bd468a34c1
paolomococci <paolomococci@gmail.com>
2022-11-22 10:45:22 +0100

    modified:   workspaces/DummyRosterTwo/DummyRoster.Web/Pages/Invoice.cshtml.cs

--- workspaces/DummyRosterTwo/DummyRoster.Web/Pages/Invoice.cshtml.cs
+++ workspaces/DummyRosterTwo/DummyRoster.Web/Pages/Invoice.cshtml.cs
@@ -6,8 +6,13 @@ using DummyRoster.Common.EntityModel.Models;
 
 namespace DummyRoster.Web.Pages;
 
-public class INvoiceModel : PageModel {
+public class InvoiceModel : PageModel {
 
   public IEnumerable<Invoice>? invoices { get; set; }
+  private DummyRosterContext dummyRosterContext;
+
+  public InvoiceModel(DummyRosterContext dummyRosterContextInjected) {
+    this.dummyRosterContext = dummyRosterContextInjected;
+  }
 
 }
