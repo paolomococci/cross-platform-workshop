ce4232708ba96aae0e3372afdd81389b0eb819ae
paolomococci <paolomococci@gmail.com>
2022-11-23 02:02:41 +0100

    modified:   workspaces/DummyRosterTwo/DummyRoster.Web/Pages/Carrier.cshtml.cs

--- workspaces/DummyRosterTwo/DummyRoster.Web/Pages/Carrier.cshtml.cs
+++ workspaces/DummyRosterTwo/DummyRoster.Web/Pages/Carrier.cshtml.cs
@@ -6,7 +6,8 @@ using DummyRoster.Common.EntityModel.Models;
 
 namespace DummyRoster.Web.Pages;
 
-public class CarrierModel : PageModel {
+public class CarrierModel : PageModel
+{
 
   [BindProperty]
   public Carrier carrier { get; set; } = new();
@@ -14,11 +15,13 @@ public class CarrierModel : PageModel {
   public IEnumerable<Carrier>? carriers { get; set; }
   private DummyRosterContext dummyRosterContext;
 
-  public CarrierModel(DummyRosterContext dummyRosterContextInjected) {
+  public CarrierModel(DummyRosterContext dummyRosterContextInjected)
+  {
     this.dummyRosterContext = dummyRosterContextInjected;
   }
 
-  public void OnGet() {
+  public void OnGet()
+  {
     ViewData["Title"] = "DummyRoster Web - Carrier";
     carriers = dummyRosterContext.Carriers.OrderBy(
       entity => entity.Country
@@ -27,7 +30,8 @@ public class CarrierModel : PageModel {
     );
   }
 
-  public IActionResult OnPost() {
+  public IActionResult OnPost()
+  {
     if ((this.carrier is not null) && ModelState.IsValid)
     {
       this.dummyRosterContext.Carriers.Add(this.carrier);
