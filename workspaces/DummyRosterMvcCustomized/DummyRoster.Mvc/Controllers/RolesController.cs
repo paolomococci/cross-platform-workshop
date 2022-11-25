6b728ea7fed2fdad2aa9220ee2a4e9f109ad2407
paolomococci <paolomococci@gmail.com>
2022-11-25 02:28:37 +0100

    modified:   workspaces/DummyRosterMvcCustomized/DummyRoster.Mvc/Controllers/RolesController.cs

--- workspaces/DummyRosterMvcCustomized/DummyRoster.Mvc/Controllers/RolesController.cs
+++ workspaces/DummyRosterMvcCustomized/DummyRoster.Mvc/Controllers/RolesController.cs
@@ -7,4 +7,6 @@ public class RolesController : Controller {
 
   private string adminRole = "Admins";
   private string userEmail = "admin.one@example.local";
+  private readonly RoleManager<IdentityRole> roleManager;
+  private readonly UserManager<IdentityUser> userManager;
 }
