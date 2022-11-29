20739e8d2f82e8483961d745039c17612cd9947e
paolomococci <paolomococci@gmail.com>
2022-11-29 07:18:58 +0100

    modified:   workspaces/DummyRosterWebApi/DummyRoster.Common.EntityModel/Models/Address.cs

--- workspaces/DummyRosterWebApi/DummyRoster.Common.EntityModel/Models/Address.cs
+++ workspaces/DummyRosterWebApi/DummyRoster.Common.EntityModel/Models/Address.cs
@@ -13,45 +13,45 @@ namespace DummyRoster.Common.EntityModel.Models;
 [Index("Postcode", Name = "IDX_AddressPostcode")]
 public partial class Address
 {
-    [Key]
-    public int Id { get; set; }
+  [Key]
+  public int Id { get; set; }
 
-    [Column(TypeName = "varchar(32)")]
-    public string? StreetOrSquare { get; set; }
+  [Column(TypeName = "varchar(32)")]
+  public string? StreetOrSquare { get; set; }
 
-    [Column(TypeName = "varchar(16)")]
-    public string? Civic { get; set; }
+  [Column(TypeName = "varchar(16)")]
+  public string? Civic { get; set; }
 
-    [Column(TypeName = "varchar(16)")]
-    public string? City { get; set; }
+  [Column(TypeName = "varchar(16)")]
+  public string? City { get; set; }
 
-    [Column(TypeName = "varchar(16)")]
-    public string? District { get; set; }
+  [Column(TypeName = "varchar(16)")]
+  public string? District { get; set; }
 
-    [Column(TypeName = "varchar(8)")]
-    public string? Postcode { get; set; }
+  [Column(TypeName = "varchar(8)")]
+  public string? Postcode { get; set; }
 
-    [Column(TypeName = "varchar(16)")]
-    public string? Country { get; set; }
+  [Column(TypeName = "varchar(16)")]
+  public string? Country { get; set; }
 
-    [Column(TypeName = "varchar(48)")]
-    public string? Email { get; set; }
+  [Column(TypeName = "varchar(48)")]
+  public string? Email { get; set; }
 
-    [Column(TypeName = "varchar(24)")]
-    public string? Phone { get; set; }
+  [Column(TypeName = "varchar(24)")]
+  public string? Phone { get; set; }
 
-    [Column(TypeName = "varchar(24)")]
-    public string? Fax { get; set; }
+  [Column(TypeName = "varchar(24)")]
+  public string? Fax { get; set; }
 
-    [InverseProperty("LocationNavigation")]
-    public virtual ICollection<Carrier> Carriers { get; } = new List<Carrier>();
+  [InverseProperty("LocationNavigation")]
+  public virtual ICollection<Carrier> Carriers { get; } = new List<Carrier>();
 
-    [InverseProperty("LocationNavigation")]
-    public virtual ICollection<Customer> Customers { get; } = new List<Customer>();
+  [InverseProperty("LocationNavigation")]
+  public virtual ICollection<Customer> Customers { get; } = new List<Customer>();
 
-    [InverseProperty("LocationNavigation")]
-    public virtual ICollection<Employee> Employees { get; } = new List<Employee>();
+  [InverseProperty("LocationNavigation")]
+  public virtual ICollection<Employee> Employees { get; } = new List<Employee>();
 
-    [InverseProperty("LocationNavigation")]
-    public virtual ICollection<Supplier> Suppliers { get; } = new List<Supplier>();
+  [InverseProperty("LocationNavigation")]
+  public virtual ICollection<Supplier> Suppliers { get; } = new List<Supplier>();
 }
