f8cf2a4af62efa00f58c7a9fcdb6ecb20160492d
paolomococci <paolomococci@gmail.com>
2022-11-29 07:16:02 +0100

    modified:   workspaces/DummyRosterWebApi/DummyRoster.Common.DataContext/Data/DummyRosterContext.cs

--- workspaces/DummyRosterWebApi/DummyRoster.Common.DataContext/Data/DummyRosterContext.cs
+++ workspaces/DummyRosterWebApi/DummyRoster.Common.DataContext/Data/DummyRosterContext.cs
@@ -6,97 +6,97 @@ namespace DummyRoster.Common.DataContext.Data;
 
 public partial class DummyRosterContext : DbContext
 {
-    public DummyRosterContext()
+  public DummyRosterContext()
+  {
+  }
+
+  public DummyRosterContext(DbContextOptions<DummyRosterContext> options)
+      : base(options)
+  {
+  }
+
+  public virtual DbSet<Address> Addresses { get; set; }
+
+  public virtual DbSet<Carrier> Carriers { get; set; }
+
+  public virtual DbSet<Category> Categories { get; set; }
+
+  public virtual DbSet<Customer> Customers { get; set; }
+
+  public virtual DbSet<Employee> Employees { get; set; }
+
+  public virtual DbSet<Form> Forms { get; set; }
+
+  public virtual DbSet<Invoice> Invoices { get; set; }
+
+  public virtual DbSet<Product> Products { get; set; }
+
+  public virtual DbSet<Supplier> Suppliers { get; set; }
+
+  protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+      => optionsBuilder.UseSqlite("Filename=../DummyRoster.db");
+
+  protected override void OnModelCreating(ModelBuilder modelBuilder)
+  {
+    modelBuilder.Entity<Address>(entity =>
     {
-    }
+      entity.Property(e => e.Id).ValueGeneratedNever();
+    });
 
-    public DummyRosterContext(DbContextOptions<DummyRosterContext> options)
-        : base(options)
+    modelBuilder.Entity<Carrier>(entity =>
     {
-    }
+      entity.Property(e => e.Id).ValueGeneratedNever();
+    });
 
-    public virtual DbSet<Address> Addresses { get; set; }
-
-    public virtual DbSet<Carrier> Carriers { get; set; }
-
-    public virtual DbSet<Category> Categories { get; set; }
-
-    public virtual DbSet<Customer> Customers { get; set; }
-
-    public virtual DbSet<Employee> Employees { get; set; }
-
-    public virtual DbSet<Form> Forms { get; set; }
-
-    public virtual DbSet<Invoice> Invoices { get; set; }
-
-    public virtual DbSet<Product> Products { get; set; }
-
-    public virtual DbSet<Supplier> Suppliers { get; set; }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        => optionsBuilder.UseSqlite("Filename=../DummyRoster.db");
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    modelBuilder.Entity<Category>(entity =>
     {
-        modelBuilder.Entity<Address>(entity =>
-        {
-            entity.Property(e => e.Id).ValueGeneratedNever();
-        });
+      entity.Property(e => e.Id).ValueGeneratedNever();
+    });
 
-        modelBuilder.Entity<Carrier>(entity =>
-        {
-            entity.Property(e => e.Id).ValueGeneratedNever();
-        });
+    modelBuilder.Entity<Customer>(entity =>
+    {
+      entity.Property(e => e.Id).ValueGeneratedNever();
+    });
 
-        modelBuilder.Entity<Category>(entity =>
-        {
-            entity.Property(e => e.Id).ValueGeneratedNever();
-        });
+    modelBuilder.Entity<Employee>(entity =>
+    {
+      entity.Property(e => e.Id).ValueGeneratedNever();
+    });
 
-        modelBuilder.Entity<Customer>(entity =>
-        {
-            entity.Property(e => e.Id).ValueGeneratedNever();
-        });
+    modelBuilder.Entity<Form>(entity =>
+    {
+      entity.Property(e => e.Id).ValueGeneratedNever();
+      entity.Property(e => e.ShippingCost).HasDefaultValueSql("0");
+    });
 
-        modelBuilder.Entity<Employee>(entity =>
-        {
-            entity.Property(e => e.Id).ValueGeneratedNever();
-        });
+    modelBuilder.Entity<Invoice>(entity =>
+    {
+      entity.Property(e => e.PriceCut).HasDefaultValueSql("0");
+      entity.Property(e => e.Quantity).HasDefaultValueSql("1");
+      entity.Property(e => e.UnitPrice).HasDefaultValueSql("0");
 
-        modelBuilder.Entity<Form>(entity =>
-        {
-            entity.Property(e => e.Id).ValueGeneratedNever();
-            entity.Property(e => e.ShippingCost).HasDefaultValueSql("0");
-        });
+      entity.HasOne(d => d.Form).WithMany(p => p.Invoices).OnDelete(DeleteBehavior.ClientSetNull);
 
-        modelBuilder.Entity<Invoice>(entity =>
-        {
-            entity.Property(e => e.PriceCut).HasDefaultValueSql("0");
-            entity.Property(e => e.Quantity).HasDefaultValueSql("1");
-            entity.Property(e => e.UnitPrice).HasDefaultValueSql("0");
+      entity.HasOne(d => d.Product).WithMany(p => p.Invoices).OnDelete(DeleteBehavior.ClientSetNull);
+    });
 
-            entity.HasOne(d => d.Form).WithMany(p => p.Invoices).OnDelete(DeleteBehavior.ClientSetNull);
+    modelBuilder.Entity<Product>(entity =>
+    {
+      entity.Property(e => e.Id).ValueGeneratedNever();
+      entity.Property(e => e.Discontinued).HasDefaultValueSql("0");
+      entity.Property(e => e.ReorderLevel).HasDefaultValueSql("0");
+      entity.Property(e => e.UnitPrice).HasDefaultValueSql("0");
+      entity.Property(e => e.UnitsInStock).HasDefaultValueSql("0");
+      entity.Property(e => e.UnitsOnOrder).HasDefaultValueSql("0");
+    });
 
-            entity.HasOne(d => d.Product).WithMany(p => p.Invoices).OnDelete(DeleteBehavior.ClientSetNull);
-        });
+    modelBuilder.Entity<Supplier>(entity =>
+    {
+      entity.Property(e => e.Id).ValueGeneratedNever();
+    });
 
-        modelBuilder.Entity<Product>(entity =>
-        {
-            entity.Property(e => e.Id).ValueGeneratedNever();
-            entity.Property(e => e.Discontinued).HasDefaultValueSql("0");
-            entity.Property(e => e.ReorderLevel).HasDefaultValueSql("0");
-            entity.Property(e => e.UnitPrice).HasDefaultValueSql("0");
-            entity.Property(e => e.UnitsInStock).HasDefaultValueSql("0");
-            entity.Property(e => e.UnitsOnOrder).HasDefaultValueSql("0");
-        });
+    OnModelCreatingPartial(modelBuilder);
+  }
 
-        modelBuilder.Entity<Supplier>(entity =>
-        {
-            entity.Property(e => e.Id).ValueGeneratedNever();
-        });
-
-        OnModelCreatingPartial(modelBuilder);
-    }
-
-    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+  partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
 }
