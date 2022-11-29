325c23a39fd1d52194f33c16f2481c52e76a1043
paolomococci <paolomococci@gmail.com>
2022-11-29 07:20:00 +0100

    modified:   workspaces/DummyRosterWebApi/DummyRoster.Common.EntityModel/Models/Invoice.cs

--- workspaces/DummyRosterWebApi/DummyRoster.Common.EntityModel/Models/Invoice.cs
+++ workspaces/DummyRosterWebApi/DummyRoster.Common.EntityModel/Models/Invoice.cs
@@ -11,30 +11,30 @@ namespace DummyRoster.Common.EntityModel.Models;
 [Index("ProductId", Name = "IDX_ProductId")]
 public partial class Invoice
 {
-    public string? Note { get; set; }
+  public string? Note { get; set; }
 
-    [Key]
-    [Column(TypeName = "INT")]
-    public int FormId { get; set; }
+  [Key]
+  [Column(TypeName = "INT")]
+  public int FormId { get; set; }
 
-    [Key]
-    [Column(TypeName = "INT")]
-    public int ProductId { get; set; }
+  [Key]
+  [Column(TypeName = "INT")]
+  public int ProductId { get; set; }
 
-    [Column(TypeName = "NUMERIC")]
-    public decimal? UnitPrice { get; set; }
+  [Column(TypeName = "NUMERIC")]
+  public decimal? UnitPrice { get; set; }
 
-    [Column(TypeName = "SMALLINT")]
-    public int Quantity { get; set; }
+  [Column(TypeName = "SMALLINT")]
+  public int Quantity { get; set; }
 
-    [Column(TypeName = "NUMERIC")]
-    public decimal? PriceCut { get; set; }
+  [Column(TypeName = "NUMERIC")]
+  public decimal? PriceCut { get; set; }
 
-    [ForeignKey("FormId")]
-    [InverseProperty("Invoices")]
-    public virtual Form Form { get; set; } = null!;
+  [ForeignKey("FormId")]
+  [InverseProperty("Invoices")]
+  public virtual Form Form { get; set; } = null!;
 
-    [ForeignKey("ProductId")]
-    [InverseProperty("Invoices")]
-    public virtual Product Product { get; set; } = null!;
+  [ForeignKey("ProductId")]
+  [InverseProperty("Invoices")]
+  public virtual Product Product { get; set; } = null!;
 }
