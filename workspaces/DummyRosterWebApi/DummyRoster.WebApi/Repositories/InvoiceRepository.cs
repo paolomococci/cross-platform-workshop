40ea26fcddb022ed9091e8ea370dc3c5f83980aa
paolomococci <paolomococci@gmail.com>
2022-11-30 10:42:55 +0100

    modified:   workspaces/DummyRosterWebApi/DummyRoster.WebApi/Repositories/InvoiceRepository.cs

--- workspaces/DummyRosterWebApi/DummyRoster.WebApi/Repositories/InvoiceRepository.cs
+++ workspaces/DummyRosterWebApi/DummyRoster.WebApi/Repositories/InvoiceRepository.cs
@@ -4,4 +4,30 @@ using DummyRoster.Common.EntityModel.Models;
 
 namespace DummyRoster.WebApi.Repositories;
 
-public class InvoiceRepository : IInvoiceRepository {}
+public class InvoiceRepository : IInvoiceRepository
+{
+  public Task<Invoice?> CreateAsync(Invoice invoice)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<bool?> DeleteAsync(int id)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<IEnumerable<Invoice>> RetrieveAllAsync()
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<Invoice?> RetrieveAsync(int id)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<Invoice?> UpdateAsync(int id, Invoice invoice)
+  {
+    throw new NotImplementedException();
+  }
+}
