fb3dc83bc79d8b1898326b7a27034820ead67e41
paolomococci <paolomococci@gmail.com>
2022-11-30 09:33:30 +0100

    modified:   workspaces/DummyRosterWebApi/DummyRoster.WebApi/Repositories/IProductRepository.cs

--- workspaces/DummyRosterWebApi/DummyRoster.WebApi/Repositories/IProductRepository.cs
+++ workspaces/DummyRosterWebApi/DummyRoster.WebApi/Repositories/IProductRepository.cs
@@ -5,4 +5,5 @@ namespace DummyRoster.WebApi.Repositories;
 public interface IProductRepository {
   Task<Product> CreateAsync(Product product);
   Task<Product> RetrieveAsync(int id);
+  Task<IEnumerable<Product>> RetrieveAllAsync();
 }
