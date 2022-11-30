181218817f342ee4399ace6d017773e7edf2b229
paolomococci <paolomococci@gmail.com>
2022-11-30 09:57:05 +0100

    modified:   workspaces/DummyRosterWebApi/DummyRoster.WebApi/Repositories/IEmployeeRepository.cs

--- workspaces/DummyRosterWebApi/DummyRoster.WebApi/Repositories/IEmployeeRepository.cs
+++ workspaces/DummyRosterWebApi/DummyRoster.WebApi/Repositories/IEmployeeRepository.cs
@@ -6,4 +6,8 @@ public interface IEmployeeRepository {
   Task<Employee> CreateAsync(Employee employee);
   Task<Employee> RetrieveAsync(int id);
   Task<IEnumerable<Employee>> RetrieveAllAsync();
+  Task<Employee> UpdateAsync(
+    int id,
+    Employee employee
+  );
 }
