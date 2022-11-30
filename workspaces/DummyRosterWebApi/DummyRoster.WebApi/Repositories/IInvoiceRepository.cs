cb2781f9e75cc9eaf03e39c6a0a80c3f38917369
paolomococci <paolomococci@gmail.com>
2022-11-30 08:37:56 +0100

    modified:   workspaces/DummyRosterWebApi/DummyRoster.WebApi/Repositories/IInvoiceRepository.cs

--- workspaces/DummyRosterWebApi/DummyRoster.WebApi/Repositories/IInvoiceRepository.cs
+++ workspaces/DummyRosterWebApi/DummyRoster.WebApi/Repositories/IInvoiceRepository.cs
@@ -1,3 +1,7 @@
+using DummyRoster.Common.EntityModel.Models;
+
 namespace DummyRoster.WebApi.Repositories;
 
-public interface IInvoiceRepository { }
+public interface IInvoiceRepository {
+  // TODO
+}
