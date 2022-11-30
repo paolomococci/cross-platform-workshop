068e4968ef084480f0b80578b25a97adeab6735b
paolomococci <paolomococci@gmail.com>
2022-11-30 08:37:05 +0100

    modified:   workspaces/DummyRosterWebApi/DummyRoster.WebApi/Repositories/ICategoryRepository.cs

--- workspaces/DummyRosterWebApi/DummyRoster.WebApi/Repositories/ICategoryRepository.cs
+++ workspaces/DummyRosterWebApi/DummyRoster.WebApi/Repositories/ICategoryRepository.cs
@@ -1,3 +1,7 @@
+using DummyRoster.Common.EntityModel.Models;
+
 namespace DummyRoster.WebApi.Repositories;
 
-public interface ICategoryRepository { }
+public interface ICategoryRepository {
+  // TODO
+}
