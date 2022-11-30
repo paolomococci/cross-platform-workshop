56261647673aa72be1eab4ad1de104f283e7b611
paolomococci <paolomococci@gmail.com>
2022-11-30 10:42:43 +0100

    modified:   workspaces/DummyRosterWebApi/DummyRoster.WebApi/Repositories/FormRepository.cs

--- workspaces/DummyRosterWebApi/DummyRoster.WebApi/Repositories/FormRepository.cs
+++ workspaces/DummyRosterWebApi/DummyRoster.WebApi/Repositories/FormRepository.cs
@@ -4,4 +4,30 @@ using DummyRoster.Common.EntityModel.Models;
 
 namespace DummyRoster.WebApi.Repositories;
 
-public class FormRepository : IFormRepository {}
+public class FormRepository : IFormRepository
+{
+  public Task<Form?> CreateAsync(Form form)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<bool?> DeleteAsync(int id)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<IEnumerable<Form>> RetrieveAllAsync()
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<Form?> RetrieveAsync(int id)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<Form?> UpdateAsync(int id, Form form)
+  {
+    throw new NotImplementedException();
+  }
+}
