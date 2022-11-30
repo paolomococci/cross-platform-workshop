2b2c21bc3e355ed06d5ae5e2509807432bf3f3e7
paolomococci <paolomococci@gmail.com>
2022-11-30 10:15:30 +0100

    modified:   workspaces/DummyRosterWebApi/DummyRoster.WebApi/Repositories/IInvoiceRepository.cs

--- workspaces/DummyRosterWebApi/DummyRoster.WebApi/Repositories/IInvoiceRepository.cs
+++ workspaces/DummyRosterWebApi/DummyRoster.WebApi/Repositories/IInvoiceRepository.cs
@@ -3,10 +3,10 @@ using DummyRoster.Common.EntityModel.Models;
 namespace DummyRoster.WebApi.Repositories;
 
 public interface IInvoiceRepository {
-  Task<Invoice> CreateAsync(Invoice invoice);
-  Task<Invoice> RetrieveAsync(int id);
+  Task<Invoice?> CreateAsync(Invoice invoice);
+  Task<Invoice?> RetrieveAsync(int id);
   Task<IEnumerable<Invoice>> RetrieveAllAsync();
-  Task<Invoice> UpdateAsync(
+  Task<Invoice?> UpdateAsync(
     int id,
     Invoice invoice
   );
