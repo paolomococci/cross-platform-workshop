287095b7a605a0a36f5f9d276a08dc975a8ee956
paolomococci <paolomococci@gmail.com>
2022-11-30 08:46:25 +0100

    modified:   workspaces/DummyRosterWebApi/DummyRoster.WebApi/Repositories/IProductRepository.cs

--- workspaces/DummyRosterWebApi/DummyRoster.WebApi/Repositories/IProductRepository.cs
+++ workspaces/DummyRosterWebApi/DummyRoster.WebApi/Repositories/IProductRepository.cs
@@ -3,5 +3,5 @@ using DummyRoster.Common.EntityModel.Models;
 namespace DummyRoster.WebApi.Repositories;
 
 public interface IProductRepository {
-  // TODO
+  Task<Product> CreateAsync(Product product);
 }
