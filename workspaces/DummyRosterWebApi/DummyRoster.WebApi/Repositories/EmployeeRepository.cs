c6bcc56cbe1f056742758e68fc0a22d2684286de
paolomococci <paolomococci@gmail.com>
2022-11-30 10:42:31 +0100

    modified:   workspaces/DummyRosterWebApi/DummyRoster.WebApi/Repositories/EmployeeRepository.cs

--- workspaces/DummyRosterWebApi/DummyRoster.WebApi/Repositories/EmployeeRepository.cs
+++ workspaces/DummyRosterWebApi/DummyRoster.WebApi/Repositories/EmployeeRepository.cs
@@ -4,4 +4,30 @@ using DummyRoster.Common.EntityModel.Models;
 
 namespace DummyRoster.WebApi.Repositories;
 
-public class EmployeeRepository : IEmployeeRepository {}
+public class EmployeeRepository : IEmployeeRepository
+{
+  public Task<Employee?> CreateAsync(Employee employee)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<bool?> DeleteAsync(int id)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<IEnumerable<Employee>> RetrieveAllAsync()
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<Employee?> RetrieveAsync(int id)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<Employee?> UpdateAsync(int id, Employee employee)
+  {
+    throw new NotImplementedException();
+  }
+}
