4c16462146abe4f5bfe8f614def3b722a2695336
paolomococci <paolomococci@gmail.com>
2022-11-30 10:42:20 +0100

    modified:   workspaces/DummyRosterWebApi/DummyRoster.WebApi/Repositories/CustomerRepository.cs

--- workspaces/DummyRosterWebApi/DummyRoster.WebApi/Repositories/CustomerRepository.cs
+++ workspaces/DummyRosterWebApi/DummyRoster.WebApi/Repositories/CustomerRepository.cs
@@ -4,4 +4,30 @@ using DummyRoster.Common.EntityModel.Models;
 
 namespace DummyRoster.WebApi.Repositories;
 
-public class CustomerRepository : ICustomerRepository {}
+public class CustomerRepository : ICustomerRepository
+{
+  public Task<Customer?> CreateAsync(Customer customer)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<bool?> DeleteAsync(int id)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<IEnumerable<Customer>> RetrieveAllAsync()
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<Customer?> RetrieveAsync(int id)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<Customer?> UpdateAsync(int id, Customer customer)
+  {
+    throw new NotImplementedException();
+  }
+}
