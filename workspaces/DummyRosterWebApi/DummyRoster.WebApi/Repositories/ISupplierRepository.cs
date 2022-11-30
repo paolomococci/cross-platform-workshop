f69d681d56121b9d8c77683600c0364b21c54280
paolomococci <paolomococci@gmail.com>
2022-11-30 08:46:37 +0100

    modified:   workspaces/DummyRosterWebApi/DummyRoster.WebApi/Repositories/ISupplierRepository.cs

--- workspaces/DummyRosterWebApi/DummyRoster.WebApi/Repositories/ISupplierRepository.cs
+++ workspaces/DummyRosterWebApi/DummyRoster.WebApi/Repositories/ISupplierRepository.cs
@@ -3,5 +3,5 @@ using DummyRoster.Common.EntityModel.Models;
 namespace DummyRoster.WebApi.Repositories;
 
 public interface ISupplierRepository {
-  // TODO
+  Task<Supplier> CreateAsync(Supplier supplier);
 }
