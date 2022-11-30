f19eb6f6f0dae7b9fba29f29283f001c9250831a
paolomococci <paolomococci@gmail.com>
2022-11-30 10:33:22 +0100

    modified:   workspaces/DummyRosterWebApi/DummyRoster.WebApi/Repositories/EmployeeRepository.cs

--- workspaces/DummyRosterWebApi/DummyRoster.WebApi/Repositories/EmployeeRepository.cs
+++ workspaces/DummyRosterWebApi/DummyRoster.WebApi/Repositories/EmployeeRepository.cs
@@ -4,4 +4,4 @@ using DummyRoster.Common.EntityModel.Models;
 
 namespace DummyRoster.WebApi.Repositories;
 
-public class EmployeeRepository {}
+public class EmployeeRepository : IEmployeeRepository {}
