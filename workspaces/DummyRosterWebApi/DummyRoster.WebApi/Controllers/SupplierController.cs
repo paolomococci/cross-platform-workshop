0c7c43879a8a3f946f0d004996b34077e307a34d
paolomococci <paolomococci@gmail.com>
2022-12-02 08:19:55 +0100

    modified:   workspaces/DummyRosterWebApi/DummyRoster.WebApi/Controllers/SupplierController.cs

--- workspaces/DummyRosterWebApi/DummyRoster.WebApi/Controllers/SupplierController.cs
+++ workspaces/DummyRosterWebApi/DummyRoster.WebApi/Controllers/SupplierController.cs
@@ -6,13 +6,15 @@ namespace DummyRoster.WebApi.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
-public class SupplierController : ControllerBase {
+public class SupplierController : ControllerBase
+{
 
   private readonly ISupplierRepository iSupplierRepository;
 
   public SupplierController(
     ISupplierRepository iRepo
-  ) {
+  )
+  {
     this.iSupplierRepository = iRepo;
   }
   // TODO
