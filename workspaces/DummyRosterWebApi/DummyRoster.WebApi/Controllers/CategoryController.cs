ec2fa55f0fadca5efd53905199580b1c7083a1a9
paolomococci <paolomococci@gmail.com>
2022-12-02 16:46:28 +0100

    modified:   workspaces/DummyRosterWebApi/DummyRoster.WebApi/Controllers/CategoryController.cs

--- workspaces/DummyRosterWebApi/DummyRoster.WebApi/Controllers/CategoryController.cs
+++ workspaces/DummyRosterWebApi/DummyRoster.WebApi/Controllers/CategoryController.cs
@@ -64,4 +64,21 @@ public class CategoryController : ControllerBase
     // TODO
   }
 
+  /* 
+    PUT: api/categories/id
+    BODY: Category (JSON, XML)
+   */
+  [HttpPut("{id}")]
+  [ProducesResponseType(204)]
+  [ProducesResponseType(400)]
+  [ProducesResponseType(404)]
+  public Task<IActionResult> Update(
+    int id,
+    [FromBody] Category entity
+  )
+  {
+    throw new NotImplementedException();
+    // TODO
+  }
+
 }
