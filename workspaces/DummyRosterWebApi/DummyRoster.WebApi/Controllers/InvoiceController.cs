e5259ce5e67d1a0a7e78d04da95bf16c50d97be7
paolomococci <paolomococci@gmail.com>
2022-12-02 08:19:34 +0100

    modified:   workspaces/DummyRosterWebApi/DummyRoster.WebApi/Controllers/InvoiceController.cs

--- workspaces/DummyRosterWebApi/DummyRoster.WebApi/Controllers/InvoiceController.cs
+++ workspaces/DummyRosterWebApi/DummyRoster.WebApi/Controllers/InvoiceController.cs
@@ -6,13 +6,15 @@ namespace DummyRoster.WebApi.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
-public class InvoiceController : ControllerBase {
+public class InvoiceController : ControllerBase
+{
 
   private readonly IInvoiceRepository iInvoiceRepository;
 
   public InvoiceController(
     IInvoiceRepository iRepo
-  ) {
+  )
+  {
     this.iInvoiceRepository = iRepo;
   }
   // TODO
