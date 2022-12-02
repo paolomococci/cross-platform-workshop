1a102965d6a0e607fe8d3afdf5341cd57f67adb3
paolomococci <paolomococci@gmail.com>
2022-12-02 16:46:47 +0100

    modified:   workspaces/DummyRosterWebApi/DummyRoster.WebApi/Controllers/EmployeeController.cs

--- workspaces/DummyRosterWebApi/DummyRoster.WebApi/Controllers/EmployeeController.cs
+++ workspaces/DummyRosterWebApi/DummyRoster.WebApi/Controllers/EmployeeController.cs
@@ -64,4 +64,21 @@ public class EmployeeController : ControllerBase
     // TODO
   }
 
+  /* 
+    PUT: api/employees/id
+    BODY: Employee (JSON, XML)
+   */
+  [HttpPut("{id}")]
+  [ProducesResponseType(204)]
+  [ProducesResponseType(400)]
+  [ProducesResponseType(404)]
+  public Task<IActionResult> Update(
+    int id,
+    [FromBody] Employee entity
+  )
+  {
+    throw new NotImplementedException();
+    // TODO
+  }
+
 }
