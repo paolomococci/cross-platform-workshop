cab832e517fe3fd6c644d26f7ad211b9469b3ac0
paolomococci <paolomococci@gmail.com>
2022-12-02 16:38:04 +0100

    modified:   workspaces/DummyRosterWebApi/DummyRoster.WebApi/Controllers/ProductController.cs

--- workspaces/DummyRosterWebApi/DummyRoster.WebApi/Controllers/ProductController.cs
+++ workspaces/DummyRosterWebApi/DummyRoster.WebApi/Controllers/ProductController.cs
@@ -48,4 +48,20 @@ public class ProductController : ControllerBase
     // TODO
   }
 
+  /* 
+    POST: api/products
+    BODY: Product (JSON, XML)
+   */
+  [HttpPost]
+  [ProducesResponseType(
+    201, 
+    Type = typeof(Product)
+  )]
+  [ProducesResponseType(400)]
+  public Task<IActionResult> Create([FromBody] Product entity)
+  {
+    throw new NotImplementedException();
+    // TODO
+  }
+
 }
