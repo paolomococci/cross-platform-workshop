0fc4da5aceea1923793425a1613a7b43ac72e035
paolomococci <paolomococci@gmail.com>
2022-12-03 04:57:32 +0100

    modified:   workspaces/DummyRosterWebApi/DummyRoster.WebApi/Controllers/ProductController.cs

--- workspaces/DummyRosterWebApi/DummyRoster.WebApi/Controllers/ProductController.cs
+++ workspaces/DummyRosterWebApi/DummyRoster.WebApi/Controllers/ProductController.cs
@@ -81,4 +81,17 @@ public class ProductController : ControllerBase
     // TODO
   }
 
+  /* 
+    DELETE: api/products/id
+   */
+  [HttpDelete("{id}")]
+  [ProducesResponseType(204)]
+  [ProducesResponseType(400)]
+  [ProducesResponseType(404)]
+  public Task<IActionResult> Delete(int id)
+  {
+    throw new NotImplementedException();
+    // TODO
+  }
+
 }
