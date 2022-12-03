6b0c28007cff71399ad48ce635b73c8c67f9b5a5
paolomococci <paolomococci@gmail.com>
2022-12-03 18:17:37 +0100

    modified:   workspaces/DummyRosterWebApi/DummyRoster.WebApi/Controllers/SupplierController.cs

--- workspaces/DummyRosterWebApi/DummyRoster.WebApi/Controllers/SupplierController.cs
+++ workspaces/DummyRosterWebApi/DummyRoster.WebApi/Controllers/SupplierController.cs
@@ -116,8 +116,18 @@ public class SupplierController : ControllerBase
   [ProducesResponseType(404)]
   public async Task<IActionResult> Delete(int id)
   {
-    throw new NotImplementedException();
-    // TODO
+    bool deleted = false;
+    Supplier? managedEntity = await this.iSupplierRepository.RetrieveAsync(id);
+    if (managedEntity == null)
+    {
+      return NotFound();
+    }
+    deleted = (bool)await this.iSupplierRepository.DeleteAsync(id);
+    if (deleted)
+    {
+      return new NoContentResult();
+    }
+    return BadRequest();
   }
 
 }
