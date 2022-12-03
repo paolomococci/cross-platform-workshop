d2a245335d16c8418ecf3cbc40d462d1915811fa
paolomococci <paolomococci@gmail.com>
2022-12-03 04:56:34 +0100

    modified:   workspaces/DummyRosterWebApi/DummyRoster.WebApi/Controllers/CategoryController.cs

--- workspaces/DummyRosterWebApi/DummyRoster.WebApi/Controllers/CategoryController.cs
+++ workspaces/DummyRosterWebApi/DummyRoster.WebApi/Controllers/CategoryController.cs
@@ -81,4 +81,17 @@ public class CategoryController : ControllerBase
     // TODO
   }
 
+  /* 
+    DELETE: api/categories/id
+   */
+  [HttpDelete("{id}")]
+  [ProducesResponseType(204)]
+  [ProducesResponseType(400)]
+  [ProducesResponseType(404)]
+  public Task<IActionResult> Delete(int id)
+  {
+    throw new NotImplementedException();
+    // TODO
+  }
+
 }
