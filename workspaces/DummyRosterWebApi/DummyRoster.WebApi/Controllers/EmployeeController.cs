fc7f920dcd27a21bd1ae14dd468acf6bac25a9bb
paolomococci <paolomococci@gmail.com>
2022-12-03 04:56:59 +0100

    modified:   workspaces/DummyRosterWebApi/DummyRoster.WebApi/Controllers/EmployeeController.cs

--- workspaces/DummyRosterWebApi/DummyRoster.WebApi/Controllers/EmployeeController.cs
+++ workspaces/DummyRosterWebApi/DummyRoster.WebApi/Controllers/EmployeeController.cs
@@ -81,4 +81,17 @@ public class EmployeeController : ControllerBase
     // TODO
   }
 
+  /* 
+    DELETE: api/employees/id
+   */
+  [HttpDelete("{id}")]
+  [ProducesResponseType(204)]
+  [ProducesResponseType(400)]
+  [ProducesResponseType(404)]
+  public Task<IActionResult> Delete(int id)
+  {
+    throw new NotImplementedException();
+    // TODO
+  }
+
 }
