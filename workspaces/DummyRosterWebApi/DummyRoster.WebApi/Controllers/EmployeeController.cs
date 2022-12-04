65ac1317759ba0272148aa46a136a85441c01611
paolomococci <paolomococci@gmail.com>
2022-12-04 09:07:22 +0100

    modified:   workspaces/DummyRosterWebApi/DummyRoster.WebApi/Controllers/EmployeeController.cs

--- workspaces/DummyRosterWebApi/DummyRoster.WebApi/Controllers/EmployeeController.cs
+++ workspaces/DummyRosterWebApi/DummyRoster.WebApi/Controllers/EmployeeController.cs
@@ -4,7 +4,7 @@ using DummyRoster.WebApi.Repositories;
 
 namespace DummyRoster.WebApi.Controllers;
 
-[Route("api/[controller]")]
+[Route("api/employees")]
 [ApiController]
 public class EmployeeController : ControllerBase
 {
