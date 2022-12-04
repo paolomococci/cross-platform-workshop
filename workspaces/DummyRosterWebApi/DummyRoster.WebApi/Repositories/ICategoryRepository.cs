81292ca74056c25abf8363e032602ac144138443
paolomococci <paolomococci@gmail.com>
2022-12-04 12:20:15 +0100

    modified:   workspaces/DummyRosterWebApi/DummyRoster.WebApi/Repositories/ICategoryRepository.cs

--- workspaces/DummyRosterWebApi/DummyRoster.WebApi/Repositories/ICategoryRepository.cs
+++ workspaces/DummyRosterWebApi/DummyRoster.WebApi/Repositories/ICategoryRepository.cs
@@ -2,7 +2,8 @@ using DummyRoster.Common.EntityModel.Models;
 
 namespace DummyRoster.WebApi.Repositories;
 
-public interface ICategoryRepository {
+public interface ICategoryRepository
+{
   Task<Category?> CreateAsync(Category category);
   Task<Category?> RetrieveAsync(int id);
   Task<IEnumerable<Category>> RetrieveAllAsync();
