0ac0350a3394950b3e465601f0942045f10d3b48
paolomococci <paolomococci@gmail.com>
2022-12-04 09:06:45 +0100

    modified:   workspaces/DummyRosterWebApi/DummyRoster.WebApi/Controllers/CarrierController.cs

--- workspaces/DummyRosterWebApi/DummyRoster.WebApi/Controllers/CarrierController.cs
+++ workspaces/DummyRosterWebApi/DummyRoster.WebApi/Controllers/CarrierController.cs
@@ -4,7 +4,7 @@ using DummyRoster.WebApi.Repositories;
 
 namespace DummyRoster.WebApi.Controllers;
 
-[Route("api/[controller]")]
+[Route("api/carriers")]
 [ApiController]
 public class CarrierController : ControllerBase
 {
