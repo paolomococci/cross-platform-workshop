5444fefdce755441ac8c1c297b9d40f7f999b555
paolomococci <paolomococci@gmail.com>
2022-12-05 17:53:18 +0100

    modified:   workspaces/DummyRosterWebApiImproved/DummyRoster.Common.DataContext/Data/DummyRosterContext.cs

--- workspaces/DummyRosterWebApiImproved/DummyRoster.Common.DataContext/Data/DummyRosterContext.cs
+++ workspaces/DummyRosterWebApiImproved/DummyRoster.Common.DataContext/Data/DummyRosterContext.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 
-namespace DummyRoster.Common.EntityModel.Models;
+namespace DummyRoster.Common.DataContext.Data;
 
 public partial class DummyRosterContext : DbContext
 {
@@ -34,7 +34,6 @@ public partial class DummyRosterContext : DbContext
     public virtual DbSet<Supplier> Suppliers { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
         => optionsBuilder.UseSqlite("Filename=../DummyRoster.db");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
