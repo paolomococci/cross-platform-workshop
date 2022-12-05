ffb80886706594cd01329d191f7771478031e85b
paolomococci <paolomococci@gmail.com>
2022-12-05 17:55:28 +0100

    modified:   workspaces/DummyRosterWebApiImproved/DummyRoster.Common.EntityModel/Models/Customer.cs

--- workspaces/DummyRosterWebApiImproved/DummyRoster.Common.EntityModel/Models/Customer.cs
+++ workspaces/DummyRosterWebApiImproved/DummyRoster.Common.EntityModel/Models/Customer.cs
@@ -10,33 +10,33 @@ namespace DummyRoster.Common.EntityModel.Models;
 [Index("Name", Name = "IDX_CustomerName")]
 public partial class Customer
 {
-    [Key]
-    public long Id { get; set; }
+  [Key]
+  public long Id { get; set; }
 
-    [Column(TypeName = "varchar(32)")]
-    public string Name { get; set; } = null!;
+  [Column(TypeName = "varchar(32)")]
+  public string Name { get; set; } = null!;
 
-    [Column(TypeName = "DATETIME")]
-    public byte[]? FoundationDate { get; set; }
+  [Column(TypeName = "DATETIME")]
+  public byte[]? FoundationDate { get; set; }
 
-    public string? Description { get; set; }
+  public string? Description { get; set; }
 
-    public byte[]? Picture { get; set; }
+  public byte[]? Picture { get; set; }
 
-    [Column(TypeName = "INT")]
-    public long? Contact { get; set; }
+  [Column(TypeName = "INT")]
+  public long? Contact { get; set; }
 
-    [Column(TypeName = "INT")]
-    public long? Location { get; set; }
+  [Column(TypeName = "INT")]
+  public long? Location { get; set; }
 
-    [ForeignKey("Contact")]
-    [InverseProperty("Customers")]
-    public virtual Employee? ContactNavigation { get; set; }
+  [ForeignKey("Contact")]
+  [InverseProperty("Customers")]
+  public virtual Employee? ContactNavigation { get; set; }
 
-    [InverseProperty("Customer")]
-    public virtual ICollection<Form> Forms { get; } = new List<Form>();
+  [InverseProperty("Customer")]
+  public virtual ICollection<Form> Forms { get; } = new List<Form>();
 
-    [ForeignKey("Location")]
-    [InverseProperty("Customers")]
-    public virtual Address? LocationNavigation { get; set; }
+  [ForeignKey("Location")]
+  [InverseProperty("Customers")]
+  public virtual Address? LocationNavigation { get; set; }
 }
