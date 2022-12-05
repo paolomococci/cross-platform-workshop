3c31db006bbf58d7d1bba2ad6bb03d21a66279b6
paolomococci <paolomococci@gmail.com>
2022-12-05 21:09:53 +0100

    modified:   workspaces/DummyRosterWebApiImproved/DummyRoster.Common.EntityModel/Models/Category.cs

--- workspaces/DummyRosterWebApiImproved/DummyRoster.Common.EntityModel/Models/Category.cs
+++ workspaces/DummyRosterWebApiImproved/DummyRoster.Common.EntityModel/Models/Category.cs
@@ -1,6 +1,7 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.EntityFrameworkCore;
+using System.Xml.Serialization;
 
 namespace DummyRoster.Common.EntityModel.Models;
 
