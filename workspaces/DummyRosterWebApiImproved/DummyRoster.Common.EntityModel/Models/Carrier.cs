2e7c0d5d0ede10d66aa296cae43f0f03bb635a56
paolomococci <paolomococci@gmail.com>
2022-12-05 21:12:39 +0100

    modified:   workspaces/DummyRosterWebApiImproved/DummyRoster.Common.EntityModel/Models/Carrier.cs

--- workspaces/DummyRosterWebApiImproved/DummyRoster.Common.EntityModel/Models/Carrier.cs
+++ workspaces/DummyRosterWebApiImproved/DummyRoster.Common.EntityModel/Models/Carrier.cs
@@ -33,6 +33,7 @@ public partial class Carrier
   public virtual Employee? ContactNavigation { get; set; }
 
   [InverseProperty("Carrier")]
+  [XmlIgnore]
   public virtual ICollection<Form> Forms { get; } = new List<Form>();
 
   [ForeignKey("Location")]
