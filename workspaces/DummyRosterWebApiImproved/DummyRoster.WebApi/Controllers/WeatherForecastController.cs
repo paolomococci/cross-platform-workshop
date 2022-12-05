f3945e11fdb5eb97fe6ec543f5f61c6d2bfd8d21
paolomococci <paolomococci@gmail.com>
2022-12-05 16:45:57 +0100

    modified:   workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Controllers/WeatherForecastController.cs

--- workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Controllers/WeatherForecastController.cs
+++ workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Controllers/WeatherForecastController.cs
@@ -8,17 +8,17 @@ public class WeatherForecastController : ControllerBase
 {
   private static readonly string[] Summaries = new[]
   {
-        "Freezing",
-        "Bracing",
-        "Chilly",
-        "Cool",
-        "Mild",
-        "Warm",
-        "Balmy",
-        "Hot",
-        "Sweltering",
-        "Scorching"
-    };
+    "Freezing",
+    "Bracing",
+    "Chilly",
+    "Cool",
+    "Mild",
+    "Warm",
+    "Balmy",
+    "Hot",
+    "Sweltering",
+    "Scorching"
+  };
 
   private readonly ILogger<WeatherForecastController> _logger;
 
