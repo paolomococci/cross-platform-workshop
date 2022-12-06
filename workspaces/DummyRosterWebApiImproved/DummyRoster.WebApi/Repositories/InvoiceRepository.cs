3d868ebc1f81cce856c5902af451a9b9c6a7c104
paolomococci <paolomococci@gmail.com>
2022-12-06 05:46:22 +0100

    modified:   workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Repositories/InvoiceRepository.cs

--- workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Repositories/InvoiceRepository.cs
+++ workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Repositories/InvoiceRepository.cs
@@ -10,4 +10,19 @@ public class InvoiceRepository
 {
   private static ConcurrentDictionary<int, Invoice>? keyValuesCache;
   private DummyRosterContext dummyRosterContext;
+
+  public InvoiceRepository(
+    DummyRosterContext dummyRosterContext
+  )
+  {
+    this.dummyRosterContext = dummyRosterContext;
+    if (keyValuesCache is null)
+    {
+      keyValuesCache = new ConcurrentDictionary<int, Invoice>(
+        this.dummyRosterContext.Invoices.ToDictionary(
+          entity => entity.Id
+        )
+      );
+    }
+  }
 }
