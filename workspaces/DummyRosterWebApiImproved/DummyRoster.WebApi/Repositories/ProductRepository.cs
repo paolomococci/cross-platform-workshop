ba6cfe7ab04503a3355e23c693e6167344b7d69d
paolomococci <paolomococci@gmail.com>
2022-12-06 05:35:00 +0100

    modified:   workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Repositories/ProductRepository.cs

--- workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Repositories/ProductRepository.cs
+++ workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Repositories/ProductRepository.cs
@@ -2,6 +2,7 @@ using Microsoft.EntityFrameworkCore.ChangeTracking;
 using System.Collections.Concurrent;
 using DummyRoster.Common.EntityModel.Models;
 using DummyRoster.Common.DataContext.Data;
+using DummyRoster.WebApi.Repositories.Interfaces;
 
 namespace DummyRoster.WebApi.Repositories;
 
