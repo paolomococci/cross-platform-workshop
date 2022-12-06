44014324651088922ae0436545406d4198141c0f
paolomococci <paolomococci@gmail.com>
2022-12-06 05:08:29 +0100

    modified:   workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Repositories/Interfaces/ISupplierRepository.cs

--- workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Repositories/Interfaces/ISupplierRepository.cs
+++ workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Repositories/Interfaces/ISupplierRepository.cs
@@ -7,4 +7,8 @@ public interface ISupplierRepository
   Task<Supplier?> CreateAsync(Supplier entity);
   Task<Supplier?> RetrieveAsync(int id);
   Task<IEnumerable<Supplier>> RetrieveAllAsync();
+  Task<Supplier?> UpdateAsync(
+    int id,
+    Supplier entity
+  );
 }
