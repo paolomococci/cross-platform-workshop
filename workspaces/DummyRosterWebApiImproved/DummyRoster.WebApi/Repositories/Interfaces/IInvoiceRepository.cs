237c6951e3fe70290c8cd466cb23222e38cb704e
paolomococci <paolomococci@gmail.com>
2022-12-06 05:08:11 +0100

    modified:   workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Repositories/Interfaces/IInvoiceRepository.cs

--- workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Repositories/Interfaces/IInvoiceRepository.cs
+++ workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Repositories/Interfaces/IInvoiceRepository.cs
@@ -7,4 +7,8 @@ public interface IInvoiceRepository
   Task<Invoice?> CreateAsync(Invoice entity);
   Task<Invoice?> RetrieveAsync(int id);
   Task<IEnumerable<Invoice>> RetrieveAllAsync();
+  Task<Invoice?> UpdateAsync(
+    int id,
+    Invoice entity
+  );
 }
