24d0f81b418fcae780c76c2a04ac2506d7ededd2
paolomococci <paolomococci@gmail.com>
2022-12-06 21:38:24 +0100

    modified:   workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Repositories/CustomerRepository.cs

--- workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Repositories/CustomerRepository.cs
+++ workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Repositories/CustomerRepository.cs
@@ -81,7 +81,7 @@ public class CustomerRepository : ICustomerRepository
     throw new NotImplementedException();
   }
 
-  public Task<bool?> DeleteAsync(int id)
+  public async Task<bool?> DeleteAsync(int id)
   {
     throw new NotImplementedException();
   }
