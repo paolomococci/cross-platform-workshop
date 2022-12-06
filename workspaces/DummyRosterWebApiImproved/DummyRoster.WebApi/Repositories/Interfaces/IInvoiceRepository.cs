1f6287313d81a3f3102b2a0f45aced9c533ab422
paolomococci <paolomococci@gmail.com>
2022-12-06 04:56:11 +0100

    modified:   workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Repositories/Interfaces/IInvoiceRepository.cs

--- workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Repositories/Interfaces/IInvoiceRepository.cs
+++ workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Repositories/Interfaces/IInvoiceRepository.cs
@@ -2,6 +2,7 @@ using DummyRoster.Common.EntityModel.Models;
 
 namespace DummyRoster.WebApi.Repositories.Interfaces;
 
-public interface IInvoiceRepository {
-  Task<Invoice?> CreateAsync(Invoice address);
+public interface IInvoiceRepository
+{
+  Task<Invoice?> CreateAsync(Invoice entity);
 }
