81d732415221cce501b5c3c73ea5817362b8638a
paolomococci <paolomococci@gmail.com>
2022-12-06 05:07:37 +0100

    modified:   workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Repositories/Interfaces/ICategoryRepository.cs

--- workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Repositories/Interfaces/ICategoryRepository.cs
+++ workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Repositories/Interfaces/ICategoryRepository.cs
@@ -7,4 +7,8 @@ public interface ICategoryRepository
   Task<Category?> CreateAsync(Category entity);
   Task<Category?> RetrieveAsync(int id);
   Task<IEnumerable<Category>> RetrieveAllAsync();
+  Task<Category?> UpdateAsync(
+    int id,
+    Category entity
+  );
 }
