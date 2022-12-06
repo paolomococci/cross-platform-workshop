e8f00e6b42ac1a5cd0e0a7ea007c968edbd44274
paolomococci <paolomococci@gmail.com>
2022-12-06 21:31:57 +0100

    modified:   workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Repositories/EmployeeRepository.cs

--- workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Repositories/EmployeeRepository.cs
+++ workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Repositories/EmployeeRepository.cs
@@ -62,9 +62,18 @@ public class EmployeeRepository : IEmployeeRepository
     );
   }
 
-  public Task<Employee?> UpdateAsync(int id, Employee entity)
+  public async Task<Employee?> UpdateAsync(
+    int id, 
+    Employee entity
+  )
   {
-    throw new NotImplementedException();
+    this.dummyRosterContext.Employees.Update(entity);
+    int changesSaved = await this.dummyRosterContext.SaveChangesAsync();
+    if (changesSaved == 1)
+    {
+      return this.UpdateCache(id, entity);
+    }
+    return null;
   }
 
   public Task<Employee?> PartialUpdateAsync(int id, Employee entity)
