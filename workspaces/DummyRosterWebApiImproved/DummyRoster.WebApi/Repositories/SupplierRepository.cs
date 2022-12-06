84840b030a0ce001dbfdee1e99164c37b048a108
paolomococci <paolomococci@gmail.com>
2022-12-06 05:52:27 +0100

    modified:   workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Repositories/SupplierRepository.cs

--- workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Repositories/SupplierRepository.cs
+++ workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Repositories/SupplierRepository.cs
@@ -25,4 +25,23 @@ public class SupplierRepository
       );
     }
   }
+
+  private Supplier UpdateCache(
+    int id, 
+    Supplier entity
+  )
+  {
+    Supplier? alreadyRegistered;
+    if (keyValuesCache is not null)
+    {
+      if (keyValuesCache.TryGetValue(id, out alreadyRegistered))
+      {
+        if (keyValuesCache.TryUpdate(id, entity, alreadyRegistered))
+        {
+          return entity;
+        }
+      }
+    }
+    return null!;
+  }
 }
