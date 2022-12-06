885eefc585c0f940c08e5d8eb187f03dab2aee0e
paolomococci <paolomococci@gmail.com>
2022-12-06 05:39:48 +0100

    modified:   workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Repositories/CarrierRepository.cs

--- workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Repositories/CarrierRepository.cs
+++ workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Repositories/CarrierRepository.cs
@@ -6,4 +6,8 @@ using DummyRoster.WebApi.Repositories.Interfaces;
 
 namespace DummyRoster.WebApi.Repositories;
 
-public class CarrierRepository { }
+public class CarrierRepository
+{
+  private static ConcurrentDictionary<int, Carrier>? keyValuesCache;
+  private DummyRosterContext dummyRosterContext;
+}
