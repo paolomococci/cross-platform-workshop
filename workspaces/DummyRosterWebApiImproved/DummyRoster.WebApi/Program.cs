b5f80e1eb26951282bd43d46641f13af53072053
paolomococci <paolomococci@gmail.com>
2022-12-06 06:22:19 +0100

    modified:   workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Program.cs

--- workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Program.cs
+++ workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Program.cs
@@ -4,11 +4,33 @@ using DummyRoster.Common.DataContext.Data;
 using DummyRoster.WebApi.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddDbContext<DummyRosterContext>();
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(
+  options =>
+  {
+    Console.WriteLine("default output formatter:");
+    foreach (IOutputFormatter item in options.OutputFormatters)
+    {
+      OutputFormatter? outputFormatter = item as OutputFormatter;
+      if (outputFormatter == null)
+      {
+        Console.WriteLine($" {item.GetType().Name}");
+      }
+      else
+      {
+        Console.WriteLine(
+          $" {0}, media type: {1}",
+          arg0: outputFormatter.GetType().Name,
+          arg1: string.Join(", ", outputFormatter.SupportedMediaTypes)
+        );
+      }
+    }
+  }
+).AddXmlDataContractSerializerFormatters().AddXmlSerializerFormatters();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
