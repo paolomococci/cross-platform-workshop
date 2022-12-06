fb6ecea2b5f3935422ca163b6d1c70297be28b90
paolomococci <paolomococci@gmail.com>
2022-12-06 06:11:51 +0100

    new file:   workspaces/DummyRosterWebApiImproved/RestRequestTests/ComputeDataLength.js

---
+++ workspaces/DummyRosterWebApiImproved/RestRequestTests/ComputeDataLength.js
@@ -0,0 +1,22 @@
+/* extract of javascript code to calculate the length of the data to be sent in JSON format */
+
+const data = {
+  "id": 3,
+  "streetOrSquare": "SomeOne avenue",
+  "civic": "20/D",
+  "city": "SomeOneCity",
+  "district": "SomeOneDistrict",
+  "postcode": "010101",
+  "country": "SomeOneCountry",
+  "email": "some.one@example.local",
+  "phone": "039035801010101",
+  "fax": "039035801010102",
+  "carriers": [],
+  "customers": [],
+  "employees": [],
+  "suppliers": []
+}
+
+const dataLength = JSON.stringify(data).length;
+
+console.log(dataLength);
