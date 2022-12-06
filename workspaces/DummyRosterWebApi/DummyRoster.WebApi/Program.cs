baa98ca602bd39c7e36cda4027f40779f983662c
paolomococci <paolomococci@gmail.com>
2022-12-06 06:21:06 +0100

    modified:   workspaces/DummyRosterWebApi/DummyRoster.WebApi/Program.cs

--- workspaces/DummyRosterWebApi/DummyRoster.WebApi/Program.cs
+++ workspaces/DummyRosterWebApi/DummyRoster.WebApi/Program.cs
@@ -5,6 +5,7 @@ using DummyRoster.Common.DataContext.Data;
 using DummyRoster.WebApi.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddDbContext<DummyRosterContext>();
 
 // Add services to the container.
