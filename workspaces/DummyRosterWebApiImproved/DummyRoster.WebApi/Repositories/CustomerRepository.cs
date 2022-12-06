0530392a77a755accf943686d6094fc762dfa738
paolomococci <paolomococci@gmail.com>
2022-12-06 05:55:45 +0100

    modified:   workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Repositories/CustomerRepository.cs

--- workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Repositories/CustomerRepository.cs
+++ workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Repositories/CustomerRepository.cs
@@ -6,7 +6,7 @@ using DummyRoster.WebApi.Repositories.Interfaces;
 
 namespace DummyRoster.WebApi.Repositories;
 
-public class CustomerRepository
+public class CustomerRepository : ICustomerRepository
 {
   private static ConcurrentDictionary<int, Customer>? keyValuesCache;
   private DummyRosterContext dummyRosterContext;
