8da5755c50fad2077197b3257fc3808bf5a6caea
paolomococci <paolomococci@gmail.com>
2022-12-06 04:55:55 +0100

    modified:   workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Repositories/Interfaces/IEmployeeRepository.cs

--- workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Repositories/Interfaces/IEmployeeRepository.cs
+++ workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Repositories/Interfaces/IEmployeeRepository.cs
@@ -2,6 +2,7 @@ using DummyRoster.Common.EntityModel.Models;
 
 namespace DummyRoster.WebApi.Repositories.Interfaces;
 
-public interface IEmployeeRepository {
-  Task<Employee?> CreateAsync(Employee address);
+public interface IEmployeeRepository
+{
+  Task<Employee?> CreateAsync(Employee entity);
 }
