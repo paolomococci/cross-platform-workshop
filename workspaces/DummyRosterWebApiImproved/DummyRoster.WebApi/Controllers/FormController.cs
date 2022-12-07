962a6305a6522c3f18a8e4564bf9e9286a331be7
paolomococci <paolomococci@gmail.com>
2022-12-07 16:33:16 +0100

    modified:   workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Controllers/FormController.cs

--- workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Controllers/FormController.cs
+++ workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Controllers/FormController.cs
@@ -78,6 +78,13 @@ public class FormController : ControllerBase, IFormController
     throw new NotImplementedException();
   }
 
+  /* 
+    DELETE: api/forms/id
+   */
+  [HttpDelete("{id}")]
+  [ProducesResponseType(204)]
+  [ProducesResponseType(400)]
+  [ProducesResponseType(404)]
   public Task<IActionResult> Delete(int id)
   {
     throw new NotImplementedException();
