0889d0d01d1f94fb7e0d34c21ac7dc7e26f5a96e
paolomococci <paolomococci@gmail.com>
2022-12-07 20:48:04 +0100

    modified:   workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Controllers/Interfaces/IInvoiceController.cs

--- workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Controllers/Interfaces/IInvoiceController.cs
+++ workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Controllers/Interfaces/IInvoiceController.cs
@@ -6,7 +6,7 @@ namespace DummyRoster.WebApi.Controllers.Interfaces;
 public interface IInvoiceController
 {
   public Task<IActionResult> Create([FromBody] Invoice entity);
-  public Task<IActionResult> Read(int id);
+  public Task<IActionResult> ReadInvoice(int id);
   public Task<IEnumerable<Invoice>> ReadAll(int? formId);
   public Task<IActionResult> Update(int id, [FromBody] Invoice entity);
   public Task<IActionResult> PartialUpdate(int id, [FromBody] Invoice entity);
