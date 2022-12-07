5df9aa8efef9bcc4ade7282aa8a426f5ed932fb7
paolomococci <paolomococci@gmail.com>
2022-12-07 17:33:37 +0100

    modified:   workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Controllers/Interfaces/ICustomerController.cs

--- workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Controllers/Interfaces/ICustomerController.cs
+++ workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Controllers/Interfaces/ICustomerController.cs
@@ -6,8 +6,8 @@ namespace DummyRoster.WebApi.Controllers.Interfaces;
 public interface ICustomerController
 {
   public Task<IActionResult> Create([FromBody] Customer entity);
-  public Task<IActionResult> Get(int id);
-  public Task<IEnumerable<Customer>> GetAll(string? name);
+  public Task<IActionResult> Read(int id);
+  public Task<IEnumerable<Customer>> ReadAll(string? name);
   public Task<IActionResult> Update(int id, [FromBody] Customer entity);
   public Task<IActionResult> PartialUpdate(int id, [FromBody] Customer entity);
   public Task<IActionResult> Delete(int id);
