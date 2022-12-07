ce9d22778a9f0f513e399b33bc3416293924c7e7
paolomococci <paolomococci@gmail.com>
2022-12-07 09:31:36 +0100

    modified:   workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Controllers/CarrierController.cs

--- workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Controllers/CarrierController.cs
+++ workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Controllers/CarrierController.cs
@@ -4,4 +4,35 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace DummyRoster.WebApi.Controllers;
 
-public class CarrierController : ControllerBase, ICarrierController { }
+public class CarrierController : ControllerBase, ICarrierController
+{
+  public Task<IActionResult> Create([FromBody] Carrier entity)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<IActionResult> Delete(int id)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<IActionResult> Get(int id)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<IEnumerable<Carrier>> GetAll(string? country)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<IActionResult> PartialUpdate(int id, [FromBody] Carrier entity)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<IActionResult> Update(int id, [FromBody] Carrier entity)
+  {
+    throw new NotImplementedException();
+  }
+}
