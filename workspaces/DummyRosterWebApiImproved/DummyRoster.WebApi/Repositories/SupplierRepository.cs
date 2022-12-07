643f76c6caf2d9050d5e005977b329a439aa6ea0
paolomococci <paolomococci@gmail.com>
2022-12-07 06:15:35 +0100

    modified:   workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Repositories/SupplierRepository.cs

--- workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Repositories/SupplierRepository.cs
+++ workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Repositories/SupplierRepository.cs
@@ -63,7 +63,7 @@ public class SupplierRepository : ISupplierRepository
   }
 
   public async Task<Supplier?> UpdateAsync(
-    int id, 
+    int id,
     Supplier entity
   )
   {
@@ -77,11 +77,28 @@ public class SupplierRepository : ISupplierRepository
   }
 
   public async Task<Supplier?> PartialUpdateAsync(
-    int id, 
+    int id,
     Supplier entity
   )
   {
-    throw new NotImplementedException();
+    if (keyValuesCache is null) return null!;
+    keyValuesCache.TryGetValue(id, out Supplier? registered);
+    if (registered != null)
+    {
+      if (entity.Name != null) registered.Name = entity.Name;
+      if (entity.FoundationDate != null) registered.FoundationDate = entity.FoundationDate;
+      if (entity.Description != null) registered.Description = entity.Description;
+      if (entity.Picture != null) registered.Picture = entity.Picture;
+      if (entity.Contact != null) registered.Contact = entity.Contact;
+      if (entity.Location != null) registered.Location = entity.Location;
+      this.dummyRosterContext.Suppliers.Update(registered);
+      int changesSaved = await this.dummyRosterContext.SaveChangesAsync();
+      if (changesSaved == 1)
+      {
+        return this.UpdateCache(id, registered);
+      }
+    }
+    return null;
   }
 
   public async Task<bool?> DeleteAsync(int id)
@@ -108,7 +125,7 @@ public class SupplierRepository : ISupplierRepository
   }
 
   private Supplier UpdateCache(
-    int id, 
+    int id,
     Supplier entity
   )
   {
