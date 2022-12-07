05e813113e6c7aee7952b146a8bba7d897ff8e2e
paolomococci <paolomococci@gmail.com>
2022-12-07 09:32:36 +0100

    modified:   workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Controllers/SupplierController.cs

--- workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Controllers/SupplierController.cs
+++ workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Controllers/SupplierController.cs
@@ -4,4 +4,35 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace DummyRoster.WebApi.Controllers;
 
-public class SupplierController : ControllerBase, ISupplierController { }
+public class SupplierController : ControllerBase, ISupplierController
+{
+  public Task<IActionResult> Create([FromBody] Supplier entity)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<IActionResult> Delete(int id)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<IActionResult> Get(int id)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<IEnumerable<Supplier>> GetAll(string? country)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<IActionResult> PartialUpdate(int id, [FromBody] Supplier entity)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<IActionResult> Update(int id, [FromBody] Supplier entity)
+  {
+    throw new NotImplementedException();
+  }
+}
