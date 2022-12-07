539aed8b6e9634a2d2ee34223e2db2531a4c3fd2
paolomococci <paolomococci@gmail.com>
2022-12-07 07:19:45 +0100

    modified:   workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Controllers/Interfaces/ICategoryController.cs

--- workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Controllers/Interfaces/ICategoryController.cs
+++ workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Controllers/Interfaces/ICategoryController.cs
@@ -3,4 +3,12 @@ using DummyRoster.Common.EntityModel.Models;
 
 namespace DummyRoster.WebApi.Controllers.Interfaces;
 
-public interface ICategoryController { }
+public interface ICategoryController
+{
+  public Task<IActionResult> Create([FromBody] Category entity);
+  public Task<IActionResult> Get(int id);
+  public Task<IEnumerable<Category>> GetAll(string? country);
+  public Task<IActionResult> Update(int id, [FromBody] Category entity);
+  public Task<IActionResult> PartialUpdate(int id, [FromBody] Category entity);
+  public Task<IActionResult> Delete(int id);
+}
