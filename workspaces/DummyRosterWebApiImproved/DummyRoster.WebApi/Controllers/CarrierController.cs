2bb1abfc98bbf0d3b8f20285b4ca799a8f677341
paolomococci <paolomococci@gmail.com>
2022-12-07 17:01:09 +0100

    modified:   workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Controllers/CarrierController.cs

--- workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Controllers/CarrierController.cs
+++ workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Controllers/CarrierController.cs
@@ -5,8 +5,8 @@ using DummyRoster.WebApi.Repositories.Interfaces;
 
 namespace DummyRoster.WebApi.Controllers;
 
-[Route("api/carriers")]
 [ApiController]
+[Route("api/carriers")]
 public class CarrierController : ControllerBase, ICarrierController
 {
 
