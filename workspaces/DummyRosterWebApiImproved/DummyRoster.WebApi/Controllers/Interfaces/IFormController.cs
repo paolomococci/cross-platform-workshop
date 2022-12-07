22e309d106c86df3ff06f2451e416756fa8544bd
paolomococci <paolomococci@gmail.com>
2022-12-07 16:10:01 +0100

    modified:   workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Controllers/Interfaces/IFormController.cs

--- workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Controllers/Interfaces/IFormController.cs
+++ workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Controllers/Interfaces/IFormController.cs
@@ -7,7 +7,7 @@ public interface IFormController
 {
   public Task<IActionResult> Create([FromBody] Form entity);
   public Task<IActionResult> Get(int id);
-  public Task<IEnumerable<Form>> GetAll(string? country);
+  public Task<IEnumerable<Form>> GetAll(int? customerId);
   public Task<IActionResult> Update(int id, [FromBody] Form entity);
   public Task<IActionResult> PartialUpdate(int id, [FromBody] Form entity);
   public Task<IActionResult> Delete(int id);
