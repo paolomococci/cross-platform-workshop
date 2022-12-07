e0f2c01af2b839401a3336145998e04a1d7fb676
paolomococci <paolomococci@gmail.com>
2022-12-07 17:24:48 +0100

    modified:   workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Controllers/ProductController.cs

--- workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Controllers/ProductController.cs
+++ workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Controllers/ProductController.cs
@@ -62,9 +62,15 @@ public class ProductController : ControllerBase, IProductController
     200,
     Type = typeof(IEnumerable<Product>)
   )]
-  public Task<IEnumerable<Product>> GetAll(string? name)
+  public async Task<IEnumerable<Product>> GetAll(string? name)
   {
-    throw new NotImplementedException();
+    if (string.IsNullOrWhiteSpace(name))
+    {
+      return await this.repository.RetrieveAllAsync();
+    }
+    return (await this.repository.RetrieveAllAsync()).Where(
+      entity => entity.Name == name
+    );
   }
 
   /* 
