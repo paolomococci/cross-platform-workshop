e22729135bf189beefee68607399710eb3802822
paolomococci <paolomococci@gmail.com>
2022-12-07 09:29:16 +0100

    modified:   workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Controllers/CarrierController.cs

--- workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Controllers/CarrierController.cs
+++ workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Controllers/CarrierController.cs
@@ -4,4 +4,4 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace DummyRoster.WebApi.Controllers;
 
-public class CarrierController : ControllerBase { }
+public class CarrierController : ControllerBase, ICarrierController { }
