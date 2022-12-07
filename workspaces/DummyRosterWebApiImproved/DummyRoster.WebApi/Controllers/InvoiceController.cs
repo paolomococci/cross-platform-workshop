29d818e9f1784fff8fe63adb753e57d3eaa0e996
paolomococci <paolomococci@gmail.com>
2022-12-07 15:50:15 +0100

    modified:   workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Controllers/InvoiceController.cs

--- workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Controllers/InvoiceController.cs
+++ workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Controllers/InvoiceController.cs
@@ -24,6 +24,15 @@ public class InvoiceController : ControllerBase, IInvoiceController
     throw new NotImplementedException();
   }
 
+  /* 
+    GET: api/invoices/[id]
+   */
+  [HttpGet("{id}", Name = nameof(Get))]
+  [ProducesResponseType(
+    200,
+    Type = typeof(Invoice)
+  )]
+  [ProducesResponseType(404)]
   public Task<IActionResult> Get(int id)
   {
     throw new NotImplementedException();
