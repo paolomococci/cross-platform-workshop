0d170dd6290c0fae1eb270d1a4c7815c50d621b6
paolomococci <paolomococci@gmail.com>
2022-12-07 07:20:17 +0100

    modified:   workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Controllers/Interfaces/IFormController.cs

--- workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Controllers/Interfaces/IFormController.cs
+++ workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Controllers/Interfaces/IFormController.cs
@@ -3,4 +3,12 @@ using DummyRoster.Common.EntityModel.Models;
 
 namespace DummyRoster.WebApi.Controllers.Interfaces;
 
-public interface IFormController { }
+public interface IFormController
+{
+  public Task<IActionResult> Create([FromBody] Form entity);
+  public Task<IActionResult> Get(int id);
+  public Task<IEnumerable<Form>> GetAll(string? country);
+  public Task<IActionResult> Update(int id, [FromBody] Form entity);
+  public Task<IActionResult> PartialUpdate(int id, [FromBody] Form entity);
+  public Task<IActionResult> Delete(int id);
+}
