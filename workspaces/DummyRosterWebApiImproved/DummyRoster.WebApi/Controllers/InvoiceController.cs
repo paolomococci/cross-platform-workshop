8f2250c38903f28c250f2d6c50c47d1d44cc7a0c
paolomococci <paolomococci@gmail.com>
2022-12-07 09:30:00 +0100

    modified:   workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Controllers/InvoiceController.cs

--- workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Controllers/InvoiceController.cs
+++ workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Controllers/InvoiceController.cs
@@ -4,4 +4,4 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace DummyRoster.WebApi.Controllers;
 
-public class InvoiceController : ControllerBase { }
+public class InvoiceController : ControllerBase, IInvoiceController { }
