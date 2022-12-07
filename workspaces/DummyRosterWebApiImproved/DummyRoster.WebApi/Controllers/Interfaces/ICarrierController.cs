98d85aa1ba30dd3543e7159653b4480d7010d433
paolomococci <paolomococci@gmail.com>
2022-12-07 07:19:35 +0100

    modified:   workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Controllers/Interfaces/ICarrierController.cs

--- workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Controllers/Interfaces/ICarrierController.cs
+++ workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Controllers/Interfaces/ICarrierController.cs
@@ -3,4 +3,12 @@ using DummyRoster.Common.EntityModel.Models;
 
 namespace DummyRoster.WebApi.Controllers.Interfaces;
 
-public interface ICarrierController { }
+public interface ICarrierController
+{
+  public Task<IActionResult> Create([FromBody] Carrier entity);
+  public Task<IActionResult> Get(int id);
+  public Task<IEnumerable<Carrier>> GetAll(string? country);
+  public Task<IActionResult> Update(int id, [FromBody] Carrier entity);
+  public Task<IActionResult> PartialUpdate(int id, [FromBody] Carrier entity);
+  public Task<IActionResult> Delete(int id);
+}
