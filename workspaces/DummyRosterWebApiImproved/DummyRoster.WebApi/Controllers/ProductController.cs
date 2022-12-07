7a2076bf37d38b8bdfa685ccffdcf8b77a963a5a
paolomococci <paolomococci@gmail.com>
2022-12-07 09:36:25 +0100

    modified:   workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Controllers/ProductController.cs

--- workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Controllers/ProductController.cs
+++ workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Controllers/ProductController.cs
@@ -11,11 +11,6 @@ public class ProductController : ControllerBase, IProductController
     throw new NotImplementedException();
   }
 
-  public Task<IActionResult> Delete(int id)
-  {
-    throw new NotImplementedException();
-  }
-
   public Task<IActionResult> Get(int id)
   {
     throw new NotImplementedException();
@@ -26,12 +21,17 @@ public class ProductController : ControllerBase, IProductController
     throw new NotImplementedException();
   }
 
+  public Task<IActionResult> Update(int id, [FromBody] Product entity)
+  {
+    throw new NotImplementedException();
+  }
+
   public Task<IActionResult> PartialUpdate(int id, [FromBody] Product entity)
   {
     throw new NotImplementedException();
   }
 
-  public Task<IActionResult> Update(int id, [FromBody] Product entity)
+  public Task<IActionResult> Delete(int id)
   {
     throw new NotImplementedException();
   }
