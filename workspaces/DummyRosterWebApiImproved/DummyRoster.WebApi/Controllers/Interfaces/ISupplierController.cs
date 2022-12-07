4140e90cbcbc171060a0e9988d6650b588a1b76a
paolomococci <paolomococci@gmail.com>
2022-12-07 16:10:30 +0100

    modified:   workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Controllers/Interfaces/ISupplierController.cs

--- workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Controllers/Interfaces/ISupplierController.cs
+++ workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Controllers/Interfaces/ISupplierController.cs
@@ -7,7 +7,7 @@ public interface ISupplierController
 {
   public Task<IActionResult> Create([FromBody] Supplier entity);
   public Task<IActionResult> Get(int id);
-  public Task<IEnumerable<Supplier>> GetAll(string? country);
+  public Task<IEnumerable<Supplier>> GetAll(string? name);
   public Task<IActionResult> Update(int id, [FromBody] Supplier entity);
   public Task<IActionResult> PartialUpdate(int id, [FromBody] Supplier entity);
   public Task<IActionResult> Delete(int id);
