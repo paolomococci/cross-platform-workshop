34f23a33939b0993dcbeba0400a17bdf16172d31
paolomococci <paolomococci@gmail.com>
2022-12-07 09:06:09 +0100

    modified:   workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Controllers/AddressController.cs

--- workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Controllers/AddressController.cs
+++ workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Controllers/AddressController.cs
@@ -1,5 +1,6 @@
+using DummyRoster.WebApi.Controllers.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DummyRoster.WebApi.Controllers;
 
-public class AddressController : ControllerBase { }
+public class AddressController : ControllerBase, IAddressController { }
