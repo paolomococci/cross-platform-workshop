95fa657607aa98f2add548efd71eb224a6d5809c
paolomococci <paolomococci@gmail.com>
2022-12-07 17:33:18 +0100

    modified:   workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Controllers/Interfaces/ICarrierController.cs

--- workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Controllers/Interfaces/ICarrierController.cs
+++ workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Controllers/Interfaces/ICarrierController.cs
@@ -6,8 +6,8 @@ namespace DummyRoster.WebApi.Controllers.Interfaces;
 public interface ICarrierController
 {
   public Task<IActionResult> Create([FromBody] Carrier entity);
-  public Task<IActionResult> Get(int id);
-  public Task<IEnumerable<Carrier>> GetAll(string? name);
+  public Task<IActionResult> Read(int id);
+  public Task<IEnumerable<Carrier>> ReadAll(string? name);
   public Task<IActionResult> Update(int id, [FromBody] Carrier entity);
   public Task<IActionResult> PartialUpdate(int id, [FromBody] Carrier entity);
   public Task<IActionResult> Delete(int id);
