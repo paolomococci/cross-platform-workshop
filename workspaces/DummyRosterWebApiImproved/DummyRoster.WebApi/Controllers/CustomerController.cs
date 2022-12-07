6dc11a1967884fa89ea9d71153ef33ed28c00853
paolomococci <paolomococci@gmail.com>
2022-12-07 09:31:54 +0100

    modified:   workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Controllers/CustomerController.cs

--- workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Controllers/CustomerController.cs
+++ workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Controllers/CustomerController.cs
@@ -4,4 +4,35 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace DummyRoster.WebApi.Controllers;
 
-public class CustomerController : ControllerBase, ICustomerController { }
+public class CustomerController : ControllerBase, ICustomerController
+{
+  public Task<IActionResult> Create([FromBody] Customer entity)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<IActionResult> Delete(int id)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<IActionResult> Get(int id)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<IEnumerable<Customer>> GetAll(string? country)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<IActionResult> PartialUpdate(int id, [FromBody] Customer entity)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<IActionResult> Update(int id, [FromBody] Customer entity)
+  {
+    throw new NotImplementedException();
+  }
+}
