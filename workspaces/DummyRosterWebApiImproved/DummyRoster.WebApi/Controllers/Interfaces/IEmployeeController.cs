ce4ba8f31dec72d79db378c97de4293fe3d9a455
paolomococci <paolomococci@gmail.com>
2022-12-07 17:33:45 +0100

    modified:   workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Controllers/Interfaces/IEmployeeController.cs

--- workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Controllers/Interfaces/IEmployeeController.cs
+++ workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Controllers/Interfaces/IEmployeeController.cs
@@ -6,8 +6,8 @@ namespace DummyRoster.WebApi.Controllers.Interfaces;
 public interface IEmployeeController
 {
   public Task<IActionResult> Create([FromBody] Employee entity);
-  public Task<IActionResult> Get(int id);
-  public Task<IEnumerable<Employee>> GetAll(string? name);
+  public Task<IActionResult> Read(int id);
+  public Task<IEnumerable<Employee>> ReadAll(string? name);
   public Task<IActionResult> Update(int id, [FromBody] Employee entity);
   public Task<IActionResult> PartialUpdate(int id, [FromBody] Employee entity);
   public Task<IActionResult> Delete(int id);
