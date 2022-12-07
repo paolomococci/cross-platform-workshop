3bc1f2fd5a381d4bea85c36d4ff2df142779120b
paolomococci <paolomococci@gmail.com>
2022-12-07 16:10:21 +0100

    modified:   workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Controllers/Interfaces/IProductController.cs

--- workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Controllers/Interfaces/IProductController.cs
+++ workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Controllers/Interfaces/IProductController.cs
@@ -7,7 +7,7 @@ public interface IProductController
 {
   public Task<IActionResult> Create([FromBody] Product entity);
   public Task<IActionResult> Get(int id);
-  public Task<IEnumerable<Product>> GetAll(string? country);
+  public Task<IEnumerable<Product>> GetAll(string? name);
   public Task<IActionResult> Update(int id, [FromBody] Product entity);
   public Task<IActionResult> PartialUpdate(int id, [FromBody] Product entity);
   public Task<IActionResult> Delete(int id);
