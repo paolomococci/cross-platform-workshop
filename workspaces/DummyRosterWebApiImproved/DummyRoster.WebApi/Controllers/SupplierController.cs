b396d099f91a953a392e6c882afd520d1e77b5c3
paolomococci <paolomococci@gmail.com>
2022-12-07 15:33:43 +0100

    modified:   workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Controllers/SupplierController.cs

--- workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Controllers/SupplierController.cs
+++ workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Controllers/SupplierController.cs
@@ -4,6 +4,8 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace DummyRoster.WebApi.Controllers;
 
+[Route("api/controllers")]
+[ApiController]
 public class SupplierController : ControllerBase, ISupplierController
 {
   public Task<IActionResult> Create([FromBody] Supplier entity)
