8134ce5ac281b39d84c225bb60b076090ce275d6
paolomococci <paolomococci@gmail.com>
2022-12-07 09:31:45 +0100

    modified:   workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Controllers/CategoryController.cs

--- workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Controllers/CategoryController.cs
+++ workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Controllers/CategoryController.cs
@@ -4,4 +4,35 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace DummyRoster.WebApi.Controllers;
 
-public class CategoryController : ControllerBase, ICategoryController { }
+public class CategoryController : ControllerBase, ICategoryController
+{
+  public Task<IActionResult> Create([FromBody] Category entity)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<IActionResult> Delete(int id)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<IActionResult> Get(int id)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<IEnumerable<Category>> GetAll(string? country)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<IActionResult> PartialUpdate(int id, [FromBody] Category entity)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<IActionResult> Update(int id, [FromBody] Category entity)
+  {
+    throw new NotImplementedException();
+  }
+}
