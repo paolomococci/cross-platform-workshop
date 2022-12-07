49803771b6f9ce0873d0b29f0853ba917085371b
paolomococci <paolomococci@gmail.com>
2022-12-07 17:10:57 +0100

    modified:   workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Controllers/CustomerController.cs

--- workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Controllers/CustomerController.cs
+++ workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Controllers/CustomerController.cs
@@ -43,9 +43,14 @@ public class CustomerController : ControllerBase, ICustomerController
     Type = typeof(Customer)
   )]
   [ProducesResponseType(404)]
-  public Task<IActionResult> Get(int id)
+  public async Task<IActionResult> Get(int id)
   {
-    throw new NotImplementedException();
+    Customer? managedEntity = await this.repository.RetrieveAsync(id);
+    if (managedEntity == null)
+    {
+      return NotFound();
+    }
+    return Ok(managedEntity);
   }
 
   /* 
