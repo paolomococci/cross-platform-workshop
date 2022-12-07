daccfc420847ab4184b75b35dba3671376893cd3
paolomococci <paolomococci@gmail.com>
2022-12-07 07:19:56 +0100

    modified:   workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Controllers/Interfaces/ICustomerController.cs

--- workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Controllers/Interfaces/ICustomerController.cs
+++ workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Controllers/Interfaces/ICustomerController.cs
@@ -3,4 +3,13 @@ using DummyRoster.Common.EntityModel.Models;
 
 namespace DummyRoster.WebApi.Controllers.Interfaces;
 
-public interface ICustomerController { }
+public interface ICustomerController
+{
+  public Task<IActionResult> Create([FromBody] Customer entity);
+  public Task<IActionResult> Get(int id);
+  public Task<IEnumerable<Customer>> GetAll(string? country);
+  public Task<IActionResult> Update(int id, [FromBody] Customer entity);
+  public Task<IActionResult> PartialUpdate(int id, [FromBody] Customer entity);
+  public Task<IActionResult> Delete(int id);
+}
+
