46fc08448dddcd35f56b0bd4f67a3ec2d573f417
paolomococci <paolomococci@gmail.com>
2022-12-07 07:20:26 +0100

    modified:   workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Controllers/Interfaces/IInvoiceController.cs

--- workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Controllers/Interfaces/IInvoiceController.cs
+++ workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Controllers/Interfaces/IInvoiceController.cs
@@ -3,4 +3,13 @@ using DummyRoster.Common.EntityModel.Models;
 
 namespace DummyRoster.WebApi.Controllers.Interfaces;
 
-public interface IInvoiceController { }
+public interface IInvoiceController
+{
+  public Task<IActionResult> Create([FromBody] Invoice entity);
+  public Task<IActionResult> Get(int id);
+  public Task<IEnumerable<Invoice>> GetAll(string? country);
+  public Task<IActionResult> Update(int id, [FromBody] Invoice entity);
+  public Task<IActionResult> PartialUpdate(int id, [FromBody] Invoice entity);
+  public Task<IActionResult> Delete(int id);
+}
+
