3153316ac375c9a949b425e96554d928b70a9188
paolomococci <paolomococci@gmail.com>
2022-12-07 20:47:16 +0100

    modified:   workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Controllers/Interfaces/IAddressController.cs

--- workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Controllers/Interfaces/IAddressController.cs
+++ workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Controllers/Interfaces/IAddressController.cs
@@ -6,7 +6,7 @@ namespace DummyRoster.WebApi.Controllers.Interfaces;
 public interface IAddressController
 {
   public Task<IActionResult> Create([FromBody] Address entity);
-  public Task<IActionResult> Read(int id);
+  public Task<IActionResult> ReadAddress(int id);
   public Task<IEnumerable<Address>> ReadAll(string? country);
   public Task<IActionResult> Update(int id, [FromBody] Address entity);
   public Task<IActionResult> PartialUpdate(int id, [FromBody] Address entity);
