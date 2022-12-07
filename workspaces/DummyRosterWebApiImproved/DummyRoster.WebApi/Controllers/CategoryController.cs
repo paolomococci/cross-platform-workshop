d79c890e86489ba0a9c19282ab8a89aa0c12af43
paolomococci <paolomococci@gmail.com>
2022-12-07 09:29:25 +0100

    modified:   workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Controllers/CategoryController.cs

--- workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Controllers/CategoryController.cs
+++ workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Controllers/CategoryController.cs
@@ -4,4 +4,4 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace DummyRoster.WebApi.Controllers;
 
-public class CategoryController : ControllerBase { }
+public class CategoryController : ControllerBase, ICategoryController { }
