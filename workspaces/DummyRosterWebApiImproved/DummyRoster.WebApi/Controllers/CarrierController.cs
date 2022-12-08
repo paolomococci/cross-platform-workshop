f78965911fe9715a4864d331f0f273e206a88775
paolomococci <paolomococci@gmail.com>
2022-12-08 17:16:56 +0100

    modified:   workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Controllers/CarrierController.cs

--- workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Controllers/CarrierController.cs
+++ workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Controllers/CarrierController.cs
@@ -29,9 +29,22 @@ public class CarrierController : ControllerBase, ICarrierController
     Type = typeof(Carrier)
   )]
   [ProducesResponseType(400)]
-  public Task<IActionResult> Create([FromBody] Carrier entity)
+  public async Task<IActionResult> Create([FromBody] Carrier entity)
   {
-    throw new NotImplementedException();
+    if (entity == null)
+    {
+      return BadRequest();
+    }
+    Carrier? managedEntity = await this.repository.CreateAsync(entity);
+    if (managedEntity == null)
+    {
+      return BadRequest("Unable to manage entity!");
+    }
+    return CreatedAtRoute(
+      routeName: nameof(ReadCarrier),
+      routeValues: new { id = managedEntity.Id },
+      value: managedEntity
+    );
   }
 
   /* 
