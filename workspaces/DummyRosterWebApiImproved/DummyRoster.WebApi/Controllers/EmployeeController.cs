15a17093007b7bee556ef20a2d9e68f668aabb73
paolomococci <paolomococci@gmail.com>
2022-12-09 05:07:18 +0100

    modified:   workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Controllers/EmployeeController.cs

--- workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Controllers/EmployeeController.cs
+++ workspaces/DummyRosterWebApiImproved/DummyRoster.WebApi/Controllers/EmployeeController.cs
@@ -117,9 +117,19 @@ public class EmployeeController : ControllerBase, IEmployeeController
   [ProducesResponseType(204)]
   [ProducesResponseType(400)]
   [ProducesResponseType(404)]
-  public Task<IActionResult> PartialUpdate(int id, [FromBody] Employee entity)
+  public async Task<IActionResult> PartialUpdate(int id, [FromBody] Employee entity)
   {
-    throw new NotImplementedException();
+    if (entity == null || entity.Id != id)
+    {
+      return BadRequest();
+    }
+    Employee? managedEntity = await this.repository.RetrieveAsync(id);
+    if (managedEntity == null)
+    {
+      return NotFound();
+    }
+    await this.repository.PartialUpdateAsync(id, entity);
+    return new NoContentResult();
   }
 
   /* 
