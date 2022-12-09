cf84c05873dd2ad09332af0db577a2521eb4eab5
paolomococci <paolomococci@gmail.com>
2022-12-09 05:56:23 +0100

    new file:   workspaces/DummyRosterWebApiHardened/DummyRoster.Common.EntityModel/Models/Customer.cs

---
+++ workspaces/DummyRosterWebApiHardened/DummyRoster.Common.EntityModel/Models/Customer.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+using System.Xml.Serialization;
+
+namespace DummyRoster.Common.EntityModel.Models;
+
+[Table("Customer")]
+[Index("Name", Name = "IDX_CustomerName")]
+public partial class Customer
+{
+  [Key]
+  public int Id { get; set; }
+
+  [Column(TypeName = "varchar(32)")]
+  public string Name { get; set; } = null!;
+
+  [Column(TypeName = "DATETIME")]
+  public DateTime? FoundationDate { get; set; }
+
+  public string? Description { get; set; }
+
+  public byte[]? Picture { get; set; }
+
+  [Column(TypeName = "INT")]
+  public int? Contact { get; set; }
+
+  [Column(TypeName = "INT")]
+  public int? Location { get; set; }
+
+  [ForeignKey("Contact")]
+  [InverseProperty("Customers")]
+  public virtual Employee? ContactNavigation { get; set; }
+
+  [InverseProperty("Customer")]
+  [XmlIgnore]
+  public virtual ICollection<Form> Forms { get; } = new List<Form>();
+
+  [ForeignKey("Location")]
+  [InverseProperty("Customers")]
+  public virtual Address? LocationNavigation { get; set; }
+}
