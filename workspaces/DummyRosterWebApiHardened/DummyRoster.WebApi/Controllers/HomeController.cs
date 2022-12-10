a1d28e8744cdccdb42649f82d88859b0f15cdeff
paolomococci <paolomococci@gmail.com>
2022-12-10 05:23:19 +0100

    modified:   workspaces/DummyRosterWebApiHardened/DummyRoster.WebApi/Controllers/HomeController.cs

--- workspaces/DummyRosterWebApiHardened/DummyRoster.WebApi/Controllers/HomeController.cs
+++ workspaces/DummyRosterWebApiHardened/DummyRoster.WebApi/Controllers/HomeController.cs
@@ -1,10 +1,14 @@
 using Microsoft.AspNetCore.Mvc;
 using DummyRoster.WebApi.Controllers.Interfaces;
+using DummyRoster.Common.DataContext.Data;
 
 namespace DummyRoster.WebApi.Controllers;
 
 public class HomeController : IHomeController
 {
+  ILogger<HomeController> logger;
+  DummyRosterContext dummyRosterContext;
+  IHttpClientFactory httpClientFactory;
   public Task<IActionResult> Customers(string name)
   {
     throw new NotImplementedException();
