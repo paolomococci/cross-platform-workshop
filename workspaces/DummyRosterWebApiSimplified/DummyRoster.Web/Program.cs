876d96f71799a95772d2ffa5bef3753bd5d0283b
paolomococci <paolomococci@gmail.com>
2022-12-10 23:09:05 +0100

    modified:   workspaces/DummyRosterWebApiSimplified/DummyRoster.Web/Program.cs

--- workspaces/DummyRosterWebApiSimplified/DummyRoster.Web/Program.cs
+++ workspaces/DummyRosterWebApiSimplified/DummyRoster.Web/Program.cs
@@ -3,10 +3,18 @@ using DummyRoster.Common;
 var builder = WebApplication.CreateBuilder(args);
 
 /* todo */
-builder.WebHost.UseUrls("https://127.0.0.1:5008");
+builder.WebHost.UseUrls("https://127.0.0.1:5002");
 builder.Services.AddCors();
 
 var app = builder.Build();
+
+/* todo */
+app.UseCors(
+  configurePolicy: options => {
+    options.WithMethods("GET");
+    options.WithOrigins("https://127.0.0.1:5001");
+  }
+);
 
 app.MapGet("/", () => "Hello World!");
 
