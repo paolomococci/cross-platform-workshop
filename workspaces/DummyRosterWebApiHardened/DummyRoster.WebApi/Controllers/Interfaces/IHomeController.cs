94c37e91f00c4ccdc6aacb2dbe8daccd15c9abe2
paolomococci <paolomococci@gmail.com>
2022-12-10 15:12:17 +0100

    deleted:    workspaces/DummyRosterWebApiHardened/DummyRoster.WebApi/Controllers/Interfaces/IHomeController.cs

--- workspaces/DummyRosterWebApiHardened/DummyRoster.WebApi/Controllers/Interfaces/IHomeController.cs
+++
@@ -1,8 +0,0 @@
-using Microsoft.AspNetCore.Mvc;
-
-namespace DummyRoster.WebApi.Controllers.Interfaces;
-
-public interface IHomeController
-{
-  public Task<IActionResult> Customers(string name);
-}
