10df0634c0ec7110c6dd966afebbc30884acfe0b
paolomococci <paolomococci@gmail.com>
2022-12-10 06:41:56 +0100

    modified:   workspaces/DummyRosterWebApiHardened/DummyRoster.WebApi/Controllers/HomeController.cs

--- workspaces/DummyRosterWebApiHardened/DummyRoster.WebApi/Controllers/HomeController.cs
+++ workspaces/DummyRosterWebApiHardened/DummyRoster.WebApi/Controllers/HomeController.cs
@@ -1,10 +1,11 @@
 using Microsoft.AspNetCore.Mvc;
 using DummyRoster.WebApi.Controllers.Interfaces;
 using DummyRoster.Common.DataContext.Data;
+using DummyRoster.Common.EntityModel.Models;
 
 namespace DummyRoster.WebApi.Controllers;
 
-public class HomeController : IHomeController
+public class HomeController : Controller, IHomeController
 {
   ILogger<HomeController> _logger;
   DummyRosterContext dummyRosterContext;
@@ -14,7 +15,8 @@ public class HomeController : IHomeController
     ILogger<HomeController> logger,
     DummyRosterContext dummyRosterContext,
     IHttpClientFactory httpClientFactory
-  ) {
+  )
+  {
     this._logger = logger;
     this.dummyRosterContext = dummyRosterContext;
     this.httpClientFactory = httpClientFactory;
@@ -22,6 +24,34 @@ public class HomeController : IHomeController
 
   public async Task<IActionResult> Customers(string name)
   {
-    throw new NotImplementedException();
+    string uri;
+
+    if (string.IsNullOrEmpty(name))
+    {
+      ViewData["Title"] = "all customers registered in the system";
+      uri = "api/customers/?name={name}";
+    }
+    else
+    {
+      ViewData["Title"] = $"customers who have a name similar to {name}";
+      uri = $"api/customers/";
+    }
+
+    HttpClient httpClient = this.httpClientFactory.CreateClient(
+      name: "DummyRoster.WebApi"
+    );
+
+    HttpRequestMessage httpRequestMessage = new(
+      method: HttpMethod.Get,
+      requestUri: uri
+    );
+
+    HttpResponseMessage httpResponseMessage = await httpClient.SendAsync(
+      httpRequestMessage
+    );
+
+    IEnumerable<Customer>? customers = await httpResponseMessage.Content.ReadFromJsonAsync<IEnumerable<Customer>>();
+
+    return View(customers);
   }
 }
