b83b5bcf5cb22f05ed39da194d108174475b0209
paolomococci <paolomococci@gmail.com>
2022-12-12 15:53:31 +0100

    modified:   workspaces/DummyRosterWebApiHardened/DummyRoster.Common.EntityModel/Models/Category.cs

--- workspaces/DummyRosterWebApiHardened/DummyRoster.Common.EntityModel/Models/Category.cs
+++ workspaces/DummyRosterWebApiHardened/DummyRoster.Common.EntityModel/Models/Category.cs
@@ -13,6 +13,7 @@ public partial class Category
   public int Id { get; set; }
 
   [Column(TypeName = "varchar(32)")]
+  [RegularExpression("[a-zA-Z]{1,32}")]
   public string Name { get; set; } = null!;
 
   public string? Description { get; set; }
