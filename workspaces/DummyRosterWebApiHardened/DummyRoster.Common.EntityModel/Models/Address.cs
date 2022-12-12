bf864529c5cea23b072d6ace198fd985b08f17b1
paolomococci <paolomococci@gmail.com>
2022-12-12 15:50:56 +0100

    modified:   workspaces/DummyRosterWebApiHardened/DummyRoster.Common.EntityModel/Models/Address.cs

--- workspaces/DummyRosterWebApiHardened/DummyRoster.Common.EntityModel/Models/Address.cs
+++ workspaces/DummyRosterWebApiHardened/DummyRoster.Common.EntityModel/Models/Address.cs
@@ -41,6 +41,7 @@ public partial class Address
   public string? Country { get; set; }
 
   [Column(TypeName = "varchar(48)")]
+  [RegularExpression(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,5})+)$")]
   public string? Email { get; set; }
 
   [Column(TypeName = "varchar(24)")]
