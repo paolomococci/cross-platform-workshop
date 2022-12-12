78cee70d0b9d48fc1866a1b0cd2e05859d4d7538
paolomococci <paolomococci@gmail.com>
2022-12-12 15:55:51 +0100

    modified:   workspaces/DummyRosterWebApiHardened/DummyRoster.Common.EntityModel/Models/Product.cs

--- workspaces/DummyRosterWebApiHardened/DummyRoster.Common.EntityModel/Models/Product.cs
+++ workspaces/DummyRosterWebApiHardened/DummyRoster.Common.EntityModel/Models/Product.cs
@@ -17,6 +17,7 @@ public partial class Product
   public int Id { get; set; }
 
   [Column(TypeName = "varchar(32)")]
+  [RegularExpression("[a-zA-Z]{1,32}")]
   public string Name { get; set; } = null!;
 
   public string? Description { get; set; }
