a8127efec957193e315ff7ce2d85531a3c45357a
paolomococci <paolomococci@gmail.com>
2022-12-13 13:50:12 +0100

    modified:   workspaces/DummyRosterRC1/DummyRoster.Common.EntityModel/Models/Supplier.cs

--- workspaces/DummyRosterRC1/DummyRoster.Common.EntityModel/Models/Supplier.cs
+++ workspaces/DummyRosterRC1/DummyRoster.Common.EntityModel/Models/Supplier.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.EntityFrameworkCore;
+using System.Xml.Serialization;
 
 namespace DummyRoster.Common.EntityModel.Models;
 
