a4ccb8b00e0575a896d9b636b25a9a60fab473de
paolomococci <paolomococci@gmail.com>
2022-12-13 13:58:58 +0100

    modified:   workspaces/DummyRosterRC1/DummyRoster.Common.EntityModel/Models/Invoice.cs

--- workspaces/DummyRosterRC1/DummyRoster.Common.EntityModel/Models/Invoice.cs
+++ workspaces/DummyRosterRC1/DummyRoster.Common.EntityModel/Models/Invoice.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.EntityFrameworkCore;
-using System.Xml.Serialization;
 
 namespace DummyRoster.Common.EntityModel.Models;
 
