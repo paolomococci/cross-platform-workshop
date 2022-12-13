e49af5803278a45e879da8b58f7ad1821e36193b
paolomococci <paolomococci@gmail.com>
2022-12-13 13:32:06 +0100

    modified:   workspaces/DummyRosterRC1/DummyRoster.Common.EntityModel/Models/Credential.cs

--- workspaces/DummyRosterRC1/DummyRoster.Common.EntityModel/Models/Credential.cs
+++ workspaces/DummyRosterRC1/DummyRoster.Common.EntityModel/Models/Credential.cs
@@ -22,6 +22,7 @@ public partial class Credential
   public string? Phone { get; set; }
 
   [Column(TypeName = "varchar(24)")]
+  [RegularExpression(@"^\+?\d{0,2}\-?\d{7,11}")]
   public string? Fax { get; set; }
 
   [InverseProperty("RefNavigation")]
