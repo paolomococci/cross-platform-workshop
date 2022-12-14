4d8edc1e859d135b4332f5d85c10e1efdfe646eb
paolomococci <paolomococci@gmail.com>
2022-12-14 21:40:06 +0100

    modified:   workspaces/DummyRosterRC1/DummyRoster.WebApi/Repositories/EmployeeRepository.cs

--- workspaces/DummyRosterRC1/DummyRoster.WebApi/Repositories/EmployeeRepository.cs
+++ workspaces/DummyRosterRC1/DummyRoster.WebApi/Repositories/EmployeeRepository.cs
@@ -49,7 +49,9 @@ public class EmployeeRepository : IEmployeeRepository
 
   public Task<Employee?> RetrieveAsync(int id)
   {
-    throw new NotImplementedException();
+    if (keyValuesCache is null) return null!;
+    keyValuesCache.TryGetValue(id, out Employee? entity);
+    return Task.FromResult(entity);
   }
 
   public Task<IEnumerable<Employee>> RetrieveAllAsync()
