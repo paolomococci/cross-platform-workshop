52eb9b7359b0749baffdb333d1083c78c6aaf388
paolomococci <paolomococci@gmail.com>
2022-12-14 21:40:16 +0100

    modified:   workspaces/DummyRosterRC1/DummyRoster.WebApi/Repositories/FormRepository.cs

--- workspaces/DummyRosterRC1/DummyRoster.WebApi/Repositories/FormRepository.cs
+++ workspaces/DummyRosterRC1/DummyRoster.WebApi/Repositories/FormRepository.cs
@@ -49,7 +49,9 @@ public class FormRepository : IFormRepository
 
   public Task<Form?> RetrieveAsync(int id)
   {
-    throw new NotImplementedException();
+    if (keyValuesCache is null) return null!;
+    keyValuesCache.TryGetValue(id, out Form? entity);
+    return Task.FromResult(entity);
   }
 
   public Task<IEnumerable<Form>> RetrieveAllAsync()
