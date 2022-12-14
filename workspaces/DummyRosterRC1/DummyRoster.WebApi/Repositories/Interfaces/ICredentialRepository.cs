1c29e099280d8cff4a84f0d09cd4494bd51ad42a
paolomococci <paolomococci@gmail.com>
2022-12-14 09:20:32 +0100

    modified:   workspaces/DummyRosterRC1/DummyRoster.WebApi/Repositories/Interfaces/ICredentialRepository.cs

--- workspaces/DummyRosterRC1/DummyRoster.WebApi/Repositories/Interfaces/ICredentialRepository.cs
+++ workspaces/DummyRosterRC1/DummyRoster.WebApi/Repositories/Interfaces/ICredentialRepository.cs
@@ -2,4 +2,18 @@ using DummyRoster.Common.EntityModel.Models;
 
 namespace DummyRoster.WebApi.Repositories.Interfaces;
 
-public interface ICredentialRepository {}
+public interface ICredentialRepository
+{
+  Task<Credential?> CreateAsync(Credential entity);
+  Task<Credential?> RetrieveAsync(int id);
+  Task<IEnumerable<Credential>> RetrieveAllAsync();
+  Task<Credential?> UpdateAsync(
+    int id,
+    Credential entity
+  );
+  Task<Credential?> PartialUpdateAsync(
+    int id,
+    Credential entity
+  );
+  Task<bool?> DeleteAsync(int id);
+}
