a6285beff09fd90145eef577c09e881e5f8f0be3
paolomococci <paolomococci@gmail.com>
2022-12-14 14:41:34 +0100

    modified:   workspaces/DummyRosterRC1/DummyRoster.WebApi/Repositories/EmployeeRepository.cs

--- workspaces/DummyRosterRC1/DummyRoster.WebApi/Repositories/EmployeeRepository.cs
+++ workspaces/DummyRosterRC1/DummyRoster.WebApi/Repositories/EmployeeRepository.cs
@@ -5,4 +5,23 @@ using DummyRoster.WebApi.Repositories.Interfaces;
 
 namespace DummyRoster.WebApi.Repositories;
 
-public class EmployeeRepository {}
+public class EmployeeRepository
+{
+  private static ConcurrentDictionary<int, Employee>? keyValuesCache;
+  private DummyRosterContext dummyRosterContext;
+
+  public EmployeeRepository(
+    DummyRosterContext dummyRosterContext
+  )
+  {
+    this.dummyRosterContext = dummyRosterContext;
+    if (keyValuesCache is null)
+    {
+      keyValuesCache = new ConcurrentDictionary<int, Employee>(
+        this.dummyRosterContext.Employees.ToDictionary(
+          entity => entity.Id
+        )
+      );
+    }
+  }
+}
