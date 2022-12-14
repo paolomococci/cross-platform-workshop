03446517b93882858116ea40e5959fd750e0261f
paolomococci <paolomococci@gmail.com>
2022-12-14 17:35:11 +0100

    modified:   workspaces/DummyRosterRC1/DummyRoster.WebApi/Repositories/ProductRepository.cs

--- workspaces/DummyRosterRC1/DummyRoster.WebApi/Repositories/ProductRepository.cs
+++ workspaces/DummyRosterRC1/DummyRoster.WebApi/Repositories/ProductRepository.cs
@@ -25,17 +25,34 @@ public class ProductRepository : IProductRepository
     }
   }
 
-  public Task<Product?> CreateAsync(Product entity)
+  public async Task<Product?> CreateAsync(Product entity)
+  {
+    EntityEntry<Product> entry = await this.dummyRosterContext.Products.AddAsync(entity);
+    int changesWereSavedAsynchronously = await this.dummyRosterContext.SaveChangesAsync();
+    if (changesWereSavedAsynchronously == 1)
+    {
+      if (keyValuesCache is null)
+      {
+        return entity;
+      }
+      return keyValuesCache.AddOrUpdate(
+        entity.Id,
+        entity,
+        UpdateCache
+      );
+    }
+    else
+    {
+      return null;
+    }
+  }
+
+  public async Task<bool?> DeleteAsync(int id)
   {
     throw new NotImplementedException();
   }
 
-  public Task<bool?> DeleteAsync(int id)
-  {
-    throw new NotImplementedException();
-  }
-
-  public Task<Product?> PartialUpdateAsync(int id, Product entity)
+  public async Task<Product?> PartialUpdateAsync(int id, Product entity)
   {
     throw new NotImplementedException();
   }
@@ -50,7 +67,7 @@ public class ProductRepository : IProductRepository
     throw new NotImplementedException();
   }
 
-  public Task<Product?> UpdateAsync(int id, Product entity)
+  public async Task<Product?> UpdateAsync(int id, Product entity)
   {
     throw new NotImplementedException();
   }
