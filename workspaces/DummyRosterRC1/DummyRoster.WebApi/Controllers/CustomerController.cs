b80da528cccb6edc35fa190df27e8976b17a12d7
paolomococci <paolomococci@gmail.com>
2022-12-14 14:22:32 +0100

    modified:   workspaces/DummyRosterRC1/DummyRoster.WebApi/Controllers/CustomerController.cs

--- workspaces/DummyRosterRC1/DummyRoster.WebApi/Controllers/CustomerController.cs
+++ workspaces/DummyRosterRC1/DummyRoster.WebApi/Controllers/CustomerController.cs
@@ -18,4 +18,34 @@ public class CustomerController : ControllerBase, ICustomerController
   {
     this.repository = repo;
   }
+
+  public Task<IActionResult> Create([FromBody] Customer entity)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<IActionResult> Delete(int id)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<IActionResult> PartialUpdate(int id, [FromBody] Customer entity)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<IEnumerable<Customer>> ReadAll(string? name)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<IActionResult> ReadCustomer(int id)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<IActionResult> Update(int id, [FromBody] Customer entity)
+  {
+    throw new NotImplementedException();
+  }
 }
