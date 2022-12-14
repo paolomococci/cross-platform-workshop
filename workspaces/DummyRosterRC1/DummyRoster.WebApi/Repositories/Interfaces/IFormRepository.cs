35c06584f3fab70654d109b52f50f33e69849921
paolomococci <paolomococci@gmail.com>
2022-12-14 09:20:57 +0100

    modified:   workspaces/DummyRosterRC1/DummyRoster.WebApi/Repositories/Interfaces/IFormRepository.cs

--- workspaces/DummyRosterRC1/DummyRoster.WebApi/Repositories/Interfaces/IFormRepository.cs
+++ workspaces/DummyRosterRC1/DummyRoster.WebApi/Repositories/Interfaces/IFormRepository.cs
@@ -2,4 +2,18 @@ using DummyRoster.Common.EntityModel.Models;
 
 namespace DummyRoster.WebApi.Repositories.Interfaces;
 
-public interface IFormRepository {}
+public interface IFormRepository
+{
+  Task<Form?> CreateAsync(Form entity);
+  Task<Form?> RetrieveAsync(int id);
+  Task<IEnumerable<Form>> RetrieveAllAsync();
+  Task<Form?> UpdateAsync(
+    int id,
+    Form entity
+  );
+  Task<Form?> PartialUpdateAsync(
+    int id,
+    Form entity
+  );
+  Task<bool?> DeleteAsync(int id);
+}
