9b9c2233e245b0dacbd4e03332fa2bc8c347441f
paolomococci <paolomococci@gmail.com>
2022-12-14 22:12:00 +0100

    modified:   workspaces/DummyRosterRC1/DummyRoster.WebApi/Repositories/EmployeeRepository.cs

--- workspaces/DummyRosterRC1/DummyRoster.WebApi/Repositories/EmployeeRepository.cs
+++ workspaces/DummyRosterRC1/DummyRoster.WebApi/Repositories/EmployeeRepository.cs
@@ -74,7 +74,25 @@ public class EmployeeRepository : IEmployeeRepository
 
   public async Task<Employee?> PartialUpdateAsync(int id, Employee entity)
   {
-    throw new NotImplementedException();
+    if (keyValuesCache is null) return null!;
+    keyValuesCache.TryGetValue(id, out Employee? registered);
+    if (registered != null)
+    {
+      if (entity.Name != null) registered.Name = entity.Name;
+      if (entity.BirthDate != null) registered.BirthDate = entity.BirthDate;
+      if (entity.Description != null) registered.Description = entity.Description;
+      if (entity.Picture != null) registered.Picture = entity.Picture;
+      if (entity.Role != null) registered.Role = entity.Role;
+      if (entity.Loc != null) registered.Loc = entity.Loc;
+      if (entity.Ref != null) registered.Ref = entity.Ref;
+      this.dummyRosterContext.Employees.Update(registered);
+      int changesSaved = await this.dummyRosterContext.SaveChangesAsync();
+      if (changesSaved == 1)
+      {
+        return this.UpdateCache(id, registered);
+      }
+    }
+    return null;
   }
 
   public async Task<bool?> DeleteAsync(int id)
