ed0bb5230d7ab28a1cf168b9720bd96273d47463
paolomococci <paolomococci@gmail.com>
2022-12-14 14:33:50 +0100

    modified:   workspaces/DummyRosterRC1/DummyRoster.WebApi/Repositories/SupplierRepository.cs

--- workspaces/DummyRosterRC1/DummyRoster.WebApi/Repositories/SupplierRepository.cs
+++ workspaces/DummyRosterRC1/DummyRoster.WebApi/Repositories/SupplierRepository.cs
@@ -1,3 +1,8 @@
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System.Collections.Concurrent;
+using DummyRoster.Common.EntityModel.Models;
+using DummyRoster.WebApi.Repositories.Interfaces;
+
 namespace DummyRoster.WebApi.Repositories;
 
 public class SupplierRepository {}
