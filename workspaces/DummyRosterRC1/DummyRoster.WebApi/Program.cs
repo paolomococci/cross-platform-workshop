96197ff78c509b58821c35f9beda00831f56a54a
paolomococci <paolomococci@gmail.com>
2022-12-14 07:18:58 +0100

    modified:   workspaces/DummyRosterRC1/DummyRoster.WebApi/Program.cs

--- workspaces/DummyRosterRC1/DummyRoster.WebApi/Program.cs
+++ workspaces/DummyRosterRC1/DummyRoster.WebApi/Program.cs
@@ -1,3 +1,5 @@
+using Swashbuckle.AspNetCore.SwaggerUI;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +7,29 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(
+  /*temp => {
+    temp.SwaggerDoc(
+      "v1",
+      new() {
+        Title = "DummyRoster.WebApi project",
+        Version = "v1"
+      }
+    );
+  }*/
+);
+
+/* From here I add the interfaces and classes regarding the repositories */
+//builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+//builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
+//builder.Services.AddScoped<ISupplierRepository, SupplierRepository>();
+//builder.Services.AddScoped<ICarrierRepository, CarrierRepository>();
+//builder.Services.AddScoped<IAddressRepository, AddressRepository>();
+//builder.Services.AddScoped<ICredentialRepository, CredentialRepository>();
+//builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+//builder.Services.AddScoped<IProductRepository, ProductRepository>();
+//builder.Services.AddScoped<IFormRepository, FormRepository>();
+//builder.Services.AddScoped<IInvoiceRepository, InvoiceRepository>();
 
 var app = builder.Build();
 
@@ -13,7 +37,23 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
   app.UseSwagger();
-  app.UseSwaggerUI();
+  app.UseSwaggerUI(
+    /*temp => {
+      temp.SwaggerEndpoint(
+        "/swagger/v1/swagger.json",
+        "DummyRoster.WebApi project Version 1"
+      );
+      temp.SupportedSubmitMethods(
+        new[] {
+          SubmitMethod.Get,
+          SubmitMethod.Post,
+          SubmitMethod.Put,
+          SubmitMethod.Patch,
+          SubmitMethod.Delete
+        }
+      );
+    }*/
+  );
 }
 
 app.UseHttpsRedirection();
