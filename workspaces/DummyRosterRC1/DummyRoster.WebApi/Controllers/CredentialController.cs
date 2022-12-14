45ac418207249b3ca2c5deda7dbf1fe29536e7fe
paolomococci <paolomococci@gmail.com>
2022-12-14 10:22:28 +0100

    modified:   workspaces/DummyRosterRC1/DummyRoster.WebApi/Controllers/CredentialController.cs

--- workspaces/DummyRosterRC1/DummyRoster.WebApi/Controllers/CredentialController.cs
+++ workspaces/DummyRosterRC1/DummyRoster.WebApi/Controllers/CredentialController.cs
@@ -1,3 +1,8 @@
+using Microsoft.AspNetCore.Mvc;
+using DummyRoster.Common.EntityModel.Models;
+using DummyRoster.WebApi.Controllers.Interfaces;
+using DummyRoster.WebApi.Repositories.Interfaces;
+
 namespace DummyRoster.WebApi.Controllers;
 
 public class CredentialController {}
