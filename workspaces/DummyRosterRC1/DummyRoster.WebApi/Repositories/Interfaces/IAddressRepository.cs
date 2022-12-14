11d007e4aa72cf0cdbc0f09096761e80158c940b
paolomococci <paolomococci@gmail.com>
2022-12-14 09:19:56 +0100

    modified:   workspaces/DummyRosterRC1/DummyRoster.WebApi/Repositories/Interfaces/IAddressRepository.cs

--- workspaces/DummyRosterRC1/DummyRoster.WebApi/Repositories/Interfaces/IAddressRepository.cs
+++ workspaces/DummyRosterRC1/DummyRoster.WebApi/Repositories/Interfaces/IAddressRepository.cs
@@ -2,4 +2,18 @@ using DummyRoster.Common.EntityModel.Models;
 
 namespace DummyRoster.WebApi.Repositories.Interfaces;
 
-public interface IAddressRepository {}
+public interface IAddressRepository
+{
+  Task<Address?> CreateAsync(Address entity);
+  Task<Address?> RetrieveAsync(int id);
+  Task<IEnumerable<Address>> RetrieveAllAsync();
+  Task<Address?> UpdateAsync(
+    int id,
+    Address entity
+  );
+  Task<Address?> PartialUpdateAsync(
+    int id,
+    Address entity
+  );
+  Task<bool?> DeleteAsync(int id);
+}
