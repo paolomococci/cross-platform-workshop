9a5a12eda93ddb6a3092a1e820b981aac3b43acd
paolomococci <paolomococci@gmail.com>
2022-12-14 14:47:18 +0100

    modified:   workspaces/DummyRosterRC1/DummyRoster.WebApi/Repositories/ProductRepository.cs

--- workspaces/DummyRosterRC1/DummyRoster.WebApi/Repositories/ProductRepository.cs
+++ workspaces/DummyRosterRC1/DummyRoster.WebApi/Repositories/ProductRepository.cs
@@ -5,7 +5,7 @@ using DummyRoster.WebApi.Repositories.Interfaces;
 
 namespace DummyRoster.WebApi.Repositories;
 
-public class ProductRepository
+public class ProductRepository : IProductRepository
 {
   private static ConcurrentDictionary<int, Product>? keyValuesCache;
   private DummyRosterContext dummyRosterContext;
