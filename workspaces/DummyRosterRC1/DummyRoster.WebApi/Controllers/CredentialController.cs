73a3b59e8889f1c629078199f369a30bcb47051e
paolomococci <paolomococci@gmail.com>
2022-12-14 22:42:45 +0100

    modified:   workspaces/DummyRosterRC1/DummyRoster.WebApi/Controllers/CredentialController.cs

--- workspaces/DummyRosterRC1/DummyRoster.WebApi/Controllers/CredentialController.cs
+++ workspaces/DummyRosterRC1/DummyRoster.WebApi/Controllers/CredentialController.cs
@@ -24,12 +24,7 @@ public class CredentialController : ControllerBase, ICredentialController
     throw new NotImplementedException();
   }
 
-  public Task<IActionResult> Delete(int id)
-  {
-    throw new NotImplementedException();
-  }
-
-  public Task<IActionResult> PartialUpdate(int id, [FromBody] Credential entity)
+  public Task<IActionResult> ReadCredential(int id)
   {
     throw new NotImplementedException();
   }
@@ -39,12 +34,17 @@ public class CredentialController : ControllerBase, ICredentialController
     throw new NotImplementedException();
   }
 
-  public Task<IActionResult> ReadCredential(int id)
+  public Task<IActionResult> Update(int id, [FromBody] Credential entity)
   {
     throw new NotImplementedException();
   }
 
-  public Task<IActionResult> Update(int id, [FromBody] Credential entity)
+  public Task<IActionResult> PartialUpdate(int id, [FromBody] Credential entity)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<IActionResult> Delete(int id)
   {
     throw new NotImplementedException();
   }
