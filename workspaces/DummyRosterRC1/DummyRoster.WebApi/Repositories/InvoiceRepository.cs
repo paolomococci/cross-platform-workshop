d6afcd45cc468adbe2970ef1414cfb975cb82b53
paolomococci <paolomococci@gmail.com>
2022-12-14 22:26:01 +0100

    modified:   workspaces/DummyRosterRC1/DummyRoster.WebApi/Repositories/InvoiceRepository.cs

--- workspaces/DummyRosterRC1/DummyRoster.WebApi/Repositories/InvoiceRepository.cs
+++ workspaces/DummyRosterRC1/DummyRoster.WebApi/Repositories/InvoiceRepository.cs
@@ -47,14 +47,14 @@ public class InvoiceRepository : IInvoiceRepository
     }
   }
 
-  public Task<Invoice?> RetrieveAsync(int id)
+  public Task<Invoice?> Retrieve(int id)
   {
     if (keyValuesCache is null) return null!;
     keyValuesCache.TryGetValue(id, out Invoice? entity);
     return Task.FromResult(entity);
   }
 
-  public Task<IEnumerable<Invoice>> RetrieveAllAsync()
+  public Task<IEnumerable<Invoice>> RetrieveAll()
   {
     return Task.FromResult(
       keyValuesCache is null ? Enumerable.Empty<Invoice>() : keyValuesCache.Values
