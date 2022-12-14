7a62fd745f9666e48ed2eaab345a6719fab9fb5f
paolomococci <paolomococci@gmail.com>
2022-12-14 17:35:00 +0100

    modified:   workspaces/DummyRosterRC1/DummyRoster.WebApi/Repositories/InvoiceRepository.cs

--- workspaces/DummyRosterRC1/DummyRoster.WebApi/Repositories/InvoiceRepository.cs
+++ workspaces/DummyRosterRC1/DummyRoster.WebApi/Repositories/InvoiceRepository.cs
@@ -25,17 +25,34 @@ public class InvoiceRepository : IInvoiceRepository
     }
   }
 
-  public Task<Invoice?> CreateAsync(Invoice entity)
+  public async Task<Invoice?> CreateAsync(Invoice entity)
+  {
+    EntityEntry<Invoice> entry = await this.dummyRosterContext.Invoices.AddAsync(entity);
+    int changesWereSavedAsynchronously = await this.dummyRosterContext.SaveChangesAsync();
+    if (changesWereSavedAsynchronously == 1)
+    {
+      if (keyValuesCache is null)
+      {
+        return entity;
+      }
+      return keyValuesCache.AddOrUpdate(
+        entity.Id,
+        entity,
+        UpdateCache
+      );
+    }
+    else
+    {
+      return null;
+    }
+  }
+
+  public async Task<bool?> DeleteAsync(int id)
   {
     throw new NotImplementedException();
   }
 
-  public Task<bool?> DeleteAsync(int id)
-  {
-    throw new NotImplementedException();
-  }
-
-  public Task<Invoice?> PartialUpdateAsync(int id, Invoice entity)
+  public async Task<Invoice?> PartialUpdateAsync(int id, Invoice entity)
   {
     throw new NotImplementedException();
   }
@@ -50,7 +67,7 @@ public class InvoiceRepository : IInvoiceRepository
     throw new NotImplementedException();
   }
 
-  public Task<Invoice?> UpdateAsync(int id, Invoice entity)
+  public async Task<Invoice?> UpdateAsync(int id, Invoice entity)
   {
     throw new NotImplementedException();
   }
