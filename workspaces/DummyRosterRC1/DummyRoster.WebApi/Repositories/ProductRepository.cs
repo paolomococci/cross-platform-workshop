553079c8500fba7151cdc579c562790270c63f13
paolomococci <paolomococci@gmail.com>
2022-12-14 14:51:53 +0100

    modified:   workspaces/DummyRosterRC1/DummyRoster.WebApi/Repositories/ProductRepository.cs

--- workspaces/DummyRosterRC1/DummyRoster.WebApi/Repositories/ProductRepository.cs
+++ workspaces/DummyRosterRC1/DummyRoster.WebApi/Repositories/ProductRepository.cs
@@ -24,4 +24,34 @@ public class ProductRepository : IProductRepository
       );
     }
   }
+
+  public Task<Product?> CreateAsync(Product entity)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<bool?> DeleteAsync(int id)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<Product?> PartialUpdateAsync(int id, Product entity)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<IEnumerable<Product>> RetrieveAllAsync()
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<Product?> RetrieveAsync(int id)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<Product?> UpdateAsync(int id, Product entity)
+  {
+    throw new NotImplementedException();
+  }
 }
