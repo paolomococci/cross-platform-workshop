7af64e979106443e7befbf5b52bca0c384061853
paolomococci <paolomococci@gmail.com>
2022-12-14 09:21:12 +0100

    modified:   workspaces/DummyRosterRC1/DummyRoster.WebApi/Repositories/Interfaces/IProductRepository.cs

--- workspaces/DummyRosterRC1/DummyRoster.WebApi/Repositories/Interfaces/IProductRepository.cs
+++ workspaces/DummyRosterRC1/DummyRoster.WebApi/Repositories/Interfaces/IProductRepository.cs
@@ -2,4 +2,18 @@ using DummyRoster.Common.EntityModel.Models;
 
 namespace DummyRoster.WebApi.Repositories.Interfaces;
 
-public interface IProductRepository {}
+public interface IProductRepository
+{
+  Task<Product?> CreateAsync(Product entity);
+  Task<Product?> RetrieveAsync(int id);
+  Task<IEnumerable<Product>> RetrieveAllAsync();
+  Task<Product?> UpdateAsync(
+    int id,
+    Product entity
+  );
+  Task<Product?> PartialUpdateAsync(
+    int id,
+    Product entity
+  );
+  Task<bool?> DeleteAsync(int id);
+}
