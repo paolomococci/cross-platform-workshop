6a8d5bb9015fe1293fc894c587ef4520082d78a6
paolomococci <paolomococci@gmail.com>
2022-12-14 21:53:41 +0100

    modified:   workspaces/DummyRosterRC1/DummyRoster.WebApi/Repositories/AddressRepository.cs

--- workspaces/DummyRosterRC1/DummyRoster.WebApi/Repositories/AddressRepository.cs
+++ workspaces/DummyRosterRC1/DummyRoster.WebApi/Repositories/AddressRepository.cs
@@ -63,7 +63,13 @@ public class AddressRepository : IAddressRepository
 
   public async Task<Address?> UpdateAsync(int id, Address entity)
   {
-    throw new NotImplementedException();
+    this.dummyRosterContext.Addresses.Update(entity);
+    int changesSaved = await this.dummyRosterContext.SaveChangesAsync();
+    if (changesSaved == 1)
+    {
+      return this.UpdateCache(id, entity);
+    }
+    return null;
   }
 
   public async Task<Address?> PartialUpdateAsync(int id, Address entity)
