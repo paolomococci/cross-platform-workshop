e9a77e0f33a7a2bcd169de9864465cb3b18d1920
paolomococci <paolomococci@gmail.com>
2022-12-14 14:51:15 +0100

    modified:   workspaces/DummyRosterRC1/DummyRoster.WebApi/Repositories/CustomerRepository.cs

--- workspaces/DummyRosterRC1/DummyRoster.WebApi/Repositories/CustomerRepository.cs
+++ workspaces/DummyRosterRC1/DummyRoster.WebApi/Repositories/CustomerRepository.cs
@@ -24,4 +24,34 @@ public class CustomerRepository : ICustomerRepository
       );
     }
   }
+
+  public Task<Customer?> CreateAsync(Customer entity)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<bool?> DeleteAsync(int id)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<Customer?> PartialUpdateAsync(int id, Customer entity)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<IEnumerable<Customer>> RetrieveAllAsync()
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<Customer?> RetrieveAsync(int id)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<Customer?> UpdateAsync(int id, Customer entity)
+  {
+    throw new NotImplementedException();
+  }
 }
