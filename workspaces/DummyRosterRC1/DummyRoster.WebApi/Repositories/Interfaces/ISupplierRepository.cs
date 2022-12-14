c18ed98d5d61c3a4de71cfc686859dd94d7ea478
paolomococci <paolomococci@gmail.com>
2022-12-14 09:21:20 +0100

    modified:   workspaces/DummyRosterRC1/DummyRoster.WebApi/Repositories/Interfaces/ISupplierRepository.cs

--- workspaces/DummyRosterRC1/DummyRoster.WebApi/Repositories/Interfaces/ISupplierRepository.cs
+++ workspaces/DummyRosterRC1/DummyRoster.WebApi/Repositories/Interfaces/ISupplierRepository.cs
@@ -2,4 +2,18 @@ using DummyRoster.Common.EntityModel.Models;
 
 namespace DummyRoster.WebApi.Repositories.Interfaces;
 
-public interface ISupplierRepository {}
+public interface ISupplierRepository
+{
+  Task<Supplier?> CreateAsync(Supplier entity);
+  Task<Supplier?> RetrieveAsync(int id);
+  Task<IEnumerable<Supplier>> RetrieveAllAsync();
+  Task<Supplier?> UpdateAsync(
+    int id,
+    Supplier entity
+  );
+  Task<Supplier?> PartialUpdateAsync(
+    int id,
+    Supplier entity
+  );
+  Task<bool?> DeleteAsync(int id);
+}
