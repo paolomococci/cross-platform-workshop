5804eb8c0fc38c43d5d76b9e5890358a93785edd
paolomococci <paolomococci@gmail.com>
2022-12-14 14:23:06 +0100

    modified:   workspaces/DummyRosterRC1/DummyRoster.WebApi/Controllers/ProductController.cs

--- workspaces/DummyRosterRC1/DummyRoster.WebApi/Controllers/ProductController.cs
+++ workspaces/DummyRosterRC1/DummyRoster.WebApi/Controllers/ProductController.cs
@@ -18,4 +18,34 @@ public class ProductController : ControllerBase, IProductController
   {
     this.repository = repo;
   }
+
+  public Task<IActionResult> Create([FromBody] Product entity)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<IActionResult> Delete(int id)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<IActionResult> PartialUpdate(int id, [FromBody] Product entity)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<IEnumerable<Product>> ReadAll(string? name)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<IActionResult> ReadProduct(int id)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<IActionResult> Update(int id, [FromBody] Product entity)
+  {
+    throw new NotImplementedException();
+  }
 }
