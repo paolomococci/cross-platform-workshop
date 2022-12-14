265472d44863c345dfc8722c21637a9eef9a6857
paolomococci <paolomococci@gmail.com>
2022-12-14 22:11:39 +0100

    modified:   workspaces/DummyRosterRC1/DummyRoster.WebApi/Repositories/CustomerRepository.cs

--- workspaces/DummyRosterRC1/DummyRoster.WebApi/Repositories/CustomerRepository.cs
+++ workspaces/DummyRosterRC1/DummyRoster.WebApi/Repositories/CustomerRepository.cs
@@ -74,7 +74,25 @@ public class CustomerRepository : ICustomerRepository
 
   public async Task<Customer?> PartialUpdateAsync(int id, Customer entity)
   {
-    throw new NotImplementedException();
+    if (keyValuesCache is null) return null!;
+    keyValuesCache.TryGetValue(id, out Customer? registered);
+    if (registered != null)
+    {
+      if (entity.Name != null) registered.Name = entity.Name;
+      if (entity.FoundationDate != null) registered.FoundationDate = entity.FoundationDate;
+      if (entity.Description != null) registered.Description = entity.Description;
+      if (entity.Picture != null) registered.Picture = entity.Picture;
+      if (entity.Contact != null) registered.Contact = entity.Contact;
+      if (entity.Loc != null) registered.Loc = entity.Loc;
+      if (entity.Ref != null) registered.Ref = entity.Ref;
+      this.dummyRosterContext.Customers.Update(registered);
+      int changesSaved = await this.dummyRosterContext.SaveChangesAsync();
+      if (changesSaved == 1)
+      {
+        return this.UpdateCache(id, registered);
+      }
+    }
+    return null;
   }
 
   public async Task<bool?> DeleteAsync(int id)
