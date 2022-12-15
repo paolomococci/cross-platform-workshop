5244a545b0068cc673586878b30d00ddc52c6574
paolomococci <paolomococci@gmail.com>
2022-12-15 06:09:50 +0100

    modified:   workspaces/DummyRosterRC1/DummyRoster.WebApi/Controllers/InvoiceController.cs

--- workspaces/DummyRosterRC1/DummyRoster.WebApi/Controllers/InvoiceController.cs
+++ workspaces/DummyRosterRC1/DummyRoster.WebApi/Controllers/InvoiceController.cs
@@ -85,7 +85,17 @@ public class InvoiceController : ControllerBase, IInvoiceController
   [ProducesResponseType(404)]
   public async Task<IActionResult> PartialUpdate(int id, [FromBody] Invoice entity)
   {
-    throw new NotImplementedException();
+    if (entity == null || entity.Id != id)
+    {
+      return BadRequest();
+    }
+    Invoice? managedEntity = await this.repository.Retrieve(id);
+    if (managedEntity == null)
+    {
+      return NotFound();
+    }
+    await this.repository.PartialUpdateAsync(id, entity);
+    return new NoContentResult();
   }
 
   /* 
