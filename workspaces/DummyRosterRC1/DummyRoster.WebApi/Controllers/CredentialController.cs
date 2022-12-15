ff6f967630a5459331cb5edfb67eb95522d22d49
paolomococci <paolomococci@gmail.com>
2022-12-15 05:41:25 +0100

    modified:   workspaces/DummyRosterRC1/DummyRoster.WebApi/Controllers/CredentialController.cs

--- workspaces/DummyRosterRC1/DummyRoster.WebApi/Controllers/CredentialController.cs
+++ workspaces/DummyRosterRC1/DummyRoster.WebApi/Controllers/CredentialController.cs
@@ -88,6 +88,13 @@ public class CredentialController : ControllerBase, ICredentialController
     throw new NotImplementedException();
   }
 
+  /* 
+    DELETE: api/credentials/id
+   */
+  [HttpDelete("{id}")]
+  [ProducesResponseType(204)]
+  [ProducesResponseType(400)]
+  [ProducesResponseType(404)]
   public Task<IActionResult> Delete(int id)
   {
     throw new NotImplementedException();
