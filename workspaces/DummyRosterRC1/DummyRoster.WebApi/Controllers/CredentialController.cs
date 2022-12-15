cd321f5898088265ff4a35dd5b7d3a1e84ce7486
paolomococci <paolomococci@gmail.com>
2022-12-15 06:09:13 +0100

    modified:   workspaces/DummyRosterRC1/DummyRoster.WebApi/Controllers/CredentialController.cs

--- workspaces/DummyRosterRC1/DummyRoster.WebApi/Controllers/CredentialController.cs
+++ workspaces/DummyRosterRC1/DummyRoster.WebApi/Controllers/CredentialController.cs
@@ -85,7 +85,17 @@ public class CredentialController : ControllerBase, ICredentialController
   [ProducesResponseType(404)]
   public async Task<IActionResult> PartialUpdate(int id, [FromBody] Credential entity)
   {
-    throw new NotImplementedException();
+    if (entity == null || entity.Id != id)
+    {
+      return BadRequest();
+    }
+    Credential? managedEntity = await this.repository.Retrieve(id);
+    if (managedEntity == null)
+    {
+      return NotFound();
+    }
+    await this.repository.PartialUpdateAsync(id, entity);
+    return new NoContentResult();
   }
 
   /* 
