7bc0bc6872e49947f5bde6dc7e2186c93c7d3197
paolomococci <paolomococci@gmail.com>
2022-12-15 06:09:04 +0100

    modified:   workspaces/DummyRosterRC1/DummyRoster.WebApi/Controllers/CategoryController.cs

--- workspaces/DummyRosterRC1/DummyRoster.WebApi/Controllers/CategoryController.cs
+++ workspaces/DummyRosterRC1/DummyRoster.WebApi/Controllers/CategoryController.cs
@@ -85,7 +85,17 @@ public class CategoryController : ControllerBase, ICategoryController
   [ProducesResponseType(404)]
   public async Task<IActionResult> PartialUpdate(int id, [FromBody] Category entity)
   {
-    throw new NotImplementedException();
+    if (entity == null || entity.Id != id)
+    {
+      return BadRequest();
+    }
+    Category? managedEntity = await this.repository.Retrieve(id);
+    if (managedEntity == null)
+    {
+      return NotFound();
+    }
+    await this.repository.PartialUpdateAsync(id, entity);
+    return new NoContentResult();
   }
 
   /* 
