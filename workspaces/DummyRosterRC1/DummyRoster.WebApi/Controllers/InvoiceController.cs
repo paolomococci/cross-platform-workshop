a993b4ef5a0f18e2f60f3150586ddfec4e7386e0
paolomococci <paolomococci@gmail.com>
2022-12-15 06:19:25 +0100

    modified:   workspaces/DummyRosterRC1/DummyRoster.WebApi/Controllers/InvoiceController.cs

--- workspaces/DummyRosterRC1/DummyRoster.WebApi/Controllers/InvoiceController.cs
+++ workspaces/DummyRosterRC1/DummyRoster.WebApi/Controllers/InvoiceController.cs
@@ -72,7 +72,17 @@ public class InvoiceController : ControllerBase, IInvoiceController
   [ProducesResponseType(404)]
   public async Task<IActionResult> Update(int id, [FromBody] Invoice entity)
   {
-    throw new NotImplementedException();
+    if (entity == null || entity.Id != id)
+    {
+      return BadRequest();
+    }
+    Invoice? managedEntity = await this.repository.Retrieve(id);
+    if (managedEntity == null)
+    {
+      return NotFound();
+    }
+    await this.repository.UpdateAsync(id, entity);
+    return new NoContentResult();
   }
 
   /* 
