216c81ca160d8bd397db6b9ee829f635f1e94bbd
paolomococci <paolomococci@gmail.com>
2022-12-15 10:44:55 +0100

    modified:   workspaces/DummyRosterRC1/DummyRoster.WebApi/Repositories/CredentialRepository.cs

--- workspaces/DummyRosterRC1/DummyRoster.WebApi/Repositories/CredentialRepository.cs
+++ workspaces/DummyRosterRC1/DummyRoster.WebApi/Repositories/CredentialRepository.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore.ChangeTracking;
 using System.Collections.Concurrent;
+using DummyRoster.Common.EntityModel.Data;
 using DummyRoster.Common.EntityModel.Models;
 using DummyRoster.WebApi.Repositories.Interfaces;
 
