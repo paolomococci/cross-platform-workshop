1d37f84da08fa4bceef95999ad65fcb366e360a8
paolomococci <paolomococci@gmail.com>
2022-12-15 11:02:52 +0100

    modified:   workspaces/DummyRosterRC1/DummyRoster.WebApi/Controllers/AddressController.cs

--- workspaces/DummyRosterRC1/DummyRoster.WebApi/Controllers/AddressController.cs
+++ workspaces/DummyRosterRC1/DummyRoster.WebApi/Controllers/AddressController.cs
@@ -6,7 +6,7 @@ using DummyRoster.WebApi.Repositories.Interfaces;
 namespace DummyRoster.WebApi.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("api/addresses")]
 public class AddressController : ControllerBase, IAddressController
 {
 
