9fd71ba821b8db8e2e82e0048b43f96386a236f5
paolomococci <paolomococci@gmail.com>
2022-12-15 05:49:27 +0100

    modified:   workspaces/DummyRosterRC1/DummyRoster.WebApi/Controllers/CarrierController.cs

--- workspaces/DummyRosterRC1/DummyRoster.WebApi/Controllers/CarrierController.cs
+++ workspaces/DummyRosterRC1/DummyRoster.WebApi/Controllers/CarrierController.cs
@@ -95,8 +95,19 @@ public class CarrierController : ControllerBase, ICarrierController
   [ProducesResponseType(204)]
   [ProducesResponseType(400)]
   [ProducesResponseType(404)]
-  public Task<IActionResult> Delete(int id)
+  public async Task<IActionResult> Delete(int id)
   {
-    throw new NotImplementedException();
+    bool deleted = false;
+    Carrier? managedEntity = await this.repository.Retrieve(id);
+    if (managedEntity == null)
+    {
+      return NotFound();
+    }
+    deleted = (bool)await this.repository.DeleteAsync(id);
+    if (deleted)
+    {
+      return new NoContentResult();
+    }
+    return BadRequest();
   }
 }
