d49f39d0c52b39660a540f9f95d7c5a7c3bbdc66
paolomococci <paolomococci@gmail.com>
2022-12-15 06:18:40 +0100

    modified:   workspaces/DummyRosterRC1/DummyRoster.WebApi/Controllers/CategoryController.cs

--- workspaces/DummyRosterRC1/DummyRoster.WebApi/Controllers/CategoryController.cs
+++ workspaces/DummyRosterRC1/DummyRoster.WebApi/Controllers/CategoryController.cs
@@ -72,7 +72,17 @@ public class CategoryController : ControllerBase, ICategoryController
   [ProducesResponseType(404)]
   public async Task<IActionResult> Update(int id, [FromBody] Category entity)
   {
-    throw new NotImplementedException();
+    if (entity == null || entity.Id != id)
+    {
+      return BadRequest();
+    }
+    Category? managedEntity = await this.repository.Retrieve(id);
+    if (managedEntity == null)
+    {
+      return NotFound();
+    }
+    await this.repository.UpdateAsync(id, entity);
+    return new NoContentResult();
   }
 
   /* 
