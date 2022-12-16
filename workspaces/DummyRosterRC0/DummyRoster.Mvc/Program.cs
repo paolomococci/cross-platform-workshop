75dfd41a83c7ffab41ae7544f1c637963516f54f
paolomococci <paolomococci@gmail.com>
2022-12-16 06:08:44 +0100

    modified:   workspaces/DummyRosterRC0/DummyRoster.Mvc/Program.cs

--- workspaces/DummyRosterRC0/DummyRoster.Mvc/Program.cs
+++ workspaces/DummyRosterRC0/DummyRoster.Mvc/Program.cs
@@ -15,7 +15,7 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(
   options => options.SignIn.RequireConfirmedAccount = true
-).AddEntityFrameworkStores<ApplicationDbContext>();
+).AddRoles<IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
 
 builder.Services.AddControllersWithViews();
 
