3124fc83142cfc698e44ad7c7ec7348042b22eb8
paolomococci <paolomococci@gmail.com>
2022-12-16 06:22:42 +0100

    new file:   workspaces/DummyRosterRC0/DummyRoster.Mvc/Controllers/RolesController.cs

---
+++ workspaces/DummyRosterRC0/DummyRoster.Mvc/Controllers/RolesController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DummyRoster.Mvc.Controllers;
+
+public class RolesController : Controller
+{
+  private string RoleAdmin = "Admin";
+  private string EmailAdmin = "";
+}
