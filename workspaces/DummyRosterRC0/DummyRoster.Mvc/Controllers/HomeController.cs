cf7af192f606c60968a3393b280fa9e00a1b43fd
paolomococci <paolomococci@gmail.com>
2022-12-17 16:56:18 +0100

    modified:   workspaces/DummyRosterRC0/DummyRoster.Mvc/Controllers/HomeController.cs

--- workspaces/DummyRosterRC0/DummyRoster.Mvc/Controllers/HomeController.cs
+++ workspaces/DummyRosterRC0/DummyRoster.Mvc/Controllers/HomeController.cs
@@ -67,6 +67,11 @@ public class HomeController : Controller
     return null;
   }
 
+  public Task<IActionResult>? Products()
+  {
+    return null;
+  }
+
   [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
   public IActionResult Error()
   {
