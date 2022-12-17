11326a15fa95328a8c69289b4f5e01c67738cced
paolomococci <paolomococci@gmail.com>
2022-12-17 16:49:51 +0100

    modified:   workspaces/DummyRosterRC0/DummyRoster.Mvc/Controllers/HomeController.cs

--- workspaces/DummyRosterRC0/DummyRoster.Mvc/Controllers/HomeController.cs
+++ workspaces/DummyRosterRC0/DummyRoster.Mvc/Controllers/HomeController.cs
@@ -32,10 +32,16 @@ public class HomeController : Controller
     return View();
   }
 
+  public Task<IActionResult>? Employees()
+  {
+    return null;
+  }
+
   [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
   public IActionResult Error()
   {
-    return View(new ErrorViewModel {
+    return View(new ErrorViewModel
+    {
       RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
     });
   }
