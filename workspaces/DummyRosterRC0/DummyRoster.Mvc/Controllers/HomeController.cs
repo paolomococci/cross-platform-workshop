e0900bbd9da7afce964ec94ffdb6bff02bddd5d6
paolomococci <paolomococci@gmail.com>
2022-12-18 07:59:11 +0100

    modified:   workspaces/DummyRosterRC0/DummyRoster.Mvc/Controllers/HomeController.cs

--- workspaces/DummyRosterRC0/DummyRoster.Mvc/Controllers/HomeController.cs
+++ workspaces/DummyRosterRC0/DummyRoster.Mvc/Controllers/HomeController.cs
@@ -89,7 +89,7 @@ public class HomeController : Controller
     return View(customers);
   }
 
-  public async Task<IActionResult>? Suppliers(string? name)
+  public async Task<IActionResult> Suppliers(string? name)
   {
     string apiUri = "";
     if (string.IsNullOrEmpty(name))
