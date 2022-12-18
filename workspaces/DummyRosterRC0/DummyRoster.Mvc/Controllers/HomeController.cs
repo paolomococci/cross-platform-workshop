09cf756e9eb359eb5222c777dbdbe9c7747f9c1e
paolomococci <paolomococci@gmail.com>
2022-12-18 08:17:27 +0100

    modified:   workspaces/DummyRosterRC0/DummyRoster.Mvc/Controllers/HomeController.cs

--- workspaces/DummyRosterRC0/DummyRoster.Mvc/Controllers/HomeController.cs
+++ workspaces/DummyRosterRC0/DummyRoster.Mvc/Controllers/HomeController.cs
@@ -206,7 +206,7 @@ public class HomeController : Controller
     return View(categories);
   }
 
-  public Task<IActionResult>? Products(string? name)
+  public async Task<IActionResult>? Products(string? name)
   {
     return null;
   }
