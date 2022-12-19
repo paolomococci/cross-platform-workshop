edb18f9de7f830c4fcb4fe77b374f43bfb4b4d73
paolomococci <paolomococci@gmail.com>
2022-12-19 21:54:35 +0100

    modified:   workspaces/DummyRosterRC2/DummyRoster.BlazorServer/Data/WeatherForecastService.cs

--- workspaces/DummyRosterRC2/DummyRoster.BlazorServer/Data/WeatherForecastService.cs
+++ workspaces/DummyRosterRC2/DummyRoster.BlazorServer/Data/WeatherForecastService.cs
@@ -2,18 +2,27 @@ namespace DummyRoster.BlazorServer.Data;
 
 public class WeatherForecastService
 {
-    private static readonly string[] Summaries = new[]
-    {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+  private static readonly string[] Summaries = new[]
+  {
+        "Freezing", 
+        "Bracing", 
+        "Chilly", 
+        "Cool", 
+        "Mild", 
+        "Warm", 
+        "Balmy", 
+        "Hot", 
+        "Sweltering", 
+        "Scorching"
     };
 
-    public Task<WeatherForecast[]> GetForecastAsync(DateOnly startDate)
+  public Task<WeatherForecast[]> GetForecastAsync(DateOnly startDate)
+  {
+    return Task.FromResult(Enumerable.Range(1, 5).Select(index => new WeatherForecast
     {
-        return Task.FromResult(Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        {
-            Date = startDate.AddDays(index),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        }).ToArray());
-    }
+      Date = startDate.AddDays(index),
+      TemperatureC = Random.Shared.Next(-20, 55),
+      Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+    }).ToArray());
+  }
 }
