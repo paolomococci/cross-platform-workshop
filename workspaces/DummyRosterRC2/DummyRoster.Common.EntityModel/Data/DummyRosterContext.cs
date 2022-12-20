dbbdd0f925c05a6b76766d04e051f2ba64adc913
paolomococci <paolomococci@gmail.com>
2022-12-20 06:34:41 +0100

    new file:   workspaces/DummyRosterRC2/DummyRoster.Common.EntityModel/Data/DummyRosterContext.cs

---
+++ workspaces/DummyRosterRC2/DummyRoster.Common.EntityModel/Data/DummyRosterContext.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace DummyRoster.Common.EntityModel.Models;
+
+public partial class DummyRosterContext : DbContext
+{
+    public DummyRosterContext()
+    {
+    }
+
+    public DummyRosterContext(DbContextOptions<DummyRosterContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Address> Addresses { get; set; }
+
+    public virtual DbSet<Carrier> Carriers { get; set; }
+
+    public virtual DbSet<Category> Categories { get; set; }
+
+    public virtual DbSet<Credential> Credentials { get; set; }
+
+    public virtual DbSet<Customer> Customers { get; set; }
+
+    public virtual DbSet<Employee> Employees { get; set; }
+
+    public virtual DbSet<Form> Forms { get; set; }
+
+    public virtual DbSet<Invoice> Invoices { get; set; }
+
+    public virtual DbSet<Product> Products { get; set; }
+
+    public virtual DbSet<Supplier> Suppliers { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlite("Filename=../DummyRoster.db");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Form>(entity =>
+        {
+            entity.Property(e => e.ShippingCost).HasDefaultValueSql("0");
+        });
+
+        modelBuilder.Entity<Invoice>(entity =>
+        {
+            entity.Property(e => e.PriceCut).HasDefaultValueSql("0");
+            entity.Property(e => e.Quantity).HasDefaultValueSql("1");
+            entity.Property(e => e.UnitPrice).HasDefaultValueSql("0");
+        });
+
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.Property(e => e.Discontinued).HasDefaultValueSql("0");
+            entity.Property(e => e.ReorderLevel).HasDefaultValueSql("0");
+            entity.Property(e => e.UnitPrice).HasDefaultValueSql("0");
+            entity.Property(e => e.UnitsInStock).HasDefaultValueSql("0");
+            entity.Property(e => e.UnitsOnOrder).HasDefaultValueSql("0");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
