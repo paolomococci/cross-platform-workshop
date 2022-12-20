b4b3261c0b02aa3ffd58a38d5459dcbc712e7d01
paolomococci <paolomococci@gmail.com>
2022-12-20 06:35:03 +0100

    new file:   workspaces/DummyRosterRC2/DummyRoster.Common.EntityModel/Models/Carrier.cs

---
+++ workspaces/DummyRosterRC2/DummyRoster.Common.EntityModel/Models/Carrier.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace DummyRoster.Common.EntityModel.Models;
+
+[Table("Carrier")]
+[Index("Name", Name = "IDX_CarrierName")]
+public partial class Carrier
+{
+    [Key]
+    public long Id { get; set; }
+
+    [Column(TypeName = "varchar(32)")]
+    public string Name { get; set; } = null!;
+
+    [Column(TypeName = "DATETIME")]
+    public byte[]? FoundationDate { get; set; }
+
+    public string? Description { get; set; }
+
+    public byte[]? Picture { get; set; }
+
+    [Column(TypeName = "varchar(8)")]
+    public string? Belonging { get; set; }
+
+    [Column(TypeName = "INT")]
+    public long? Contact { get; set; }
+
+    [Column(TypeName = "INT")]
+    public long? Loc { get; set; }
+
+    [Column(TypeName = "INT")]
+    public long? Ref { get; set; }
+
+    [ForeignKey("Contact")]
+    [InverseProperty("Carriers")]
+    public virtual Employee? ContactNavigation { get; set; }
+
+    [InverseProperty("Carrier")]
+    public virtual ICollection<Form> Forms { get; } = new List<Form>();
+
+    [ForeignKey("Loc")]
+    [InverseProperty("Carriers")]
+    public virtual Address? LocNavigation { get; set; }
+
+    [ForeignKey("Ref")]
+    [InverseProperty("Carriers")]
+    public virtual Credential? RefNavigation { get; set; }
+}
