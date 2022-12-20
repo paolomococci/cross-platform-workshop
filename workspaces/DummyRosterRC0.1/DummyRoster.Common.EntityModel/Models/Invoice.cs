03f9f911c2b638d93a2918a29ab8a5bb9e6bd9df
paolomococci <paolomococci@gmail.com>
2022-12-20 06:20:29 +0100

    modified:   workspaces/DummyRosterRC0.1/DummyRoster.Common.EntityModel/Models/Invoice.cs

--- workspaces/DummyRosterRC0.1/DummyRoster.Common.EntityModel/Models/Invoice.cs
+++ workspaces/DummyRosterRC0.1/DummyRoster.Common.EntityModel/Models/Invoice.cs
@@ -11,31 +11,31 @@ namespace DummyRoster.Common.EntityModel.Models;
 [Index("ProductId", Name = "IDX_ProductId")]
 public partial class Invoice
 {
-    [Key]
-    public long Id { get; set; }
+  [Key]
+  public long Id { get; set; }
 
-    [Column(TypeName = "INT")]
-    public long? FormId { get; set; }
+  [Column(TypeName = "INT")]
+  public long? FormId { get; set; }
 
-    [Column(TypeName = "INT")]
-    public long? ProductId { get; set; }
+  [Column(TypeName = "INT")]
+  public long? ProductId { get; set; }
 
-    public string? Note { get; set; }
+  public string? Note { get; set; }
 
-    [Column(TypeName = "NUMERIC")]
-    public byte[] UnitPrice { get; set; } = null!;
+  [Column(TypeName = "NUMERIC")]
+  public byte[] UnitPrice { get; set; } = null!;
 
-    [Column(TypeName = "SMALLINT")]
-    public long Quantity { get; set; }
+  [Column(TypeName = "SMALLINT")]
+  public long Quantity { get; set; }
 
-    [Column(TypeName = "NUMERIC")]
-    public byte[] PriceCut { get; set; } = null!;
+  [Column(TypeName = "NUMERIC")]
+  public byte[] PriceCut { get; set; } = null!;
 
-    [ForeignKey("FormId")]
-    [InverseProperty("Invoices")]
-    public virtual Form? Form { get; set; }
+  [ForeignKey("FormId")]
+  [InverseProperty("Invoices")]
+  public virtual Form? Form { get; set; }
 
-    [ForeignKey("ProductId")]
-    [InverseProperty("Invoices")]
-    public virtual Product? Product { get; set; }
+  [ForeignKey("ProductId")]
+  [InverseProperty("Invoices")]
+  public virtual Product? Product { get; set; }
 }
