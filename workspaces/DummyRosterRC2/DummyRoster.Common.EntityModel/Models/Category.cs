89d0f0579ae2911a98e9fd42d2a1880c7da4a9b0
paolomococci <paolomococci@gmail.com>
2022-12-20 06:35:12 +0100

    new file:   workspaces/DummyRosterRC2/DummyRoster.Common.EntityModel/Models/Category.cs

---
+++ workspaces/DummyRosterRC2/DummyRoster.Common.EntityModel/Models/Category.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace DummyRoster.Common.EntityModel.Models;
+
+[Table("Category")]
+[Index("Name", Name = "IDX_CategoryName")]
+public partial class Category
+{
+    [Key]
+    public long Id { get; set; }
+
+    [Column(TypeName = "varchar(32)")]
+    public string Name { get; set; } = null!;
+
+    public string? Description { get; set; }
+
+    [Column(TypeName = "varchar(8)")]
+    public string? Belonging { get; set; }
+
+    public byte[]? Picture { get; set; }
+
+    [InverseProperty("Category")]
+    public virtual ICollection<Product> Products { get; } = new List<Product>();
+}
