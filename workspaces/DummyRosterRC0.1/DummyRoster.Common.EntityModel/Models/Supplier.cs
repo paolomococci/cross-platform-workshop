720580f1e419a7fc6f0cb1a4bd603acb09be7506
paolomococci <paolomococci@gmail.com>
2022-12-20 06:15:17 +0100

    new file:   workspaces/DummyRosterRC0.1/DummyRoster.Common.EntityModel/Models/Supplier.cs

---
+++ workspaces/DummyRosterRC0.1/DummyRoster.Common.EntityModel/Models/Supplier.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace DummyRoster.Common.EntityModel.Models;
+
+[Table("Supplier")]
+[Index("Name", Name = "IDX_SupplierName")]
+public partial class Supplier
+{
+    [Key]
+    public long Id { get; set; }
+
+    [Column(TypeName = "varchar(32)")]
+    public string Name { get; set; } = null!;
+
+    [Column(TypeName = "DATETIME")]
+    public byte[]? FoundationDate { get; set; }
+
+    public string? Description { get; set; }
+
+    public byte[]? Picture { get; set; }
+
+    [Column(TypeName = "varchar(8)")]
+    public string? Belonging { get; set; }
+
+    [Column(TypeName = "INT")]
+    public long? Contact { get; set; }
+
+    [Column(TypeName = "INT")]
+    public long? Loc { get; set; }
+
+    [Column(TypeName = "INT")]
+    public long? Ref { get; set; }
+
+    [ForeignKey("Contact")]
+    [InverseProperty("Suppliers")]
+    public virtual Employee? ContactNavigation { get; set; }
+
+    [ForeignKey("Loc")]
+    [InverseProperty("Suppliers")]
+    public virtual Address? LocNavigation { get; set; }
+
+    [InverseProperty("Supplier")]
+    public virtual ICollection<Product> Products { get; } = new List<Product>();
+
+    [ForeignKey("Ref")]
+    [InverseProperty("Suppliers")]
+    public virtual Credential? RefNavigation { get; set; }
+}
