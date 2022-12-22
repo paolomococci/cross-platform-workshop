522e9cb626ae3fae26712fba82be6366b1236172
paolomococci <paolomococci@gmail.com>
2022-12-22 15:54:51 +0100

    modified:   workspaces/DummyRosterRC2/DummyRoster.Common.EntityModel/Models/Product.cs

--- workspaces/DummyRosterRC2/DummyRoster.Common.EntityModel/Models/Product.cs
+++ workspaces/DummyRosterRC2/DummyRoster.Common.EntityModel/Models/Product.cs
@@ -12,51 +12,51 @@ namespace DummyRoster.Common.EntityModel.Models;
 [Index("SupplierId", Name = "IDX_SupplierId")]
 public partial class Product
 {
-    [Key]
-    public int Id { get; set; }
+  [Key]
+  public int Id { get; set; }
 
-    [Column(TypeName = "varchar(32)")]
-    public string Name { get; set; } = null!;
+  [Column(TypeName = "varchar(32)")]
+  public string Name { get; set; } = null!;
 
-    public string? Description { get; set; }
+  public string? Description { get; set; }
 
-    [Column(TypeName = "varchar(8)")]
-    public string? Belonging { get; set; }
+  [Column(TypeName = "varchar(8)")]
+  public string? Belonging { get; set; }
 
-    public byte[]? Picture { get; set; }
+  public byte[]? Picture { get; set; }
 
-    [Column(TypeName = "INT")]
-    public int? CategoryId { get; set; }
+  [Column(TypeName = "INT")]
+  public int? CategoryId { get; set; }
 
-    [Column(TypeName = "INT")]
-    public int? SupplierId { get; set; }
+  [Column(TypeName = "INT")]
+  public int? SupplierId { get; set; }
 
-    [Column(TypeName = "varchar(24)")]
-    public string? QuantityPerUnit { get; set; }
+  [Column(TypeName = "varchar(24)")]
+  public string? QuantityPerUnit { get; set; }
 
-    [Column(TypeName = "NUMERIC")]
-    public decimal? UnitPrice { get; set; }
+  [Column(TypeName = "NUMERIC")]
+  public decimal? UnitPrice { get; set; }
 
-    [Column(TypeName = "SMALLINT")]
-    public int? UnitsInStock { get; set; }
+  [Column(TypeName = "SMALLINT")]
+  public int? UnitsInStock { get; set; }
 
-    [Column(TypeName = "SMALLINT")]
-    public int? UnitsOnOrder { get; set; }
+  [Column(TypeName = "SMALLINT")]
+  public int? UnitsOnOrder { get; set; }
 
-    [Column(TypeName = "SMALLINT")]
-    public int? ReorderLevel { get; set; }
+  [Column(TypeName = "SMALLINT")]
+  public int? ReorderLevel { get; set; }
 
-    [Column(TypeName = "BOOLEAN")]
-    public bool Discontinued { get; set; }
+  [Column(TypeName = "BOOLEAN")]
+  public bool Discontinued { get; set; }
 
-    [ForeignKey("CategoryId")]
-    [InverseProperty("Products")]
-    public virtual Category? Category { get; set; }
+  [ForeignKey("CategoryId")]
+  [InverseProperty("Products")]
+  public virtual Category? Category { get; set; }
 
-    [InverseProperty("Product")]
-    public virtual ICollection<Invoice> Invoices { get; } = new List<Invoice>();
+  [InverseProperty("Product")]
+  public virtual ICollection<Invoice> Invoices { get; } = new List<Invoice>();
 
-    [ForeignKey("SupplierId")]
-    [InverseProperty("Products")]
-    public virtual Supplier? Supplier { get; set; }
+  [ForeignKey("SupplierId")]
+  [InverseProperty("Products")]
+  public virtual Supplier? Supplier { get; set; }
 }
