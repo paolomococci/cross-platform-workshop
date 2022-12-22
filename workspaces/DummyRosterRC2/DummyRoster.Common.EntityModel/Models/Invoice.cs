873f3312cc30361d084c26e0afa065e1983c9e7f
paolomococci <paolomococci@gmail.com>
2022-12-22 15:54:41 +0100

    modified:   workspaces/DummyRosterRC2/DummyRoster.Common.EntityModel/Models/Invoice.cs

--- workspaces/DummyRosterRC2/DummyRoster.Common.EntityModel/Models/Invoice.cs
+++ workspaces/DummyRosterRC2/DummyRoster.Common.EntityModel/Models/Invoice.cs
@@ -9,34 +9,34 @@ namespace DummyRoster.Common.EntityModel.Models;
 [Index("ProductId", Name = "IDX_ProductId")]
 public partial class Invoice
 {
-    [Key]
-    public int Id { get; set; }
+  [Key]
+  public int Id { get; set; }
 
-    [Column(TypeName = "INT")]
-    public int? FormId { get; set; }
+  [Column(TypeName = "INT")]
+  public int? FormId { get; set; }
 
-    [Column(TypeName = "INT")]
-    public int? ProductId { get; set; }
+  [Column(TypeName = "INT")]
+  public int? ProductId { get; set; }
 
-    [Column(TypeName = "DATETIME")]
-    public DateTime? IssuingDate { get; set; }
+  [Column(TypeName = "DATETIME")]
+  public DateTime? IssuingDate { get; set; }
 
-    public string? Note { get; set; }
+  public string? Note { get; set; }
 
-    [Column(TypeName = "NUMERIC")]
-    public decimal UnitPrice { get; set; }
+  [Column(TypeName = "NUMERIC")]
+  public decimal UnitPrice { get; set; }
 
-    [Column(TypeName = "SMALLINT")]
-    public int Quantity { get; set; }
+  [Column(TypeName = "SMALLINT")]
+  public int Quantity { get; set; }
 
-    [Column(TypeName = "NUMERIC")]
-    public decimal PriceCut { get; set; }
+  [Column(TypeName = "NUMERIC")]
+  public decimal PriceCut { get; set; }
 
-    [ForeignKey("FormId")]
-    [InverseProperty("Invoices")]
-    public virtual Form? Form { get; set; }
+  [ForeignKey("FormId")]
+  [InverseProperty("Invoices")]
+  public virtual Form? Form { get; set; }
 
-    [ForeignKey("ProductId")]
-    [InverseProperty("Invoices")]
-    public virtual Product? Product { get; set; }
+  [ForeignKey("ProductId")]
+  [InverseProperty("Invoices")]
+  public virtual Product? Product { get; set; }
 }
