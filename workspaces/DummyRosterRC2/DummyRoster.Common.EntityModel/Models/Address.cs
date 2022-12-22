ad1de4046f8a7fffca67733cec70b4672966acea
paolomococci <paolomococci@gmail.com>
2022-12-22 15:53:30 +0100

    modified:   workspaces/DummyRosterRC2/DummyRoster.Common.EntityModel/Models/Address.cs

--- workspaces/DummyRosterRC2/DummyRoster.Common.EntityModel/Models/Address.cs
+++ workspaces/DummyRosterRC2/DummyRoster.Common.EntityModel/Models/Address.cs
@@ -12,36 +12,37 @@ namespace DummyRoster.Common.EntityModel.Models;
 [Index("Postcode", Name = "IDX_AddressPostcode")]
 public partial class Address
 {
-    [Key]
-    public int Id { get; set; }
+  [Key]
+  public int Id { get; set; }
 
-    [Column(TypeName = "varchar(32)")]
-    public string? Name { get; set; }
+  [Column(TypeName = "varchar(32)")]
+  [RegularExpression(@"^([A-Z]{1,}[a-zA-Z0-9\s\.\,]{1,31})$")]
+  public string? Name { get; set; }
 
-    [Column(TypeName = "varchar(16)")]
-    public string? Civic { get; set; }
+  [Column(TypeName = "varchar(16)")]
+  public string? Civic { get; set; }
 
-    [Column(TypeName = "varchar(16)")]
-    public string? City { get; set; }
+  [Column(TypeName = "varchar(16)")]
+  public string? City { get; set; }
 
-    [Column(TypeName = "varchar(16)")]
-    public string? District { get; set; }
+  [Column(TypeName = "varchar(16)")]
+  public string? District { get; set; }
 
-    [Column(TypeName = "varchar(8)")]
-    public string? Postcode { get; set; }
+  [Column(TypeName = "varchar(8)")]
+  public string? Postcode { get; set; }
 
-    [Column(TypeName = "varchar(16)")]
-    public string? Country { get; set; }
+  [Column(TypeName = "varchar(16)")]
+  public string? Country { get; set; }
 
-    [InverseProperty("LocNavigation")]
-    public virtual ICollection<Carrier> Carriers { get; } = new List<Carrier>();
+  [InverseProperty("LocNavigation")]
+  public virtual ICollection<Carrier> Carriers { get; } = new List<Carrier>();
 
-    [InverseProperty("LocNavigation")]
-    public virtual ICollection<Customer> Customers { get; } = new List<Customer>();
+  [InverseProperty("LocNavigation")]
+  public virtual ICollection<Customer> Customers { get; } = new List<Customer>();
 
-    [InverseProperty("LocNavigation")]
-    public virtual ICollection<Employee> Employees { get; } = new List<Employee>();
+  [InverseProperty("LocNavigation")]
+  public virtual ICollection<Employee> Employees { get; } = new List<Employee>();
 
-    [InverseProperty("LocNavigation")]
-    public virtual ICollection<Supplier> Suppliers { get; } = new List<Supplier>();
+  [InverseProperty("LocNavigation")]
+  public virtual ICollection<Supplier> Suppliers { get; } = new List<Supplier>();
 }
