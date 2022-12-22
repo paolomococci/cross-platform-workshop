98b730aa7097e36e03a1b40da135406b9242a9d2
paolomococci <paolomococci@gmail.com>
2022-12-22 15:55:01 +0100

    modified:   workspaces/DummyRosterRC2/DummyRoster.Common.EntityModel/Models/Supplier.cs

--- workspaces/DummyRosterRC2/DummyRoster.Common.EntityModel/Models/Supplier.cs
+++ workspaces/DummyRosterRC2/DummyRoster.Common.EntityModel/Models/Supplier.cs
@@ -8,43 +8,43 @@ namespace DummyRoster.Common.EntityModel.Models;
 [Index("Name", Name = "IDX_SupplierName")]
 public partial class Supplier
 {
-    [Key]
-    public int Id { get; set; }
+  [Key]
+  public int Id { get; set; }
 
-    [Column(TypeName = "varchar(32)")]
-    public string Name { get; set; } = null!;
+  [Column(TypeName = "varchar(32)")]
+  public string Name { get; set; } = null!;
 
-    [Column(TypeName = "DATETIME")]
-    public DateTime? FoundationDate { get; set; }
+  [Column(TypeName = "DATETIME")]
+  public DateTime? FoundationDate { get; set; }
 
-    public string? Description { get; set; }
+  public string? Description { get; set; }
 
-    public byte[]? Picture { get; set; }
+  public byte[]? Picture { get; set; }
 
-    [Column(TypeName = "varchar(8)")]
-    public string? Belonging { get; set; }
+  [Column(TypeName = "varchar(8)")]
+  public string? Belonging { get; set; }
 
-    [Column(TypeName = "INT")]
-    public int? Contact { get; set; }
+  [Column(TypeName = "INT")]
+  public int? Contact { get; set; }
 
-    [Column(TypeName = "INT")]
-    public int? Loc { get; set; }
+  [Column(TypeName = "INT")]
+  public int? Loc { get; set; }
 
-    [Column(TypeName = "INT")]
-    public int? Ref { get; set; }
+  [Column(TypeName = "INT")]
+  public int? Ref { get; set; }
 
-    [ForeignKey("Contact")]
-    [InverseProperty("Suppliers")]
-    public virtual Employee? ContactNavigation { get; set; }
+  [ForeignKey("Contact")]
+  [InverseProperty("Suppliers")]
+  public virtual Employee? ContactNavigation { get; set; }
 
-    [ForeignKey("Loc")]
-    [InverseProperty("Suppliers")]
-    public virtual Address? LocNavigation { get; set; }
+  [ForeignKey("Loc")]
+  [InverseProperty("Suppliers")]
+  public virtual Address? LocNavigation { get; set; }
 
-    [InverseProperty("Supplier")]
-    public virtual ICollection<Product> Products { get; } = new List<Product>();
+  [InverseProperty("Supplier")]
+  public virtual ICollection<Product> Products { get; } = new List<Product>();
 
-    [ForeignKey("Ref")]
-    [InverseProperty("Suppliers")]
-    public virtual Credential? RefNavigation { get; set; }
+  [ForeignKey("Ref")]
+  [InverseProperty("Suppliers")]
+  public virtual Credential? RefNavigation { get; set; }
 }
