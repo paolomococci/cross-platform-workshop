1c546c61397024105ae24137f2adbd3dec5cb5bf
paolomococci <paolomococci@gmail.com>
2022-12-22 15:54:20 +0100

    modified:   workspaces/DummyRosterRC2/DummyRoster.Common.EntityModel/Models/Employee.cs

--- workspaces/DummyRosterRC2/DummyRoster.Common.EntityModel/Models/Employee.cs
+++ workspaces/DummyRosterRC2/DummyRoster.Common.EntityModel/Models/Employee.cs
@@ -8,57 +8,57 @@ namespace DummyRoster.Common.EntityModel.Models;
 [Index("Name", Name = "IDX_EmployeeName")]
 public partial class Employee
 {
-    [Key]
-    public int Id { get; set; }
+  [Key]
+  public int Id { get; set; }
 
-    [Column(TypeName = "varchar(16)")]
-    public string Name { get; set; } = null!;
+  [Column(TypeName = "varchar(16)")]
+  public string Name { get; set; } = null!;
 
-    [Column(TypeName = "varchar(16)")]
-    public string Surname { get; set; } = null!;
+  [Column(TypeName = "varchar(16)")]
+  public string Surname { get; set; } = null!;
 
-    [Column(TypeName = "varchar(16)")]
-    public string? Nickname { get; set; }
+  [Column(TypeName = "varchar(16)")]
+  public string? Nickname { get; set; }
 
-    [Column(TypeName = "varchar(32)")]
-    public string? Username { get; set; }
+  [Column(TypeName = "varchar(32)")]
+  public string? Username { get; set; }
 
-    [Column(TypeName = "DATETIME")]
-    public DateTime? BirthDate { get; set; }
+  [Column(TypeName = "DATETIME")]
+  public DateTime? BirthDate { get; set; }
 
-    public string? Description { get; set; }
+  public string? Description { get; set; }
 
-    public byte[]? Picture { get; set; }
+  public byte[]? Picture { get; set; }
 
-    [Column(TypeName = "varchar(32)")]
-    public string? Role { get; set; }
+  [Column(TypeName = "varchar(32)")]
+  public string? Role { get; set; }
 
-    [Column(TypeName = "varchar(8)")]
-    public string? Belonging { get; set; }
+  [Column(TypeName = "varchar(8)")]
+  public string? Belonging { get; set; }
 
-    [Column(TypeName = "INT")]
-    public int? Loc { get; set; }
+  [Column(TypeName = "INT")]
+  public int? Loc { get; set; }
 
-    [Column(TypeName = "INT")]
-    public int? Ref { get; set; }
+  [Column(TypeName = "INT")]
+  public int? Ref { get; set; }
 
-    [InverseProperty("ContactNavigation")]
-    public virtual ICollection<Carrier> Carriers { get; } = new List<Carrier>();
+  [InverseProperty("ContactNavigation")]
+  public virtual ICollection<Carrier> Carriers { get; } = new List<Carrier>();
 
-    [InverseProperty("ContactNavigation")]
-    public virtual ICollection<Customer> Customers { get; } = new List<Customer>();
+  [InverseProperty("ContactNavigation")]
+  public virtual ICollection<Customer> Customers { get; } = new List<Customer>();
 
-    [InverseProperty("Employee")]
-    public virtual ICollection<Form> Forms { get; } = new List<Form>();
+  [InverseProperty("Employee")]
+  public virtual ICollection<Form> Forms { get; } = new List<Form>();
 
-    [ForeignKey("Loc")]
-    [InverseProperty("Employees")]
-    public virtual Address? LocNavigation { get; set; }
+  [ForeignKey("Loc")]
+  [InverseProperty("Employees")]
+  public virtual Address? LocNavigation { get; set; }
 
-    [ForeignKey("Ref")]
-    [InverseProperty("Employees")]
-    public virtual Credential? RefNavigation { get; set; }
+  [ForeignKey("Ref")]
+  [InverseProperty("Employees")]
+  public virtual Credential? RefNavigation { get; set; }
 
-    [InverseProperty("ContactNavigation")]
-    public virtual ICollection<Supplier> Suppliers { get; } = new List<Supplier>();
+  [InverseProperty("ContactNavigation")]
+  public virtual ICollection<Supplier> Suppliers { get; } = new List<Supplier>();
 }
