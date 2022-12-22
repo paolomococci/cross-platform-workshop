f0701afa3f5768fabe176e6247dd529705cf1cae
paolomococci <paolomococci@gmail.com>
2022-12-22 15:53:41 +0100

    modified:   workspaces/DummyRosterRC2/DummyRoster.Common.EntityModel/Models/Carrier.cs

--- workspaces/DummyRosterRC2/DummyRoster.Common.EntityModel/Models/Carrier.cs
+++ workspaces/DummyRosterRC2/DummyRoster.Common.EntityModel/Models/Carrier.cs
@@ -8,43 +8,43 @@ namespace DummyRoster.Common.EntityModel.Models;
 [Index("Name", Name = "IDX_CarrierName")]
 public partial class Carrier
 {
-    [Key]
-    public int Id { get; set; }
+  [Key]
+  public int Id { get; set; }
 
-    [Column(TypeName = "varchar(32)")]
-    public string Name { get; set; } = null!;
+  [Column(TypeName = "varchar(32)")]
+  public string Name { get; set; } = null!;
 
-    [Column(TypeName = "DATETIME")]
-    public DateTime? FoundationDate { get; set; }
+  [Column(TypeName = "DATETIME")]
+  public DateTime? FoundationDate { get; set; }
 
-    public string? Description { get; set; }
+  public string? Description { get; set; }
 
-    public byte[]? Picture { get; set; }
+  public byte[]? Picture { get; set; }
 
-    [Column(TypeName = "varchar(8)")]
-    public string? Belonging { get; set; }
+  [Column(TypeName = "varchar(8)")]
+  public string? Belonging { get; set; }
 
-    [Column(TypeName = "INT")]
-    public int? Contact { get; set; }
+  [Column(TypeName = "INT")]
+  public int? Contact { get; set; }
 
-    [Column(TypeName = "INT")]
-    public int? Loc { get; set; }
+  [Column(TypeName = "INT")]
+  public int? Loc { get; set; }
 
-    [Column(TypeName = "INT")]
-    public int? Ref { get; set; }
+  [Column(TypeName = "INT")]
+  public int? Ref { get; set; }
 
-    [ForeignKey("Contact")]
-    [InverseProperty("Carriers")]
-    public virtual Employee? ContactNavigation { get; set; }
+  [ForeignKey("Contact")]
+  [InverseProperty("Carriers")]
+  public virtual Employee? ContactNavigation { get; set; }
 
-    [InverseProperty("Carrier")]
-    public virtual ICollection<Form> Forms { get; } = new List<Form>();
+  [InverseProperty("Carrier")]
+  public virtual ICollection<Form> Forms { get; } = new List<Form>();
 
-    [ForeignKey("Loc")]
-    [InverseProperty("Carriers")]
-    public virtual Address? LocNavigation { get; set; }
+  [ForeignKey("Loc")]
+  [InverseProperty("Carriers")]
+  public virtual Address? LocNavigation { get; set; }
 
-    [ForeignKey("Ref")]
-    [InverseProperty("Carriers")]
-    public virtual Credential? RefNavigation { get; set; }
+  [ForeignKey("Ref")]
+  [InverseProperty("Carriers")]
+  public virtual Credential? RefNavigation { get; set; }
 }
