ccf767c11084e115dab6ea14d9d40dac39649fb2
paolomococci <paolomococci@gmail.com>
2022-12-22 15:53:51 +0100

    modified:   workspaces/DummyRosterRC2/DummyRoster.Common.EntityModel/Models/Category.cs

--- workspaces/DummyRosterRC2/DummyRoster.Common.EntityModel/Models/Category.cs
+++ workspaces/DummyRosterRC2/DummyRoster.Common.EntityModel/Models/Category.cs
@@ -8,19 +8,19 @@ namespace DummyRoster.Common.EntityModel.Models;
 [Index("Name", Name = "IDX_CategoryName")]
 public partial class Category
 {
-    [Key]
-    public int Id { get; set; }
+  [Key]
+  public int Id { get; set; }
 
-    [Column(TypeName = "varchar(32)")]
-    public string Name { get; set; } = null!;
+  [Column(TypeName = "varchar(32)")]
+  public string Name { get; set; } = null!;
 
-    public string? Description { get; set; }
+  public string? Description { get; set; }
 
-    [Column(TypeName = "varchar(8)")]
-    public string? Belonging { get; set; }
+  [Column(TypeName = "varchar(8)")]
+  public string? Belonging { get; set; }
 
-    public byte[]? Picture { get; set; }
+  public byte[]? Picture { get; set; }
 
-    [InverseProperty("Category")]
-    public virtual ICollection<Product> Products { get; } = new List<Product>();
+  [InverseProperty("Category")]
+  public virtual ICollection<Product> Products { get; } = new List<Product>();
 }
