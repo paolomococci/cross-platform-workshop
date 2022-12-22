7b55d1bb20ccd4ae52743c5593106d8fc246ccce
paolomococci <paolomococci@gmail.com>
2022-12-22 22:39:38 +0100

    modified:   workspaces/DummyRosterRC2/DummyRoster.Common.EntityModel/Models/Product.cs

--- workspaces/DummyRosterRC2/DummyRoster.Common.EntityModel/Models/Product.cs
+++ workspaces/DummyRosterRC2/DummyRoster.Common.EntityModel/Models/Product.cs
@@ -19,6 +19,7 @@ public partial class Product
   [RegularExpression(@"[a-zA-Z0-9\s\-]{1,32}")]
   public string Name { get; set; } = null!;
 
+  [RegularExpression(@"^([A-Z]{1,}[a-zA-Z0-9\s\.\,]{1,63})$")]
   public string? Description { get; set; }
 
   [Column(TypeName = "varchar(8)")]
