a641528f178f63e5aeb22f9d7c5655ac1f39c93a
paolomococci <paolomococci@gmail.com>
2022-12-23 16:41:02 +0100

    modified:   workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Controllers/SupplierController.cs

--- workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Controllers/SupplierController.cs
+++ workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Controllers/SupplierController.cs
@@ -1,5 +1,38 @@
+using DummyRoster.Common.EntityModel.Models;
 using DummyRoster.WebApi.Controllers.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DummyRoster.WebApi.Controllers;
 
-public class SupplierController : ISupplierController {}
+public class SupplierController : ISupplierController
+{
+  public Task<IActionResult> Create([FromBody] Supplier entity)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<IActionResult> Delete(int id)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<IActionResult> PartialUpdate(int id, [FromBody] Supplier entity)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<IEnumerable<Supplier>> ReadAll(string? name)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<IActionResult> ReadSupplier(int id)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<IActionResult> Update(int id, [FromBody] Supplier entity)
+  {
+    throw new NotImplementedException();
+  }
+}
