a51c600f1345485975b1a818bcb4a12f442cd3fa
paolomococci <paolomococci@gmail.com>
2022-12-23 16:38:38 +0100

    modified:   workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Controllers/CategoryController.cs

--- workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Controllers/CategoryController.cs
+++ workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Controllers/CategoryController.cs
@@ -1,5 +1,38 @@
+using DummyRoster.Common.EntityModel.Models;
 using DummyRoster.WebApi.Controllers.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DummyRoster.WebApi.Controllers;
 
-public class CategoryController : ICategoryController {}
+public class CategoryController : ICategoryController
+{
+  public Task<IActionResult> Create([FromBody] Category entity)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<IActionResult> Delete(int id)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<IActionResult> PartialUpdate(int id, [FromBody] Category entity)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<IEnumerable<Category>> ReadAll(string? name)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<IActionResult> ReadCategory(int id)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<IActionResult> Update(int id, [FromBody] Category entity)
+  {
+    throw new NotImplementedException();
+  }
+}
