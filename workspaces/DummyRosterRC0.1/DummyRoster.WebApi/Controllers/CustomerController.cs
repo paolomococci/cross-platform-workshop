fd9bdb88ab5d04b19066044bc18326a1e451074b
paolomococci <paolomococci@gmail.com>
2022-12-23 16:35:49 +0100

    modified:   workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Controllers/CustomerController.cs

--- workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Controllers/CustomerController.cs
+++ workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Controllers/CustomerController.cs
@@ -1,3 +1,5 @@
+using DummyRoster.WebApi.Controllers.Interfaces;
+
 namespace DummyRoster.WebApi.Controllers;
 
-public class CustomerController {}
+public class CustomerController : ICustomerController {}
