aa462f9e6930d00d69874541d4c37541e3c1b9c8
paolomococci <paolomococci@gmail.com>
2022-12-23 16:25:40 +0100

    modified:   workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Repositories/AddressRepository.cs

--- workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Repositories/AddressRepository.cs
+++ workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Repositories/AddressRepository.cs
@@ -1,5 +1,37 @@
+using DummyRoster.Common.EntityModel.Models;
 using DummyRoster.WebApi.Repositories.Interfaces;
 
 namespace DummyRoster.WebApi.Repositories;
 
-public class AddressRepository : IAddressRepository {}
+public class AddressRepository : IAddressRepository
+{
+  public Task<Address?> CreateAsync(Address entity)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<bool?> DeleteAsync(int id)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<Address?> PartialUpdateAsync(int id, Address entity)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<Address?> Retrieve(int id)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<IEnumerable<Address>> RetrieveAll()
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<Address?> UpdateAsync(int id, Address entity)
+  {
+    throw new NotImplementedException();
+  }
+}
