4f1eec79663e93b65bcb20f4b549259fee62c87e
paolomococci <paolomococci@gmail.com>
2022-12-23 16:40:29 +0100

    modified:   workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Controllers/FormController.cs

--- workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Controllers/FormController.cs
+++ workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Controllers/FormController.cs
@@ -1,5 +1,38 @@
+using DummyRoster.Common.EntityModel.Models;
 using DummyRoster.WebApi.Controllers.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DummyRoster.WebApi.Controllers;
 
-public class FormController : IFormController {}
+public class FormController : IFormController
+{
+  public Task<IActionResult> Create([FromBody] Form entity)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<IActionResult> Delete(int id)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<IActionResult> PartialUpdate(int id, [FromBody] Form entity)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<IEnumerable<Form>> ReadAll(int? customerId)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<IActionResult> ReadForm(int id)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<IActionResult> Update(int id, [FromBody] Form entity)
+  {
+    throw new NotImplementedException();
+  }
+}
