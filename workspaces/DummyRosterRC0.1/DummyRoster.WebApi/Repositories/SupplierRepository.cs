a36266e144d8c5646c9817200122946bfa57b68b
paolomococci <paolomococci@gmail.com>
2022-12-23 16:27:11 +0100

    modified:   workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Repositories/SupplierRepository.cs

--- workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Repositories/SupplierRepository.cs
+++ workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Repositories/SupplierRepository.cs
@@ -1,5 +1,37 @@
+using DummyRoster.Common.EntityModel.Models;
 using DummyRoster.WebApi.Repositories.Interfaces;
 
 namespace DummyRoster.WebApi.Repositories;
 
-public class SupplierRepository : ISupplierRepository {}
+public class SupplierRepository : ISupplierRepository
+{
+  public Task<Supplier?> CreateAsync(Supplier entity)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<bool?> DeleteAsync(int id)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<Supplier?> PartialUpdateAsync(int id, Supplier entity)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<Supplier?> Retrieve(int id)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<IEnumerable<Supplier>> RetrieveAll()
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<Supplier?> UpdateAsync(int id, Supplier entity)
+  {
+    throw new NotImplementedException();
+  }
+}
