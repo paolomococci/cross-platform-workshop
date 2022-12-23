072a28b0a0fadfc7db45f5ebc9ee9a5c16cf1a96
paolomococci <paolomococci@gmail.com>
2022-12-23 16:38:15 +0100

    modified:   workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Controllers/AddressController.cs

--- workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Controllers/AddressController.cs
+++ workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Controllers/AddressController.cs
@@ -1,5 +1,38 @@
+using DummyRoster.Common.EntityModel.Models;
 using DummyRoster.WebApi.Controllers.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DummyRoster.WebApi.Controllers;
 
-public class AddressController : IAddressController {}
+public class AddressController : IAddressController
+{
+  public Task<IActionResult> Create([FromBody] Address entity)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<IActionResult> Delete(int id)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<IActionResult> PartialUpdate(int id, [FromBody] Address entity)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<IActionResult> ReadAddress(int id)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<IEnumerable<Address>> ReadAll(string? country)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<IActionResult> Update(int id, [FromBody] Address entity)
+  {
+    throw new NotImplementedException();
+  }
+}
