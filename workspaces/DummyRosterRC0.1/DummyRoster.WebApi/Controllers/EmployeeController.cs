70122b2cb53fbe5a76d0377e45ac3a16fd43a3c9
paolomococci <paolomococci@gmail.com>
2022-12-23 16:40:18 +0100

    modified:   workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Controllers/EmployeeController.cs

--- workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Controllers/EmployeeController.cs
+++ workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Controllers/EmployeeController.cs
@@ -1,5 +1,38 @@
+using DummyRoster.Common.EntityModel.Models;
 using DummyRoster.WebApi.Controllers.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DummyRoster.WebApi.Controllers;
 
-public class EmployeeController : IEmployeeController {}
+public class EmployeeController : IEmployeeController
+{
+  public Task<IActionResult> Create([FromBody] Employee entity)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<IActionResult> Delete(int id)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<IActionResult> PartialUpdate(int id, [FromBody] Employee entity)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<IEnumerable<Employee>> ReadAll(string? name)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<IActionResult> ReadEmployee(int id)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<IActionResult> Update(int id, [FromBody] Employee entity)
+  {
+    throw new NotImplementedException();
+  }
+}
