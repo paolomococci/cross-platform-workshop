e6159254938b69195d302169302b5e75bd9ade17
paolomococci <paolomococci@gmail.com>
2022-12-23 16:26:51 +0100

    modified:   workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Repositories/InvoiceRepository.cs

--- workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Repositories/InvoiceRepository.cs
+++ workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Repositories/InvoiceRepository.cs
@@ -1,5 +1,37 @@
+using DummyRoster.Common.EntityModel.Models;
 using DummyRoster.WebApi.Repositories.Interfaces;
 
 namespace DummyRoster.WebApi.Repositories;
 
-public class InvoiceRepository : IInvoiceRepository {}
+public class InvoiceRepository : IInvoiceRepository
+{
+  public Task<Invoice?> CreateAsync(Invoice entity)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<bool?> DeleteAsync(int id)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<Invoice?> PartialUpdateAsync(int id, Invoice entity)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<Invoice?> Retrieve(int id)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<IEnumerable<Invoice>> RetrieveAll()
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<Invoice?> UpdateAsync(int id, Invoice entity)
+  {
+    throw new NotImplementedException();
+  }
+}
