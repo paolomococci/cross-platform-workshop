1a07365aa79a39238d27e690561622b8b8f810e7
paolomococci <paolomococci@gmail.com>
2022-12-23 16:22:50 +0100

    modified:   workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Repositories/CustomerRepository.cs

--- workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Repositories/CustomerRepository.cs
+++ workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Repositories/CustomerRepository.cs
@@ -1,3 +1,5 @@
+using DummyRoster.WebApi.Repositories.Interfaces;
+
 namespace DummyRoster.WebApi.Repositories;
 
-public class CustomerRepository {}
+public class CustomerRepository : ICustomerRepository {}
