b59ebfa15ec5f643bbdff196e66574a6d1a3f267
paolomococci <paolomococci@gmail.com>
2022-12-23 21:03:37 +0100

    modified:   workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Controllers/ProductController.cs

--- workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Controllers/ProductController.cs
+++ workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Controllers/ProductController.cs
@@ -1,11 +1,21 @@
 using Microsoft.AspNetCore.Mvc;
 using DummyRoster.Common.EntityModel.Models;
 using DummyRoster.WebApi.Controllers.Interfaces;
+using DummyRoster.WebApi.Repositories.Interfaces;
 
 namespace DummyRoster.WebApi.Controllers;
 
 public class ProductController : ControllerBase, IProductController
 {
+
+  private readonly IProductRepository repository;
+
+  public ProductController(
+    IProductRepository repo
+  )
+  {
+    this.repository = repo;
+  }
   public Task<IActionResult> Create([FromBody] Product entity)
   {
     throw new NotImplementedException();
