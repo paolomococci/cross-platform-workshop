f04d463496c942e3b07899971514f2fd44dc8d89
paolomococci <paolomococci@gmail.com>
2022-12-23 16:40:52 +0100

    modified:   workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Controllers/ProductController.cs

--- workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Controllers/ProductController.cs
+++ workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Controllers/ProductController.cs
@@ -1,5 +1,38 @@
+using DummyRoster.Common.EntityModel.Models;
 using DummyRoster.WebApi.Controllers.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DummyRoster.WebApi.Controllers;
 
-public class ProductController : IProductController {}
+public class ProductController : IProductController
+{
+  public Task<IActionResult> Create([FromBody] Product entity)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<IActionResult> Delete(int id)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<IActionResult> PartialUpdate(int id, [FromBody] Product entity)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<IEnumerable<Product>> ReadAll(string? name)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<IActionResult> ReadProduct(int id)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<IActionResult> Update(int id, [FromBody] Product entity)
+  {
+    throw new NotImplementedException();
+  }
+}
