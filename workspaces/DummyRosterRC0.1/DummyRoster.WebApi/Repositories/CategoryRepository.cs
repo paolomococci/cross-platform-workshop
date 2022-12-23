a85af295bf01c4e45339339fad2d1a8daabe2e16
paolomococci <paolomococci@gmail.com>
2022-12-23 17:58:50 +0100

    modified:   workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Repositories/CategoryRepository.cs

--- workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Repositories/CategoryRepository.cs
+++ workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Repositories/CategoryRepository.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System.Collections.Concurrent;
+using DummyRoster.Common.EntityModel.Data;
 using DummyRoster.Common.EntityModel.Models;
 using DummyRoster.WebApi.Repositories.Interfaces;
 
@@ -5,33 +8,129 @@ namespace DummyRoster.WebApi.Repositories;
 
 public class CategoryRepository : ICategoryRepository
 {
-  public Task<Category?> CreateAsync(Category entity)
+  private static ConcurrentDictionary<int, Category>? keyValuesCache;
+  private DummyRosterContext dummyRosterContext;
+
+  public CategoryRepository(
+    DummyRosterContext dummyRosterContext
+  )
   {
-    throw new NotImplementedException();
+    this.dummyRosterContext = dummyRosterContext;
+    if (keyValuesCache is null)
+    {
+      keyValuesCache = new ConcurrentDictionary<int, Category>(
+        this.dummyRosterContext.Categories.ToDictionary(
+          entity => entity.Id
+        )
+      );
+    }
   }
 
-  public Task<bool?> DeleteAsync(int id)
+  public async Task<Category?> CreateAsync(Category entity)
   {
-    throw new NotImplementedException();
-  }
-
-  public Task<Category?> PartialUpdateAsync(int id, Category entity)
-  {
-    throw new NotImplementedException();
+    EntityEntry<Category> entry = await this.dummyRosterContext.Categories.AddAsync(entity);
+    int changesWereSavedAsynchronously = await this.dummyRosterContext.SaveChangesAsync();
+    if (changesWereSavedAsynchronously == 1)
+    {
+      if (keyValuesCache is null)
+      {
+        return entity;
+      }
+      return keyValuesCache.AddOrUpdate(
+        entity.Id,
+        entity,
+        UpdateCache
+      );
+    }
+    else
+    {
+      return null;
+    }
   }
 
   public Task<Category?> Retrieve(int id)
   {
-    throw new NotImplementedException();
+    if (keyValuesCache is null) return null!;
+    keyValuesCache.TryGetValue(id, out Category? entity);
+    return Task.FromResult(entity);
   }
 
   public Task<IEnumerable<Category>> RetrieveAll()
   {
-    throw new NotImplementedException();
+    return Task.FromResult(
+      keyValuesCache is null ? Enumerable.Empty<Category>() : keyValuesCache.Values
+    );
   }
 
-  public Task<Category?> UpdateAsync(int id, Category entity)
+  public async Task<Category?> UpdateAsync(int id, Category entity)
   {
-    throw new NotImplementedException();
+    this.dummyRosterContext.Categories.Update(entity);
+    int changesSaved = await this.dummyRosterContext.SaveChangesAsync();
+    if (changesSaved == 1)
+    {
+      return this.UpdateCache(id, entity);
+    }
+    return null;
+  }
+
+  public async Task<Category?> PartialUpdateAsync(int id, Category entity)
+  {
+    if (keyValuesCache is null) return null!;
+    keyValuesCache.TryGetValue(id, out Category? registered);
+    if (registered != null)
+    {
+      if (entity.Name != null) registered.Name = entity.Name;
+      if (entity.Description != null) registered.Description = entity.Description;
+      if (entity.Picture != null) registered.Picture = entity.Picture;
+      this.dummyRosterContext.Categories.Update(registered);
+      int changesSaved = await this.dummyRosterContext.SaveChangesAsync();
+      if (changesSaved == 1)
+      {
+        return this.UpdateCache(id, registered);
+      }
+    }
+    return null;
+  }
+
+  public async Task<bool?> DeleteAsync(int id)
+  {
+    Category? entity = this.dummyRosterContext.Categories.Find(id);
+    if (entity is null)
+    {
+      return null;
+    }
+    this.dummyRosterContext.Categories.Remove(entity);
+    int changesSaved = await this.dummyRosterContext.SaveChangesAsync();
+    if (changesSaved == 1)
+    {
+      if (keyValuesCache is null)
+      {
+        return null;
+      }
+      return keyValuesCache.TryRemove(id, out entity);
+    }
+    else
+    {
+      return null;
+    }
+  }
+
+  private Category UpdateCache(
+    int id, 
+    Category entity
+  )
+  {
+    Category? alreadyRegistered;
+    if (keyValuesCache is not null)
+    {
+      if (keyValuesCache.TryGetValue(id, out alreadyRegistered))
+      {
+        if (keyValuesCache.TryUpdate(id, entity, alreadyRegistered))
+        {
+          return entity;
+        }
+      }
+    }
+    return null!;
   }
 }
