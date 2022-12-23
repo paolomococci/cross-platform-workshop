5d189fecf4f29bac583c53366351e7d10fda50c1
paolomococci <paolomococci@gmail.com>
2022-12-23 16:27:01 +0100

    modified:   workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Repositories/ProductRepository.cs

--- workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Repositories/ProductRepository.cs
+++ workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Repositories/ProductRepository.cs
@@ -1,5 +1,37 @@
+using DummyRoster.Common.EntityModel.Models;
 using DummyRoster.WebApi.Repositories.Interfaces;
 
 namespace DummyRoster.WebApi.Repositories;
 
-public class ProductRepository : IProductRepository {}
+public class ProductRepository : IProductRepository
+{
+  public Task<Product?> CreateAsync(Product entity)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<bool?> DeleteAsync(int id)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<Product?> PartialUpdateAsync(int id, Product entity)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<Product?> Retrieve(int id)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<IEnumerable<Product>> RetrieveAll()
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<Product?> UpdateAsync(int id, Product entity)
+  {
+    throw new NotImplementedException();
+  }
+}
