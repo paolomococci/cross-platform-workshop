232275ba34edab509743b6ed740122c598cb81c7
paolomococci <paolomococci@gmail.com>
2022-12-23 21:13:41 +0100

    modified:   workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Controllers/ProductController.cs

--- workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Controllers/ProductController.cs
+++ workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Controllers/ProductController.cs
@@ -16,33 +16,140 @@ public class ProductController : ControllerBase, IProductController
   {
     this.repository = repo;
   }
-  public Task<IActionResult> Create([FromBody] Product entity)
+
+  /* 
+    POST: api/products
+    BODY: Product (JSON, XML)
+   */
+  [HttpPost]
+  [ProducesResponseType(
+    201,
+    Type = typeof(Product)
+  )]
+  [ProducesResponseType(400)]
+  public async Task<IActionResult> Create([FromBody] Product entity)
   {
-    throw new NotImplementedException();
+    if (entity == null)
+    {
+      return BadRequest();
+    }
+    Product? managedEntity = await this.repository.CreateAsync(entity);
+    if (managedEntity == null)
+    {
+      return BadRequest("Unable to manage entity!");
+    }
+    return CreatedAtRoute(
+      routeName: nameof(ReadProduct),
+      routeValues: new { id = managedEntity.Id },
+      value: managedEntity
+    );
   }
 
-  public Task<IActionResult> Delete(int id)
+  /* 
+    GET: api/products/[id]
+   */
+  [HttpGet("{id}", Name = nameof(ReadProduct))]
+  [ProducesResponseType(
+    200,
+    Type = typeof(Product)
+  )]
+  [ProducesResponseType(404)]
+  public async Task<IActionResult> ReadProduct(int id)
   {
-    throw new NotImplementedException();
+    Product? managedEntity = await this.repository.Retrieve(id);
+    if (managedEntity == null)
+    {
+      return NotFound();
+    }
+    return Ok(managedEntity);
   }
 
-  public Task<IActionResult> PartialUpdate(int id, [FromBody] Product entity)
+  /* 
+    GET: api/products
+    GET: api/products/?name=[name]
+   */
+  [HttpGet]
+  [ProducesResponseType(
+    200,
+    Type = typeof(IEnumerable<Product>)
+  )]
+  public async Task<IEnumerable<Product>> ReadAll(string? name)
   {
-    throw new NotImplementedException();
+    if (string.IsNullOrWhiteSpace(name))
+    {
+      return await this.repository.RetrieveAll();
+    }
+    return (await this.repository.RetrieveAll()).Where(
+      entity => entity.Name == name
+    );
   }
 
-  public Task<IEnumerable<Product>> ReadAll(string? name)
+  /* 
+    PUT: api/products/id
+    BODY: Product (JSON, XML)
+   */
+  [HttpPut("{id}")]
+  [ProducesResponseType(204)]
+  [ProducesResponseType(400)]
+  [ProducesResponseType(404)]
+  public async Task<IActionResult> Update(int id, [FromBody] Product entity)
   {
-    throw new NotImplementedException();
+    if (entity == null || entity.Id != id)
+    {
+      return BadRequest();
+    }
+    Product? managedEntity = await this.repository.Retrieve(id);
+    if (managedEntity == null)
+    {
+      return NotFound();
+    }
+    await this.repository.UpdateAsync(id, entity);
+    return new NoContentResult();
   }
 
-  public Task<IActionResult> ReadProduct(int id)
+  /* 
+    PATCH: api/products/id
+    BODY: Product (JSON, XML)
+   */
+  [HttpPatch("{id}")]
+  [ProducesResponseType(204)]
+  [ProducesResponseType(400)]
+  [ProducesResponseType(404)]
+  public async Task<IActionResult> PartialUpdate(int id, [FromBody] Product entity)
   {
-    throw new NotImplementedException();
+    if (entity == null || entity.Id != id)
+    {
+      return BadRequest();
+    }
+    Product? managedEntity = await this.repository.Retrieve(id);
+    if (managedEntity == null)
+    {
+      return NotFound();
+    }
+    await this.repository.PartialUpdateAsync(id, entity);
+    return new NoContentResult();
   }
 
-  public Task<IActionResult> Update(int id, [FromBody] Product entity)
+  /* 
+    DELETE: api/products/id
+   */
+  [HttpDelete("{id}")]
+  [ProducesResponseType(204)]
+  [ProducesResponseType(400)]
+  [ProducesResponseType(404)]
+  public async Task<IActionResult> Delete(int id)
   {
-    throw new NotImplementedException();
+    bool deleted = false;
+    Product? managedEntity = await this.repository.Retrieve(id);
+    if (managedEntity == null)
+    {
+      return NotFound();
+    }
+    deleted = (bool)await this.repository.DeleteAsync(id);
+    if (deleted)
+    {
+      return new NoContentResult();
+    }
+    return BadRequest();
   }
 }
