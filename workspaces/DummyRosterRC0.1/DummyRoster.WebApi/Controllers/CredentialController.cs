5aafeb95951f537681063af12bf8cd478896e6cc
paolomococci <paolomococci@gmail.com>
2022-12-23 16:39:54 +0100

    modified:   workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Controllers/CredentialController.cs

--- workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Controllers/CredentialController.cs
+++ workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Controllers/CredentialController.cs
@@ -1,5 +1,38 @@
+using DummyRoster.Common.EntityModel.Models;
 using DummyRoster.WebApi.Controllers.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DummyRoster.WebApi.Controllers;
 
-public class CredentialController : ICredentialController {}
+public class CredentialController : ICredentialController
+{
+  public Task<IActionResult> Create([FromBody] Credential entity)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<IActionResult> Delete(int id)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<IActionResult> PartialUpdate(int id, [FromBody] Credential entity)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<IEnumerable<Credential>> ReadAll(string? email)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<IActionResult> ReadCredential(int id)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<IActionResult> Update(int id, [FromBody] Credential entity)
+  {
+    throw new NotImplementedException();
+  }
+}
