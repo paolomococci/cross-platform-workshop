f9922faaaf376a20a4ef5bdb5ccbbbadab23f190
paolomococci <paolomococci@gmail.com>
2022-12-23 16:38:29 +0100

    modified:   workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Controllers/CarrierController.cs

--- workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Controllers/CarrierController.cs
+++ workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Controllers/CarrierController.cs
@@ -1,5 +1,38 @@
+using DummyRoster.Common.EntityModel.Models;
 using DummyRoster.WebApi.Controllers.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DummyRoster.WebApi.Controllers;
 
-public class CarrierController : ICarrierController {}
+public class CarrierController : ICarrierController
+{
+  public Task<IActionResult> Create([FromBody] Carrier entity)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<IActionResult> Delete(int id)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<IActionResult> PartialUpdate(int id, [FromBody] Carrier entity)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<IEnumerable<Carrier>> ReadAll(string? name)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<IActionResult> ReadCarrier(int id)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<IActionResult> Update(int id, [FromBody] Carrier entity)
+  {
+    throw new NotImplementedException();
+  }
+}
