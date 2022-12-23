eb7617b4d2a5783a2c0f9e68d273801535949291
paolomococci <paolomococci@gmail.com>
2022-12-23 21:03:24 +0100

    modified:   workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Controllers/InvoiceController.cs

--- workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Controllers/InvoiceController.cs
+++ workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Controllers/InvoiceController.cs
@@ -1,11 +1,21 @@
 using Microsoft.AspNetCore.Mvc;
 using DummyRoster.Common.EntityModel.Models;
 using DummyRoster.WebApi.Controllers.Interfaces;
+using DummyRoster.WebApi.Repositories.Interfaces;
 
 namespace DummyRoster.WebApi.Controllers;
 
 public class InvoiceController : ControllerBase, IInvoiceController
 {
+
+  private readonly IInvoiceRepository repository;
+
+  public InvoiceController(
+    IInvoiceRepository repo
+  )
+  {
+    this.repository = repo;
+  }
   public Task<IActionResult> Create([FromBody] Invoice entity)
   {
     throw new NotImplementedException();
