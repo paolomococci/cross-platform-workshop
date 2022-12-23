a170406aa1ec23f426bae782b864d201065539ea
paolomococci <paolomococci@gmail.com>
2022-12-23 16:13:11 +0100

    modified:   workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Controllers/Interfaces/ICategoryController.cs

--- workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Controllers/Interfaces/ICategoryController.cs
+++ workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Controllers/Interfaces/ICategoryController.cs
@@ -1,3 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+using DummyRoster.Common.EntityModel.Models;
+
 namespace DummyRoster.WebApi.Controllers.Interfaces;
 
-public interface ICategoryController {}
+public interface ICategoryController
+{
+  public Task<IActionResult> Create([FromBody] Category entity);
+  public Task<IActionResult> ReadCategory(int id);
+  public Task<IEnumerable<Category>> ReadAll(string? name);
+  public Task<IActionResult> Update(int id, [FromBody] Category entity);
+  public Task<IActionResult> PartialUpdate(int id, [FromBody] Category entity);
+  public Task<IActionResult> Delete(int id);
+}
