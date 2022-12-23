138b38837c5571a82ec90e3b131ed3648847e871
paolomococci <paolomococci@gmail.com>
2022-12-23 16:26:02 +0100

    modified:   workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Repositories/CategoryRepository.cs

--- workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Repositories/CategoryRepository.cs
+++ workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Repositories/CategoryRepository.cs
@@ -1,5 +1,37 @@
+using DummyRoster.Common.EntityModel.Models;
 using DummyRoster.WebApi.Repositories.Interfaces;
 
 namespace DummyRoster.WebApi.Repositories;
 
-public class CategoryRepository : ICategoryRepository {}
+public class CategoryRepository : ICategoryRepository
+{
+  public Task<Category?> CreateAsync(Category entity)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<bool?> DeleteAsync(int id)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<Category?> PartialUpdateAsync(int id, Category entity)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<Category?> Retrieve(int id)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<IEnumerable<Category>> RetrieveAll()
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<Category?> UpdateAsync(int id, Category entity)
+  {
+    throw new NotImplementedException();
+  }
+}
