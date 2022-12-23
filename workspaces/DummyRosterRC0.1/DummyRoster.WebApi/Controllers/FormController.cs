fe084d79c72b9cf8feb3b8793c4070fe3e3112fd
paolomococci <paolomococci@gmail.com>
2022-12-23 21:03:14 +0100

    modified:   workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Controllers/FormController.cs

--- workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Controllers/FormController.cs
+++ workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Controllers/FormController.cs
@@ -1,11 +1,21 @@
 using Microsoft.AspNetCore.Mvc;
 using DummyRoster.Common.EntityModel.Models;
 using DummyRoster.WebApi.Controllers.Interfaces;
+using DummyRoster.WebApi.Repositories.Interfaces;
 
 namespace DummyRoster.WebApi.Controllers;
 
 public class FormController : ControllerBase, IFormController
 {
+
+  private readonly IFormRepository repository;
+
+  public FormController(
+    IFormRepository repo
+  )
+  {
+    this.repository = repo;
+  }
   public Task<IActionResult> Create([FromBody] Form entity)
   {
     throw new NotImplementedException();
