dac2be536003a44ea9624b8cc4f23bcbed276ad1
paolomococci <paolomococci@gmail.com>
2022-12-23 16:26:12 +0100

    modified:   workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Repositories/CredentialRepository.cs

--- workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Repositories/CredentialRepository.cs
+++ workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Repositories/CredentialRepository.cs
@@ -1,5 +1,37 @@
+using DummyRoster.Common.EntityModel.Models;
 using DummyRoster.WebApi.Repositories.Interfaces;
 
 namespace DummyRoster.WebApi.Repositories;
 
-public class CredentialRepository : ICredentialRepository {}
+public class CredentialRepository : ICredentialRepository
+{
+  public Task<Credential?> CreateAsync(Credential entity)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<bool?> DeleteAsync(int id)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<Credential?> PartialUpdateAsync(int id, Credential entity)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<Credential?> Retrieve(int id)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<IEnumerable<Credential>> RetrieveAll()
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<Credential?> UpdateAsync(int id, Credential entity)
+  {
+    throw new NotImplementedException();
+  }
+}
