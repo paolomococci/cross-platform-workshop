8ce36709464774a4e73e1abadcaf9b8c7caf296d
paolomococci <paolomococci@gmail.com>
2022-12-23 16:00:56 +0100

    modified:   workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Repositories/Interfaces/ICarrierRepository.cs

--- workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Repositories/Interfaces/ICarrierRepository.cs
+++ workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Repositories/Interfaces/ICarrierRepository.cs
@@ -1,3 +1,19 @@
+using DummyRoster.Common.EntityModel.Models;
+
 namespace DummyRoster.WebApi.Repositories.Interfaces;
 
-public interface ICarrierRepository {}
+public interface ICarrierRepository
+{
+  Task<Carrier?> CreateAsync(Carrier entity);
+  Task<Carrier?> Retrieve(int id);
+  Task<IEnumerable<Carrier>> RetrieveAll();
+  Task<Carrier?> UpdateAsync(
+    int id,
+    Carrier entity
+  );
+  Task<Carrier?> PartialUpdateAsync(
+    int id,
+    Carrier entity
+  );
+  Task<bool?> DeleteAsync(int id);
+}
