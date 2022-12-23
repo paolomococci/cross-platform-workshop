08f9acd5c22c78b754118685d43a120b725fed25
paolomococci <paolomococci@gmail.com>
2022-12-23 16:22:41 +0100

    modified:   workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Repositories/CredentialRepository.cs

--- workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Repositories/CredentialRepository.cs
+++ workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Repositories/CredentialRepository.cs
@@ -1,3 +1,5 @@
+using DummyRoster.WebApi.Repositories.Interfaces;
+
 namespace DummyRoster.WebApi.Repositories;
 
-public class CredentialRepository {}
+public class CredentialRepository : ICredentialRepository {}
