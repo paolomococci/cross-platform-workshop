e1a1e826794019229edab1675894f2f99d096ff1
paolomococci <paolomococci@gmail.com>
2022-12-23 21:12:25 +0100

    modified:   workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Controllers/AddressController.cs

--- workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Controllers/AddressController.cs
+++ workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Controllers/AddressController.cs
@@ -16,33 +16,140 @@ public class AddressController : ControllerBase, IAddressController
   {
     this.repository = repo;
   }
-  public Task<IActionResult> Create([FromBody] Address entity)
+
+  /* 
+    POST: api/addresses
+    BODY: Address (JSON, XML)
+   */
+  [HttpPost]
+  [ProducesResponseType(
+    201,
+    Type = typeof(Address)
+  )]
+  [ProducesResponseType(400)]
+  public async Task<IActionResult> Create([FromBody] Address entity)
   {
-    throw new NotImplementedException();
+    if (entity == null)
+    {
+      return BadRequest();
+    }
+    Address? managedEntity = await this.repository.CreateAsync(entity);
+    if (managedEntity == null)
+    {
+      return BadRequest("Unable to manage entity!");
+    }
+    return CreatedAtRoute(
+      routeName: nameof(ReadAddress),
+      routeValues: new { id = managedEntity.Id },
+      value: managedEntity
+    );
   }
 
-  public Task<IActionResult> Delete(int id)
+  /* 
+    GET: api/addresses/[id]
+   */
+  [HttpGet("{id}", Name = nameof(ReadAddress))]
+  [ProducesResponseType(
+    200,
+    Type = typeof(Address)
+  )]
+  [ProducesResponseType(404)]
+  public async Task<IActionResult> ReadAddress(int id)
   {
-    throw new NotImplementedException();
+    Address? managedEntity = await this.repository.Retrieve(id);
+    if (managedEntity == null)
+    {
+      return NotFound();
+    }
+    return Ok(managedEntity);
   }
 
-  public Task<IActionResult> PartialUpdate(int id, [FromBody] Address entity)
+  /* 
+    GET: api/addresses
+    GET: api/addresses/?country=[country]
+   */
+  [HttpGet]
+  [ProducesResponseType(
+    200,
+    Type = typeof(IEnumerable<Address>)
+  )]
+  public async Task<IEnumerable<Address>> ReadAll(string? country)
   {
-    throw new NotImplementedException();
+    if (string.IsNullOrWhiteSpace(country))
+    {
+      return await this.repository.RetrieveAll();
+    }
+    return (await this.repository.RetrieveAll()).Where(
+      entity => entity.Country == country
+    );
   }
 
-  public Task<IActionResult> ReadAddress(int id)
+  /* 
+    PUT: api/addresses/id
+    BODY: Address (JSON, XML)
+   */
+  [HttpPut("{id}")]
+  [ProducesResponseType(204)]
+  [ProducesResponseType(400)]
+  [ProducesResponseType(404)]
+  public async Task<IActionResult> Update(int id, [FromBody] Address entity)
   {
-    throw new NotImplementedException();
+    if (entity == null || entity.Id != id)
+    {
+      return BadRequest();
+    }
+    Address? managedEntity = await this.repository.Retrieve(id);
+    if (managedEntity == null)
+    {
+      return NotFound();
+    }
+    await this.repository.UpdateAsync(id, entity);
+    return new NoContentResult();
   }
 
-  public Task<IEnumerable<Address>> ReadAll(string? country)
+  /* 
+    PATCH: api/addresses/id
+    BODY: Address (JSON, XML)
+   */
+  [HttpPatch("{id}")]
+  [ProducesResponseType(204)]
+  [ProducesResponseType(400)]
+  [ProducesResponseType(404)]
+  public async Task<IActionResult> PartialUpdate(int id, [FromBody] Address entity)
   {
-    throw new NotImplementedException();
+    if (entity == null || entity.Id != id)
+    {
+      return BadRequest();
+    }
+    Address? managedEntity = await this.repository.Retrieve(id);
+    if (managedEntity == null)
+    {
+      return NotFound();
+    }
+    await this.repository.PartialUpdateAsync(id, entity);
+    return new NoContentResult();
   }
 
-  public Task<IActionResult> Update(int id, [FromBody] Address entity)
+  /* 
+    DELETE: api/addresses/id
+   */
+  [HttpDelete("{id}")]
+  [ProducesResponseType(204)]
+  [ProducesResponseType(400)]
+  [ProducesResponseType(404)]
+  public async Task<IActionResult> Delete(int id)
   {
-    throw new NotImplementedException();
+    bool deleted = false;
+    Address? managedEntity = await this.repository.Retrieve(id);
+    if (managedEntity == null)
+    {
+      return NotFound();
+    }
+    deleted = (bool)await this.repository.DeleteAsync(id);
+    if (deleted)
+    {
+      return new NoContentResult();
+    }
+    return BadRequest();
   }
 }
