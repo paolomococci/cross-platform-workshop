4500c0cc3c334d26a3b85d183a320992471e627e
paolomococci <paolomococci@gmail.com>
2022-12-23 21:12:54 +0100

    modified:   workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Controllers/CredentialController.cs

--- workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Controllers/CredentialController.cs
+++ workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Controllers/CredentialController.cs
@@ -16,33 +16,140 @@ public class CredentialController : ControllerBase, ICredentialController
   {
     this.repository = repo;
   }
-  public Task<IActionResult> Create([FromBody] Credential entity)
+
+  /* 
+    POST: api/credentials
+    BODY: Credential (JSON, XML)
+   */
+  [HttpPost]
+  [ProducesResponseType(
+    201,
+    Type = typeof(Credential)
+  )]
+  [ProducesResponseType(400)]
+  public async Task<IActionResult> Create([FromBody] Credential entity)
   {
-    throw new NotImplementedException();
+    if (entity == null)
+    {
+      return BadRequest();
+    }
+    Credential? managedEntity = await this.repository.CreateAsync(entity);
+    if (managedEntity == null)
+    {
+      return BadRequest("Unable to manage entity!");
+    }
+    return CreatedAtRoute(
+      routeName: nameof(ReadCredential),
+      routeValues: new { id = managedEntity.Id },
+      value: managedEntity
+    );
   }
 
-  public Task<IActionResult> Delete(int id)
+  /* 
+    GET: api/credentials/[id]
+   */
+  [HttpGet("{id}", Name = nameof(ReadCredential))]
+  [ProducesResponseType(
+    200,
+    Type = typeof(Credential)
+  )]
+  [ProducesResponseType(404)]
+  public async Task<IActionResult> ReadCredential(int id)
   {
-    throw new NotImplementedException();
+    Credential? managedEntity = await this.repository.Retrieve(id);
+    if (managedEntity == null)
+    {
+      return NotFound();
+    }
+    return Ok(managedEntity);
   }
 
-  public Task<IActionResult> PartialUpdate(int id, [FromBody] Credential entity)
+  /* 
+    GET: api/credentials
+    GET: api/credentials/?email=[email]
+   */
+  [HttpGet]
+  [ProducesResponseType(
+    200,
+    Type = typeof(IEnumerable<Credential>)
+  )]
+  public async Task<IEnumerable<Credential>> ReadAll(string? email)
   {
-    throw new NotImplementedException();
+    if (string.IsNullOrWhiteSpace(email))
+    {
+      return await this.repository.RetrieveAll();
+    }
+    return (await this.repository.RetrieveAll()).Where(
+      entity => entity.Email == email
+    );
   }
 
-  public Task<IEnumerable<Credential>> ReadAll(string? email)
+  /* 
+    PUT: api/credentials/id
+    BODY: Credential (JSON, XML)
+   */
+  [HttpPut("{id}")]
+  [ProducesResponseType(204)]
+  [ProducesResponseType(400)]
+  [ProducesResponseType(404)]
+  public async Task<IActionResult> Update(int id, [FromBody] Credential entity)
   {
-    throw new NotImplementedException();
+    if (entity == null || entity.Id != id)
+    {
+      return BadRequest();
+    }
+    Credential? managedEntity = await this.repository.Retrieve(id);
+    if (managedEntity == null)
+    {
+      return NotFound();
+    }
+    await this.repository.UpdateAsync(id, entity);
+    return new NoContentResult();
   }
 
-  public Task<IActionResult> ReadCredential(int id)
+  /* 
+    PATCH: api/credentials/id
+    BODY: Credential (JSON, XML)
+   */
+  [HttpPatch("{id}")]
+  [ProducesResponseType(204)]
+  [ProducesResponseType(400)]
+  [ProducesResponseType(404)]
+  public async Task<IActionResult> PartialUpdate(int id, [FromBody] Credential entity)
   {
-    throw new NotImplementedException();
+    if (entity == null || entity.Id != id)
+    {
+      return BadRequest();
+    }
+    Credential? managedEntity = await this.repository.Retrieve(id);
+    if (managedEntity == null)
+    {
+      return NotFound();
+    }
+    await this.repository.PartialUpdateAsync(id, entity);
+    return new NoContentResult();
   }
 
-  public Task<IActionResult> Update(int id, [FromBody] Credential entity)
+  /* 
+    DELETE: api/credentials/id
+   */
+  [HttpDelete("{id}")]
+  [ProducesResponseType(204)]
+  [ProducesResponseType(400)]
+  [ProducesResponseType(404)]
+  public async Task<IActionResult> Delete(int id)
   {
-    throw new NotImplementedException();
+    bool deleted = false;
+    Credential? managedEntity = await this.repository.Retrieve(id);
+    if (managedEntity == null)
+    {
+      return NotFound();
+    }
+    deleted = (bool)await this.repository.DeleteAsync(id);
+    if (deleted)
+    {
+      return new NoContentResult();
+    }
+    return BadRequest();
   }
 }
