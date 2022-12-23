78f7afbea547e99546b2c9505bd8da619fb67f03
paolomococci <paolomococci@gmail.com>
2022-12-23 21:03:47 +0100

    modified:   workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Controllers/SupplierController.cs

--- workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Controllers/SupplierController.cs
+++ workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Controllers/SupplierController.cs
@@ -1,11 +1,21 @@
 using Microsoft.AspNetCore.Mvc;
 using DummyRoster.Common.EntityModel.Models;
 using DummyRoster.WebApi.Controllers.Interfaces;
+using DummyRoster.WebApi.Repositories.Interfaces;
 
 namespace DummyRoster.WebApi.Controllers;
 
 public class SupplierController : ControllerBase, ISupplierController
 {
+
+  private readonly ISupplierRepository repository;
+
+  public SupplierController(
+    ISupplierRepository repo
+  )
+  {
+    this.repository = repo;
+  }
   public Task<IActionResult> Create([FromBody] Supplier entity)
   {
     throw new NotImplementedException();
