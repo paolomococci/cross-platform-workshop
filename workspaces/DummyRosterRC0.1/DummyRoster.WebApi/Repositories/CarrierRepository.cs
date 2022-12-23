245e10cc1db9ecd7b9e6d09c9fe4db9300dbd5f4
paolomococci <paolomococci@gmail.com>
2022-12-23 16:25:51 +0100

    modified:   workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Repositories/CarrierRepository.cs

--- workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Repositories/CarrierRepository.cs
+++ workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Repositories/CarrierRepository.cs
@@ -1,5 +1,37 @@
+using DummyRoster.Common.EntityModel.Models;
 using DummyRoster.WebApi.Repositories.Interfaces;
 
 namespace DummyRoster.WebApi.Repositories;
 
-public class CarrierRepository : ICarrierRepository {}
+public class CarrierRepository : ICarrierRepository
+{
+  public Task<Carrier?> CreateAsync(Carrier entity)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<bool?> DeleteAsync(int id)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<Carrier?> PartialUpdateAsync(int id, Carrier entity)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<Carrier?> Retrieve(int id)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<IEnumerable<Carrier>> RetrieveAll()
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<Carrier?> UpdateAsync(int id, Carrier entity)
+  {
+    throw new NotImplementedException();
+  }
+}
