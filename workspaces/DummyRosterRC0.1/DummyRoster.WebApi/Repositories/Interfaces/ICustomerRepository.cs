884cd0e243ee92a7b882d5bf2c7ff74ceca1d84b
paolomococci <paolomococci@gmail.com>
2022-12-23 16:01:33 +0100

    modified:   workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Repositories/Interfaces/ICustomerRepository.cs

--- workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Repositories/Interfaces/ICustomerRepository.cs
+++ workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Repositories/Interfaces/ICustomerRepository.cs
@@ -1,3 +1,19 @@
+using DummyRoster.Common.EntityModel.Models;
+
 namespace DummyRoster.WebApi.Repositories.Interfaces;
 
-public interface ICustomerRepository {}
+public interface ICustomerRepository
+{
+  Task<Customer?> CreateAsync(Customer entity);
+  Task<Customer?> Retrieve(int id);
+  Task<IEnumerable<Customer>> RetrieveAll();
+  Task<Customer?> UpdateAsync(
+    int id,
+    Customer entity
+  );
+  Task<Customer?> PartialUpdateAsync(
+    int id,
+    Customer entity
+  );
+  Task<bool?> DeleteAsync(int id);
+}
