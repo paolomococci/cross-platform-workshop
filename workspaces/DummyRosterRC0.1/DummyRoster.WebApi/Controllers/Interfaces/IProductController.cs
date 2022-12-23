a37ccfa4bafa96a47b7a63a11db4e60116a79eb8
paolomococci <paolomococci@gmail.com>
2022-12-23 16:14:14 +0100

    modified:   workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Controllers/Interfaces/IProductController.cs

--- workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Controllers/Interfaces/IProductController.cs
+++ workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Controllers/Interfaces/IProductController.cs
@@ -1,3 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+using DummyRoster.Common.EntityModel.Models;
+
 namespace DummyRoster.WebApi.Controllers.Interfaces;
 
-public interface IProductController {}
+public interface IProductController
+{
+  public Task<IActionResult> Create([FromBody] Product entity);
+  public Task<IActionResult> ReadProduct(int id);
+  public Task<IEnumerable<Product>> ReadAll(string? name);
+  public Task<IActionResult> Update(int id, [FromBody] Product entity);
+  public Task<IActionResult> PartialUpdate(int id, [FromBody] Product entity);
+  public Task<IActionResult> Delete(int id);
+}
