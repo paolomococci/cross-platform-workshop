5e0bed1064af021444238af5c9a2b530369805f8
paolomococci <paolomococci@gmail.com>
2022-12-23 16:01:43 +0100

    modified:   workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Repositories/Interfaces/IEmployeeRepository.cs

--- workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Repositories/Interfaces/IEmployeeRepository.cs
+++ workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Repositories/Interfaces/IEmployeeRepository.cs
@@ -1,3 +1,19 @@
+using DummyRoster.Common.EntityModel.Models;
+
 namespace DummyRoster.WebApi.Repositories.Interfaces;
 
-public interface IEmployeeRepository {}
+public interface IEmployeeRepository
+{
+  Task<Employee?> CreateAsync(Employee entity);
+  Task<Employee?> Retrieve(int id);
+  Task<IEnumerable<Employee>> RetrieveAll();
+  Task<Employee?> UpdateAsync(
+    int id,
+    Employee entity
+  );
+  Task<Employee?> PartialUpdateAsync(
+    int id,
+    Employee entity
+  );
+  Task<bool?> DeleteAsync(int id);
+}
