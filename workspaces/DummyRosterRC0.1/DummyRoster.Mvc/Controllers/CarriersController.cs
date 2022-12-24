3798fd55fbef758a88e69549aa9e9f11e88e1f26
paolomococci <paolomococci@gmail.com>
2022-12-24 07:20:00 +0100

    modified:   workspaces/DummyRosterRC0.1/DummyRoster.Mvc/Controllers/CarriersController.cs

--- workspaces/DummyRosterRC0.1/DummyRoster.Mvc/Controllers/CarriersController.cs
+++ workspaces/DummyRosterRC0.1/DummyRoster.Mvc/Controllers/CarriersController.cs
@@ -2,4 +2,17 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace DummyRoster.Mvc.Controllers;
 
-public class CarriersController : Controller {}
+public class CarriersController : Controller {
+  private const string baseUri = "https://localhost:5001/api/carriers";
+  private readonly ILogger<CarriersController> _logger;
+  private readonly HttpClient httpClient;
+
+  public CarriersController(
+    ILogger<CarriersController> logger,
+    IHttpClientFactory httpClientFactory
+  )
+  {
+    this._logger = logger;
+    this.httpClient = new HttpClient();
+  }
+}
