cd1ad75541999a90aec05c7e43a3b09e82572d3f
paolomococci <paolomococci@gmail.com>
2022-12-24 07:20:08 +0100

    modified:   workspaces/DummyRosterRC0.1/DummyRoster.Mvc/Controllers/CategoriesController.cs

--- workspaces/DummyRosterRC0.1/DummyRoster.Mvc/Controllers/CategoriesController.cs
+++ workspaces/DummyRosterRC0.1/DummyRoster.Mvc/Controllers/CategoriesController.cs
@@ -2,4 +2,17 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace DummyRoster.Mvc.Controllers;
 
-public class CategoriesController : Controller {}
+public class CategoriesController : Controller {
+  private const string baseUri = "https://localhost:5001/api/categories";
+  private readonly ILogger<CategoriesController> _logger;
+  private readonly HttpClient httpClient;
+
+  public CategoriesController(
+    ILogger<CategoriesController> logger,
+    IHttpClientFactory httpClientFactory
+  )
+  {
+    this._logger = logger;
+    this.httpClient = new HttpClient();
+  }
+}
