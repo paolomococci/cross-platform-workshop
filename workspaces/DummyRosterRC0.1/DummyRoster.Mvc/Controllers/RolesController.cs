63e5aa96300bdbf6ddfd1593066321007fdd71f5
paolomococci <paolomococci@gmail.com>
2022-12-24 07:29:36 +0100

    modified:   workspaces/DummyRosterRC0.1/DummyRoster.Mvc/Controllers/RolesController.cs

--- workspaces/DummyRosterRC0.1/DummyRoster.Mvc/Controllers/RolesController.cs
+++ workspaces/DummyRosterRC0.1/DummyRoster.Mvc/Controllers/RolesController.cs
@@ -1,5 +1,21 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DummyRoster.Mvc.Controllers;
 
-public class RolesController : Controller {}
+public class RolesController : Controller {
+  private readonly IConfiguration configuration;
+  private readonly RoleManager<IdentityRole> roleManager;
+  private readonly UserManager<IdentityUser> userManager;
+
+  public RolesController(
+    IConfiguration configuration,
+    RoleManager<IdentityRole> roleManager,
+    UserManager<IdentityUser> userManager
+  )
+  {
+    this.configuration = configuration;
+    this.roleManager = roleManager;
+    this.userManager = userManager;
+  }
+}
