7e5079c810568c8e24129df915632cf0abc946ac
paolomococci <paolomococci@gmail.com>
2022-12-24 22:05:20 +0100

    modified:   workspaces/DummyRosterRC0.1/DummyRoster.Mvc/Controllers/FormsController.cs

--- workspaces/DummyRosterRC0.1/DummyRoster.Mvc/Controllers/FormsController.cs
+++ workspaces/DummyRosterRC0.1/DummyRoster.Mvc/Controllers/FormsController.cs
@@ -27,4 +27,29 @@ public class FormsController : Controller
     };
     this.httpClient = new HttpClient(httpClientHandler);
   }
+
+  public async Task<IActionResult> Forms(int? customerId)
+  {
+    string apiUri = "";
+    if (string.IsNullOrEmpty(customerId.ToString()))
+    {
+      ViewData["Title"] = "All Forms";
+      apiUri = $"{baseUri}";
+    }
+    else
+    {
+      ViewData["Title"] = $"Forms with the customer id {customerId}";
+      apiUri = $"{baseUri}/?customerId={customerId.ToString()}";
+    }
+    HttpRequestMessage httpRequestMessage = new(
+      method: HttpMethod.Get,
+      requestUri: apiUri
+    );
+    HttpResponseMessage httpResponseMessage = await this.httpClient.SendAsync(
+      httpRequestMessage
+    );
+    IEnumerable<Form>? forms = await httpResponseMessage
+      .Content.ReadFromJsonAsync<IEnumerable<Form>>();
+    return View(forms);
+  }
 }
