bf9f3c2a7ebcfb93dcd96396f1246418db363e78
paolomococci <paolomococci@gmail.com>
2022-12-24 22:58:51 +0100

    modified:   workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Controllers/CredentialController.cs

--- workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Controllers/CredentialController.cs
+++ workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Controllers/CredentialController.cs
@@ -5,6 +5,8 @@ using DummyRoster.WebApi.Repositories.Interfaces;
 
 namespace DummyRoster.WebApi.Controllers;
 
+[ApiController]
+[Route("api/credentials")]
 public class CredentialController : ControllerBase, ICredentialController
 {
 
