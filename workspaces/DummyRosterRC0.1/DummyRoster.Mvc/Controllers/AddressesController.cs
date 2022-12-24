1e0773cc6227c08f8354d22cca87981448eff50d
paolomococci <paolomococci@gmail.com>
2022-12-24 07:19:50 +0100

    modified:   workspaces/DummyRosterRC0.1/DummyRoster.Mvc/Controllers/AddressesController.cs

--- workspaces/DummyRosterRC0.1/DummyRoster.Mvc/Controllers/AddressesController.cs
+++ workspaces/DummyRosterRC0.1/DummyRoster.Mvc/Controllers/AddressesController.cs
@@ -2,4 +2,17 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace DummyRoster.Mvc.Controllers;
 
-public class AddressesController : Controller {}
+public class AddressesController : Controller {
+  private const string baseUri = "https://localhost:5001/api/addresses";
+  private readonly ILogger<AddressesController> _logger;
+  private readonly HttpClient httpClient;
+
+  public AddressesController(
+    ILogger<AddressesController> logger,
+    IHttpClientFactory httpClientFactory
+  )
+  {
+    this._logger = logger;
+    this.httpClient = new HttpClient();
+  }
+}
