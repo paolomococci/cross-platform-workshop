e7acc202a78f559c0abc89c374429709d5edf2fd
paolomococci <paolomococci@gmail.com>
2022-12-24 07:20:34 +0100

    modified:   workspaces/DummyRosterRC0.1/DummyRoster.Mvc/Controllers/CustomersController.cs

--- workspaces/DummyRosterRC0.1/DummyRoster.Mvc/Controllers/CustomersController.cs
+++ workspaces/DummyRosterRC0.1/DummyRoster.Mvc/Controllers/CustomersController.cs
@@ -2,4 +2,17 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace DummyRoster.Mvc.Controllers;
 
-public class CustomersController : Controller {}
+public class CustomersController : Controller {
+  private const string baseUri = "https://localhost:5001/api/customers";
+  private readonly ILogger<CustomersController> _logger;
+  private readonly HttpClient httpClient;
+
+  public CustomersController(
+    ILogger<CustomersController> logger,
+    IHttpClientFactory httpClientFactory
+  )
+  {
+    this._logger = logger;
+    this.httpClient = new HttpClient();
+  }
+}
