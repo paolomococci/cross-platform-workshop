b757e3115cd9b3449bd83306bab2fda96ac59376
paolomococci <paolomococci@gmail.com>
2022-12-24 07:21:17 +0100

    modified:   workspaces/DummyRosterRC0.1/DummyRoster.Mvc/Controllers/ProductsController.cs

--- workspaces/DummyRosterRC0.1/DummyRoster.Mvc/Controllers/ProductsController.cs
+++ workspaces/DummyRosterRC0.1/DummyRoster.Mvc/Controllers/ProductsController.cs
@@ -2,4 +2,17 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace DummyRoster.Mvc.Controllers;
 
-public class ProductsController : Controller {}
+public class ProductsController : Controller {
+  private const string baseUri = "https://localhost:5001/api/products";
+  private readonly ILogger<ProductsController> _logger;
+  private readonly HttpClient httpClient;
+
+  public ProductsController(
+    ILogger<ProductsController> logger,
+    IHttpClientFactory httpClientFactory
+  )
+  {
+    this._logger = logger;
+    this.httpClient = new HttpClient();
+  }
+}
