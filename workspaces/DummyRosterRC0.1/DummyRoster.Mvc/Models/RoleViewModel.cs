a14861c795e7f5465e5e8c03b2a25768cf4a60f0
paolomococci <paolomococci@gmail.com>
2022-12-24 08:57:52 +0100

    deleted:    workspaces/DummyRosterRC0.1/DummyRoster.Mvc/Models/RoleViewModel.cs

--- workspaces/DummyRosterRC0.1/DummyRoster.Mvc/Models/RoleViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace DummyRoster.Mvc.Models;
-
-public class RoleViewModel
-{
-  public string Role { get; set; } = string.Empty;
-  public string Email { get; set; } = string.Empty;
-  public string Password { get; set; } = string.Empty;
-}
