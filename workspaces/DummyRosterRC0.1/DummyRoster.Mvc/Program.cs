5966cddda2e6c7ae9cc11e4e179ffa11c0b2b2d5
paolomococci <paolomococci@gmail.com>
2022-12-24 20:27:16 +0100

    modified:   workspaces/DummyRosterRC0.1/DummyRoster.Mvc/Program.cs

--- workspaces/DummyRosterRC0.1/DummyRoster.Mvc/Program.cs
+++ workspaces/DummyRosterRC0.1/DummyRoster.Mvc/Program.cs
@@ -1,11 +1,12 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using System.Net.Http.Headers;
 using DummyRoster.Mvc.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
 builder.Services.AddDbContext<ApplicationDbContext>(
   options => options.UseSqlite(connectionString)
@@ -15,9 +16,23 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(
   options => options.SignIn.RequireConfirmedAccount = true
-).AddEntityFrameworkStores<ApplicationDbContext>();
+).AddRoles<IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
 
 builder.Services.AddControllersWithViews();
+
+/* Configure HttpClient */
+builder.Services.AddHttpClient(
+  name: "",
+  configureClient: options =>
+  {
+    options.BaseAddress = new Uri("https://127.0.0.1:5001");
+    options.DefaultRequestHeaders.Accept.Add(
+      new MediaTypeWithQualityHeaderValue(
+        "application/json", 1.0
+      )
+    );
+  }
+);
 
 var app = builder.Build();
 
@@ -37,11 +52,14 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}"
+);
+
 app.MapRazorPages();
 
 app.Run();
