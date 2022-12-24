034fa4082802e9217e8d10a804ba9609f8d9d8f8
paolomococci <paolomococci@gmail.com>
2022-12-24 22:59:50 +0100

    modified:   workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Controllers/SupplierController.cs

--- workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Controllers/SupplierController.cs
+++ workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Controllers/SupplierController.cs
@@ -5,6 +5,8 @@ using DummyRoster.WebApi.Repositories.Interfaces;
 
 namespace DummyRoster.WebApi.Controllers;
 
+[ApiController]
+[Route("api/suppliers")]
 public class SupplierController : ControllerBase, ISupplierController
 {
 
