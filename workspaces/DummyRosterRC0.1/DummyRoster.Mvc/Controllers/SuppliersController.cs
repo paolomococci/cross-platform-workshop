da03e871edffb931678edf3fa113011b4b0d24f4
paolomococci <paolomococci@gmail.com>
2022-12-24 07:21:34 +0100

    modified:   workspaces/DummyRosterRC0.1/DummyRoster.Mvc/Controllers/SuppliersController.cs

--- workspaces/DummyRosterRC0.1/DummyRoster.Mvc/Controllers/SuppliersController.cs
+++ workspaces/DummyRosterRC0.1/DummyRoster.Mvc/Controllers/SuppliersController.cs
@@ -2,4 +2,17 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace DummyRoster.Mvc.Controllers;
 
-public class SuppliersController : Controller {}
+public class SuppliersController : Controller {
+  private const string baseUri = "https://localhost:5001/api/addresses";
+  private readonly ILogger<SuppliersController> _logger;
+  private readonly HttpClient httpClient;
+
+  public SuppliersController(
+    ILogger<SuppliersController> logger,
+    IHttpClientFactory httpClientFactory
+  )
+  {
+    this._logger = logger;
+    this.httpClient = new HttpClient();
+  }
+}
