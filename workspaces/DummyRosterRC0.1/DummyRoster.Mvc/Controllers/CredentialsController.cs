e6ded27a507dbcbf4e63fe536f8b1e55eb3dfade
paolomococci <paolomococci@gmail.com>
2022-12-24 07:20:20 +0100

    modified:   workspaces/DummyRosterRC0.1/DummyRoster.Mvc/Controllers/CredentialsController.cs

--- workspaces/DummyRosterRC0.1/DummyRoster.Mvc/Controllers/CredentialsController.cs
+++ workspaces/DummyRosterRC0.1/DummyRoster.Mvc/Controllers/CredentialsController.cs
@@ -2,4 +2,17 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace DummyRoster.Mvc.Controllers;
 
-public class CredentialsController : Controller {}
+public class CredentialsController : Controller {
+  private const string baseUri = "https://localhost:5001/api/credentials";
+  private readonly ILogger<CredentialsController> _logger;
+  private readonly HttpClient httpClient;
+
+  public CredentialsController(
+    ILogger<CredentialsController> logger,
+    IHttpClientFactory httpClientFactory
+  )
+  {
+    this._logger = logger;
+    this.httpClient = new HttpClient();
+  }
+}
