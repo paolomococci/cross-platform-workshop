af0917bd30b6664416b50f6ed3baa6be6976beb3
paolomococci <paolomococci@gmail.com>
2022-12-24 07:20:45 +0100

    modified:   workspaces/DummyRosterRC0.1/DummyRoster.Mvc/Controllers/EmployeesController.cs

--- workspaces/DummyRosterRC0.1/DummyRoster.Mvc/Controllers/EmployeesController.cs
+++ workspaces/DummyRosterRC0.1/DummyRoster.Mvc/Controllers/EmployeesController.cs
@@ -2,4 +2,17 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace DummyRoster.Mvc.Controllers;
 
-public class EmployeesController : Controller {}
+public class EmployeesController : Controller {
+  private const string baseUri = "https://localhost:5001/api/employees";
+  private readonly ILogger<EmployeesController> _logger;
+  private readonly HttpClient httpClient;
+
+  public EmployeesController(
+    ILogger<EmployeesController> logger,
+    IHttpClientFactory httpClientFactory
+  )
+  {
+    this._logger = logger;
+    this.httpClient = new HttpClient();
+  }
+}
