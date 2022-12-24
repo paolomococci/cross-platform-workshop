684f705593cd6fe29588d8621d12505e0fe0dffc
paolomococci <paolomococci@gmail.com>
2022-12-24 07:20:57 +0100

    modified:   workspaces/DummyRosterRC0.1/DummyRoster.Mvc/Controllers/FormsController.cs

--- workspaces/DummyRosterRC0.1/DummyRoster.Mvc/Controllers/FormsController.cs
+++ workspaces/DummyRosterRC0.1/DummyRoster.Mvc/Controllers/FormsController.cs
@@ -2,4 +2,17 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace DummyRoster.Mvc.Controllers;
 
-public class FormsController : Controller {}
+public class FormsController : Controller {
+  private const string baseUri = "https://localhost:5001/api/forms";
+  private readonly ILogger<FormsController> _logger;
+  private readonly HttpClient httpClient;
+
+  public FormsController(
+    ILogger<FormsController> logger,
+    IHttpClientFactory httpClientFactory
+  )
+  {
+    this._logger = logger;
+    this.httpClient = new HttpClient();
+  }
+}
