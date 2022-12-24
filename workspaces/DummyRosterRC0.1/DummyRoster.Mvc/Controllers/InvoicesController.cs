11a78de9c694bf8af2b2298372b42d207c7b47ac
paolomococci <paolomococci@gmail.com>
2022-12-24 07:21:07 +0100

    modified:   workspaces/DummyRosterRC0.1/DummyRoster.Mvc/Controllers/InvoicesController.cs

--- workspaces/DummyRosterRC0.1/DummyRoster.Mvc/Controllers/InvoicesController.cs
+++ workspaces/DummyRosterRC0.1/DummyRoster.Mvc/Controllers/InvoicesController.cs
@@ -2,4 +2,17 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace DummyRoster.Mvc.Controllers;
 
-public class InvoicesController : Controller {}
+public class InvoicesController : Controller {
+  private const string baseUri = "https://localhost:5001/api/invoices";
+  private readonly ILogger<InvoicesController> _logger;
+  private readonly HttpClient httpClient;
+
+  public InvoicesController(
+    ILogger<InvoicesController> logger,
+    IHttpClientFactory httpClientFactory
+  )
+  {
+    this._logger = logger;
+    this.httpClient = new HttpClient();
+  }
+}
