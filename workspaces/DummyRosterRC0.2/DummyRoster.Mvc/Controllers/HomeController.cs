eac2a19574436fba6ad891d69b3141c188fd0e0f
paolomococci <paolomococci@gmail.com>
2022-12-26 03:31:05 +0100

    new file:   workspaces/DummyRosterRC0.2/DummyRoster.Mvc/Controllers/HomeController.cs

---
+++ workspaces/DummyRosterRC0.2/DummyRoster.Mvc/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using DummyRoster.Mvc.Models;
+
+namespace DummyRoster.Mvc.Controllers;
+
+public class HomeController : Controller
+{
+  private readonly ILogger<HomeController> _logger;
+
+  public HomeController(ILogger<HomeController> logger)
+  {
+    _logger = logger;
+  }
+
+  [ResponseCache(Duration = 10, Location = ResponseCacheLocation.Any)]
+  public IActionResult Index()
+  {
+    return View();
+  }
+
+  [Route("Restricted")]
+  [Authorize(Roles = "Admins")]
+  public IActionResult Restricted()
+  {
+    return View();
+  }
+
+  [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+  public IActionResult Error()
+  {
+    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+  }
+}
