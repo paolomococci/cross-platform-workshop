88642a68068948785caa9a859759fa7e3d5a0b0e
paolomococci <paolomococci@gmail.com>
2022-12-26 03:36:42 +0100

    new file:   workspaces/DummyRosterRC0.2/DummyRoster.WebApi/Controllers/AddressController.cs

---
+++ workspaces/DummyRosterRC0.2/DummyRoster.WebApi/Controllers/AddressController.cs
@@ -0,0 +1,157 @@
+using Microsoft.AspNetCore.Mvc;
+using DummyRoster.Common.EntityModel.Models;
+using DummyRoster.WebApi.Controllers.Interfaces;
+using DummyRoster.WebApi.Repositories.Interfaces;
+
+namespace DummyRoster.WebApi.Controllers;
+
+[ApiController]
+[Route("api/addresses")]
+public class AddressController : ControllerBase, IAddressController
+{
+
+  private readonly IAddressRepository repository;
+
+  public AddressController(
+    IAddressRepository repo
+  )
+  {
+    this.repository = repo;
+  }
+
+  /* 
+    POST: api/addresses
+    BODY: Address (JSON, XML)
+   */
+  [HttpPost]
+  [ProducesResponseType(
+    201,
+    Type = typeof(Address)
+  )]
+  [ProducesResponseType(400)]
+  public async Task<IActionResult> Create([FromBody] Address entity)
+  {
+    if (entity == null)
+    {
+      return BadRequest();
+    }
+    Address? managedEntity = await this.repository.CreateAsync(entity);
+    if (managedEntity == null)
+    {
+      return BadRequest("Unable to manage entity!");
+    }
+    return CreatedAtRoute(
+      routeName: nameof(ReadAddress),
+      routeValues: new { id = managedEntity.Id },
+      value: managedEntity
+    );
+  }
+
+  /* 
+    GET: api/addresses/[id]
+   */
+  [HttpGet("{id}", Name = nameof(ReadAddress))]
+  [ProducesResponseType(
+    200,
+    Type = typeof(Address)
+  )]
+  [ProducesResponseType(404)]
+  public async Task<IActionResult> ReadAddress(int id)
+  {
+    Address? managedEntity = await this.repository.Retrieve(id);
+    if (managedEntity == null)
+    {
+      return NotFound();
+    }
+    return Ok(managedEntity);
+  }
+
+  /* 
+    GET: api/addresses
+    GET: api/addresses/?country=[country]
+   */
+  [HttpGet]
+  [ProducesResponseType(
+    200,
+    Type = typeof(IEnumerable<Address>)
+  )]
+  public async Task<IEnumerable<Address>> ReadAll(string? country)
+  {
+    if (string.IsNullOrWhiteSpace(country))
+    {
+      return await this.repository.RetrieveAll();
+    }
+    return (await this.repository.RetrieveAll()).Where(
+      entity => entity.Country == country
+    );
+  }
+
+  /* 
+    PUT: api/addresses/id
+    BODY: Address (JSON, XML)
+   */
+  [HttpPut("{id}")]
+  [ProducesResponseType(204)]
+  [ProducesResponseType(400)]
+  [ProducesResponseType(404)]
+  public async Task<IActionResult> Update(int id, [FromBody] Address entity)
+  {
+    if (entity == null || entity.Id != id)
+    {
+      return BadRequest();
+    }
+    Address? managedEntity = await this.repository.Retrieve(id);
+    if (managedEntity == null)
+    {
+      return NotFound();
+    }
+    await this.repository.UpdateAsync(id, entity);
+    return new NoContentResult();
+  }
+
+  /* 
+    PATCH: api/addresses/id
+    BODY: Address (JSON, XML)
+   */
+  [HttpPatch("{id}")]
+  [ProducesResponseType(204)]
+  [ProducesResponseType(400)]
+  [ProducesResponseType(404)]
+  public async Task<IActionResult> PartialUpdate(int id, [FromBody] Address entity)
+  {
+    if (entity == null || entity.Id != id)
+    {
+      return BadRequest();
+    }
+    Address? managedEntity = await this.repository.Retrieve(id);
+    if (managedEntity == null)
+    {
+      return NotFound();
+    }
+    await this.repository.PartialUpdateAsync(id, entity);
+    return new NoContentResult();
+  }
+
+  /* 
+    DELETE: api/addresses/id
+   */
+  [HttpDelete("{id}")]
+  [ProducesResponseType(204)]
+  [ProducesResponseType(400)]
+  [ProducesResponseType(404)]
+  public async Task<IActionResult> Delete(int id)
+  {
+    bool deleted = false;
+    Address? managedEntity = await this.repository.Retrieve(id);
+    if (managedEntity == null)
+    {
+      return NotFound();
+    }
+    deleted = (bool)await this.repository.DeleteAsync(id);
+    if (deleted)
+    {
+      return new NoContentResult();
+    }
+    return BadRequest();
+  }
+}
