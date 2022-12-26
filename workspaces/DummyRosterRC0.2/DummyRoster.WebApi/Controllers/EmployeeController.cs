e34fda61e06a509fd985cf7b960dac96793a4917
paolomococci <paolomococci@gmail.com>
2022-12-26 03:37:30 +0100

    new file:   workspaces/DummyRosterRC0.2/DummyRoster.WebApi/Controllers/EmployeeController.cs

---
+++ workspaces/DummyRosterRC0.2/DummyRoster.WebApi/Controllers/EmployeeController.cs
@@ -0,0 +1,157 @@
+using Microsoft.AspNetCore.Mvc;
+using DummyRoster.Common.EntityModel.Models;
+using DummyRoster.WebApi.Controllers.Interfaces;
+using DummyRoster.WebApi.Repositories.Interfaces;
+
+namespace DummyRoster.WebApi.Controllers;
+
+[ApiController]
+[Route("api/employees")]
+public class EmployeeController : ControllerBase, IEmployeeController
+{
+
+  private readonly IEmployeeRepository repository;
+
+  public EmployeeController(
+    IEmployeeRepository repo
+  )
+  {
+    this.repository = repo;
+  }
+
+  /* 
+    POST: api/employees
+    BODY: Employee (JSON, XML)
+   */
+  [HttpPost]
+  [ProducesResponseType(
+    201,
+    Type = typeof(Employee)
+  )]
+  [ProducesResponseType(400)]
+  public async Task<IActionResult> Create([FromBody] Employee entity)
+  {
+    if (entity == null)
+    {
+      return BadRequest();
+    }
+    Employee? managedEntity = await this.repository.CreateAsync(entity);
+    if (managedEntity == null)
+    {
+      return BadRequest("Unable to manage entity!");
+    }
+    return CreatedAtRoute(
+      routeName: nameof(ReadEmployee),
+      routeValues: new { id = managedEntity.Id },
+      value: managedEntity
+    );
+  }
+
+  /* 
+    GET: api/employees/[id]
+   */
+  [HttpGet("{id}", Name = nameof(ReadEmployee))]
+  [ProducesResponseType(
+    200,
+    Type = typeof(Employee)
+  )]
+  [ProducesResponseType(404)]
+  public async Task<IActionResult> ReadEmployee(int id)
+  {
+    Employee? managedEntity = await this.repository.Retrieve(id);
+    if (managedEntity == null)
+    {
+      return NotFound();
+    }
+    return Ok(managedEntity);
+  }
+
+  /* 
+    GET: api/employees
+    GET: api/employees/?name=[name]
+   */
+  [HttpGet]
+  [ProducesResponseType(
+    200,
+    Type = typeof(IEnumerable<Employee>)
+  )]
+  public async Task<IEnumerable<Employee>> ReadAll(string? name)
+  {
+    if (string.IsNullOrWhiteSpace(name))
+    {
+      return await this.repository.RetrieveAll();
+    }
+    return (await this.repository.RetrieveAll()).Where(
+      entity => entity.Name == name
+    );
+  }
+
+  /* 
+    PUT: api/employees/id
+    BODY: Employee (JSON, XML)
+   */
+  [HttpPut("{id}")]
+  [ProducesResponseType(204)]
+  [ProducesResponseType(400)]
+  [ProducesResponseType(404)]
+  public async Task<IActionResult> Update(int id, [FromBody] Employee entity)
+  {
+    if (entity == null || entity.Id != id)
+    {
+      return BadRequest();
+    }
+    Employee? managedEntity = await this.repository.Retrieve(id);
+    if (managedEntity == null)
+    {
+      return NotFound();
+    }
+    await this.repository.UpdateAsync(id, entity);
+    return new NoContentResult();
+  }
+
+  /* 
+    PATCH: api/employees/id
+    BODY: Employee (JSON, XML)
+   */
+  [HttpPatch("{id}")]
+  [ProducesResponseType(204)]
+  [ProducesResponseType(400)]
+  [ProducesResponseType(404)]
+  public async Task<IActionResult> PartialUpdate(int id, [FromBody] Employee entity)
+  {
+    if (entity == null || entity.Id != id)
+    {
+      return BadRequest();
+    }
+    Employee? managedEntity = await this.repository.Retrieve(id);
+    if (managedEntity == null)
+    {
+      return NotFound();
+    }
+    await this.repository.PartialUpdateAsync(id, entity);
+    return new NoContentResult();
+  }
+
+  /* 
+    DELETE: api/employees/id
+   */
+  [HttpDelete("{id}")]
+  [ProducesResponseType(204)]
+  [ProducesResponseType(400)]
+  [ProducesResponseType(404)]
+  public async Task<IActionResult> Delete(int id)
+  {
+    bool deleted = false;
+    Employee? managedEntity = await this.repository.Retrieve(id);
+    if (managedEntity == null)
+    {
+      return NotFound();
+    }
+    deleted = (bool)await this.repository.DeleteAsync(id);
+    if (deleted)
+    {
+      return new NoContentResult();
+    }
+    return BadRequest();
+  }
+}
