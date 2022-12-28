16511f03007ec14d71be80c3e97fc01cf19dd442
paolomococci <paolomococci@gmail.com>
2022-12-28 16:17:03 +0100

    modified:   spreadsheets/HelloWeb/Spreadsheet.Web/Program.cs

--- spreadsheets/HelloWeb/Spreadsheet.Web/Program.cs
+++ spreadsheets/HelloWeb/Spreadsheet.Web/Program.cs
@@ -1,3 +1,8 @@
+using ClosedXML.Excel;
+
+XLWorkbook xlWorkbook = new XLWorkbook();
+xlWorkbook.Worksheets.Add("Hello").Cell("A1").SetValue<string>("Hello World!");
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
