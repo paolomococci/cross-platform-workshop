97ec9b7f586adde100aed0e5c7be08e9922293e8
paolomococci <paolomococci@gmail.com>
2022-12-28 20:19:43 +0100

    modified:   spreadsheets/HelloWeb/Spreadsheet.Web/Program.cs

--- spreadsheets/HelloWeb/Spreadsheet.Web/Program.cs
+++ spreadsheets/HelloWeb/Spreadsheet.Web/Program.cs
@@ -3,8 +3,6 @@ using ClosedXML.Excel;
 XLWorkbook xlWorkbook = new XLWorkbook();
 xlWorkbook.Worksheets.Add("Hello").Cell("A1").SetValue<string>("Hello World!");
 
-
-
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
