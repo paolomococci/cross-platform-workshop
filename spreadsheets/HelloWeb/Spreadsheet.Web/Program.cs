9641dea66e43a34ff0cfe8721dd758fb8d7b2d80
paolomococci <paolomococci@gmail.com>
2022-12-28 16:30:41 +0100

    modified:   spreadsheets/HelloWeb/Spreadsheet.Web/Program.cs

--- spreadsheets/HelloWeb/Spreadsheet.Web/Program.cs
+++ spreadsheets/HelloWeb/Spreadsheet.Web/Program.cs
@@ -3,9 +3,16 @@ using ClosedXML.Excel;
 XLWorkbook xlWorkbook = new XLWorkbook();
 xlWorkbook.Worksheets.Add("Hello").Cell("A1").SetValue<string>("Hello World!");
 
+
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet(
+  "/", 
+  () => {
+    return xlWorkbook.Worksheets.Worksheet("Hello").Cell("A1").GetValue<string>();
+  }
+);
 
 app.Run();
