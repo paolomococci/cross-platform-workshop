ca5add4bf10655c4fe0c12f27aaad3f220bd5ad9
paolomococci <paolomococci@gmail.com>
2022-12-29 09:06:18 +0100

    modified:   spreadsheets/FormulasWebApi/Spreadsheet.WebApi/Controllers/SpreadsheetController.cs

--- spreadsheets/FormulasWebApi/Spreadsheet.WebApi/Controllers/SpreadsheetController.cs
+++ spreadsheets/FormulasWebApi/Spreadsheet.WebApi/Controllers/SpreadsheetController.cs
@@ -1,5 +1,6 @@
 using ClosedXML.Excel;
 using Microsoft.AspNetCore.Mvc;
+using Spreadsheet.WebApi.Templates;
 
 namespace Spreadsheet.WebApi.Controllers;
 
@@ -17,24 +18,8 @@ public class SpreadsheetController : ControllerBase
   {
     using (MemoryStream memoryStream = new MemoryStream())
     {
-      var xlWorkbook = new XLWorkbook();
-      var sheetNames = new List<string>() {
-        "Sheet1",
-        "Sheet2",
-        "Sheet3"
-      };
-      foreach (var sheetName in sheetNames)
-      {
-        var xlWorksheet = xlWorkbook.Worksheets.Add(sheetName);
-        xlWorksheet.Cell("A1").Value = sheetName;
-      }
-      xlWorkbook.SaveAs(memoryStream);
-      memoryStream.Seek(
-        0,
-        SeekOrigin.Begin
-      );
       return this.File(
-        fileContents: memoryStream.ToArray(),
+        fileContents: AdjuvantTemplate.Perform(memoryStream),
         contentType: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
         fileDownloadName: "Generated.xlsx"
       );
