7aa70083377ee02d05a67699e19f9cf070b1a504
paolomococci <paolomococci@gmail.com>
2022-12-29 17:44:06 +0100

    modified:   spreadsheets/FormulasWebApi/Spreadsheet.WebApi/Templates/AdjuvantTemplate.cs

--- spreadsheets/FormulasWebApi/Spreadsheet.WebApi/Templates/AdjuvantTemplate.cs
+++ spreadsheets/FormulasWebApi/Spreadsheet.WebApi/Templates/AdjuvantTemplate.cs
@@ -33,17 +33,17 @@ public class AdjuvantTemplate
     sheet1.Cell("B6").FormulaA1 = "SUM(B2:B5)";
     sheet1.Cell("B7").FormulaA1 = "AVERAGE(B2:B5)";
     /* performs calculations programmatically */
-    decimal[] decimalNumbers = new decimal[4];
-    decimalNumbers[0] = sheet1.Cell("B2").GetValue<decimal>();
-    decimalNumbers[1] = sheet1.Cell("B3").GetValue<decimal>();
-    decimalNumbers[2] = sheet1.Cell("B4").GetValue<decimal>();
-    decimalNumbers[3] = sheet1.Cell("B5").GetValue<decimal>();
-
-    sheet1.Cell("B12").SetValue(
-      (decimalNumbers[0] + decimalNumbers[1] + decimalNumbers[2] + decimalNumbers[3])
+    List<decimal> decimalNumbers = new();
+    decimalNumbers.Add(sheet1.Cell("B2").GetValue<decimal>());
+    decimalNumbers.Add(sheet1.Cell("B3").GetValue<decimal>());
+    decimalNumbers.Add(sheet1.Cell("B4").GetValue<decimal>());
+    decimalNumbers.Add(sheet1.Cell("B5").GetValue<decimal>());
+    sheet1.Cell("C1").SetValue("compute programmatically");
+    sheet1.Cell("C6").SetValue(
+      decimalNumbers.Sum()
     );
-    sheet1.Cell("B13").SetValue(
-      (decimalNumbers[0] + decimalNumbers[1] + decimalNumbers[2] + decimalNumbers[3]) / decimalNumbers.Length
+    sheet1.Cell("C7").SetValue(
+      decimalNumbers.Sum() / decimalNumbers.Count()
     );
 
     /* pack it all up */
