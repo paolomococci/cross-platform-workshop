c83c80a6f36e297f69946a75831eafc40b0c1ac2
paolomococci <paolomococci@gmail.com>
2022-12-29 09:06:28 +0100

    modified:   spreadsheets/FormulasWebApi/Spreadsheet.WebApi/Templates/AdjuvantTemplate.cs

--- spreadsheets/FormulasWebApi/Spreadsheet.WebApi/Templates/AdjuvantTemplate.cs
+++ spreadsheets/FormulasWebApi/Spreadsheet.WebApi/Templates/AdjuvantTemplate.cs
@@ -1,6 +1,27 @@
+using ClosedXML.Excel;
+
 namespace Spreadsheet.WebApi.Templates;
 
 public class AdjuvantTemplate
 {
-  public void Perform() { }
+  public static byte[] Perform(MemoryStream memoryStream)
+  {
+    XLWorkbook xlWorkbook = new XLWorkbook();
+    List<string> sheetNames = new List<string>() {
+        "Sheet1",
+        "Sheet2",
+        "Sheet3"
+      };
+    foreach (var sheetName in sheetNames)
+    {
+      var xlWorksheet = xlWorkbook.Worksheets.Add(sheetName);
+      xlWorksheet.Cell("A1").Value = sheetName;
+    }
+    xlWorkbook.SaveAs(memoryStream);
+    memoryStream.Seek(
+      0,
+      SeekOrigin.Begin
+    );
+    return memoryStream.ToArray();
+  }
 }
