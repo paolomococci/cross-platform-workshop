dd1437e2c7930fd8fa028c3b8c9c3a2ec9944c20
paolomococci <paolomococci@gmail.com>
2022-12-30 11:14:51 +0100

    modified:   spreadsheets/CountryMenuWebApi/Spreadsheet.WebApi/Models/TableModel.cs

--- spreadsheets/CountryMenuWebApi/Spreadsheet.WebApi/Models/TableModel.cs
+++ spreadsheets/CountryMenuWebApi/Spreadsheet.WebApi/Models/TableModel.cs
@@ -1,6 +1,7 @@
 namespace Spreadsheet.WebApi.Models;
 
-public class TableModel {
+public class TableModel
+{
   public int Id { get; set; }
   public string Name { get; set; } = string.Empty;
   public string Description { get; set; } = string.Empty;
