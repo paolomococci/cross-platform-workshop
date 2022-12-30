ecce06b3d473310df5120a0c6f12a7f8bf824c0d
paolomococci <paolomococci@gmail.com>
2022-12-30 11:49:50 +0100

    modified:   spreadsheets/CountryMenuWebApi/Spreadsheet.WebApi/Models/BillModel.cs

--- spreadsheets/CountryMenuWebApi/Spreadsheet.WebApi/Models/BillModel.cs
+++ spreadsheets/CountryMenuWebApi/Spreadsheet.WebApi/Models/BillModel.cs
@@ -1,6 +1,7 @@
 namespace Spreadsheet.WebApi.Models;
 
-public class OrderModel {
+public class OrderModel
+{
   public int Id { get; set; }
   public int TableId { get; set; }
   public int MenuId { get; set; }
