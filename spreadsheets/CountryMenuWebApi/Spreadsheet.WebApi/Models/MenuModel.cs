802d9d634b2d02408c8a8d4b3f3a86241684b82d
paolomococci <paolomococci@gmail.com>
2022-12-30 11:14:12 +0100

    modified:   spreadsheets/CountryMenuWebApi/Spreadsheet.WebApi/Models/MenuModel.cs

--- spreadsheets/CountryMenuWebApi/Spreadsheet.WebApi/Models/MenuModel.cs
+++ spreadsheets/CountryMenuWebApi/Spreadsheet.WebApi/Models/MenuModel.cs
@@ -3,4 +3,5 @@ namespace Spreadsheet.WebApi.Models;
 public class MenuModel {
   public int Id { get; set; }
   public string Name { get; set; } = string.Empty;
+  public string Description { get; set; } = string.Empty;
 }
