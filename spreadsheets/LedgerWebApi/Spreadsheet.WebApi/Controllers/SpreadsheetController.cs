4b190fe0d8ce993ba68b1bb14882bee4eb2d41aa
paolomococci <paolomococci@gmail.com>
2022-12-31 07:30:24 +0100

    new file:   spreadsheets/LedgerWebApi/Spreadsheet.WebApi/Controllers/SpreadsheetController.cs

---
+++ spreadsheets/LedgerWebApi/Spreadsheet.WebApi/Controllers/SpreadsheetController.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+using Spreadsheet.WebApi.Templates;
+
+namespace Spreadsheet.WebApi.Controllers;
+
+[ApiController]
+[Route("api/spreadsheets")]
+public class SpreadsheetController : ControllerBase
+{
+
+  /* 
+    GET: api/spreadsheets/generated
+   */
+  [HttpGet("generated")]
+  [ProducesResponseType(200)]
+  public IActionResult Generate()
+  {
+    using (MemoryStream memoryStream = new MemoryStream())
+    {
+      return this.File(
+        fileContents: AdjuvantTemplate.Perform(memoryStream),
+        contentType: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+        fileDownloadName: "Adjuvant.xlsx"
+      );
+    }
+  }
+}
