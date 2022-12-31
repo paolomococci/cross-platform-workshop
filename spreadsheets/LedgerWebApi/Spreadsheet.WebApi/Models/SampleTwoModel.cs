4f01135a5d0c7544f40ce5b55ca25b9f36ac0d1a
paolomococci <paolomococci@gmail.com>
2022-12-31 07:30:59 +0100

    new file:   spreadsheets/LedgerWebApi/Spreadsheet.WebApi/Models/SampleTwoModel.cs

---
+++ spreadsheets/LedgerWebApi/Spreadsheet.WebApi/Models/SampleTwoModel.cs
@@ -0,0 +1,8 @@
+namespace Spreadsheet.WebApi.Models;
+
+public class SampleTwoModel
+{
+  public int Id { get; set; }
+  public string Name { get; set; } = string.Empty;
+  public int Number { get; set; }
+}
