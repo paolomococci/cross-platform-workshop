e86b029f7ebf7b027eeb184445c76eb73e3b3bfd
paolomococci <paolomococci@gmail.com>
2023-01-01 16:36:27 +0100

    modified:   spreadsheets/KaleidoscopeWebApi/Spreadsheet.WebApi/Models/LedgerModel.cs

--- spreadsheets/KaleidoscopeWebApi/Spreadsheet.WebApi/Models/LedgerModel.cs
+++ spreadsheets/KaleidoscopeWebApi/Spreadsheet.WebApi/Models/LedgerModel.cs
@@ -1,3 +1,9 @@
 namespace Spreadsheet.WebApi.Models;
 
-public class LedgerModel {}
+public class LedgerModel
+{
+  internal static byte[] Perform(MemoryStream memoryStream)
+  {
+    throw new NotImplementedException();
+  }
+}
