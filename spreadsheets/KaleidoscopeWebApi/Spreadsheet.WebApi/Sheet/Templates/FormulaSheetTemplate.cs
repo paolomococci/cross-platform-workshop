835466c0183d492359799fe4dac8c349a0d410bd
paolomococci <paolomococci@gmail.com>
2023-01-01 18:01:39 +0100

    modified:   spreadsheets/KaleidoscopeWebApi/Spreadsheet.WebApi/Sheet/Templates/FormulaSheetTemplate.cs

--- spreadsheets/KaleidoscopeWebApi/Spreadsheet.WebApi/Sheet/Templates/FormulaSheetTemplate.cs
+++ spreadsheets/KaleidoscopeWebApi/Spreadsheet.WebApi/Sheet/Templates/FormulaSheetTemplate.cs
@@ -4,5 +4,7 @@ namespace Spreadsheet.WebApi.Sheet.Templates;
 
 public class FormulaSheetTemplate
 {
-  internal static void Transcribe(XLWorkbook xLWorkbook) { }
+  internal static void Transcribe(XLWorkbook xLWorkbook) {
+    var formulaSheet = xLWorkbook.Worksheet("FormulaSheet");
+  }
 }
