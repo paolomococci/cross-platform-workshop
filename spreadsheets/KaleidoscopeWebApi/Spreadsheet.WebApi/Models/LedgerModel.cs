c767b8a69072cd46b28ee7a67e1c3dc53df0ed32
paolomococci <paolomococci@gmail.com>
2023-01-01 18:07:55 +0100

    modified:   spreadsheets/KaleidoscopeWebApi/Spreadsheet.WebApi/Models/LedgerModel.cs

--- spreadsheets/KaleidoscopeWebApi/Spreadsheet.WebApi/Models/LedgerModel.cs
+++ spreadsheets/KaleidoscopeWebApi/Spreadsheet.WebApi/Models/LedgerModel.cs
@@ -1,4 +1,5 @@
 using ClosedXML.Excel;
+using Spreadsheet.WebApi.Sheet.Templates;
 
 namespace Spreadsheet.WebApi.Models;
 
@@ -21,30 +22,7 @@ public class LedgerModel
       sheet.Cell("A1").Value = sheetName;
     }
 
-    /* adds data and formulas to a specific spreadsheet */
-    var formulaSheet = ledger.Worksheet("FormulaSheet");
-    formulaSheet.Cell("B1").SetValue("Values");
-    formulaSheet.Cell("B2").SetValue(4.55);
-    formulaSheet.Cell("B3").SetValue(2.55);
-    formulaSheet.Cell("B4").SetValue(4.2);
-    formulaSheet.Cell("B5").SetValue(3.33);
-    formulaSheet.Cell("A6").SetValue("Sum");
-    formulaSheet.Cell("A7").SetValue("Average");
-    formulaSheet.Cell("B6").FormulaA1 = "SUM(B2:B5)";
-    formulaSheet.Cell("B7").FormulaA1 = "AVERAGE(B2:B5)";
-    /* performs calculations programmatically */
-    List<decimal> decimalNumbers = new();
-    decimalNumbers.Add(formulaSheet.Cell("B2").GetValue<decimal>());
-    decimalNumbers.Add(formulaSheet.Cell("B3").GetValue<decimal>());
-    decimalNumbers.Add(formulaSheet.Cell("B4").GetValue<decimal>());
-    decimalNumbers.Add(formulaSheet.Cell("B5").GetValue<decimal>());
-    formulaSheet.Cell("C1").SetValue("compute programmatically");
-    formulaSheet.Cell("C6").SetValue(
-      decimalNumbers.Sum()
-    );
-    formulaSheet.Cell("C7").SetValue(
-      decimalNumbers.Sum() / decimalNumbers.Count()
-    );
+    FormulaSheetTemplate.Transcribe(ledger);
 
     /* pack it all up */
     ledger.SaveAs(memoryStream);
