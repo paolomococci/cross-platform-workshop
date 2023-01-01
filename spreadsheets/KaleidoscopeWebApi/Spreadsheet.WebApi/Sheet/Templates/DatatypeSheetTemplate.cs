e6b8f253d1d1c346fccabf2286c0990559dfc58c
paolomococci <paolomococci@gmail.com>
2023-01-01 18:01:30 +0100

    modified:   spreadsheets/KaleidoscopeWebApi/Spreadsheet.WebApi/Sheet/Templates/DatatypeSheetTemplate.cs

--- spreadsheets/KaleidoscopeWebApi/Spreadsheet.WebApi/Sheet/Templates/DatatypeSheetTemplate.cs
+++ spreadsheets/KaleidoscopeWebApi/Spreadsheet.WebApi/Sheet/Templates/DatatypeSheetTemplate.cs
@@ -4,5 +4,7 @@ namespace Spreadsheet.WebApi.Sheet.Templates;
 
 public class DatatypeSheetTemplate
 {
-  internal static void Transcribe(XLWorkbook xLWorkbook) { }
+  internal static void Transcribe(XLWorkbook xLWorkbook) {
+    var formulaSheet = xLWorkbook.Worksheet("DatatypeSheet");
+  }
 }
