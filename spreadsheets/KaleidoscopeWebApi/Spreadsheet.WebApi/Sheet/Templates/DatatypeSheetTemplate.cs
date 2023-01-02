9768c6f6c0791abe753e13aa538f9f189c6d1277
paolomococci <paolomococci@gmail.com>
2023-01-02 03:09:41 +0100

    modified:   spreadsheets/KaleidoscopeWebApi/Spreadsheet.WebApi/Sheet/Templates/DatatypeSheetTemplate.cs

--- spreadsheets/KaleidoscopeWebApi/Spreadsheet.WebApi/Sheet/Templates/DatatypeSheetTemplate.cs
+++ spreadsheets/KaleidoscopeWebApi/Spreadsheet.WebApi/Sheet/Templates/DatatypeSheetTemplate.cs
@@ -6,7 +6,11 @@ public class DatatypeSheetTemplate
 {
   internal static void Transcribe(XLWorkbook xLWorkbook)
   {
+    int row = 2;
+    int column = 2;
     /* adds data and formulas to the spreadsheet named DatatypeSheet */
     var datatypeSheet = xLWorkbook.Worksheet("DatatypeSheet");
+    
+    datatypeSheet.Cell(row, column).SetValue<string>("Sample text data type:");
   }
 }
