f6fe09e1016bf7ccaad58c84b7727ba56a825e62
paolomococci <paolomococci@gmail.com>
2023-01-02 16:19:33 +0100

    modified:   spreadsheets/KaleidoscopeWebApi/Spreadsheet.WebApi/Models/ItemModel.cs

--- spreadsheets/KaleidoscopeWebApi/Spreadsheet.WebApi/Models/ItemModel.cs
+++ spreadsheets/KaleidoscopeWebApi/Spreadsheet.WebApi/Models/ItemModel.cs
@@ -5,4 +5,6 @@ public class ItemModel
   public string Name { get; set; } = string.Empty;
   public int Orders { get; set; }
   public string Quarter { get; set; } = string.Empty;
+
+  public ItemModel() {}
 }
