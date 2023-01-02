30c8072ccc9696b925d96262322fb8f051adbc4e
paolomococci <paolomococci@gmail.com>
2023-01-02 03:30:51 +0100

    modified:   spreadsheets/KaleidoscopeWebApi/Spreadsheet.WebApi/Sheet/Templates/DatatypeSheetTemplate.cs

--- spreadsheets/KaleidoscopeWebApi/Spreadsheet.WebApi/Sheet/Templates/DatatypeSheetTemplate.cs
+++ spreadsheets/KaleidoscopeWebApi/Spreadsheet.WebApi/Sheet/Templates/DatatypeSheetTemplate.cs
@@ -12,15 +12,21 @@ public class DatatypeSheetTemplate
     var datatypeSheet = xLWorkbook.Worksheet("DatatypeSheet");
 
     datatypeSheet.Cell(row, column).SetValue<string>("Simple text examples:");
+    datatypeSheet.Cell(row, (column + 1)).SetValue<string>("Hello everyone!");
 
     datatypeSheet.Cell(++row, column).SetValue<string>("Date examples:");
+    datatypeSheet.Cell(row, (column + 1)).SetValue<DateTime>(new DateTime(2023, 1, 2));
 
     datatypeSheet.Cell(++row, column).SetValue<string>("Examples of date and time:");
+    datatypeSheet.Cell(row, (column + 1)).SetValue<DateTime>(new DateTime(2023, 1, 2, 2, 0, 0));
 
     datatypeSheet.Cell(++row, column).SetValue<string>("Examples of Boolean values:");
+    datatypeSheet.Cell(row, (column + 1)).SetValue<bool>(true);
 
     datatypeSheet.Cell(++row, column).SetValue<string>("Examples of numeric values:");
+    datatypeSheet.Cell(row, (column + 1)).SetValue<int>(10);
 
     datatypeSheet.Cell(++row, column).SetValue<string>("Examples of time span:");
+    datatypeSheet.Cell(row, (column + 1)).SetValue<TimeSpan>(new TimeSpan(1000));
   }
 }
