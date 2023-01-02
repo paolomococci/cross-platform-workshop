75b132846983ae333cc52749f835698aec37e008
paolomococci <paolomococci@gmail.com>
2023-01-02 16:26:28 +0100

    modified:   spreadsheets/KaleidoscopeWebApi/Spreadsheet.WebApi/Sheet/Templates/PinnedSheetTemplate.cs

--- spreadsheets/KaleidoscopeWebApi/Spreadsheet.WebApi/Sheet/Templates/PinnedSheetTemplate.cs
+++ spreadsheets/KaleidoscopeWebApi/Spreadsheet.WebApi/Sheet/Templates/PinnedSheetTemplate.cs
@@ -1,8 +1,11 @@
 using ClosedXML.Excel;
+using Spreadsheet.WebApi.Models;
 
 namespace Spreadsheet.WebApi.Sheet.Templates;
 
 public class PinnedSheetTemplate
 {
-  internal static void Transcribe(XLWorkbook xLWorkbook) { }
+  internal static void Transcribe(XLWorkbook xLWorkbook) {
+    List<ItemModel> items = new();
+  }
 }
