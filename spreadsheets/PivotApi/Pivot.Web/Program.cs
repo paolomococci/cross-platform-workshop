77fcf53f3d2a655cd1772539a7e86995a75d6978
paolomococci <paolomococci@gmail.com>
2023-01-03 16:50:14 +0100

    modified:   spreadsheets/PivotApi/Pivot.Web/Program.cs

--- spreadsheets/PivotApi/Pivot.Web/Program.cs
+++ spreadsheets/PivotApi/Pivot.Web/Program.cs
@@ -1,10 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
 
 var app = builder.Build();
-
-app.MapGet(
-  "/", 
-  () => "Pivot.Web project feedback message."
-);
-
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
 app.Run();
