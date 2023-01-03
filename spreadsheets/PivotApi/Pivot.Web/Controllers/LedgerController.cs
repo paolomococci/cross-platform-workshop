e115b9b6bd6e64ffe6d6556c5d27cb3ff47522bf
paolomococci <paolomococci@gmail.com>
2023-01-03 16:42:45 +0100

    modified:   spreadsheets/PivotApi/Pivot.Web/Controllers/LedgerController.cs

--- spreadsheets/PivotApi/Pivot.Web/Controllers/LedgerController.cs
+++ spreadsheets/PivotApi/Pivot.Web/Controllers/LedgerController.cs
@@ -1,8 +1,26 @@
 using Microsoft.AspNetCore.Mvc;
+using Pivot.Common.Models;
 
 namespace Pivot.Web.Controllers;
 
 
 [ApiController]
 [Route("api/spreadsheets")]
-public class LedgerController : ControllerBase {}
+public class LedgerController : ControllerBase {
+  /* 
+    GET: api/spreadsheets/generated
+   */
+  [HttpGet("generated")]
+  [ProducesResponseType(200)]
+  public IActionResult Generate()
+  {
+    using (MemoryStream memoryStream = new MemoryStream())
+    {
+      return this.File(
+        fileContents: LedgerModel.Perform(memoryStream),
+        contentType: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+        fileDownloadName: "Ledger.xlsx"
+      );
+    }
+  }
+}
