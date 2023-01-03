b291e61d5f15155b2051abf674c140e7c96ee332
paolomococci <paolomococci@gmail.com>
2023-01-03 19:39:49 +0100

    new file:   spreadsheets/PivotUpRC0/Pivot.Api/Controllers/LedgerController.cs

---
+++ spreadsheets/PivotUpRC0/Pivot.Api/Controllers/LedgerController.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+using Pivot.Common.Models;
+
+namespace Pivot.Api.Controllers;
+
+
+[ApiController]
+[Route("api")]
+public class LedgerController : ControllerBase
+{
+  /* 
+    GET: http://localhost:8081/api/pivot
+   */
+  [HttpGet("pivot")]
+  [ProducesResponseType(200)]
+  public IActionResult Generate()
+  {
+    using (MemoryStream memoryStream = new MemoryStream())
+    {
+      return this.File(
+        fileContents: LedgerModel.Perform(memoryStream),
+        contentType: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+        fileDownloadName: "Pivot.xlsx"
+      );
+    }
+  }
+}
