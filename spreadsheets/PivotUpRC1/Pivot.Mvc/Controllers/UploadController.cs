ab9ab6a742f481c362a7c89be60a13cf0ca271a0
paolomococci <paolomococci@gmail.com>
2023-01-04 09:53:48 +0100

    modified:   spreadsheets/PivotUpRC1/Pivot.Mvc/Controllers/UploadController.cs

--- spreadsheets/PivotUpRC1/Pivot.Mvc/Controllers/UploadController.cs
+++ spreadsheets/PivotUpRC1/Pivot.Mvc/Controllers/UploadController.cs
@@ -2,4 +2,11 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Pivot.Mvc.Controllers;
 
-public class UploadController : Controller {}
+public class UploadController : Controller {
+  private readonly ILogger<HomeController> _logger;
+
+  public UploadController(ILogger<HomeController> logger)
+  {
+    _logger = logger;
+  }
+}
