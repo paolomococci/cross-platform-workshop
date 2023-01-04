fdd1ecf945363cb1fd6f1715cfdff5f62ea243b5
paolomococci <paolomococci@gmail.com>
2023-01-04 09:32:57 +0100

    modified:   spreadsheets/PivotUpRC1/Pivot.Mvc/Program.cs

--- spreadsheets/PivotUpRC1/Pivot.Mvc/Program.cs
+++ spreadsheets/PivotUpRC1/Pivot.Mvc/Program.cs
@@ -1,41 +1,47 @@
 using System.Net.Http.Headers;
-using Microsoft.AspNetCore.Mvc;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-
-builder.Services.AddHttpClient(
-  name: "Pivot.Api",
-  configureClient: options => {
-    options.BaseAddress = new Uri("https://localhost:8083/");
-    options.DefaultRequestHeaders.Accept.Add(
-      new MediaTypeWithQualityHeaderValue(
-        "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-      )
-    );
-  }
-);
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+internal class Program
 {
-  app.UseExceptionHandler("/Home/Error");
-  app.UseHsts();
+  private static void Main(string[] args)
+  {
+    var builder = WebApplication.CreateBuilder(args);
+
+    // Add services to the container.
+    builder.Services.AddControllersWithViews();
+
+    builder.Services.AddHttpClient(
+      name: "Pivot.Api",
+      configureClient: options =>
+      {
+        options.BaseAddress = new Uri("https://localhost:8083/");
+        options.DefaultRequestHeaders.Accept.Add(
+          new MediaTypeWithQualityHeaderValue(
+            "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+          )
+        );
+      }
+    );
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (!app.Environment.IsDevelopment())
+    {
+      app.UseExceptionHandler("/Home/Error");
+      app.UseHsts();
+    }
+
+    app.UseHttpsRedirection();
+    app.UseStaticFiles();
+
+    app.UseRouting();
+
+    app.UseAuthorization();
+
+    app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+
+    app.Run();
+  }
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.Run();
