4bb61a94a1122d79d00c45c50ddeed2578effe0e
paolomococci <paolomococci@gmail.com>
2023-01-05 08:59:51 +0100

    modified:   spreadsheets/PivotUpRC2/Pivot.Mvc/Controllers/UploadController.cs

--- spreadsheets/PivotUpRC2/Pivot.Mvc/Controllers/UploadController.cs
+++ spreadsheets/PivotUpRC2/Pivot.Mvc/Controllers/UploadController.cs
@@ -11,30 +11,16 @@ public class UploadController : Controller
     _logger = logger;
   }
 
-  public IActionResult Index()
+  [HttpPost]
+  public IActionResult Upload()
   {
+    // todo
     return View();
   }
 
   [HttpGet]
   public IActionResult Uploaded()
   {
-    return View();
-  }
-
-  [HttpPost]
-  public IActionResult Upload(List<IFormFile> items)
-  {
-    string uploadedPath = Path.Combine("../Uploaded", "Uploads");
-    List<string> uploadedFiles = new List<string>();
-    foreach (IFormFile item in items) {
-      string fileName = Path.GetFileName(item.FileName);
-      using (FileStream stream = new FileStream(Path.Combine(uploadedPath, fileName), FileMode.Create)) {
-        item.CopyTo(stream);
-        uploadedFiles.Add(fileName);
-        ViewBag.Message += string.Format("<b>{0}</b> uploaded.<br />", fileName);
-      }
-    }
     return View();
   }
 }
