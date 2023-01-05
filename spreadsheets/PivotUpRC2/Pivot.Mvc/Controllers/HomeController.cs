7e1bf940121f7506f453e61dfef07d5ebe664011
paolomococci <paolomococci@gmail.com>
2023-01-05 06:48:43 +0100

    modified:   spreadsheets/PivotUpRC2/Pivot.Mvc/Controllers/HomeController.cs

--- spreadsheets/PivotUpRC2/Pivot.Mvc/Controllers/HomeController.cs
+++ spreadsheets/PivotUpRC2/Pivot.Mvc/Controllers/HomeController.cs
@@ -28,27 +28,13 @@ public class HomeController : Controller
     return View();
   }
 
-  [HttpPost]
-  public IActionResult Upload(List<IFormFile> items)
-  {
-    string uploadedPath = Path.Combine("../Uploaded", "Uploads");
-    List<string> uploadedFiles = new List<string>();
-    foreach (IFormFile item in items)
-    {
-      string fileName = Path.GetFileName(item.FileName);
-      using (FileStream stream = new FileStream(Path.Combine(uploadedPath, fileName), FileMode.Create))
-      {
-        item.CopyTo(stream);
-        uploadedFiles.Add(fileName);
-        ViewBag.Message += string.Format("<em>{0}</em> uploaded.<br>", fileName);
-      }
-    }
-    return View();
-  }
-
   [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
   public IActionResult Error()
   {
-    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    return View(
+      new ErrorViewModel {
+        RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
+      }
+    );
   }
 }
