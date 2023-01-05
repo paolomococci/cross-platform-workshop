f40c954a9478654503bd8c57c6b01c658491ec05
paolomococci <paolomococci@gmail.com>
2023-01-06 00:25:30 +0100

    modified:   sample/StoaAppRC0/Stoa.Web/Models/PostModel.cs

--- sample/StoaAppRC0/Stoa.Web/Models/PostModel.cs
+++ sample/StoaAppRC0/Stoa.Web/Models/PostModel.cs
@@ -3,4 +3,5 @@ namespace Stoa.Web.Models;
 public class PostModel {
   public string Caption { get; set; } = string.Empty;
   public string Description { get; set; } = string.Empty;
+  public IFormFile? Concept { get; set; }
 }
