81861808d3a07d0e18c860c021ec3db3e08631ba
paolomococci <paolomococci@gmail.com>
2023-01-05 23:10:27 +0100

    modified:   sample/StoaAppRC0/Stoa.Web/Models/ErrorViewModel.cs

--- sample/StoaAppRC0/Stoa.Web/Models/ErrorViewModel.cs
+++ sample/StoaAppRC0/Stoa.Web/Models/ErrorViewModel.cs
@@ -1,3 +1,8 @@
 namespace Stoa.Web.Models;
 
-public class ErrorViewModel {}
+public class ErrorViewModel
+{
+  public string? RequestId { get; set; }
+
+  public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
