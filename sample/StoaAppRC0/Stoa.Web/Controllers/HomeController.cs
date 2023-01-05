a76cfb09515403f6a0a89ddefa5d0be64361253d
paolomococci <paolomococci@gmail.com>
2023-01-05 23:10:17 +0100

    modified:   sample/StoaAppRC0/Stoa.Web/Controllers/HomeController.cs

--- sample/StoaAppRC0/Stoa.Web/Controllers/HomeController.cs
+++ sample/StoaAppRC0/Stoa.Web/Controllers/HomeController.cs
@@ -1,3 +1,41 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Stoa.Web.Models;
+
 namespace Stoa.Web.Controllers;
 
-public class HomeController {}
+public class HomeController : Controller
+{
+  private readonly ILogger<HomeController> _logger;
+
+  public HomeController(ILogger<HomeController> logger)
+  {
+    _logger = logger;
+  }
+
+  public IActionResult Index()
+  {
+    return View();
+  }
+
+  public IActionResult Upload()
+  {
+    return View();
+  }
+
+  public IActionResult Uploaded()
+  {
+    return View();
+  }
+
+  [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+  public IActionResult Error()
+  {
+    return View(
+      new ErrorViewModel
+      {
+        RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
+      }
+    );
+  }
+}
