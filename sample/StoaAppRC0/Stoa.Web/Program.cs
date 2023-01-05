d5d841420368de6f45c0bc3b078c603c1d3b1f0c
paolomococci <paolomococci@gmail.com>
2023-01-05 23:10:06 +0100

    modified:   sample/StoaAppRC0/Stoa.Web/Program.cs

--- sample/StoaAppRC0/Stoa.Web/Program.cs
+++ sample/StoaAppRC0/Stoa.Web/Program.cs
@@ -1,6 +1,34 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+namespace Stoa.Web;
 
-app.MapGet("/", () => "Hello World!");
+internal class Program
+{
+  private static void Main(string[] args)
+  {
+    var builder = WebApplication.CreateBuilder(args);
 
-app.Run();
+    // Add services to the container.
+    builder.Services.AddControllersWithViews();
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (!app.Environment.IsDevelopment())
+    {
+      app.UseExceptionHandler("/Home/Error");
+      app.UseHsts();
+    }
+
+    app.UseHttpsRedirection();
+    app.UseStaticFiles();
+
+    app.UseRouting();
+
+    app.UseAuthorization();
+
+    app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+
+    app.Run();
+  }
+}
