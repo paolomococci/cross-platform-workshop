66f99cea530f98eb6797f7af19f5be0607f09ea2
paolomococci <paolomococci@gmail.com>
2023-01-06 22:37:19 +0100

    new file:   examples/PeopleLibrary/RoyalFlightComfort.cs

---
+++ examples/PeopleLibrary/RoyalFlightComfort.cs
@@ -0,0 +1,10 @@
+﻿namespace Sample.Shared;
+public class RoyalClass
+{
+    public int LoyaltyPoints { get; set; }
+
+    public override string ToString()
+    {
+        return $"royal class with {LoyaltyPoints:N0} air miles";
+    }
+}
