f40e28fcff41af0965578be5235c9f539a31e88f
paolomococci <paolomococci@gmail.com>
2023-01-09 02:54:02 +0100

    modified:   spreadsheets/PivotUpRC3/Pivot.Mvc.Feather/Models/ErrorViewModel.cs

--- spreadsheets/PivotUpRC3/Pivot.Mvc.Feather/Models/ErrorViewModel.cs
+++ spreadsheets/PivotUpRC3/Pivot.Mvc.Feather/Models/ErrorViewModel.cs
@@ -1,6 +1,7 @@
 namespace Pivot.Mvc.Feather.Models;
 
-public class ErrorViewModel {
+public class ErrorViewModel
+{
   public string? RequestId { get; set; }
 
   public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
