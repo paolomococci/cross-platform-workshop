071ca6b376b5d156812202465a716d1b7e3f9473
paolomococci <paolomococci@gmail.com>
2023-01-09 02:54:33 +0100

    modified:   spreadsheets/PivotUpRC3/Pivot.Mvc.Feather/Models/PostModel.cs

--- spreadsheets/PivotUpRC3/Pivot.Mvc.Feather/Models/PostModel.cs
+++ spreadsheets/PivotUpRC3/Pivot.Mvc.Feather/Models/PostModel.cs
@@ -1,6 +1,7 @@
 namespace Pivot.Mvc.Feather.Models;
 
-public class PostModel {
+public class PostModel
+{
   public string Caption { get; set; } = string.Empty;
   public string Description { get; set; } = string.Empty;
   public IFormFile? Concept { get; set; }
