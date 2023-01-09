06460485ac941df420228939151804472cf21446
paolomococci <paolomococci@gmail.com>
2023-01-09 03:01:44 +0100

    modified:   spreadsheets/PivotUpRC3/Pivot.Mvc.Feather/Controllers/LedgerController.cs

--- spreadsheets/PivotUpRC3/Pivot.Mvc.Feather/Controllers/LedgerController.cs
+++ spreadsheets/PivotUpRC3/Pivot.Mvc.Feather/Controllers/LedgerController.cs
@@ -2,4 +2,22 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Pivot.Mvc.Feather.Controllers;
 
-public class LedgerController : Controller { }
+public class LedgerController : Controller {
+  
+  private readonly ILogger<HomeController> _logger;
+  private readonly IWebHostEnvironment webHostEnvironment;
+
+  public LedgerController(
+    ILogger<HomeController> logger,
+    IWebHostEnvironment webHostEnvironment
+  )
+  {
+    _logger = logger;
+    this.webHostEnvironment = webHostEnvironment;
+  }
+
+  public IActionResult Index()
+  {
+    return View();
+  }
+}
