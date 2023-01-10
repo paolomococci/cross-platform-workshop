17585746ada11ac2e03c20594b75600004c4b45d
paolomococci <paolomococci@gmail.com>
2023-01-10 19:35:36 +0100

    modified:   spreadsheets/PivotUpRC5/Pivot.Mvc.Feather/Models/CoordsModel.cs

--- spreadsheets/PivotUpRC5/Pivot.Mvc.Feather/Models/CoordsModel.cs
+++ spreadsheets/PivotUpRC5/Pivot.Mvc.Feather/Models/CoordsModel.cs
@@ -1,6 +1,6 @@
 namespace Pivot.Mvc.Feather.Models;
 
 public class CoordsModel {
-  public DateOnly? Session { get; set; }
+  public DateOnly Session { get; set; }
   public List<int>? HyperDots { get; set; }
 }
