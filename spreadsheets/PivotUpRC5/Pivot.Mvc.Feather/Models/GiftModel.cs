5e876253fb4c1e53ff450ab74183a6596aecbb9c
paolomococci <paolomococci@gmail.com>
2023-01-10 19:33:43 +0100

    modified:   spreadsheets/PivotUpRC5/Pivot.Mvc.Feather/Models/GiftModel.cs

--- spreadsheets/PivotUpRC5/Pivot.Mvc.Feather/Models/GiftModel.cs
+++ spreadsheets/PivotUpRC5/Pivot.Mvc.Feather/Models/GiftModel.cs
@@ -3,6 +3,4 @@ namespace Pivot.Mvc.Feather.Models;
 public class GiftModel
 {
   public string Id { get; set; } = string.Empty;
-  public DateOnly? Session { get; set; }
-  public List<int>? HyperDots { get; set; }
 }
