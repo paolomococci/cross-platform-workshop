fc66e79eb6e424665433ffa094ab1d7acea151ad
paolomococci <paolomococci@gmail.com>
2023-01-10 19:53:53 +0100

    modified:   spreadsheets/PivotUpRC5/Pivot.Mvc.Feather/Models/CoordsModel.cs

--- spreadsheets/PivotUpRC5/Pivot.Mvc.Feather/Models/CoordsModel.cs
+++ spreadsheets/PivotUpRC5/Pivot.Mvc.Feather/Models/CoordsModel.cs
@@ -5,14 +5,16 @@ public record CoordsModel
   public DateOnly Session { get; set; }
   public List<int>? HyperDots { get; set; }
 
-  public CoordsModel(DateOnly session) {
+  public CoordsModel(DateOnly session)
+  {
     this.Session = session;
   }
 
   public CoordsModel(
     DateOnly session,
     List<int> hyperDots
-  ) {
+  )
+  {
     this.Session = session;
     this.HyperDots = hyperDots;
   }
