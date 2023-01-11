f6ef558e92957420697be79f8618544e581370ac
paolomococci <paolomococci@gmail.com>
2023-01-11 17:13:36 +0100

    modified:   spreadsheets/PivotUpRC6/Pivot.Mvc.Feather/Models/WorkbookModel.cs

--- spreadsheets/PivotUpRC6/Pivot.Mvc.Feather/Models/WorkbookModel.cs
+++ spreadsheets/PivotUpRC6/Pivot.Mvc.Feather/Models/WorkbookModel.cs
@@ -3,4 +3,9 @@ namespace Pivot.Mvc.Feather.Models;
 public class WorkbookModel
 {
   public List<DataSheetModel> SheetBinder { get; set; } = new();
+
+  internal void Sift(string[] subdivided)
+  {
+    throw new NotImplementedException();
+  }
 }
