c6063f6759d4b32340e283ab28dc60d8e12dd1bb
paolomococci <paolomococci@gmail.com>
2023-01-11 05:36:36 +0100

    modified:   spreadsheets/PivotUpRC5/Pivot.Mvc.Feather/Models/AssetModel.cs

--- spreadsheets/PivotUpRC5/Pivot.Mvc.Feather/Models/AssetModel.cs
+++ spreadsheets/PivotUpRC5/Pivot.Mvc.Feather/Models/AssetModel.cs
@@ -2,9 +2,11 @@ namespace Pivot.Mvc.Feather.Models;
 
 public class AssetModel : IComparable<AssetModel>
 {
-  public List<string>? Assets { get; set; }
+  public List<string> Assets { get; set; } = new();
 
-  public void Add() {}
+  public void Add(string tag) {
+    this.Assets.Add(tag);
+  }
 
   public int CompareTo(AssetModel? other)
   {
