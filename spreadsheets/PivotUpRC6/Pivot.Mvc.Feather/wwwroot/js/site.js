6cf8c6455472cae14909f879dd82de61f225cf52
paolomococci <paolomococci@gmail.com>
2023-01-11 16:25:17 +0100

    new file:   spreadsheets/PivotUpRC6/Pivot.Mvc.Feather/wwwroot/js/site.js

---
+++ spreadsheets/PivotUpRC6/Pivot.Mvc.Feather/wwwroot/js/site.js
@@ -0,0 +1,12 @@
+﻿
+UIkit.util.ready(function () {
+  if (document.getElementById('js-progress') != null) {
+    let bar = document.getElementById('js-progress');
+    let animate = setInterval(function () {
+      bar.value += 10;
+      if (bar.value >= bar.max) {
+        clearInterval(animate);
+      }
+    }, 500);
+  }
+});
