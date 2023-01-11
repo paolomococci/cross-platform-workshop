3acf813af04bc2fd824031e370358e7e5d6e086e
paolomococci <paolomococci@gmail.com>
2023-01-11 16:23:05 +0100

    new file:   spreadsheets/PivotUpRC6/Pivot.Mvc.Feather/Controllers/LedgerController.cs

---
+++ spreadsheets/PivotUpRC6/Pivot.Mvc.Feather/Controllers/LedgerController.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Mvc;
+using Pivot.Common.Models;
+
+namespace Pivot.Mvc.Feather.Controllers;
+
+public class LedgerController : Controller
+{
+
+  private readonly ILogger<HomeController> _logger;
+  private readonly IWebHostEnvironment webHostEnvironment;
+
+  public LedgerController(
+    ILogger<HomeController> logger,
+    IWebHostEnvironment webHostEnvironment
+  )
+  {
+    _logger = logger;
+    this.webHostEnvironment = webHostEnvironment;
+  }
+
+  public IActionResult Index()
+  {
+    return View();
+  }
+
+  public IActionResult Generate()
+  {
+    using (MemoryStream memoryStream = new MemoryStream())
+    {
+      return this.File(
+        fileContents: LedgerModel.Perform(memoryStream),
+        contentType: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+        fileDownloadName: "Pivot.xlsx"
+      );
+    }
+  }
+}
