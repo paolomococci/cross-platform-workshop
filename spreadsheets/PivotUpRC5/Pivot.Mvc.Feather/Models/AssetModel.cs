82153d74109ec66d514889fab92b87fcf89f0a34
paolomococci <paolomococci@gmail.com>
2023-01-11 05:34:47 +0100

    modified:   spreadsheets/PivotUpRC5/Pivot.Mvc.Feather/Models/AssetModel.cs

--- spreadsheets/PivotUpRC5/Pivot.Mvc.Feather/Models/AssetModel.cs
+++ spreadsheets/PivotUpRC5/Pivot.Mvc.Feather/Models/AssetModel.cs
@@ -3,7 +3,9 @@ namespace Pivot.Mvc.Feather.Models;
 public class AssetModel : IComparable<AssetModel>
 {
   public List<string>? Assets { get; set; }
-  
+
+  public void Add() {}
+
   public int CompareTo(AssetModel? other)
   {
     throw new NotImplementedException();
