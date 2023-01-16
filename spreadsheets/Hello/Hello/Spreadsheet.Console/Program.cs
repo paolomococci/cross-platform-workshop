307e6b282b75965eae213ee2dfb53dc11d6fdadc
paolomococci <paolomococci@gmail.com>
2023-01-16 09:39:38 +0100

    new file:   spreadsheets/Hello/Hello/Spreadsheet.Console/Program.cs

---
+++ spreadsheets/Hello/Hello/Spreadsheet.Console/Program.cs
@@ -0,0 +1,11 @@
+﻿using ClosedXML.Excel;
+
+/* base path of spreadsheets */
+string basePath = "../spreadsheets/";
+
+XLWorkbook xlWorkbook = new XLWorkbook();
+
+xlWorkbook.Worksheets.Add("Hello").Cell("A1").SetValue<string>("Hello World!");
+
+/* save the workbook */
+xlWorkbook.SaveAs($"{basePath}Hello.xlsx");
