6f42a4563fee0e8f53ac548ba0be01a1d87d7924
paolomococci <paolomococci@gmail.com>
2023-01-18 19:39:26 +0100

    reorganization of projects

---
+++ workspaces/DummyRoster/DummyRosterRC0.1/DummyRoster.Mvc/Controllers/ProductsController.cs
@@ -0,0 +1,56 @@
+using Microsoft.AspNetCore.Mvc;
+using DummyRoster.Common.EntityModel.Models;
+
+namespace DummyRoster.Mvc.Controllers;
+
+public class ProductsController : Controller
+{
+  private const string baseUri = "https://localhost:5001/api/products";
+  private readonly ILogger<ProductsController> _logger;
+  private readonly HttpClient httpClient;
+
+  public ProductsController(
+    ILogger<ProductsController> logger,
+    IHttpClientFactory httpClientFactory
+  )
+  {
+    this._logger = logger;
+    HttpClientHandler httpClientHandler = new HttpClientHandler();
+    httpClientHandler.ServerCertificateCustomValidationCallback += (
+      sender,
+      certificate,
+      chain,
+      error
+    ) =>
+    {
+      return true;
+    };
+    this.httpClient = new HttpClient(httpClientHandler);
+  }
+
+  [Route("Home/Products")]
+  public async Task<IActionResult> Products(string? name)
+  {
+    string apiUri = "";
+    if (string.IsNullOrEmpty(name))
+    {
+      ViewData["Title"] = "All Products";
+      apiUri = $"{baseUri}";
+    }
+    else
+    {
+      ViewData["Title"] = $"Products with the name equal to {name}";
+      apiUri = $"{baseUri}/?name={name}";
+    }
+    HttpRequestMessage httpRequestMessage = new(
+      method: HttpMethod.Get,
+      requestUri: apiUri
+    );
+    HttpResponseMessage httpResponseMessage = await this.httpClient.SendAsync(
+      httpRequestMessage
+    );
+    IEnumerable<Product>? products = await httpResponseMessage
+      .Content.ReadFromJsonAsync<IEnumerable<Product>>();
+    return View(products);
+  }
+}
