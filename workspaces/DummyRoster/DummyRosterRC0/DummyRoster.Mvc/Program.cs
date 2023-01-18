fc8d6dc4d5c8f95ac986e894757342d60f782bbd
paolomococci <paolomococci@gmail.com>
2023-01-18 20:09:59 +0100

    reorganization of projects

---
+++ workspaces/DummyRoster/DummyRosterRC0/DummyRoster.Mvc/Program.cs
@@ -0,0 +1,62 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.Net.Http.Headers;
+using DummyRoster.Mvc.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<ApplicationDbContext>(
+  options => options.UseSqlite(connectionString)
+);
+
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddDefaultIdentity<IdentityUser>(
+  options => options.SignIn.RequireConfirmedAccount = true
+).AddRoles<IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Services.AddControllersWithViews();
+
+/* Configure HttpClient */
+builder.Services.AddHttpClient(
+  name: "",
+  configureClient: options => {
+    options.BaseAddress = new Uri("https://127.0.0.1:5001");
+    options.DefaultRequestHeaders.Accept.Add(
+      new MediaTypeWithQualityHeaderValue(
+        "application/json", 1.0
+      )
+    );
+  }
+);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+  app.UseMigrationsEndPoint();
+}
+else
+{
+  app.UseExceptionHandler("/Home/Error");
+  app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
+
+app.Run();
