86845ea3a5105d8771a6cae94ecd2201c277359b
paolomococci <paolomococci@gmail.com>
2023-01-18 05:28:22 +0100

    modified:   knowledge/KnowledgeRC1/Knowledge.Mvc.Feather/Program.cs

--- knowledge/KnowledgeRC1/Knowledge.Mvc.Feather/Program.cs
+++ knowledge/KnowledgeRC1/Knowledge.Mvc.Feather/Program.cs
@@ -1,9 +1,24 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
 
-app.MapGet(
-  "/", 
-  () => "Simple visual feedback from web application Knowledge!"
-);
+if (!app.Environment.IsDevelopment())
+{
+  app.UseExceptionHandler("/Home/Error");
+  app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
