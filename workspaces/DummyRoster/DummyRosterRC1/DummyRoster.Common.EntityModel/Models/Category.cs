f2ebcc1ab74d8f171dfefe0a50617e42d0d8413c
paolomococci <paolomococci@gmail.com>
2023-01-18 20:26:11 +0100

    reorganization of projects

---
+++ workspaces/DummyRoster/DummyRosterRC1/DummyRoster.Common.EntityModel/Models/Category.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+using System.Xml.Serialization;
+
+namespace DummyRoster.Common.EntityModel.Models;
+
+[Table("Category")]
+[Index("Name", Name = "IDX_CategoryName")]
+public partial class Category
+{
+  [Key]
+  public int Id { get; set; }
+
+  [Column(TypeName = "varchar(32)")]
+  [RegularExpression(@"[a-zA-Z0-9\s\-]{1,32}")]
+  public string Name { get; set; } = null!;
+
+  [RegularExpression(@"^([A-Z]{1,}[a-zA-Z0-9\s\.\,]{1,63})$")]
+  public string? Description { get; set; }
+
+  public byte[]? Picture { get; set; }
+
+  [InverseProperty("Category")]
+  [XmlIgnore]
+  public virtual ICollection<Product> Products { get; } = new List<Product>();
+}
