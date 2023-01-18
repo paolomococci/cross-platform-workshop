cf08c2e8b3b8f829ed2c40d292696971ee947f0e
paolomococci <paolomococci@gmail.com>
2023-01-18 05:29:20 +0100

    modified:   knowledge/KnowledgeRC1/Knowledge.Mvc.Feather/Controllers/HomeController.cs

--- knowledge/KnowledgeRC1/Knowledge.Mvc.Feather/Controllers/HomeController.cs
+++ knowledge/KnowledgeRC1/Knowledge.Mvc.Feather/Controllers/HomeController.cs
@@ -2,4 +2,17 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Knowledge.Mvc.Feather.Controllers;
 
-public class HomeController : Controller {}
+public class HomeController : Controller
+{
+  private readonly ILogger<HomeController> _logger;
+  private readonly IWebHostEnvironment webHostEnvironment;
+
+  public HomeController(
+    ILogger<HomeController> logger,
+    IWebHostEnvironment webHostEnvironment
+  )
+  {
+    _logger = logger;
+    this.webHostEnvironment = webHostEnvironment;
+  }
+}
