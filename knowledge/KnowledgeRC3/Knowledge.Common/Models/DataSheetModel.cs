f84d15896dfc9ef20a225c54c80e8c14690fc754
paolomococci <paolomococci@gmail.com>
2023-01-19 15:48:56 +0100

    modified:   knowledge/KnowledgeRC3/Knowledge.Common/Models/DataSheetModel.cs

--- knowledge/KnowledgeRC3/Knowledge.Common/Models/DataSheetModel.cs
+++ knowledge/KnowledgeRC3/Knowledge.Common/Models/DataSheetModel.cs
@@ -1,6 +1,7 @@
 namespace Knowledge.Common.Models;
 
-public class DataSheetModel {
+public class DataSheetModel
+{
   public string Label { get; set; } = string.Empty;
   public List<AssetModel> Assets { get; set; } = new();
 }
