5fff2d09d8b8dd1dee715d51c446215725a71ce0
paolomococci <paolomococci@gmail.com>
2023-01-19 16:21:37 +0100

    modified:   knowledge/KnowledgeRC3/Knowledge.Mvc.Feather/Models/DataCollectionModel.cs

--- knowledge/KnowledgeRC3/Knowledge.Mvc.Feather/Models/DataCollectionModel.cs
+++ knowledge/KnowledgeRC3/Knowledge.Mvc.Feather/Models/DataCollectionModel.cs
@@ -2,4 +2,5 @@ namespace Knowledge.Mvc.Feather.Models;
 
 public class DataCollectionModel {
   public string Id { get; } = DateTime.Now.ToString();
+  public IFormFile? DatasetFilename { get; set; }
   }
