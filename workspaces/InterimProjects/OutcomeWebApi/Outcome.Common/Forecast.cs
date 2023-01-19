bf2dc06daf864633edd51366fcb97619a80f531e
paolomococci <paolomococci@gmail.com>
2023-01-19 11:08:00 +0100

    reorganization of projects

---
+++ workspaces/InterimProjects/OutcomeWebApi/Outcome.Common/Forecast.cs
@@ -0,0 +1,20 @@
+﻿namespace Outcome.Common;
+
+public class Forecast
+{
+  public static readonly string[] Forecasts = new[] {
+    "zero",
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine"
+  };
+  public DateTime NextDays { get; set; }
+  public int Sample { get; set; }
+  public string? Foregone { get; set; }
+}
