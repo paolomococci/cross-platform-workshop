808f47e00b3a73c2231014681a2379661b898630
paolomococci <paolomococci@gmail.com>
2023-01-19 09:37:23 +0100

    reorganization of projects

---
+++ workspaces/InterimProjects/DummyRosterTwo/DummyRoster.Common.EntityModel/Models/Customer.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace DummyRoster.Common.EntityModel.Models;
+
+[Table("Customer")]
+[Index("City", Name = "IDX_CustomerCity")]
+[Index("District", Name = "IDX_CustomerDistrict")]
+[Index("Name", Name = "IDX_CustomerName")]
+[Index("Postcode", Name = "IDX_CustomerPostcode")]
+public partial class Customer
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Column(TypeName = "varchar(32)")]
+    public string Name { get; set; } = null!;
+
+    [Column(TypeName = "DATETIME")]
+    public DateTime? FoundationDate { get; set; }
+
+    public string? Description { get; set; }
+
+    public byte[]? Picture { get; set; }
+
+    [Column(TypeName = "INT")]
+    public int? Contact { get; set; }
+
+    [Column(TypeName = "varchar(64)")]
+    public string? Address { get; set; }
+
+    [Column(TypeName = "varchar(16)")]
+    public string? City { get; set; }
+
+    [Column(TypeName = "varchar(16)")]
+    public string? District { get; set; }
+
+    [Column(TypeName = "varchar(8)")]
+    public string? Postcode { get; set; }
+
+    [Column(TypeName = "varchar(16)")]
+    public string? Country { get; set; }
+
+    [Column(TypeName = "varchar(48)")]
+    public string? Email { get; set; }
+
+    [Column(TypeName = "varchar(24)")]
+    public string? Phone { get; set; }
+
+    [Column(TypeName = "varchar(24)")]
+    public string? Fax { get; set; }
+
+    [ForeignKey("Contact")]
+    [InverseProperty("Customers")]
+    public virtual Employee? ContactNavigation { get; set; }
+
+    [InverseProperty("Customer")]
+    public virtual ICollection<Form> Forms { get; } = new List<Form>();
+}
