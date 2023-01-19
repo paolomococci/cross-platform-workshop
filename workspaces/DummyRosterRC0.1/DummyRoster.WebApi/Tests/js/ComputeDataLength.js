7dac2f96edda7439ae67b7032c5035398a492a7f
paolomococci <paolomococci@gmail.com>
2023-01-19 03:52:09 +0100

    reorganization of projects

--- workspaces/DummyRosterRC0.1/DummyRoster.WebApi/Tests/js/ComputeDataLength.js
+++
@@ -1,9 +0,0 @@
-/* extract of javascript code to calculate the length of the data to be sent in JSON format */
-
-const data = {
-  "": ""
-}
-
-const dataLength = JSON.stringify(data).length;
-
-console.log(dataLength);
