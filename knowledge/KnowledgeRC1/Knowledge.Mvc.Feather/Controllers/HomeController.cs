10492f217995602234f77f526550a1e1d7ae80e1
paolomococci <paolomococci@gmail.com>
2023-01-19 16:49:26 +0100

    modified:   knowledge/KnowledgeRC1/Knowledge.Mvc.Feather/Controllers/HomeController.cs

--- knowledge/KnowledgeRC1/Knowledge.Mvc.Feather/Controllers/HomeController.cs
+++ knowledge/KnowledgeRC1/Knowledge.Mvc.Feather/Controllers/HomeController.cs
@@ -29,4 +29,15 @@ public class HomeController : Controller
       "Home"
     );
   }
+
+  [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+  public IActionResult Error()
+  {
+    return View(
+      new ErrorViewModel
+      {
+        RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
+      }
+    );
+  }
 }
