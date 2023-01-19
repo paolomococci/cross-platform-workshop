b4bda9225b352d907d11fd675ba3f630f7774aed
paolomococci <paolomococci@gmail.com>
2023-01-19 09:37:17 +0100

    reorganization of projects

---
+++ workspaces/InterimProjects/DummyRosterTwo/DummyRoster.Common.EntityModel/Models/Category.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace DummyRoster.Common.EntityModel.Models;
+
+[Table("Category")]
+[Index("Name", Name = "IDX_CategoryName")]
+public partial class Category
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Column(TypeName = "varchar(32)")]
+    public string Name { get; set; } = null!;
+
+    public string? Description { get; set; }
+
+    public byte[]? Picture { get; set; }
+
+    [InverseProperty("Category")]
+    public virtual ICollection<Product> Products { get; } = new List<Product>();
+}
