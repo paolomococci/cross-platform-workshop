7e5f23ddae01984a5a6fecf399bc22e2a54d8580
paolomococci <paolomococci@gmail.com>
2023-01-19 16:19:34 +0100

    modified:   knowledge/KnowledgeRC3/Knowledge.Common/Models/AssetModel.cs

--- knowledge/KnowledgeRC3/Knowledge.Common/Models/AssetModel.cs
+++ knowledge/KnowledgeRC3/Knowledge.Common/Models/AssetModel.cs
@@ -2,5 +2,5 @@ namespace Knowledge.Common.Models;
 
 public class AssetModel
 {
-  public string Id { get; set; } = Guid.NewGuid().ToString();
+  public string Id { get; } = Guid.NewGuid().ToString();
 }
