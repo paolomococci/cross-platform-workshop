2e8303d495cb9700a99086d2fad7f7337e883420
paolomococci <paolomococci@gmail.com>
2023-01-19 09:39:17 +0100

    reorganization of projects

---
+++ workspaces/InterimProjects/DummyRosterTwo/DummyRoster.Web.Customer/Areas/DummyRosterFeature/Pages/Customer.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+using DummyRoster.Common.DataContext.Data;
+
+namespace DummyRoster.Web.Customer.DummyRosterFeature.Pages;
+
+public class CustomerModel : PageModel
+{
+  private DummyRosterContext dummyRosterContext;
+  public IQueryable<DummyRoster.Common.EntityModel.Models.Customer> customers { get; set; } = null!;
+
+  public CustomerModel(
+      DummyRosterContext dummyRosterContext
+  )
+  {
+    this.dummyRosterContext = dummyRosterContext;
+  }
+
+  public void OnGet()
+  {
+    ViewData["Title"] = "DummyRoster Admin - Customer";
+    this.customers = this.dummyRosterContext.Customers.OrderBy(
+      entity => entity.Country
+    ).ThenBy(
+      entity => entity.Name
+    );
+  }
+}
