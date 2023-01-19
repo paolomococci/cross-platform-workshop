a12a1db0016bcbbcd59b3ac479d76ffdf082a05f
paolomococci <paolomococci@gmail.com>
2023-01-19 09:44:07 +0100

    reorganization of projects

---
+++ workspaces/InterimProjects/DummyRosterTwo/DummyRoster.Web/Program.cs
@@ -0,0 +1,10 @@
+using DummyRoster.Web;
+
+  Host.CreateDefaultBuilder(
+    args
+  ).ConfigureWebHostDefaults(
+    webBuilder => {
+      webBuilder.UseStartup<Startup>();
+    }
+  ).Build().Run();
+  
