859178acb6e445038c77d8e2d73ad56256d75426
paolomococci <paolomococci@gmail.com>
2023-01-19 10:47:50 +0100

    reorganization of projects

---
+++ workspaces/InterimProjects/DummyRosterWebApiHardened/DummyRoster.Common.EntityModel/Models/Customer.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+using System.Xml.Serialization;
+
+namespace DummyRoster.Common.EntityModel.Models;
+
+[Table("Customer")]
+[Index("Name", Name = "IDX_CustomerName")]
+public partial class Customer
+{
+  [Key]
+  public int Id { get; set; }
+
+  [Column(TypeName = "varchar(32)")]
+  [RegularExpression("[a-zA-Z]{1,32}")]
+  public string Name { get; set; } = null!;
+
+  [Column(TypeName = "DATETIME")]
+  public DateTime? FoundationDate { get; set; }
+
+  [RegularExpression(@"^([A-Z]{1,}[a-zA-Z0-9\s\.\,]{1,63})$")]
+  public string? Description { get; set; }
+
+  public byte[]? Picture { get; set; }
+
+  [Column(TypeName = "INT")]
+  public int? Contact { get; set; }
+
+  [Column(TypeName = "INT")]
+  public int? Location { get; set; }
+
+  [ForeignKey("Contact")]
+  [InverseProperty("Customers")]
+  public virtual Employee? ContactNavigation { get; set; }
+
+  [InverseProperty("Customer")]
+  [XmlIgnore]
+  public virtual ICollection<Form> Forms { get; } = new List<Form>();
+
+  [ForeignKey("Location")]
+  [InverseProperty("Customers")]
+  public virtual Address? LocationNavigation { get; set; }
+}
