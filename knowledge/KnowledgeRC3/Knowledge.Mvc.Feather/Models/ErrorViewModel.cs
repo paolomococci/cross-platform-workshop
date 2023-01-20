3676bd08fa40da1488866b26fcec1eabc33e4767
paolomococci <paolomococci@gmail.com>
2023-01-21 00:07:17 +0100

    deleted:    knowledge/KnowledgeRC3/Knowledge.Mvc.Feather/Models/ErrorViewModel.cs

--- knowledge/KnowledgeRC3/Knowledge.Mvc.Feather/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace Knowledge.Mvc.Feather.Models;
-
-public class ErrorViewModel
-{
-  public string? RequestId { get; set; }
-
-  public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
