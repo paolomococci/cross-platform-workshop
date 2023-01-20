3c5656a876946ba924a82e2e55ae84748349d417
paolomococci <paolomococci@gmail.com>
2023-01-20 23:55:37 +0100

    deleted:    knowledge/KnowledgeRC0/Knowledge.Mvc.Feather/Program.cs

--- knowledge/KnowledgeRC0/Knowledge.Mvc.Feather/Program.cs
+++
@@ -1,9 +0,0 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
-
-app.MapGet(
-  "/", 
-  () => "Simple visual feedback from web application Knowledge!"
-);
-
-app.Run();
