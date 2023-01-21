4b0f62f1db5b204b18e52c689a740a14534ef0db
paolomococci <paolomococci@gmail.com>
2023-01-21 08:08:18 +0100

    modified:   knowledge/KnowledgeRC0/Knowledge.Mvc.Feather/Models/ErrorViewModel.cs

--- knowledge/KnowledgeRC0/Knowledge.Mvc.Feather/Models/ErrorViewModel.cs
+++ knowledge/KnowledgeRC0/Knowledge.Mvc.Feather/Models/ErrorViewModel.cs
@@ -1,3 +1,8 @@
 namespace Knowledge.Mvc.Feather.Models;
 
-public class ErrorViewModel {}
+public class ErrorViewModel
+{
+  public string? RequestId { get; set; }
+
+  public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
