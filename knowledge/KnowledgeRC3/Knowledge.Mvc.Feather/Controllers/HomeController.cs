aefddee33c66944041016bf6719fb11f7b619b26
paolomococci <paolomococci@gmail.com>
2023-01-25 07:41:11 +0100

    new file:   knowledge/KnowledgeRC3/Knowledge.Mvc.Feather/Controllers/HomeController.cs

---
+++ knowledge/KnowledgeRC3/Knowledge.Mvc.Feather/Controllers/HomeController.cs
@@ -0,0 +1,100 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Knowledge.Mvc.Feather.Models;
+
+namespace Knowledge.Mvc.Feather.Controllers;
+
+public class HomeController : Controller
+{
+  private readonly ILogger<HomeController> _logger;
+  private readonly IWebHostEnvironment webHostEnvironment;
+
+  public HomeController(
+    ILogger<HomeController> logger,
+    IWebHostEnvironment webHostEnvironment
+  )
+  {
+    _logger = logger;
+    this.webHostEnvironment = webHostEnvironment;
+  }
+
+  public IActionResult Index()
+  {
+    return View();
+  }
+
+  public IActionResult Upload()
+  {
+    return View(new DataCollectionModel());
+  }
+
+  [HttpPost]
+  public IActionResult Upload(DataCollectionModel dataCollection)
+  {
+    if (dataCollection.Dataset != null)
+    {
+      var concept = dataCollection.Dataset;
+      var conceptName = Path.GetFileName(concept.FileName);
+      var unique = this.AppendDateTime(conceptName);
+      var upload = Path.Combine(
+        this.webHostEnvironment.WebRootPath,
+        "Store/datasets"
+      );
+      var path = Path.Combine(upload, unique);
+      dataCollection.Dataset.CopyTo(
+        new FileStream(path, FileMode.Create)
+      );
+    }
+    return RedirectToAction(
+      "Index",
+      "Home"
+    );
+  }
+
+  public IActionResult Uploaded()
+  {
+    List<string> listOfUploadedDataset = new();
+    var store = Path.Combine(
+        this.webHostEnvironment.WebRootPath,
+        "Store/datasets"
+      );
+    var uploadedFiles = Directory.GetFiles(store);
+    foreach (var item in uploadedFiles)
+    {
+      var itemName = Path.GetFileName(item);
+      listOfUploadedDataset.Add(itemName);
+      ViewData["datasetList"] = listOfUploadedDataset;
+    }
+    return View();
+  }
+
+  [HttpPost]
+  public IActionResult Process(string unique)
+  {
+    if (unique != null && unique != string.Empty)
+    {
+      // todo
+    }
+    return RedirectToAction(
+      "Index",
+      "Home"
+    );
+  }
+
+  [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+  public IActionResult Error()
+  {
+    return View(
+      new ErrorViewModel
+      {
+        RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
+      }
+    );
+  }
+
+  private string AppendDateTime(string file)
+  {
+    var name = Path.GetFileName(file);
+    return Path.GetFileNameWithoutExtension(name) + $"_{DateTime.Now:yyyy-MM-dd_hh-mm-ss}" + Path.GetExtension(name);
+  }
+}
