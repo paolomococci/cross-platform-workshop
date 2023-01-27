fee087ac16593b7bc376c4df4ae7660071485b28
paolomococci <paolomococci@gmail.com>
2023-01-27 10:48:46 +0100

    modified:   knowledge/LikingRatingRC1/Liking.Common/Models/DatasetRawModel.cs

--- knowledge/LikingRatingRC1/Liking.Common/Models/DatasetRawModel.cs
+++ knowledge/LikingRatingRC1/Liking.Common/Models/DatasetRawModel.cs
@@ -3,7 +3,10 @@ using Microsoft.ML.Data;
 namespace Liking.Common.Models;
 
 public class DatasetRawModel {
-  
+
   [LoadColumn(0)]
-  public bool Label { get; set; }
+  public bool Label { get; set; } = false;
+
+  [LoadColumn(2)]
+  public string Text { get; set; } = string.Empty;
 }
