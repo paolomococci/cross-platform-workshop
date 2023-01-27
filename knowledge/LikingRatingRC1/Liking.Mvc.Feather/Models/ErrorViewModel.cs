02ec81009bafc19f242e94356deb67c1f2270286
paolomococci <paolomococci@gmail.com>
2023-01-27 10:24:19 +0100

    new file:   knowledge/LikingRatingRC1/Liking.Mvc.Feather/Models/ErrorViewModel.cs

---
+++ knowledge/LikingRatingRC1/Liking.Mvc.Feather/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Liking.Mvc.Feather.Models;
+
+public class ErrorViewModel
+{
+  public string? RequestId { get; set; }
+
+  public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
