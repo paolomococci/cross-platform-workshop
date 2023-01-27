3218a07832033c18f5aea135d3ba2a5558fa69df
paolomococci <paolomococci@gmail.com>
2023-01-27 15:10:59 +0100

    modified:   knowledge/LikingRatingRC1/Liking.Common/Models/AssetModel.cs

--- knowledge/LikingRatingRC1/Liking.Common/Models/AssetModel.cs
+++ knowledge/LikingRatingRC1/Liking.Common/Models/AssetModel.cs
@@ -1,8 +1,12 @@
+using Microsoft.ML;
+
 namespace Liking.Common.Models;
 
 public class AssetModel
 {
   public string Id { get; } = Guid.NewGuid().ToString();
 
-  public void CreateMLContext() {}
+  public void CreateMLContext() {
+    MLContext mlContext = new MLContext(seed: 1);
+  }
 }
