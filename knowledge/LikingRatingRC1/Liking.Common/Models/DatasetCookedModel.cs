4059dedf7b1655cb20cc5096434417f913c717fc
paolomococci <paolomococci@gmail.com>
2023-01-27 10:58:19 +0100

    modified:   knowledge/LikingRatingRC1/Liking.Common/Models/DatasetCookedModel.cs

--- knowledge/LikingRatingRC1/Liking.Common/Models/DatasetCookedModel.cs
+++ knowledge/LikingRatingRC1/Liking.Common/Models/DatasetCookedModel.cs
@@ -1,3 +1,9 @@
+using Microsoft.ML.Data;
+
 namespace Liking.Common.Models;
 
-public class DatasetCookedModel {}
+public class DatasetCookedModel {
+
+  [ColumnName("Label")]
+  public bool Label { get; set; } = false;
+}
