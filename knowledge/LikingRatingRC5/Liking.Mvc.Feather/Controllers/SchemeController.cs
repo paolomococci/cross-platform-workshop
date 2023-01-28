a66e111567bd5767633c5ffa010836ec8e109d75
paolomococci <paolomococci@gmail.com>
2023-01-28 18:50:11 +0100

    new file:   knowledge/LikingRatingRC5/Liking.Mvc.Feather/Controllers/SchemeController.cs

---
+++ knowledge/LikingRatingRC5/Liking.Mvc.Feather/Controllers/SchemeController.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Liking.Mvc.Feather.Controllers;
+
+public class SchemeController : Controller
+{
+
+  private readonly ILogger<HomeController> _logger;
+  private readonly IWebHostEnvironment webHostEnvironment;
+
+  public SchemeController(
+    ILogger<HomeController> logger,
+    IWebHostEnvironment webHostEnvironment
+  )
+  {
+    _logger = logger;
+    this.webHostEnvironment = webHostEnvironment;
+  }
+
+  public IActionResult Index()
+  {
+    List<string> schemeProcessedList = new();
+    var store = Path.Combine(
+        this.webHostEnvironment.WebRootPath,
+        "Store/schemes"
+      );
+    var uploadedFiles = Directory.GetFiles(store);
+    foreach (var item in uploadedFiles)
+    {
+      var itemName = Path.GetFileName(item);
+      schemeProcessedList.Add(itemName);
+      ViewData["schemeListNames"] = schemeProcessedList;
+    }
+    return View();
+  }
+}
