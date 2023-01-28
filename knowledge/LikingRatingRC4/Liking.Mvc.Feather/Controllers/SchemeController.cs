cb92f5fc219f26162a50c4d1993165c3819107e9
paolomococci <paolomococci@gmail.com>
2023-01-28 16:31:47 +0100

    modified:   knowledge/LikingRatingRC4/Liking.Mvc.Feather/Controllers/SchemeController.cs

--- knowledge/LikingRatingRC4/Liking.Mvc.Feather/Controllers/SchemeController.cs
+++ knowledge/LikingRatingRC4/Liking.Mvc.Feather/Controllers/SchemeController.cs
@@ -2,4 +2,17 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Liking.Mvc.Feather.Controllers;
 
-public class SchemeController : Controller {}
+public class SchemeController : Controller {
+
+  private readonly ILogger<HomeController> _logger;
+  private readonly IWebHostEnvironment webHostEnvironment;
+
+  public SchemeController(
+    ILogger<HomeController> logger,
+    IWebHostEnvironment webHostEnvironment
+  )
+  {
+    _logger = logger;
+    this.webHostEnvironment = webHostEnvironment;
+  }
+}
