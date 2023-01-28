2113fdcb4825bd45428d36745094f4215bb5321e
paolomococci <paolomococci@gmail.com>
2023-01-28 18:45:43 +0100

    new file:   knowledge/LikingRatingRC5/Liking.Common/Models/DatasetCookedModel.cs

---
+++ knowledge/LikingRatingRC5/Liking.Common/Models/DatasetCookedModel.cs
@@ -0,0 +1,14 @@
+using Microsoft.ML.Data;
+
+namespace Liking.Common.Models;
+
+public class DatasetCookedModel
+{
+
+  [ColumnName("Label")]
+  public bool Label { get; set; } = false;
+
+  public decimal Likelihood { get; set; } = 0.0M;
+
+  public decimal Score { get; set; } = 0.0M;
+}
