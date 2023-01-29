ad108ea664afdb8e5aa072f0cd17e0f57f1a9ad4
paolomococci <paolomococci@gmail.com>
2023-01-29 07:15:48 +0100

    new file:   knowledge/LikingRatingRC6/Liking.Common/Models/DatasetRawModel.cs

---
+++ knowledge/LikingRatingRC6/Liking.Common/Models/DatasetRawModel.cs
@@ -0,0 +1,13 @@
+using Microsoft.ML.Data;
+
+namespace Liking.Common.Models;
+
+public class DatasetRawModel
+{
+
+  [LoadColumn(0)]
+  public bool Label { get; set; } = false;
+
+  [LoadColumn(2)]
+  public string Text { get; set; } = string.Empty;
+}
