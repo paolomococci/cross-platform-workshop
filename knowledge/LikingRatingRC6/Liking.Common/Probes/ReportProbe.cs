5d2cad428e39f10928ee5aa7d6a94151bef7f09f
paolomococci <paolomococci@gmail.com>
2023-01-30 05:45:01 +0100

    modified:   knowledge/LikingRatingRC6/Liking.Common/Probes/ReportProbe.cs

--- knowledge/LikingRatingRC6/Liking.Common/Probes/ReportProbe.cs
+++ knowledge/LikingRatingRC6/Liking.Common/Probes/ReportProbe.cs
@@ -1,3 +1,11 @@
+using Microsoft.ML.Data;
+
 namespace Liking.Common.Probes;
 
-public class ReportProbe {}
+public class ReportProbe {
+  public static void BinaryClassificationMetricsProbe(
+    string? name,
+    CalibratedBinaryClassificationMetrics calibratedBinaryClassificationMetrics
+  )
+  {}
+}
