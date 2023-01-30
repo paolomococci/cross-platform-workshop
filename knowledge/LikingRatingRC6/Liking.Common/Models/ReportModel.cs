765169c789dece809a7b7c1a2dc719883fdb2cec
paolomococci <paolomococci@gmail.com>
2023-01-30 06:56:31 +0100

    modified:   knowledge/LikingRatingRC6/Liking.Common/Models/ReportModel.cs

--- knowledge/LikingRatingRC6/Liking.Common/Models/ReportModel.cs
+++ knowledge/LikingRatingRC6/Liking.Common/Models/ReportModel.cs
@@ -3,4 +3,5 @@ namespace Liking.Common.Models;
 public class ReportModel {
   public string Name { get; set; } = string.Empty;
   public string Accuracy { get; set; } = string.Empty;
+  public string AreaUnderRocCurve { get; set; } = string.Empty;
 }
