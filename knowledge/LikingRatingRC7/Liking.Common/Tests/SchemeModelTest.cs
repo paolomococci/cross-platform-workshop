bbd6a55664fd6d7d021ae6b5b4aa7a64f4d794ec
paolomococci <paolomococci@gmail.com>
2023-01-31 18:34:16 +0100

    modified:   knowledge/LikingRatingRC7/Liking.Common/Tests/SchemeModelTest.cs

--- knowledge/LikingRatingRC7/Liking.Common/Tests/SchemeModelTest.cs
+++ knowledge/LikingRatingRC7/Liking.Common/Tests/SchemeModelTest.cs
@@ -1,11 +1,11 @@
 using Microsoft.ML;
 using Liking.Common.Models;
 
-namespace Liking.Common.Probe;
+namespace Liking.Common.Tests;
 
 public class SchemeModelTest
 {
-  public static void predictionEngineTest(
+  public static void PredictionEngineTest(
     MLContext mlContext,
     IDataView testSetDataView,
     ITransformer transformer,
@@ -14,14 +14,14 @@ public class SchemeModelTest
   {
     DatasetRawModel datasetRawModel = new DatasetRawModel(Text: sampleText);
     // todo: specified argument was out of the range of valid values
-    var predictionEngine = mlContext.Model.CreatePredictionEngine<DatasetRawModel, DatasetCookedModel>(
+    /*var predictionEngine = mlContext.Model.CreatePredictionEngine<DatasetRawModel, DatasetCookedModel>(
       transformer: transformer
-    );
-    DatasetCookedModel datasetCookedModelResult = predictionEngine.Predict(datasetRawModel);
+    );*/
+    //DatasetCookedModel datasetCookedModelResult = predictionEngine.Predict(datasetRawModel);
     System.Console.WriteLine("--------------------> results of the test phase <--------------------");
     System.Console.WriteLine($"Text: {datasetRawModel.Text}");
-    System.Console.WriteLine($"Prediction: {Evaluate(datasetCookedModelResult)}");
-    System.Console.WriteLine($"Probability of expressing a negative feeling: {datasetCookedModelResult.Likelihood}");
+    //System.Console.WriteLine($"Prediction: {Evaluate(datasetCookedModelResult)}");
+    //System.Console.WriteLine($"Probability of expressing a negative feeling: {datasetCookedModelResult.Likelihood}");
     System.Console.WriteLine("--------------------> end of the test phase results <--------------------");
   }
 
