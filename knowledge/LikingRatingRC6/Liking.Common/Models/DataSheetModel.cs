80f46f5d3bbaa3bdd37fa2a6852c3a1c36f2aa40
paolomococci <paolomococci@gmail.com>
2023-01-31 10:28:49 +0100

    modified:   knowledge/LikingRatingRC6/Liking.Common/Models/DataSheetModel.cs

--- knowledge/LikingRatingRC6/Liking.Common/Models/DataSheetModel.cs
+++ knowledge/LikingRatingRC6/Liking.Common/Models/DataSheetModel.cs
@@ -3,5 +3,5 @@ namespace Liking.Common.Models;
 public class DataSheetModel
 {
   public string Label { get; set; } = string.Empty;
-  public List<SchemeModel> Assets { get; set; } = new();
+  public List<SchemeModel> Schemes { get; set; } = new();
 }
