11a33b1d1f259663560cad4ccf39ea9121963483
paolomococci <paolomococci@gmail.com>
2023-02-01 05:32:48 +0100

    new file:   knowledge/MachineLearningWebAppTemplate/Template.Common/Models/ReportModel.cs

---
+++ knowledge/MachineLearningWebAppTemplate/Template.Common/Models/ReportModel.cs
@@ -0,0 +1,17 @@
+namespace Template.Common.Models;
+
+[Serializable]
+public class ReportModel
+{
+  public string Name { get; set; } = string.Empty;
+  public string Accuracy { get; set; } = string.Empty;
+  public string AreaUnderRocCurve { get; set; } = string.Empty;
+  public string AreaUnderPrecisionRecallCurve { get; set; } = string.Empty;
+  public string F1Score { get; set; } = string.Empty;
+  public string LogLoss { get; set; } = string.Empty;
+  public string LogLossReduction { get; set; } = string.Empty;
+  public string PositivePrecision { get; set; } = string.Empty;
+  public string PositiveRecall { get; set; } = string.Empty;
+  public string NegativePrecision { get; set; } = string.Empty;
+  public string NegativeRecall { get; set; } = string.Empty;
+}
