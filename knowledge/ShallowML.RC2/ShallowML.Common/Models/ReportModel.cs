6a58be7e3e8d7e9c8393f07fc57ceb83f124df31
paolomococci <paolomococci@gmail.com>
2023-02-01 23:15:25 +0100

    new file:   knowledge/ShallowML.RC2/ShallowML.Common/Models/ReportModel.cs

---
+++ knowledge/ShallowML.RC2/ShallowML.Common/Models/ReportModel.cs
@@ -0,0 +1,9 @@
+namespace ShallowML.Common.Models;
+
+[Serializable]
+public class ReportModel
+{
+  public string Name { get; set; } = string.Empty;
+  public string Area { get; set; } = string.Empty;
+  public string Price { get; set; } = string.Empty;
+}
