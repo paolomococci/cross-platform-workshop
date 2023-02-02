f8276a53c8dd37896f7db4d31c2a8dd19f20c43a
paolomococci <paolomococci@gmail.com>
2023-02-02 11:41:02 +0100

    modified:   knowledge/ShallowML.RC2/ShallowML.Common/Models/WorkbookModel.cs

--- knowledge/ShallowML.RC2/ShallowML.Common/Models/WorkbookModel.cs
+++ knowledge/ShallowML.RC2/ShallowML.Common/Models/WorkbookModel.cs
@@ -6,8 +6,20 @@ public class WorkbookModel
 {
   public static DomainModel[] GetDataset(string datasetPath)
   {
+    List<DomainModel> domains = new();
     XLWorkbook xlWorkbook = new XLWorkbook(datasetPath);
     var sheet = xlWorkbook.Worksheet(1);
-    throw new NotImplementedException();
+    var rows = sheet.RangeUsed().RowsUsed().Skip(1);
+    foreach (var item in rows)
+    {
+      domains.Add(
+        new DomainModel() {
+          Area = float.Parse((string)item.Cell(1).Value),
+          Price = float.Parse((string)item.Cell(2).Value)
+        }
+      );
+      System.Console.WriteLine($"area: {float.Parse((string)item.Cell(1).Value)}, price: {float.Parse((string)item.Cell(2).Value)}");
+    }
+    return domains.ToArray();
   }
 }
