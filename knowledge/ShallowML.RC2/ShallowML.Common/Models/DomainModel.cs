c655c4cc9b7ab61b5dd4b3398afd9654423f8abd
paolomococci <paolomococci@gmail.com>
2023-02-02 09:44:27 +0100

    modified:   knowledge/ShallowML.RC2/ShallowML.Common/Models/DomainModel.cs

--- knowledge/ShallowML.RC2/ShallowML.Common/Models/DomainModel.cs
+++ knowledge/ShallowML.RC2/ShallowML.Common/Models/DomainModel.cs
@@ -1,13 +1,9 @@
-using Microsoft.ML.Data;
-
 namespace ShallowML.Common.Models;
 
 public class DomainModel
 {
 
-  [LoadColumn(0)]
   public float Area { get; set; } = 0.0F;
 
-  [LoadColumn(1)]
   public float Price { get; set; } = 0.0F;
 }
