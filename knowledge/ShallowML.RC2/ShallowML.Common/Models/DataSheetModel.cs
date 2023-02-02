49442e3517e9f383b5be62cccdb1b02456ccb79a
paolomococci <paolomococci@gmail.com>
2023-02-02 11:42:41 +0100

    deleted:    knowledge/ShallowML.RC2/ShallowML.Common/Models/DataSheetModel.cs

--- knowledge/ShallowML.RC2/ShallowML.Common/Models/DataSheetModel.cs
+++
@@ -1,7 +0,0 @@
-namespace ShallowML.Common.Models;
-
-public class DataSheetModel
-{
-  public string Label { get; set; } = string.Empty;
-  public List<DomainModel> Domains { get; set; } = new();
-}
