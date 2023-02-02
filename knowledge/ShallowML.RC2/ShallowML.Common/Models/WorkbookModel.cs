31558c473e818ec669ab60f8cc108f1b436f6238
paolomococci <paolomococci@gmail.com>
2023-02-02 09:52:54 +0100

    modified:   knowledge/ShallowML.RC2/ShallowML.Common/Models/WorkbookModel.cs

--- knowledge/ShallowML.RC2/ShallowML.Common/Models/WorkbookModel.cs
+++ knowledge/ShallowML.RC2/ShallowML.Common/Models/WorkbookModel.cs
@@ -1,6 +1,9 @@
+using ClosedXML.Excel;
+
 namespace ShallowML.Common.Models;
 
 public class WorkbookModel
 {
-  public List<DataSheetModel> dataSheets { get; set; } = new();
+  public List<DataSheetModel> Sheets { get; set; } = new();
+  public XLWorkbook xlWorkbook { get; set; } = new();
 }
