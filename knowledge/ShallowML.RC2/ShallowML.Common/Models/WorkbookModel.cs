34b89ff638dcdb99304966ea4872643814638cbb
paolomococci <paolomococci@gmail.com>
2023-02-02 10:40:04 +0100

    modified:   knowledge/ShallowML.RC2/ShallowML.Common/Models/WorkbookModel.cs

--- knowledge/ShallowML.RC2/ShallowML.Common/Models/WorkbookModel.cs
+++ knowledge/ShallowML.RC2/ShallowML.Common/Models/WorkbookModel.cs
@@ -6,4 +6,9 @@ public class WorkbookModel
 {
   public List<DataSheetModel> Sheets { get; set; } = new();
   public XLWorkbook xlWorkbook { get; set; } = new();
+
+  public static DomainModel[] GetDataset(string datasetPath)
+  {
+    throw new NotImplementedException();
+  }
 }
