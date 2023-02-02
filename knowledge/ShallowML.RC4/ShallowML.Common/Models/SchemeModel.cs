79bad66b1025efda8eb9d48cce015a63242dccd5
paolomococci <paolomococci@gmail.com>
2023-02-02 22:37:43 +0100

    modified:   knowledge/ShallowML.RC4/ShallowML.Common/Models/SchemeModel.cs

--- knowledge/ShallowML.RC4/ShallowML.Common/Models/SchemeModel.cs
+++ knowledge/ShallowML.RC4/ShallowML.Common/Models/SchemeModel.cs
@@ -40,4 +40,39 @@ public class SchemeModel
       Price = priced.Price.ToString()
     };
   }
+
+  public ReportModel CreateMLContextAndSaveSchema(
+    DomainModel[] dataset,
+    DomainModel priced,
+    string schemePath
+  )
+  {
+    this.mlContext = new MLContext();
+    /* loading dataset step */
+    IDataView dataView = this.mlContext.Data.LoadFromEnumerable(dataset);
+
+    /* create pipeline step */
+    var pipeline = this.mlContext.Transforms.Concatenate(
+      "Features",
+      new[] { "Area" }
+    ).Append(
+      this.mlContext.Regression.Trainers.Sdca(
+        labelColumnName: "Price",
+        maximumNumberOfIterations: 50
+      )
+    );
+
+    /* training step */
+    var scheme = pipeline.Fit(dataView);
+
+    /* prediction step */
+    priced.Price = this.mlContext.Model.CreatePredictionEngine<DomainModel, PredictionModel>(scheme).Predict(priced).Price;
+
+    return new ReportModel()
+    {
+      Name = this.Id,
+      Area = priced.Area.ToString(),
+      Price = priced.Price.ToString()
+    };
+  }
 }
