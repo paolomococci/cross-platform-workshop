9382f05b95925e2f2fb2fc4f3d5e321eb0dff20f
paolomococci <paolomococci@gmail.com>
2023-02-02 09:52:32 +0100

    modified:   knowledge/ShallowML.RC2/ShallowML.Common/Models/DataSheetModel.cs

--- knowledge/ShallowML.RC2/ShallowML.Common/Models/DataSheetModel.cs
+++ knowledge/ShallowML.RC2/ShallowML.Common/Models/DataSheetModel.cs
@@ -3,5 +3,5 @@ namespace ShallowML.Common.Models;
 public class DataSheetModel
 {
   public string Label { get; set; } = string.Empty;
-  public List<SchemeModel> Schemes { get; set; } = new();
+  public List<DomainModel> Domains { get; set; } = new();
 }
