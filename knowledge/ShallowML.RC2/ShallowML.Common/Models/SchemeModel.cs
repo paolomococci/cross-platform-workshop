315ac54a83773acedd1be2903539aaee69e6e7e4
paolomococci <paolomococci@gmail.com>
2023-02-02 10:39:54 +0100

    modified:   knowledge/ShallowML.RC2/ShallowML.Common/Models/SchemeModel.cs

--- knowledge/ShallowML.RC2/ShallowML.Common/Models/SchemeModel.cs
+++ knowledge/ShallowML.RC2/ShallowML.Common/Models/SchemeModel.cs
@@ -8,33 +8,13 @@ public class SchemeModel
   public MLContext? mlContext;
 
   public ReportModel CreateMLContext(
-    string datasetPath
+    DomainModel[] dataset,
+    DomainModel priced
   )
   {
     this.mlContext = new MLContext();
     /* loading dataset step */
-    /*IDataView dataView = mlContext.Data.LoadFromTextFile<DomainModel>(
-      datasetPath,
-      hasHeader: true
-    );*/
-    // todo: extract data from a spreadsheet and then assign the values ​​to an array representing the domain to be processed
-    DomainModel[] domains = {
-      new DomainModel() { Area = 12000.0F, Price = 35000.0F },
-      new DomainModel() { Area = 15000.0F, Price = 47000.0F },
-      new DomainModel() { Area = 11500.0F, Price = 37500.0F },
-      new DomainModel() { Area = 14500.0F, Price = 41200.0F },
-      new DomainModel() { Area = 14000.0F, Price = 40500.0F },
-      new DomainModel() { Area = 9000.0F, Price = 27000.0F },
-      new DomainModel() { Area = 10500.0F, Price = 32500.0F },
-      new DomainModel() { Area = 12500.0F, Price = 35500.0F },
-      new DomainModel() { Area = 15500.0F, Price = 47500.0F },
-      new DomainModel() { Area = 11500.0F, Price = 37000.0F },
-      new DomainModel() { Area = 14900.0F, Price = 41500.0F },
-      new DomainModel() { Area = 14000.0F, Price = 40250.0F },
-      new DomainModel() { Area = 9000.0F, Price = 26000.0F },
-      new DomainModel() { Area = 10500.0F, Price = 33500.0F }
-    };
-    IDataView dataView = this.mlContext.Data.LoadFromEnumerable(domains);
+    IDataView dataView = this.mlContext.Data.LoadFromEnumerable(dataset);
 
     /* create pipeline step */
     var pipeline = this.mlContext.Transforms.Concatenate(
@@ -51,7 +31,6 @@ public class SchemeModel
     var scheme = pipeline.Fit(dataView);
 
     /* prediction step */
-    var priced = new DomainModel() { Area = 15500F };
     priced.Price = this.mlContext.Model.CreatePredictionEngine<DomainModel, PredictionModel>(scheme).Predict(priced).Price;
 
     return new ReportModel()
