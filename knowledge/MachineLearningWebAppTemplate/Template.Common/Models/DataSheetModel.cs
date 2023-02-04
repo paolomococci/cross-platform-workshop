6ac48dd127c54cdecadca42de3f754d344999093
paolomococci <paolomococci@gmail.com>
2023-02-04 15:21:39 +0100

    deleted:    knowledge/MachineLearningWebAppTemplate/Template.Common/Models/DataSheetModel.cs

--- knowledge/MachineLearningWebAppTemplate/Template.Common/Models/DataSheetModel.cs
+++
@@ -1,7 +0,0 @@
-namespace Template.Common.Models;
-
-public class DataSheetModel
-{
-  public string Label { get; set; } = string.Empty;
-  public List<SchemeModel> Schemes { get; set; } = new();
-}
