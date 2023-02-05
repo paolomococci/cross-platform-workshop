e62a9b97a989e9b77f2d1d62465d30900dbd8e84
paolomococci <paolomococci@gmail.com>
2023-02-05 23:48:58 +0100

    modified:   knowledge/AttitudeML.RC2/AttitudeML.Mvc.Feather/Controllers/HomeController.cs

--- knowledge/AttitudeML.RC2/AttitudeML.Mvc.Feather/Controllers/HomeController.cs
+++ knowledge/AttitudeML.RC2/AttitudeML.Mvc.Feather/Controllers/HomeController.cs
@@ -87,11 +87,12 @@ public class HomeController : Controller
       SchemeModel scheme = new();
       string datasetPath = Path.Combine(storeDatasetPath, workbook);
       string schemePath = Path.Combine(storeSchemePath, $"scheme_{scheme.Id}.zip");
-      this.report = scheme.CreateMLContextAndSaveSchema(
+      // todo
+      /*this.report = scheme.CreateMLContextAndSaveSchema(
         dataset: WorkbookModel.GetDataset(datasetPath),
         disposition: new DomainModel() {},
         schemePath: schemePath
-      );
+      );*/
     }
     if (this.report != null)
     {
