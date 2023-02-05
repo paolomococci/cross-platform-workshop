b3bc9f3af4414c682d0e9841687dbfe5f993a86c
paolomococci <paolomococci@gmail.com>
2023-02-05 12:30:38 +0100

    modified:   knowledge/AttitudeML.RC2/AttitudeML.Common/Models/PredictionModel.cs

--- knowledge/AttitudeML.RC2/AttitudeML.Common/Models/PredictionModel.cs
+++ knowledge/AttitudeML.RC2/AttitudeML.Common/Models/PredictionModel.cs
@@ -4,6 +4,8 @@ namespace AttitudeML.Common.Models;
 
 public class PredictionModel
 {
+  [ColumnName("PredictedLabel")]
+  public float PredictedLabel { get; set; } = 0.0F;
 
   [ColumnName("Score")]
   public float Score { get; set; } = 0.0F;
