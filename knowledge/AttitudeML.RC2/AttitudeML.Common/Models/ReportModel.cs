dfad7b3b2bbe48a485d2ea3fefbc6595528ca220
paolomococci <paolomococci@gmail.com>
2023-02-06 18:31:27 +0100

    modified:   knowledge/AttitudeML.RC2/AttitudeML.Common/Models/ReportModel.cs

--- knowledge/AttitudeML.RC2/AttitudeML.Common/Models/ReportModel.cs
+++ knowledge/AttitudeML.RC2/AttitudeML.Common/Models/ReportModel.cs
@@ -3,6 +3,7 @@ namespace AttitudeML.Common.Models;
 [Serializable]
 public class ReportModel
 {
+  public string Comment { get; set; } = string.Empty;
   public bool Disposition { get; set; } = false;
 
   public string GetResponse()
