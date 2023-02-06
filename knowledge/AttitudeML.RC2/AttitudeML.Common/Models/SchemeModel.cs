17e516531ec1e93875ec697e35b806ac55d6a95a
paolomococci <paolomococci@gmail.com>
2023-02-06 17:02:23 +0100

    modified:   knowledge/AttitudeML.RC2/AttitudeML.Common/Models/SchemeModel.cs

--- knowledge/AttitudeML.RC2/AttitudeML.Common/Models/SchemeModel.cs
+++ knowledge/AttitudeML.RC2/AttitudeML.Common/Models/SchemeModel.cs
@@ -9,6 +9,31 @@ public class SchemeModel
 {
   public string Id { get; } = Guid.NewGuid().ToString();
 
+  private static PredictionEngine<DomainModel, PredictionModel> CreatePredictionEngine(
+    string schemePath
+  )
+  {
+    MLContext mLContext = new();
+    ITransformer transformer = mLContext.Model.Load(
+      schemePath,
+      out var _
+    );
+    return mLContext.Model.CreatePredictionEngine<DomainModel, PredictionModel>(transformer);
+  }
+
+  public static PredictionModel Prediction(
+    DomainModel domainModel,
+    string schemePath
+  )
+  {
+    Lazy<PredictionEngine<DomainModel, PredictionModel>> PredictEngine;
+    PredictEngine = new Lazy<PredictionEngine<DomainModel, PredictionModel>>(
+      () => CreatePredictionEngine(schemePath),
+      true
+    );
+    return PredictEngine.Value.Predict(domainModel);
+  }
+
   public static IEstimator<ITransformer> MakePipeline(
     MLContext mlContext
   )
@@ -35,30 +60,5 @@ public class SchemeModel
   )
   {
     return MakePipeline(mlContext).Fit(trainingDataView);
-  }
-
-  private static PredictionEngine<DomainModel, PredictionModel> CreatePredictionEngine(
-    string schemePath
-  )
-  {
-    MLContext mLContext = new();
-    ITransformer transformer = mLContext.Model.Load(
-      schemePath,
-      out var _
-    );
-    return mLContext.Model.CreatePredictionEngine<DomainModel, PredictionModel>(transformer);
-  }
-
-  public static PredictionModel Prediction(
-    DomainModel domainModel,
-    string schemePath
-  )
-  {
-    Lazy<PredictionEngine<DomainModel, PredictionModel>> PredictEngine;
-    PredictEngine = new Lazy<PredictionEngine<DomainModel, PredictionModel>>(
-      () => CreatePredictionEngine(schemePath),
-      true
-    );
-    return PredictEngine.Value.Predict(domainModel);
   }
 }
