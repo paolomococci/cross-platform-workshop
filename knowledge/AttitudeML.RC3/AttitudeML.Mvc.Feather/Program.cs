818afd642d57e22b033fb52d2d1ed61829f5a13e
paolomococci <paolomococci@gmail.com>
2023-02-07 10:04:21 +0100

    new file:   knowledge/AttitudeML.RC3/AttitudeML.Mvc.Feather/Program.cs

---
+++ knowledge/AttitudeML.RC3/AttitudeML.Mvc.Feather/Program.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.DataProtection.AuthenticatedEncryption;
+using Microsoft.AspNetCore.DataProtection.AuthenticatedEncryption.ConfigurationModel;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDataProtection().UseCryptographicAlgorithms(
+  new AuthenticatedEncryptorConfiguration {
+    EncryptionAlgorithm = EncryptionAlgorithm.AES_256_CBC,
+    ValidationAlgorithm = ValidationAlgorithm.HMACSHA256
+  }
+);
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+  app.UseExceptionHandler("/Home/Error");
+  app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
