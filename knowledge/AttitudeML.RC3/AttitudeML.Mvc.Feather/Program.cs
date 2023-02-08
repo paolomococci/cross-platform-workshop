026ce8f196876f97a94fb8155e6ccf7a36fd6e01
paolomococci <paolomococci@gmail.com>
2023-02-08 09:31:33 +0100

    modified:   knowledge/AttitudeML.RC3/AttitudeML.Mvc.Feather/Program.cs

--- knowledge/AttitudeML.RC3/AttitudeML.Mvc.Feather/Program.cs
+++ knowledge/AttitudeML.RC3/AttitudeML.Mvc.Feather/Program.cs
@@ -6,12 +6,12 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddDataProtection().UseCryptographicAlgorithms(
+/*builder.Services.AddDataProtection().UseCryptographicAlgorithms(
   new AuthenticatedEncryptorConfiguration {
     EncryptionAlgorithm = EncryptionAlgorithm.AES_256_CBC,
     ValidationAlgorithm = ValidationAlgorithm.HMACSHA256
   }
-);
+);*/
 
 var app = builder.Build();
 
