7173637a5155d29c1a2b0f40dc9feaff75679339
paolomococci <paolomococci@gmail.com>
2023-02-12 20:38:27 +0100

    new file:   knowledge/AttitudeML.RC4/AttitudeML.Common/Models/WorkbookModel.cs

---
+++ knowledge/AttitudeML.RC4/AttitudeML.Common/Models/WorkbookModel.cs
@@ -0,0 +1,27 @@
+using ClosedXML.Excel;
+
+namespace AttitudeML.Common.Models;
+
+public class WorkbookModel
+{
+  public static DomainModel[] GetDataset(
+    string datasetPath
+  )
+  {
+    List<DomainModel> domains = new();
+    XLWorkbook xlWorkbook = new XLWorkbook(datasetPath);
+    var sheet = xlWorkbook.Worksheet(1);
+    var rows = sheet.RangeUsed().RowsUsed().Skip(1);
+    foreach (var item in rows)
+    {
+      domains.Add(
+        new DomainModel() {
+          Sentiment = item.Cell(1).GetValue<int>(),
+          Comment = item.Cell(2).GetValue<string>(),
+          Registered = item.Cell(3).GetValue<bool>()
+        }
+      );
+    }
+    return domains.ToArray();
+  }
+}
