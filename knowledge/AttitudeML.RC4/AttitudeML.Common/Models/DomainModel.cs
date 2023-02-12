f7651210c447569c73af802eef1377ca0d7b721d
paolomococci <paolomococci@gmail.com>
2023-02-12 20:37:47 +0100

    new file:   knowledge/AttitudeML.RC4/AttitudeML.Common/Models/DomainModel.cs

---
+++ knowledge/AttitudeML.RC4/AttitudeML.Common/Models/DomainModel.cs
@@ -0,0 +1,18 @@
+using Microsoft.ML.Data;
+
+namespace AttitudeML.Common.Models;
+
+public class DomainModel
+{
+  [LoadColumn(0)]
+  [ColumnName(@"Sentiment")]
+  public int Sentiment { get; set; } = 0;
+
+  [LoadColumn(1)]
+  [ColumnName(@"Comment")]
+  public string Comment { get; set; } = string.Empty;
+  
+  [LoadColumn(2)]
+  [ColumnName(@"Registered")]
+  public bool Registered { get; set; } = false;
+}
